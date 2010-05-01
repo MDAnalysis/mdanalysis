94995d302e627043c69220c257100232a6b3f5e8
Oliver Beckstein <orbeckst@gmail.com>
2010-05-01 05:27:11 +0000

    notes on what to do when unittests are not running
    git-svn-id: https://mdanalysis.googlecode.com/svn/trunk@246 dcee7792-c13e-0410-88f5-272e5f544b36

--- MDAnalysis/tests/__init__.py
+++ MDAnalysis/tests/__init__.py
@@ -12,8 +12,16 @@ Run the tests with
    >>> import MDAnalysis.tests
    >>> MDAnalysis.tests.test()
 
+Note that if no tests are being run then one has to remove the executable bit
+from all *.py files in the test directory::
+  chmod a-X ..../MDAnalysis/tests/*.py
+as described in `nose, subversion and executable bits`_.
 
-The simulation data used in some tests are from [Beckstein2009]_.
+.. _nose, subversion and executable bits:
+   http://lacostej.blogspot.com/2009/09/nose-subversion-and-exectubale-bits.html
+
+The simulation data used in some tests are from [Beckstein2009]_ (adk.psf,
+adk_dims.dcd) or unpublished simulations (O. Beckstein).
 
 [Beckstein2009] O. Beckstein, E.J. Denning, J.R. Perilla and T.B. Woolf,
                 Zipping and Unzipping of Adenylate Kinase: Atomistic Insights
@@ -25,5 +33,6 @@ try:
     from numpy.testing import Tester
     test = Tester().test
 except ImportError:
-    raise ImportError("NumPy is required to run the test suite. Please install it first.")
+    raise ImportError("numpy>=1.3  is required to run the test suite. Please install it first. " 
+                      "(For example, try \"easy_install 'numpy>=1.3'\")".)
 
