37aaf0426bf867b8419ba5447a3972a584cbe38b
Oliver Beckstein <orbeckst@gmail.com>
2010-05-27 14:47:55 +0000

    reST docs for top __init__ & canonical licence blurb header
    git-svn-id: https://mdanalysis.googlecode.com/svn/branches/release-0-6-2@309 dcee7792-c13e-0410-88f5-272e5f544b36

--- MDAnalysis/__init__.py
+++ MDAnalysis/__init__.py
@@ -1,4 +1,8 @@
 # $Id$
+# MDAnalysis http://mdanalysis.googlecode.com
+# Copyright (c) 2006-2010 Naveen Michaud-Agrawal, Elizabeth J. Denning, Oliver Beckstein
+# Released under the GNU Public Licence, v2
+
 """
 :mod:`MDAnalysis` --- analysis of molecular simulations in python
 =================================================================
@@ -22,6 +26,87 @@ http://groups.google.com/group/mdnalysis-discussion
 
 Please report bugs and feature requests through the issue tracker at
 http://code.google.com/p/mdanalysis/issues/ 
+
+Getting started
+---------------
+
+Import the package::
+ 
+  >>> import MDAnalysis
+
+(note that not everything in MDAnalysis is imported right away; for
+additional functionality you might have to import sub-modules
+separately, e.g. for RMS fitting ``import MDAnalysis.core.rms_fitting``.)
+
+Build a "universe" from a topology (PSF, PDB) and a trajectory (DCD, XTC/TRR);
+here we are assuming that PSF, DCD, etc contain file names. If you don't have
+trajectories at hand you can play with the ones that come with MDAnalysis for
+testing (see below under `Examples`_)::
+
+  >>> u = MDAnalysis.Universe(PSF, DCD)
+
+Select the C-alpha atoms and store them as a group of atoms::
+
+  >>> ca = u.selectAtoms('name CA')
+  >>> len(ca)
+  214  
+
+Calculate the centre of mass of the CA and of all atoms::
+
+  >>> ca.centerOfMass()
+  array([ 0.06873595, -0.04605918, -0.24643682])
+  >>> u.atoms.centerOfMass()
+  array([-0.01094035,  0.05727601, -0.12885778])
+
+Calculate the CA end-to-end distance (in angstroem)::
+  >>> from numpy import sqrt, dot
+  >>> coord = ca.coordinates()
+  >>> v = coord[-1] - coord[0]   # last Ca minus first one
+  >>> sqrt(dot(v, v,))
+  10.938133
+
+Define a function eedist():
+  >>> def eedist(atoms):
+  ...     coord = atoms.coordinates()
+  ...     v = coord[-1] - coord[0] 
+  ...     return sqrt(dot(v, v,))
+  ... 
+  >>> eedist(ca)
+  10.938133
+
+and analyze all timesteps *ts* of the trajectory::
+  >>> for ts in u.trajectory:
+  ...      print eedist(ca)
+  10.9381
+  10.8459
+  10.4141
+   9.72062 
+  ....
+
+.. SeeAlso:: :class:`MDAnalysis.core.AtomGroup.Universe` for details
+
+
+Examples
+--------
+
+MDAnalysis comes with a number of real trajectories for testing. You
+can also use them to explore the functionality and ensure that
+everything is working properly::
+
+  from MDAnalysis import *
+  from MDAnalysis.tests.datafiles import PSF,DCD, PDB,XTC
+  u_dims_adk = Universe(PSF,DCD)
+  u_eq_adk = Universe(PDB, XTC)
+
+The PSF and DCD file are a closed-form-to-open-form transition of
+Adenylate Kinase (from [Beckstein2009]_) and the PDB+XTC file are ten
+frames from a Gromacs simulation of AdK solvated in TIP4P water with
+the OPLS/AA force field.
+
+[Beckstein2009] O. Beckstein, E.J. Denning, J.R. Perilla and
+                T.B. Woolf, Zipping and Unzipping of Adenylate Kinase: Atomistic
+                Insights into the Ensemble of Open <--> Closed Transitions. J Mol Biol
+                394 (2009), 160--176, doi:10.1016/j.jmb.2009.09.009
 """
 
 # Only import often used modules and objects; anything else should be imported
