a214ac00d18680a9c4ead8136299046c96f84a12
Oliver Beckstein <orbeckst@gmail.com>
2010-05-27 14:48:03 +0000

    release 0.6.2 (increased version number)
    git-svn-id: https://mdanalysis.googlecode.com/svn/branches/release-0-6-2@310 dcee7792-c13e-0410-88f5-272e5f544b36

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ else:
     fast_numeric_link = ["-llapack"]
 
 if __name__ == '__main__':
-    RELEASE = "0.6.2-rc1"
+    RELEASE = "0.6.2"
     LONG_DESCRIPTION = \
 """MDAnalysis is a tool for analyzing molecular dynamics trajectories.
 """
