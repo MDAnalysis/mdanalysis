6d88101e43ae54553d4bb523f58c25a239f29fe2
Oliver Beckstein <orbeckst@gmail.com>
2010-06-30 16:46:55 +0000

    reduced precision for distance tests from default to 5 decimals so that they also pass on 32 bit
    git-svn-id: https://mdanalysis.googlecode.com/svn/branches/release-0-6-3@358 dcee7792-c13e-0410-88f5-272e5f544b36

--- MDAnalysis/tests/test_distances.py
+++ MDAnalysis/tests/test_distances.py
@@ -11,6 +11,13 @@ class TestDistanceArrayDCD(TestCase):
         self.universe = MDAnalysis.Universe(PSF, DCD)
         self.dcd = self.universe.trajectory
         self.ca = self.universe.selectAtoms('name CA')
+        # reasonable precision so that tests succeed on 32 and 64 bit machines
+        # (the reference values were obtained on 64 bit)
+        # Example:
+        #   Items are not equal: wrong maximum distance value
+        #   ACTUAL: 52.470254967456412
+        #   DESIRED: 52.470257062419059
+        self.prec = 5
 
     @attr('issue')    
     def test_simple(self):
@@ -21,8 +28,10 @@ class TestDistanceArrayDCD(TestCase):
         x1 = U.atoms.coordinates(copy=True)
         d = MDAnalysis.core.distances.distance_array(x0, x1)
         assert_equal(d.shape, (3341, 3341), "wrong shape (should be (Natoms,Natoms))")
-        assert_almost_equal(d.min(), 0.11981228170520701, err_msg="wrong minimum distance value")
-        assert_almost_equal(d.max(), 53.572192429459619,  err_msg="wrong maximum distance value")
+        assert_almost_equal(d.min(), 0.11981228170520701, self.prec, 
+                            err_msg="wrong minimum distance value")
+        assert_almost_equal(d.max(), 53.572192429459619, self.prec,
+                            err_msg="wrong maximum distance value")
 
     @attr('issue')
     def test_copyTrue(self):
@@ -33,8 +42,10 @@ class TestDistanceArrayDCD(TestCase):
         x1 = U.atoms.coordinates(copy=True)
         d = MDAnalysis.core.distances.distance_array(x0, x1, copy=True)
         assert_equal(d.shape, (3341, 3341), "wrong shape, should be (Natoms,Natoms)")
-        assert_almost_equal(d.min(), 0.11981228170520701, err_msg="wrong minimum distance value")
-        assert_almost_equal(d.max(), 53.572192429459619,  err_msg="wrong maximum distance value")
+        assert_almost_equal(d.min(), 0.11981228170520701, self.prec, 
+                            err_msg="wrong minimum distance value")
+        assert_almost_equal(d.max(), 53.572192429459619, self.prec, 
+                            err_msg="wrong maximum distance value")
 
     @dec.knownfailureif(True, "Using copy=False in a distance array is a known failure.")
     @attr('issue')
@@ -49,9 +60,9 @@ class TestDistanceArrayDCD(TestCase):
         d = MDAnalysis.core.distances.distance_array(x0, x1, copy=False)
         assert_equal(d.shape, (3341, 3341), "wrong shape, should be (Natoms,Natoms) entries")
         # should give the same answer as copy=True but does not, see Issue 4
-        assert_almost_equal(d.min(), 0.11981228170520701, 
+        assert_almost_equal(d.min(), 0.11981228170520701,self.prec,
                             err_msg="wrong minimum distance value with copy=False, see Issue 4")
-        assert_almost_equal(d.max(), 53.572192429459619,  
+        assert_almost_equal(d.max(), 53.572192429459619, self.prec,
                             err_msg="wrong maximum distance value with copy=False, see Issue 4")
 
     @dec.knownfailureif(True, "Using copy=False in a distance array is a known failure.")
@@ -64,12 +75,14 @@ class TestDistanceArrayDCD(TestCase):
         x1 = self.ca.coordinates(copy=True)
         d_ok = MDAnalysis.core.distances.distance_array(x0, x1, copy=True)
         d_wrong = MDAnalysis.core.distances.distance_array(x0, x1, copy=False)
-        assert_equal(d_ok.shape, (len(self.ca), len(self.ca)), "copy=True square matrix with N_CA entries")
-        assert_equal(d_wrong.shape, (len(self.ca), len(self.ca)), "copy=False square matrix with N_CA entries")
+        assert_equal(d_ok.shape, (len(self.ca), len(self.ca)), 
+                     "copy=True square matrix with N_CA entries")
+        assert_equal(d_wrong.shape, (len(self.ca), len(self.ca)), 
+                     "copy=False square matrix with N_CA entries")
         # should give the same answer as copy=True but does not, see Issue 4
-        assert_almost_equal(d_ok.min(), d_wrong.min(), 
+        assert_almost_equal(d_ok.min(), d_wrong.min(), self.prec,
                             err_msg="copy=False distances != copy=True distances (tested min), known bug")
-        assert_almost_equal(d_ok.max(), d_wrong.max(), 
+        assert_almost_equal(d_ok.max(), d_wrong.max(), self.prec,
                             err_msg="copy=False distances != copy=True distances (tested max), known bug")
 
     def test_outarray(self):
@@ -82,8 +95,10 @@ class TestDistanceArrayDCD(TestCase):
         d = np.zeros((natoms, natoms), np.float64)
         MDAnalysis.core.distances.distance_array(x0, x1, result=d)
         assert_equal(d.shape, (natoms, natoms), "wrong shape, shoud be  (Natoms,Natoms) entries")
-        assert_almost_equal(d.min(), 0.11981228170520701, err_msg="wrong minimum distance value")
-        assert_almost_equal(d.max(), 53.572192429459619,  err_msg="wrong maximum distance value")
+        assert_almost_equal(d.min(), 0.11981228170520701, self.prec, 
+                            err_msg="wrong minimum distance value")
+        assert_almost_equal(d.max(), 53.572192429459619, self.prec, 
+                            err_msg="wrong maximum distance value")
 
     def test_periodic(self):
         # boring with the current dcd as that has no PBC
@@ -94,8 +109,10 @@ class TestDistanceArrayDCD(TestCase):
         x1 = U.atoms.coordinates(copy=True)
         d = MDAnalysis.core.distances.distance_array(x0, x1, box=U.coord.dimensions)
         assert_equal(d.shape, (3341, 3341), "should be square matrix with Natoms entries")
-        assert_almost_equal(d.min(), 0.11981228170520701, err_msg="wrong minimum distance value with PBC")
-        assert_almost_equal(d.max(), 53.572192429459619,  err_msg="wrong maximum distance value with PBC")
+        assert_almost_equal(d.min(), 0.11981228170520701, self.prec, 
+                            err_msg="wrong minimum distance value with PBC")
+        assert_almost_equal(d.max(), 53.572192429459619, self.prec,
+                            err_msg="wrong maximum distance value with PBC")
 
         
 class TestSelfDistanceArrayDCD(TestCase):
@@ -103,6 +120,8 @@ class TestSelfDistanceArrayDCD(TestCase):
         self.universe = MDAnalysis.Universe(PSF, DCD)
         self.dcd = self.universe.trajectory
         self.ca = self.universe.selectAtoms('name CA')
+        # see comments above on precision
+        self.prec = 5
     
     def test_simple(self):
         U = self.universe
@@ -111,8 +130,10 @@ class TestSelfDistanceArrayDCD(TestCase):
         d = MDAnalysis.core.distances.self_distance_array(x0)
         N = 3341 * (3341 - 1) / 2
         assert_equal(d.shape, (N,), "wrong shape (should be (Natoms*(Natoms-1)/2,))")
-        assert_almost_equal(d.min(), 0.92905562402529318, err_msg="wrong minimum distance value")
-        assert_almost_equal(d.max(), 52.4702570624190590, err_msg="wrong maximum distance value")
+        assert_almost_equal(d.min(), 0.92905562402529318, self.prec,
+                            err_msg="wrong minimum distance value")
+        assert_almost_equal(d.max(), 52.4702570624190590, self.prec,
+                            err_msg="wrong maximum distance value")
 
     def test_copyTrue(self):
         U = self.universe
@@ -121,8 +142,10 @@ class TestSelfDistanceArrayDCD(TestCase):
         d = MDAnalysis.core.distances.self_distance_array(x0, copy=True)
         N = 3341 * (3341 - 1) / 2
         assert_equal(d.shape, (N,), "wrong shape (should be (Natoms*(Natoms-1)/2,))")
-        assert_almost_equal(d.min(), 0.92905562402529318, err_msg="wrong minimum distance value")
-        assert_almost_equal(d.max(), 52.4702570624190590, err_msg="wrong maximum distance value")
+        assert_almost_equal(d.min(), 0.92905562402529318, self.prec,
+                            err_msg="wrong minimum distance value")
+        assert_almost_equal(d.max(), 52.4702570624190590, self.prec,
+                            err_msg="wrong maximum distance value")
 
     @dec.knownfailureif(True, "Using copy=False in a distance array is a known failure.")
     @attr('issue')
@@ -136,9 +159,9 @@ class TestSelfDistanceArrayDCD(TestCase):
         N = 3341 * (3341 - 1) / 2
         assert_equal(d.shape, (N,), "wrong shape (should be (Natoms*(Natoms-1)/2,))")
         # should give the same answer as copy=True but does not, see Issue 4
-        assert_almost_equal(d.min(), 0.92905562402529318, 
+        assert_almost_equal(d.min(), 0.92905562402529318, self.prec, 
                             err_msg="wrong minimum distance value with copy=False, see Issue 4")
-        assert_almost_equal(d.max(), 52.4702570624190590,  
+        assert_almost_equal(d.max(), 52.4702570624190590, self.prec,  
                             err_msg="wrong maximum distance value with copy=False, see Issue 4")
 
     @dec.knownfailureif(True, "Using copy=False in a distance array is a known failure.")
@@ -153,9 +176,9 @@ class TestSelfDistanceArrayDCD(TestCase):
         assert_equal(d_ok.shape, (N,), "copy=True 1D array with N_CA entries")
         assert_equal(d_wrong.shape, (N,), "copy=False 1D array with N_CA entries")
         # should give the same answer as copy=True but does not, see Issue 4
-        assert_almost_equal(d_ok.min(), d_wrong.min(), 
+        assert_almost_equal(d_ok.min(), d_wrong.min(), self.prec, 
                             err_msg="copy=False distances != copy=True distances (tested min), known bug")
-        assert_almost_equal(d_ok.max(), d_wrong.max(), 
+        assert_almost_equal(d_ok.max(), d_wrong.max(), self.prec,
                             err_msg="copy=False distances != copy=True distances (tested max), known bug")
 
 
@@ -168,8 +191,10 @@ class TestSelfDistanceArrayDCD(TestCase):
         d = np.zeros((N,), np.float64)
         MDAnalysis.core.distances.self_distance_array(x0, result=d)
         assert_equal(d.shape, (N,), "wrong shape (should be (Natoms*(Natoms-1)/2,))")
-        assert_almost_equal(d.min(), 0.92905562402529318, err_msg="wrong minimum distance value")
-        assert_almost_equal(d.max(), 52.4702570624190590, err_msg="wrong maximum distance value")
+        assert_almost_equal(d.min(), 0.92905562402529318, self.prec,
+                            err_msg="wrong minimum distance value")
+        assert_almost_equal(d.max(), 52.4702570624190590, self.prec,
+                            err_msg="wrong maximum distance value")
 
     def test_periodic(self):
         # boring with the current dcd as that has no PBC
@@ -180,7 +205,9 @@ class TestSelfDistanceArrayDCD(TestCase):
         N = natoms*(natoms-1) / 2
         d = MDAnalysis.core.distances.self_distance_array(x0, box=U.coord.dimensions)
         assert_equal(d.shape, (N,), "wrong shape (should be (Natoms*(Natoms-1)/2,))")
-        assert_almost_equal(d.min(), 0.92905562402529318, err_msg="wrong minimum distance value with PBC")
-        assert_almost_equal(d.max(), 52.4702570624190590, err_msg="wrong maximum distance value with PBC")
+        assert_almost_equal(d.min(), 0.92905562402529318, self.prec,
+                            err_msg="wrong minimum distance value with PBC")
+        assert_almost_equal(d.max(), 52.4702570624190590, self.prec,
+                            err_msg="wrong maximum distance value with PBC")
 
         
