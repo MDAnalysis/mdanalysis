ffc775327da9a9d866b7c9c5b3def6f0dcdf11dc
Oliver Beckstein <orbeckst@gmail.com>
2010-07-08 09:53:41 +0000

    release 0.6.3 (updated/reformatted CHANGELOG and bumped version) - included all changes since branching (XYZ reader, fixes) - all unit tests pass (Mac OS X 10.6.4 + MacPorts) for this revision
    git-svn-id: https://mdanalysis.googlecode.com/svn/branches/release-0-6-3@395 dcee7792-c13e-0410-88f5-272e5f544b36

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ else:
     fast_numeric_link = ["-llapack"]
 
 if __name__ == '__main__':
-    RELEASE = "0.6.3-rc2"
+    RELEASE = "0.6.3"
     LONG_DESCRIPTION = \
 """MDAnalysis is a tool for analyzing molecular dynamics trajectories.
 """
