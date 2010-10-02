6ee42c375eb0ae40cb5c8eb2486e287c19f0848c
Oliver Beckstein <orbeckst@gmail.com>
2010-10-02 23:51:23 +0000

    additional testcases - added testcases for selections (but need more)   NOTE: testcase for Issue 43 (parenthesis without spaces) will currently throw and ERROR         until Issue 43 is fixed - added initial testcases for important methods of AtomGroup
    git-svn-id: https://mdanalysis.googlecode.com/svn/trunk@456 dcee7792-c13e-0410-88f5-272e5f544b36

--- MDAnalysis/tests/test_atomgroup.py
+++ MDAnalysis/tests/test_atomgroup.py
@@ -1,10 +1,71 @@
 import MDAnalysis
 from MDAnalysis.tests.datafiles import PSF,DCD
+import MDAnalysis.core.AtomGroup
 
 from numpy.testing import *
+from numpy import array, float32
 from nose.plugins.attrib import attr
 
 import tempfile
+
+class TestAtomGroup(TestCase):
+    """Tests of AtomGroup; selections are tested separately."""
+    def setUp(self):
+        """Set up the standard AdK system in implicit solvent."""
+        self.universe = MDAnalysis.Universe(PSF, DCD)
+        self.ag = self.universe.atoms  # prototypical AtomGroup
+
+    def test_newAtomGroup(self):
+        newag = MDAnalysis.core.AtomGroup.AtomGroup(self.ag[1000:2000:200])
+        assert_equal(type(newag), type(self.ag), "Failed to make a new AtomGroup: type mismatch")
+        assert_equal(newag.numberOfAtoms(), len(self.ag[1000:2000:200]))
+        assert_equal(newag.numberOfResidues(), 5)
+        assert_almost_equal(newag.totalMass(),  40.044999999999995) # check any special method
+
+    def test_numberOfAtoms(self):
+        assert_equal(self.ag.numberOfAtoms(), 3341)
+
+    def test_numberOfResidues(self):
+        assert_equal(self.ag.numberOfResidues(), 214)
+
+    def test_len(self):
+        """testing that len(atomgroup) == atomgroup.numberOfAtoms()"""
+        assert_equal(len(self.ag), self.ag.numberOfAtoms(), "len and numberOfAtoms() disagree")
+
+    def test_centerOfGeometry(self):
+        assert_array_almost_equal(self.ag.centerOfGeometry(), 
+                                  array([-0.04223963,  0.0141824 , -0.03505163], dtype=float32))
+    def test_centerOfMass(self):
+        assert_array_almost_equal(self.ag.centerOfMass(), 
+                                  array([-0.01094035,  0.05727601, -0.12885778]))
+
+    def test_charges(self):
+        assert_array_almost_equal(self.ag.charges()[1000:2000:200], 
+                                  array([-0.09,  0.09, -0.47,  0.51,  0.09]))
+        
+    def test_coordinates(self):
+        assert_array_almost_equal(self.ag.coordinates()[1000:2000:200], 
+                                  array([[  3.94543672, -12.4060812 ,  -7.26820087],
+                                         [ 13.21632767,   5.879035  , -14.67914867],
+                                         [ 12.07735443,  -9.00604534,   4.09301519],
+                                         [ 11.35541916,   7.0690732 ,  -0.32511973],
+                                         [-13.26763439,   4.90658951,  10.6880455 ]], dtype=float32))
+    def test_indices(self):
+        assert_array_equal(self.ag.indices()[:5], array([0, 1, 2, 3, 4]))
+
+    def test_principalAxes(self):
+        assert_array_almost_equal(self.ag.principalAxes(),
+                                  array([[ -9.99925632e-01,   1.21546132e-02,   9.98264877e-04],
+                                         [  1.20986911e-02,   9.98951474e-01,  -4.41539838e-02],
+                                         [  1.53389276e-03,   4.41386224e-02,   9.99024239e-01]]))
+
+    def test_totalCharge(self):
+        assert_almost_equal(self.ag.totalCharge(), -4.0)
+
+    # TODO: add all other methods except selectAtoms(), see test_selections.py
+
+    # add new methods here...
+
 
 class _WriteAtoms(TestCase):
     """Set up the standard AdK system in implicit solvent."""
@@ -27,7 +88,7 @@ class _WriteAtoms(TestCase):
                                   err_msg="atom coordinate mismatch between original and %s file" % self.ext)
 
     def test_write_selection(self):
-        """Test writing CA atoms to external file."""
+        """test_write_selection: Test writing CA atoms to external file."""
         CA = self.universe.selectAtoms('name CA')
         CA.write(self.outfile)
         u2 = self.universe_from_tmp()
---
+++ MDAnalysis/tests/test_selections.py
@@ -0,0 +1,91 @@
+import MDAnalysis
+import MDAnalysis.core.Selection
+from MDAnalysis.tests.datafiles import PSF,DCD
+
+from numpy.testing import *
+from numpy import array, float32
+from nose.plugins.attrib import attr
+
+
+class TestSelections(TestCase):
+    def setUp(self):
+        """Set up the standard AdK system in implicit solvent."""
+        self.universe = MDAnalysis.Universe(PSF, DCD)
+
+    def test_segid(self):
+        sel = self.universe.selectAtoms('segid 4AKE')
+        assert_equal(sel.numberOfAtoms(), 3341, "failed to select segment 4AKE")
+        assert_equal(sel.atoms, self.universe.s4AKE.atoms, 
+                     "selected segment 4AKE is not the same as auto-generated segment s4AKE")
+    def test_protein(self):
+        sel = self.universe.selectAtoms('protein')
+        assert_equal(sel.numberOfAtoms(), 3341, "failed to select protein")
+        assert_equal(sel.atoms, self.universe.s4AKE.atoms, 
+                     "selected protein is not the same as auto-generated protein segment s4AKE")
+
+    def test_backbone(self):
+        sel = self.universe.selectAtoms('backbone')
+        assert_equal(sel.numberOfAtoms(), 855)
+
+    def test_resid_single(self):
+        sel = self.universe.selectAtoms('resid 100')
+        assert_equal(sel.numberOfAtoms(), 7)
+        assert_equal(sel.resnames(), ['GLY'])        
+
+    def test_resid_range(self):
+        sel = self.universe.selectAtoms('resid 100:105')
+        assert_equal(sel.numberOfAtoms(), 89)
+        assert_equal(sel.resnames(),  ['GLY', 'ILE', 'ASN', 'VAL', 'ASP', 'TYR'])        
+
+    def test_resname(self):
+        sel = self.universe.selectAtoms('resname LEU')
+        assert_equal(sel.numberOfAtoms(), 304, "Failed to find all 'resname LEU' atoms.")
+        assert_equal(sel.numberOfResidues(), 16, "Failed to find all 'resname LEU' residues.")
+        assert_equal(sel.atoms, self.universe.s4AKE.LEU.atoms,
+                     "selected 'resname LEU' atoms are not the same as aut-generated s4AKE.LEU")
+
+    def test_name(self):
+        sel = self.universe.selectAtoms('name CA')
+        assert_equal(sel.numberOfAtoms(), 214)
+
+    def test_atom(self):
+        sel = self.universe.selectAtoms('atom 4AKE 100 CA')
+        assert_equal(len(sel), 1)
+        assert_equal(sel.resnames(), ['GLY'])
+        assert_array_almost_equal(sel.coordinates(), 
+                                  array([[ 20.38685226,  -3.44224262,  -5.92158318]], dtype=float32))
+
+    def test_and(self):
+        sel = self.universe.selectAtoms('resname GLY and resid 100')
+        assert_equal(len(sel), 7)
+
+    def test_or(self):
+        sel = self.universe.selectAtoms('resname LYS or resname ARG')
+        assert_equal(sel.numberOfResidues(), 31)
+
+    def test_not(self):
+        sel = self.universe.selectAtoms('not backbone')
+        assert_equal(len(sel), 2486)
+
+
+    # TODO:
+    # add more test cases for around, point, prop, byres, bynum
+    # and also for selection keywords such as 'nucleic' 
+
+    def test_empty_selection(self):
+        """Test empty selection: raises Excption but might be subject to change (see Issue 12)"""
+        assert_raises(Exception, self.universe.selectAtoms, 'resname TRP')  # no Trp in AdK
+
+    def test_parenthesized_expression(self):
+        sel = self.universe.selectAtoms('( name CA or name CB ) and resname LEU')
+        assert_equal(len(sel), 32)
+
+    def test_no_space_around_parentheses(self):
+        """Test that no space is needed around parentheses (Issue 43)."""
+        # note: will currently be ERROR because it throws a ParseError
+        sel = self.universe.selectAtoms('(name CA or name CB) and resname LEU')
+        assert_equal(len(sel), 32)
+
+
+    # TODO:
+    # test for checking ordering and multiple comma-separated selections
