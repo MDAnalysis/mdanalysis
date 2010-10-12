1d9fd027adc4ac1aea75461be70d32a721f5776a
Oliver Beckstein <orbeckst@gmail.com>
2010-10-12 01:10:24 +0000

    fixes to distances test so that it runs with deprecated code removed
    git-svn-id: https://mdanalysis.googlecode.com/svn/trunk@481 dcee7792-c13e-0410-88f5-272e5f544b36

--- MDAnalysis/tests/test_distances.py
+++ MDAnalysis/tests/test_distances.py
@@ -1,10 +1,11 @@
 import MDAnalysis
+import MDAnalysis.core.distances
 
 import numpy as np
 from numpy.testing import *
 from nose.plugins.attrib import attr
 
-from MDAnalysis.tests.datafiles import PSF,DCD,PDB_small,PDB,XTC,TRR
+from MDAnalysis.tests.datafiles import PSF,DCD
 
 class TestDistanceArrayDCD(TestCase):
     def setUp(self):
@@ -31,20 +32,6 @@ class TestDistanceArrayDCD(TestCase):
         assert_almost_equal(d.min(), 0.11981228170520701, self.prec, 
                             err_msg="wrong minimum distance value")
         assert_almost_equal(d.max(), 53.572192429459619, self.prec,
-                            err_msg="wrong maximum distance value")
-
-    @attr('issue')
-    def test_copyTrue(self):
-        U = self.universe
-        self.dcd.rewind()
-        x0 = U.atoms.coordinates(copy=True)
-        self.dcd[10]
-        x1 = U.atoms.coordinates(copy=True)
-        d = MDAnalysis.core.distances.distance_array(x0, x1)
-        assert_equal(d.shape, (3341, 3341), "wrong shape, should be (Natoms,Natoms)")
-        assert_almost_equal(d.min(), 0.11981228170520701, self.prec, 
-                            err_msg="wrong minimum distance value")
-        assert_almost_equal(d.max(), 53.572192429459619, self.prec, 
                             err_msg="wrong maximum distance value")
 
     def test_outarray(self):
@@ -90,18 +77,6 @@ class TestSelfDistanceArrayDCD(TestCase):
         self.dcd.rewind()
         x0 = U.atoms.coordinates(copy=True)
         d = MDAnalysis.core.distances.self_distance_array(x0)
-        N = 3341 * (3341 - 1) / 2
-        assert_equal(d.shape, (N,), "wrong shape (should be (Natoms*(Natoms-1)/2,))")
-        assert_almost_equal(d.min(), 0.92905562402529318, self.prec,
-                            err_msg="wrong minimum distance value")
-        assert_almost_equal(d.max(), 52.4702570624190590, self.prec,
-                            err_msg="wrong maximum distance value")
-
-    def test_copyTrue(self):
-        U = self.universe
-        self.dcd.rewind()
-        x0 = U.atoms.coordinates(copy=True)
-        d = MDAnalysis.core.distances.self_distance_array(x0, copy=True)
         N = 3341 * (3341 - 1) / 2
         assert_equal(d.shape, (N,), "wrong shape (should be (Natoms*(Natoms-1)/2,))")
         assert_almost_equal(d.min(), 0.92905562402529318, self.prec,
