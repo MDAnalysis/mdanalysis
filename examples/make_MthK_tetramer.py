4249e9520de7c726ba4effc8a172b6ee892d98cd
Oliver Beckstein <orbeckst@gmail.com>
2010-10-19 20:34:10 +0000

    simple example for using rotateby(): build the MthK tetramer (sort of...)
    git-svn-id: https://mdanalysis.googlecode.com/svn/trunk@495 dcee7792-c13e-0410-88f5-272e5f544b36

---
+++ examples/make_MthK_tetramer.py
@@ -0,0 +1,23 @@
+"""Example: building a MthK K-channel tetramer
+
+Rotate the monomer in pdb:3LDD around the 4-fold axis formed by the
+K+-ions in the filter.
+
+.. Note:: This example is not exact; use the BIOMT record in the PDB
+          to create the real tetramer.
+"""
+import MDAnalysis
+
+# need permissive to read HETATM (apparently...)
+P = MDAnalysis.Universe('./data/3ldd.pdb', permissive=True)
+filterK = P.selectAtoms('resname K and resid 1:4')
+monomer = P.selectAtoms('protein')
+axis = (filterK[0], filterK[-1])  # first to last filter ion
+monomer.write('A.pdb')
+monomer.rotateby(90, axis, filterK)
+monomer.write('B.pdb')
+monomer.rotateby(90, axis, filterK)
+monomer.write('C.pdb')
+monomer.rotateby(90, axis, filterK)
+monomer.write('D.pdb')
+
