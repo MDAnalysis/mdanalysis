fba02f016170e9ed49644630126a404d006e301b
Oliver Beckstein <orbeckst@gmail.com>
2011-06-14 11:36:02 +0000

    fixed: stop_logging()
    git-svn-id: https://mdanalysis.googlecode.com/svn/trunk@818 dcee7792-c13e-0410-88f5-272e5f544b36

--- MDAnalysis/__init__.py
+++ MDAnalysis/__init__.py
@@ -186,7 +186,8 @@ def start_logging(logfile="MDAnalysis.log"):
 def stop_logging():
     """Stop logging to logfile and console."""
     import core.log
-    logging.getLogger("MDAnalysis").info("MDAnalysis STOPPED logging")
+    logger = logging.getLogger("MDAnalysis")
+    logger.info("MDAnalysis STOPPED logging")
     core.log.clear_handlers(logger)  # this _should_ do the job...
 
 # custom exceptions and warnings
