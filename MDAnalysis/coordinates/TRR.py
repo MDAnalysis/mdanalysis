134951b82d84cf7269f8efc49bcf600342056841
Oliver Beckstein <orbeckst@gmail.com>
2011-10-05 23:10:12 +0000

    Issue 79: recipe for making PCA TRRs from arrays
    Ramon Crehuet's recipe from Issue 79 and the mailing list,
    slightly commented and some missing context added.
    (If there are problems, re-open Issue 79.)
    
    git-svn-id: https://mdanalysis.googlecode.com/svn/trunk@906 dcee7792-c13e-0410-88f5-272e5f544b36

--- MDAnalysis/coordinates/TRR.py
+++ MDAnalysis/coordinates/TRR.py
@@ -19,6 +19,72 @@
 Gromacs TRR file IO --- :mod:`MDAnalysis.coordinates.TRR`
 =========================================================
 
+The Gromacs `TRR trajectory format`_ is a lossless format like
+e.g. the DCD format (see :mod:`~MDAnalysis.coordinates.DCD`) and
+unlike the :mod:`~MDAnalysis.coordinates.XTC` format, which stores
+reduced precision coordinates. Therefore, if one wants to convert
+*to* Gromacs trajectories without loss of precision then one should
+use the TRR format.
+
+The TRR format can store *velocities* and *forces* in addition to
+coordinates. It is also used by other Gromacs tools to store and
+process other data such as modes from a principal component analysis.
+
+.. _TRR trajectory format:
+   http://www.gromacs.org/Documentation/File_Formats/.trr_File
+
+
+Tips and Tricks
+---------------
+
+Filling a TRR with PCA modes
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The following `recipe by Ramon Crehuet`_ shows how to convert modes
+stored in a NumPy-like array (e.g. from a PCA analysis with MMTK_) to
+a TRR usable by Gromacs. The idea is to manually fill a
+:class:`~MDAnalysis.coordinates.xdrfile.TRR.Timestep` with the desired
+values and then write it to a file with the appropriate
+:class:`~MDAnalysis.coordinates.xdrfile.TRR.TRRWriter`. In order to
+respect the Gromacs format for modes in a TRR file, one must write the
+average coordinates in the first frame of the TRR and the modes into
+subsequent ones. The mode number is stored in the
+:attr:`~MDAnalysis.coordinates.xdrfile.TRR.Timestep.step` attribute
+and the mode coordinates are filling the
+:attr:`~MDAnalysis.coordinates.xdrfile.TRR.Timestep._pos` attribute of
+:class:`~MDAnalysis.coordinates.xdrfile.TRR.Timestep`::
+
+   # 'modes' is a mode object with M PCs, similar to a MxNx3 array
+   # 'xav' the average coordinates, a Nx3 array for N atoms
+
+   N = len(xav)   # number of atoms, i.e. number of coordinates
+
+   W = Writer('pca.trr', numatoms=N)            # TRR writer
+   ts = MDAnalysis.coordinates.TRR.Timestep(N)  # TRR time step
+
+   for frame,mode in enumerate(modes[4:16]):
+       ts.lmbda = -1
+       if frame<=1:
+          ts._pos[:] = xav
+       else:
+          ts._pos[:] = mode.scaledToNorm(1.).array*10   # nm to angstroms
+       ts.frame = frame         # manually change the frame number
+       ts.step = frame - 1
+       if frame <= 1:
+          ts.time = frame-1
+       else:
+          ts.time = mode.frequency
+       W.write(ts)             # converts angstrom to nm for gmx
+
+    W.close()
+
+.. _MMTK: http://dirac.cnrs-orleans.fr/Manuals/MMTK/index.html
+
+.. _`recipe by Ramon Crehuet`: http://code.google.com/p/mdanalysis/issues/detail?id=79
+
+Module reference
+----------------
+
 .. automodule:: MDAnalysis.coordinates.xdrfile.TRR
    :members:
 """
