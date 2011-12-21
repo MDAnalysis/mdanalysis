b26bbffe6bcc820dd4981c39b5dea4f86771c9d9
Oliver Beckstein <orbeckst@gmail.com>
2011-12-21 09:35:53 -0700

    Issue 87: testsuite along package

---
+++ testsuite/MDAnalysisTestData/__init__.py
@@ -0,0 +1,55 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+# MDAnalysis --- http://mdanalysis.googlecode.com
+# Copyright (c) 2006-2011 Naveen Michaud-Agrawal,
+#               Elizabeth J. Denning, Oliver Beckstein,
+#               and contributors (see website for details)
+# Released under the GNU Public Licence, v2 or any higher version
+#
+# Please cite your use of MDAnalysis in published work:
+#
+#     N. Michaud-Agrawal, E. J. Denning, T. B. Woolf, and
+#     O. Beckstein. MDAnalysis: A Toolkit for the Analysis of
+#     Molecular Dynamics Simulations. J. Comput. Chem. 32 (2011), 2319--2327,
+#     doi:10.1002/jcc.21787
+#
+
+"""
+=========================
+Test data for MDAnalysis
+=========================
+
+This package only contains real-life data files that are used in the
+MDAnalysis unit tests (see
+http://code.google.com/p/mdanalysis/wiki/UnitTests for details and
+:mod:`MDAnalysis.tests`)
+
+Data
+====
+
+The simulation data used in some tests are from [Beckstein2009]_
+(``adk.psf``, ``adk_dims.dcd``) or unpublished simulations
+(O. Beckstein). Other data are user contributed.
+
+   adk_dims
+      Trajectory of a macromolecular transition of the enzyme adenylate kinase
+      between a closed and an open conformation. The simulation was run in
+      Charmm_ c35a1.
+
+   adk_oplsaa
+      Ten frames from the first 1 ns of a equilibrium trajectory of AdK in
+      water with Na+ counter ions. The OPLS/AA forcefield is used with the
+      TIP4P water model. The simulation was run with Gromacs_ 4.0.2.
+
+
+[Beckstein2009] O. Beckstein, E.J. Denning, J.R. Perilla and T.B. Woolf,
+                Zipping and Unzipping of Adenylate Kinase: Atomistic Insights
+                into the Ensemble of Open ↔ Closed Transitions. J Mol Biol 394
+                (2009), 160–176, doi:10.1016/j.jmb.2009.09.009
+
+
+"""
+__all__ = ['datafiles']
+
+import datafiles
---
+++ testsuite/setup.py
@@ -0,0 +1,101 @@
+# $Id$
+"""Setuptools-based setup script for MDAnalysis.
+
+A working installation of NumPy <http://numpy.scipy.org> is required.
+
+For a basic installation just type the command::
+
+  python setup.py install
+
+For more in-depth instructions, see the installation section at the
+MDAnalysis Wiki:
+
+  http://code.google.com/p/mdanalysis/wiki/Install
+
+Also free to ask on the MDAnalysis mailing list for help:
+
+  http://groups.google.com/group/mdnalysis-discussion
+
+(Note that the group really is called `mdnalysis-discussion' because
+Google groups forbids any name that contains the string `anal'.)
+
+By default we use setuptools <http://pypi.python.org/pypi/setuptools>.  The
+details of such an "EasyInstall" installation procedure are shown on
+
+  http://peak.telecommunity.com/DevCenter/EasyInstall
+
+By changing the code below you can also switch to a standard distutils
+installation.
+"""
+
+#------------------------------------------------------------
+# selection of the installation system
+#------------------------------------------------------------
+#
+# Standard distutils-based installation:
+#
+##from distutils.core import setup, Extension
+
+# setuptools ("EasyInstall") installation:
+#
+# If you want EasyInstall features then enable the next three lines and comment
+# out the preceding line 'from distutils.core import ...'
+#
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, Extension
+#
+#------------------------------------------------------------
+
+import sys, os
+import glob
+
+# Make sure I have the right Python version.
+if sys.version_info[:2] < (2, 5):
+    print "MDAnalysis requires Python 2.5 or better. Python %d.%d detected" % \
+        sys.version_info[:2]
+    print "Please upgrade your version of Python."
+    sys.exit(-1)
+
+
+if __name__ == '__main__':
+    RELEASE = "0.7.4"
+    LONG_DESCRIPTION = \
+"""MDAnalysis is a tool for analyzing molecular dynamics trajectories.
+
+This package contains the trajectory data that are used for the test
+cases. In order to make downloads more efficient, these data were
+split up into a separate package.
+"""
+    CLASSIFIERS = ['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Scientists',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: POSIX',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics',
+                   'Topic :: Scientific/Engineering :: Chemistry',
+                   ]
+
+    setup(name              = 'MDAnalysisTestData',
+          version           = RELEASE,
+          description       = 'Python tools to support analysis of trajectories (test data files)',
+          author            = 'Naveen Michaud-Agrawal',
+          author_email      = 'naveen.michaudagrawal@gmail.com',
+          url               = 'http://mdanalysis.googlecode.com/',
+          license           = 'GPL 2',
+          packages          = ['MDAnalysisTestData'],
+          package_dir       = {'MDAnalysisTestData': 'MDAnalysisTestData'},
+          package_data      = {'MDAnalysisTestData':
+                                   ['data/*.psf','data/*.dcd','data/*.pdb',
+                                    'data/*.gro', 'data/*.xtc','data/*.trr',
+                                    'data/*.crd', 'data/*.xyz',
+                                    'data/*.prmtop', 'data/*.trj', 'data/*.mdcrd',
+                                    'data/*.pqr', 'data/*.pdbqt', 'data/*.bz2',
+                                    ],
+                               },
+          classifiers       = CLASSIFIERS,
+          long_description  = LONG_DESCRIPTION,
+          zip_safe = False,   # had 'KeyError' as zipped egg (2MB savings are not worth the trouble)
+          )
