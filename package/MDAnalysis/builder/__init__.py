0c9cf4612e4ec889c34d8ac2d441a93386672efd
Oliver Beckstein <orbeckst@gmail.com>
2012-02-03 12:49:44 -0700

    new builder module: helper for creating CHARMM-ready structures

---
+++ package/MDAnalysis/builder/__init__.py
@@ -0,0 +1,27 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+# MDAnalysis --- http://mdanalysis.googlecode.com
+# Copyright (c) 2006-2012 Naveen Michaud-Agrawal,
+#               Elizabeth J. Denning, Oliver Beckstein,
+#               and contributors (see website for details)
+# Released under the GNU Public Licence, v2 or any higher version
+#
+# Please cite your use of MDAnalysis in published work:
+#
+#     N. Michaud-Agrawal, E. J. Denning, T. B. Woolf, and
+#     O. Beckstein. MDAnalysis: A Toolkit for the Analysis of
+#     Molecular Dynamics Simulations. J. Comput. Chem. 32 (2011), 2319--2327,
+#     doi:10.1002/jcc.21787
+#
+
+"""
+Building models and setting up systems
+======================================
+
+The :mod:`MDAnalysis.build` module contains a growing collection of
+helper functions and classes to construct models and systems.
+
+"""
+
+__all__ = ['charmm']
