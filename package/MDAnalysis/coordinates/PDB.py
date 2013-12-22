MDAnalysis/mdanalysis

commit cf13de7d55c3fa5e326026ee5f40a02383462d0c

fixed: PDB with blank lines gave IndexError (fixes Issue 158)

package/MDAnalysis/coordinates/PDB.py
@@ -418,10 +418,11 @@ class PDBWriter(base.Writer):
 class PrimitivePDBReader(base.Reader):
     """PDBReader that reads a PDB-formatted file, no frills.
 
-    Records read (see `PDB coordinate section`_ for details):
-     - CRYST1 for unitcell A,B,C, alpha,beta,gamm
-     - ATOM or HETATM for x,y,z
+    The following PDB records are parsed (see `PDB coordinate section`_ for details):
+     - CRYST1 for unitcell A,B,C, alpha,beta,gamma
+     - ATOM or HETATM for serial,name,resName,chainID,resSeq,x,y,z,occupancy,tempFactor
      - HEADER, TITLE, COMPND
+     - all other lines are ignored
 
     Reads multi-`MODEL`_ PDB files as trajectories.
 
@@ -509,7 +510,7 @@ class PrimitivePDBReader(base.Reader):
             for i, line in enumerate(pdbfile):
                 def _c(start, stop, typeclass=float):
                     return self._col(line, start, stop, typeclass=typeclass)
-                if line.split()[0] == 'END':
+                if line[:3] == 'END':
                     break
                 elif line[:6] == 'CRYST1':
                     A, B, C = _c(7, 15), _c(16, 24), _c(25, 33)

commit b6fff8dbc11343b857e8e9bc65ec10699b0ad02f

Fixed regression in PDB.py introduced by commit cf13de7d55c3

package/MDAnalysis/coordinates/PDB.py
@@ -508,32 +508,31 @@ class PrimitivePDBReader(base.Reader):
 
         with util.openany(filename, 'r') as pdbfile:
             for i, line in enumerate(pdbfile):
+                line = line.strip() # Remove extra spaces
+                if len(line) == 0: # Skip line if empty
+                    continue
+                section = line.split()[0]
                 def _c(start, stop, typeclass=float):
                     return self._col(line, start, stop, typeclass=typeclass)
-                if line[:3] == 'END':
+                if section == 'END':
                     break
-                elif line[:6] == 'CRYST1':
+                elif section == 'CRYST1':
                     A, B, C = _c(7, 15), _c(16, 24), _c(25, 33)
                     alpha, beta, gamma = _c(34, 40), _c(41, 47), _c(48, 54)
                     unitcell[:] = A, B, C, alpha, beta, gamma
                     continue
-                elif line[:6] == 'HEADER':
+                elif section == 'HEADER':
                     header = line[6:-1]
                     continue
-                elif line[:6] == 'COMPND':
+                elif section == 'COMPND':
                     l = line[6:-1]
                     compound.append(l)
                     continue
-                elif line[:6] == 'REMARK':
+                elif section == 'REMARK':
                     content = line[6:-1]
                     remarks.append(content)
-                elif line[:5] == 'MODEL':
-                    frameno = int(line.split()[1])
-                    if frameno == 0:
-                        # detect if MODEL starts at 0 or at 1; switch model_offset appropriately
-                        # Will break/lose frames if MODELs are nonconsecutive in the PDB file...
-                        self.model_offset = 1
-                    frames[frameno + self.model_offset] = i # 1-based indexing
+                elif section == 'MODEL':
+                    frames[len(frames) + 1] = i  # 1-based indexing
                 elif line[:6] in ('ATOM  ', 'HETATM'):
                     # skip atom/hetatm for frames other than the first - they will be read in when next() is called on the trajectory reader
                     if len(frames) > 1: