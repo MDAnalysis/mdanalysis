54b2860e881cc2fe7b5e52b763dbd3f7c326f258
Tyler Reddy <tyler.je.reddy@gmail.com>
2014-02-23 09:12:52 +0000

    MDAnalysis.visualization.streamlines adjusted in the following ways: -an ImportError with appropriate message should now be raised when a non-standard dependency of this module is not available (rather than a print statement only) -the only non-standard (i.e., outside of regular MDAnalysis) module requirement has been simplified to matplotlib.nxutils, and ideally this requirement will eventually be removed because it is a deprecated module of matplotlib (and also an analytical rather than plotting use of matplotlib) -the standard dependencies (MDAnalysis, multiprocessing, numpy, scipy) are now imported in separate statements for improved dependency sanity -a substantial amount of extraneous debugging code has been removed (although a bit more clean-up could likely still be done) -the placeholder main() function has been renamed to generate_streamlines() and the workflow may eventually be adjusted (perhaps encapsulated in a class)

--- package/MDAnalysis/visualization/streamlines.py
+++ package/MDAnalysis/visualization/streamlines.py
@@ -5,11 +5,14 @@
 :Copyright: GNU Public License v3'''
 
 try:
-    import matplotlib, matplotlib.path, matplotlib.nxutils, matplotlib.pyplot
+    import matplotlib, matplotlib.nxutils
 except ImportError:
-    '2d streamplot module requires:matplotlib, matplotlib.path, matplotlib.nxutils, matplotlib.pyplot'
+    raise ImportError('2d streamplot module requires: matplotlib.nxutils for its points_inside_poly method') #deprecated, aim to remove dependency in future
 
-import MDAnalysis, multiprocessing, numpy, scipy
+import MDAnalysis
+import multiprocessing
+import numpy
+import scipy
 
 def produce_grid(tuple_of_limits,grid_spacing):
     '''Produce a grid for the simulation system based on the tuple of Cartesian Coordinate limits calculated in an earlier step.'''
@@ -22,8 +25,6 @@ def split_grid(grid,num_cores):
     #produce an array containing the cartesian coordinates of all vertices in the grid:
     x_array, y_array = grid 
     grid_vertex_cartesian_array = numpy.dstack((x_array,y_array))
-    #print grid_vertex_cartesian_array
-    #print grid_vertex_cartesian_array.shape 
     #the grid_vertex_cartesian_array has N_rows, with each row corresponding to a column of coordinates in the grid (so a given row has shape N_rows, 2); overall shape (N_columns_in_grid, N_rows_in_a_column, 2)
     #although I'll eventually want a pure numpy/scipy/vector-based solution, for now I'll allow loops to simplify the division of the cartesian coordinates into a list of the squares in the grid
     list_all_squares_in_grid = [] #should eventually be a nested list of all the square vertices in the grid/system
@@ -42,17 +43,13 @@ def split_grid(grid,num_cores):
             current_row += 1
         current_column += 1
     #split the list of square vertices [[v1,v2,v3,v4],[v1,v2,v3,v4],...,...] into roughly equally-sized sublists to be distributed over the available cores on the system:
-    #print list_all_squares_in_grid
     list_square_vertex_arrays_per_core = numpy.array_split(list_all_squares_in_grid,num_cores)
     list_parent_index_values = numpy.array_split(list_parent_index_values,num_cores)
-    #print list_square_vertex_arrays_per_core
     return [list_square_vertex_arrays_per_core,list_parent_index_values,current_row,current_column]
 
 def per_core_work(coordinate_file_path,trajectory_file_path,list_square_vertex_arrays_this_core,MDA_selection,start_frame,end_frame,reconstruction_index_list,maximum_delta_magnitude):
     '''The code to perform on a given core given the list of square vertices assigned to it.'''
     print multiprocessing.current_process().name, 'Starting'
-    #print 'reconstruction index list:', reconstruction_index_list
-    #print list_square_vertex_arrays_this_core
     #obtain the relevant coordinates for particles of interest
     universe_object = MDAnalysis.Universe(coordinate_file_path,trajectory_file_path)
     list_previous_frame_centroids = []
@@ -62,7 +59,6 @@ def per_core_work(coordinate_file_path,trajectory_file_path,list_square_vertex_a
         list_indices_point_in_polygon = []
         for square_vertices in vertex_coord_list:
             index_list_in_polygon = numpy.where(matplotlib.nxutils.points_inside_poly(relevant_particle_coordinate_array_xy, square_vertices))
-            #if index_list_in_polygon[0].size < 1: print 'null index list'
             list_indices_point_in_polygon.append(index_list_in_polygon)
         return list_indices_point_in_polygon
 
@@ -71,26 +67,20 @@ def per_core_work(coordinate_file_path,trajectory_file_path,list_square_vertex_a
         for indices in list_indices_in_polygon:
             if not indices[0].size > 0: #if there are no particles of interest in this particular square
                 list_centroids_this_frame.append('empty')
-                #print 'empty square'
             else: 
                 current_coordinate_array_in_square = relevant_particle_coordinate_array_xy[indices]
-                #print 'current_coordinate_array_in_square:',current_coordinate_array_in_square
                 current_square_indices_centroid = numpy.average(current_coordinate_array_in_square,axis=0)
                 list_centroids_this_frame.append(current_square_indices_centroid)
         return list_centroids_this_frame #a list of numpy xy centroid arrays for this frame
-    #print 'Marker 1'
     for ts in universe_object.trajectory:
         if ts.frame < start_frame: #don't start until first specified frame
             print multiprocessing.current_process().name, 'skipping frame',ts.frame
             continue
         relevant_particle_coordinate_array_xy = universe_object.selectAtoms(MDA_selection).coordinates()[...,:-1] #only 2D / xy coords for now
-        #print 'Marker 2'
         #I will need a list of indices for relevant particles falling within each square in THIS frame:
         list_indices_in_squares_this_frame = produce_list_indices_point_in_polygon_this_frame(list_square_vertex_arrays_this_core)
-        #print 'Marker 3'
         #likewise, I will need a list of centroids of particles in each square (same order as above list):
         list_centroids_in_squares_this_frame = produce_list_centroids_this_frame(list_indices_in_squares_this_frame)
-        #print 'Marker 4'
         if list_previous_frame_indices: #if the previous frame had indices in at least one square I will need to use those indices to generate the updates to the corresponding centroids in this frame:
             list_centroids_this_frame_using_indices_from_last_frame = produce_list_centroids_this_frame(list_previous_frame_indices)
             #I need to write a velocity of zero if there are any 'empty' squares in either frame:
@@ -114,17 +104,12 @@ def per_core_work(coordinate_file_path,trajectory_file_path,list_square_vertex_a
         else: #either no points in squares or after the first frame I'll just reset the 'previous' values so they can be used when consecutive frames have proper values
             list_previous_frame_centroids = list_centroids_in_squares_this_frame[:]
             list_previous_frame_indices = list_indices_in_squares_this_frame[:]
-        #print 'Marker 5'
         if ts.frame > end_frame: break #stop here 
         print multiprocessing.current_process().name,'frame:',ts.frame
     print multiprocessing.current_process().name, 'Finishing'
-    #I'm going to take the list of square vertices given to this core and convert it to a list of square centroids to simplify downstream sorting and reconstruction of the overall data matrix in the parent
-    #list_square_centroids = [numpy.average(array,axis=0).tolist() for array in list_square_vertex_arrays_this_core]
-    #instead of providing a list of square centroids to the parent, what I really want is the actual assignment position within the appropriately-shaped numpy array
-
     return zip(reconstruction_index_list,xy_deltas_to_write.tolist())
 
-def main(coordinate_file_path,trajectory_file_path,grid_spacing,MDA_selection,start_frame,end_frame,xmin,xmax,ymin,ymax,maximum_delta_magnitude,num_cores='maximum'):
+def generate_streamlines(coordinate_file_path,trajectory_file_path,grid_spacing,MDA_selection,start_frame,end_frame,xmin,xmax,ymin,ymax,maximum_delta_magnitude,num_cores='maximum'):
     '''The main control function of the code. The various steps in the workflow should be performed in order here.'''
     #work out the number of cores to use:
     if num_cores == 'maximum':
@@ -161,15 +146,12 @@ def main(coordinate_file_path,trajectory_file_path,grid_spacing,MDA_selection,st
         else:
             dy_array[index_1,index_2] = delta_array[1]
     
-    
     #at Matthieu's request, we now want to calculate the average and standard deviation of the displacement values:
     displacement_array = numpy.sqrt(dx_array ** 2 + dy_array ** 2)
     average_displacement = numpy.average(displacement_array)
     standard_deviation_of_displacement = numpy.std(displacement_array)
 
     return (dx_array,dy_array,average_displacement,standard_deviation_of_displacement)
-
-
 
 #if __name__ == '__main__': #execute the main control function only if this file is called as a top-level script
     #will probably mostly use this for testing on a trajectory:
