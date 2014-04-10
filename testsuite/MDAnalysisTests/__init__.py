177d5e956b08e0851a73d4bb06d60c010755f722
Oliver Beckstein <orbeckst@gmail.com>
2014-04-10 15:30:02 -0700

    bumped release to 0.8.2-dev
    (Also corrected the 0.8.1 release date in CHANGELOG)

--- testsuite/MDAnalysisTests/__init__.py
+++ testsuite/MDAnalysisTests/__init__.py
@@ -102,7 +102,7 @@ especially as we are directly using this framework (imported from numpy).
 .. _Gromacs: http://www.gromacs.org
 """
 
-__version__ = "0.8.1"   # keep in sync with RELEASE in setup.py
+__version__ = "0.8.2-dev"   # keep in sync with RELEASE in setup.py
 
 try:
     from numpy.testing import Tester
