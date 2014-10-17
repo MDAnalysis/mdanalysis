a8ed84cf527e31d18c9f042c1d35a1943c6651f4
Oliver Beckstein <orbeckst@gmail.com>
2014-10-17 12:07:17 -0700

    Vagrant/VirtualBox virtual machines with full feature MDAnalysis
    - Ubuntu 14.04
    - Ubuntu 12.04

---
+++ vm/Ubuntu/14.04/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Packages for MDAnalysis on Ubuntu 14.04
+apt-get update
+
+echo "provisioning Python development stack..."
+apt-get install -y build-essential python-dev python-setuptools python-pip
+
+echo "provisioning scientific Python stack..."
+apt-get install -y python-numpy python-scipy python-matplotlib python-biopython python-networkx ipython
+
+echo "provisioning NETCDF4 for Python..."
+apt-get install -y libhdf5-serial-dev libnetcdf-dev
+pip install netCDF4
+
+# support
+#apt-get install -y clustalo
+
+echo "provisioning MDAnalysis..."
+pip install MDAnalysis MDAnalysisTests
+
+
+ 
+
