3d9b19ffb7a3ca182dcbecd95a63f0a6c18a413e
Tyler Reddy <tyler.je.reddy@gmail.com>
2015-02-17 09:03:33 -0800

    Sphinx conf.py now specifies secure https mathjax_path. Rebuilt html docs. Should resolve Issue 182.

--- package/doc/sphinx/source/conf.py
+++ package/doc/sphinx/source/conf.py
@@ -66,6 +66,7 @@ if os.path.exists(build_path):
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
               'sphinx.ext.mathjax', 'sphinx.ext.viewcode']
+mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
