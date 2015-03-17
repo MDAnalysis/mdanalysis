5a45de19b328bb119da448a716abc4ef0668243e
Sébastien Buchoux <sebastien.buchoux@gmail.com>
2015-03-17 10:58:36 +0100

    Shebang lines fixed

--- package/setup.py
+++ package/setup.py
@@ -1,4 +1,4 @@
-#!python
+#!/usr/bin/env python
 # -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
 #
