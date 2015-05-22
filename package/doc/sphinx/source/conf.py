0dee7a5332e0e3995ba9f9cf05e7da3e07b71319
Sean L. Seyler <slseyler@asu.edu>
2015-05-21 17:30:41 -0700

    added sseyler to AUTHORS and updated CHANGELOG

--- package/doc/sphinx/source/conf.py
+++ package/doc/sphinx/source/conf.py
@@ -91,7 +91,8 @@ authors = u"""Naveen Michaud-Agrawal, Elizabeth J. Denning, Joshua Adelman,
     Lennard van der Feltz, Philip Fowler, Joseph Goose, Richard J. Gowers, Lukas Grossar,
     Benjamin Hall, Joe Jordan, Jinju Lu, Robert McGibbon, Alex Nesterenko,
     Manuel Nuno Melo, Caio S. Souza, Danny Parton, Joshua L. Phillips, Tyler Reddy,
-    Paul Rigor, Andy Somogyi, Lukas Stelzl, Zhuyi Xue, and Oliver Beckstein"""
+    Paul Rigor, Sean L. Seyler, Andy Somogyi, Lukas Stelzl, Zhuyi Xue,
+    and Oliver Beckstein"""
 project = u'MDAnalysis'
 copyright = u'2005-2015, ' + authors
 
