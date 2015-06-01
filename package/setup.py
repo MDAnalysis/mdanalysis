60cfb5a7bc0024794e0bd0495740ad2f3e596103
Sébastien Buchoux <sebastien.buchoux@gmail.com>
2015-06-01 18:58:07 +0200

    Version changed to 0.11.0 and doc regenerated

--- package/setup.py
+++ package/setup.py
@@ -172,7 +172,7 @@ def detect_openmp():
 
 
 if __name__ == '__main__':
-    RELEASE = "0.10.0"  # NOTE: keep in sync with MDAnalysis.version in __init__.py
+    RELEASE = "0.11.0-dev"  # NOTE: keep in sync with MDAnalysis.version in __init__.py
     with open("SUMMARY.txt") as summary:
         LONG_DESCRIPTION = summary.read()
     CLASSIFIERS = [
