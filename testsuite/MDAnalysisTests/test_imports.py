21d48ccf4c939586c72d4de9c5e3a03e87dabd7b
Oliver Beckstein <orbeckst@gmail.com>
2015-06-23 02:54:10 -0700

    test_imports: test each test_*.py file individually
    - use test generator
    - cleaned out pretty much all other imports but us os.path for path manipulation
    - added linenumber to error message for easier debugging
    Note that only the first relative import triggers a FAIL.

--- testsuite/MDAnalysisTests/test_imports.py
+++ testsuite/MDAnalysisTests/test_imports.py
@@ -13,25 +13,26 @@
 # MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
 # J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
 #
-from __future__ import print_function
-
-from numpy.testing import TestCase, assert_equal
 
 import os
 import glob
 
-class TestRelativeImports(TestCase):
+class TestRelativeImports(object):
     '''Relative imports are banned in unit testing modules (Issue #189), so run tests to enforce this policy.'''
 
-    def setUp(self):
-        self.path_to_testing_modules = os.curdir
+    path_to_testing_modules = os.curdir
+
+    @staticmethod
+    def _run_test_relative_import(testing_module):
+        with open(testing_module, 'r') as test_module_file_object:
+            for lineno, line in enumerate(test_module_file_object, start=1):
+                if 'from .' in line and 'import' in line \
+                        and not 'test_imports' in testing_module:
+                    raise AssertionError(
+                        "A relative import statement was found in "
+                        "module {testing_module} at linenumber {lineno}.".format(**vars()))
 
     def test_relative_imports(self):
         list_testing_modules = glob.glob(os.path.join(self.path_to_testing_modules, '*.py'))
         for testing_module in list_testing_modules:
-            relative_import_counts = 0
-            with open(testing_module, 'r') as test_module_file_object:
-                for line in test_module_file_object:
-                    if 'from .' in line and 'import' in line and not 'test_imports' in testing_module:
-                        relative_import_counts += 1
-            assert_equal(relative_import_counts, 0, "A relative import statement was found in module {module_name}.".format(module_name = testing_module))
+            yield self._run_test_relative_import, testing_module
