526139054f3b0d9d1cae7066a4f6c8ee6a3b89d6
Max Linke <max_linke@gmx.de>
2015-09-09 21:24:34 +0200

    Activate sphinx.ext.napoleon for numpy-style docs
    This also changes one of the docs strings to demonstrate that
    normal ReST files and the numpy-style can live side by side.
    
    [skip ci]

--- package/doc/sphinx/source/conf.py
+++ package/doc/sphinx/source/conf.py
@@ -57,7 +57,8 @@ sys.path.insert(0, os.path.abspath('../../..'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
-              'sphinx.ext.mathjax', 'sphinx.ext.viewcode']
+              'sphinx.ext.mathjax', 'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon',]
 mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 
 # Add any paths that contain templates here, relative to this directory.
