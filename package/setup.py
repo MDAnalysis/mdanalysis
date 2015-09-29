a811877259644898d184cd5151723232aad83fbe
Oliver Beckstein <orbeckst@gmail.com>
2015-09-28 18:44:23 -0700

    added semantic versioning to CHANGELOG rules
    Also bumped release number to 0.12.0-dev (instead of 0.11.1-dev)

--- package/setup.py
+++ package/setup.py
@@ -172,7 +172,7 @@ def detect_openmp():
 
 
 if __name__ == '__main__':
-    RELEASE = "0.11.1-dev"  # NOTE: keep in sync with MDAnalysis.__version__ in version.py
+    RELEASE = "0.12.0-dev"  # NOTE: keep in sync with MDAnalysis.__version__ in version.py
     with open("SUMMARY.txt") as summary:
         LONG_DESCRIPTION = summary.read()
     CLASSIFIERS = [
