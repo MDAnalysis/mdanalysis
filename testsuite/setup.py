e596de2b5b06e1fb4ae0e21b1793dc0d87753719
Max Linke <max_linke@gmx.de>
2015-10-04 13:33:42 +0200

    add tempdir install to travis and setup.py

--- testsuite/setup.py
+++ testsuite/setup.py
@@ -129,6 +129,7 @@ For details see the report for `Issue 87`_.
               'MDAnalysis==%s' % RELEASE,  # same as this release!
               'numpy>=1.5',
               'nose>=1.3.7',
+              'tempdir',
           ],
           zip_safe=False,  # had 'KeyError' as zipped egg (2MB savings are not worth the trouble)
           )
