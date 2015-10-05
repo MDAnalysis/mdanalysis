0256699f041291646d7c033f1d5630b36c2022a7
Richard Gowers <richardjgowers@gmail.com>
2015-10-05 14:37:13 -0700

    Bumped version to 0.12.1-dev

--- package/setup.py
+++ package/setup.py
@@ -284,7 +284,7 @@ def extensions(config):
 
 if __name__ == '__main__':
     # NOTE: keep in sync with MDAnalysis.__version__ in version.py
-    RELEASE = "0.12.0"
+    RELEASE = "0.12.1-dev"
     with open("SUMMARY.txt") as summary:
         LONG_DESCRIPTION = summary.read()
     CLASSIFIERS = [
