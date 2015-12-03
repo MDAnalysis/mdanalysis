5ac4dd5eec47b2a5a7d1f9b245f5758df2fd1ad0
Hai Nguyen <hainm.comp@gmail.com>
2015-12-02 19:00:16 -0500

    dirty fix for issue 556

--- testsuite/MDAnalysisTests/analysis/test_helanal.py
+++ testsuite/MDAnalysisTests/analysis/test_helanal.py
@@ -30,6 +30,7 @@ from MDAnalysisTests.datafiles import GRO, XTC
 
 class Test_Helanal(TestCase):
     def setUp(self):
+        self.cwd = os.getcwd()
         self.universe = MDAnalysis.Universe(GRO, XTC)
         self.selection = 'name CA'
         self.tempdir = tempfile.mkdtemp()
@@ -38,6 +39,7 @@ class Test_Helanal(TestCase):
     def tearDown(self):
         del self.universe
         shutil.rmtree(self.tempdir, ignore_errors=True)
+        os.chdir(self.cwd)
 
     def test_xtc_striding(self):
         """MDAnalysis.analysis.helanal: Check for resolution of Issue 188."""
