6b00adf558d068a92be6f8a886f7c7bc1a49a05f
David Dotson <dotsdl@gmail.com>
2015-12-02 22:01:16 -0700

    Topology object mappings set up.
    The Topology object keeps arrays giving the mappings of atom membership
    in residues, residue membership in segments, and the necessary sparse
    matrices to efficiently go the other direction. We now have a set of
    accessors for converting between one set of indices to another, which
    will often be needed by TopologyAttr methods.

--- package/MDAnalysis/core/topology.py
+++ package/MDAnalysis/core/topology.py
@@ -20,7 +20,7 @@ class Topology(object):
         Returns
         -------
         rix : array
-            residue indices
+            residue index for each atom 
 
         """
         pass
@@ -36,29 +36,126 @@ class Topology(object):
         Returns
         -------
         rix : array
-            residue indices
+            segment index for each atom
 
         """
         pass
 
     def r2a(self, rix):
+        """Get atom indices collectively represented by given residue indices.
+
+        Parameters
+        ----------
+        rix : array
+            residue indices
+
+        Returns
+        -------
+        aix : array
+            sorted indices of atoms present in residues, collectively
+
+        """
         pass
 
     def r2ra(self, rix):
+        """Get atom indices represented by each residue index.
+
+        Parameters
+        ----------
+        rix : array
+            residue indices
+
+        Returns
+        -------
+        raix : sparse matrix 
+            each row corresponds to a residue index, in order given in `rix`,
+            each column corresponds to an atom, with either a 1 or 0 as the
+            value indicating membership or not, respectively, in that residue
+
+        """
         pass
 
     def r2s(self, rix):
+        """Get segment indices for each residue.
+
+        Parameters
+        ----------
+        rix : array
+            residue indices 
+
+        Returns
+        -------
+        six : array
+            segment index for each residue
+
+        """
         pass
 
     def s2r(self, six):
+        """Get residue indices collectively represented by given segment indices.
+
+        Parameters
+        ----------
+        six : array
+            segment indices
+
+        Returns
+        -------
+        rix : array
+            sorted indices of residues present in segments, collectively
+
+        """
         pass
 
     def s2sr(self, six):
+        """Get residue indices represented by each segment index.
+
+        Parameters
+        ----------
+        six : array
+            residue indices
+
+        Returns
+        -------
+        srix : sparse matrix 
+            each row corresponds to a segment index, in order given in `six`,
+            each column corresponds to a residue, with either a 1 or 0 as the
+            value indicating membership or not, respectively, in that segment
+
+        """
         pass
 
     def s2a(self, six):
+        """Get atom indices collectively represented by given segment indices.
+
+        Parameters
+        ----------
+        six : array
+            segment indices
+
+        Returns
+        -------
+        aix : array
+            sorted indices of atoms present in segments, collectively
+
+        """
         pass
 
     def s2sa(self, six):
+        """Get atom indices represented by each segment index.
+
+        Parameters
+        ----------
+        six : array
+            residue indices
+
+        Returns
+        -------
+        saix : sparse matrix 
+            each row corresponds to a segment index, in order given in `six`,
+            each column corresponds to a atom, with either a 1 or 0 as the
+            value indicating membership or not, respectively, in that segment
+
+        """
         pass
 
