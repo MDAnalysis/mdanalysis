9cbd2733c11dd35b8dd52ab15ac5beb97207d13f
David Dotson <dotsdl@gmail.com>
2015-12-04 15:09:11 -0700

    Moved some tests around for new topology objects.

---
+++ testsuite/MDAnalysisTests/test_topologyattrs.py
@@ -0,0 +1,13 @@
+"""Tests for MDAnalysis.core.topologyattrs objects.
+
+"""
+
+from numpy.testing import (
+    assert_,
+    assert_array_equal,
+)
+import numpy as np
+
+import MDAnalysis.core.topologyattrs as tpattrs
+
+
