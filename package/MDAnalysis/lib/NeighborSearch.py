94d940e304fd29a83281b7d3021a9f81adae5b88
Jonathan Barnoud <jonathan@barnoud.net>
2015-12-09 12:05:32 +0100

    Use new-style Python classes
    Fix #564

--- package/MDAnalysis/lib/NeighborSearch.py
+++ package/MDAnalysis/lib/NeighborSearch.py
@@ -28,7 +28,7 @@ from Bio.KDTree import KDTree
 from MDAnalysis.core.AtomGroup import AtomGroup
 
 
-class AtomNeighborSearch():
+class AtomNeighborSearch(object):
     """This class can be used to find all atoms/residues/segements within the
     radius of a given query position.
 
