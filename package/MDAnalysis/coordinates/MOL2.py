5b5278390f72e5953eab6e67929881214081299a
Richard Gowers <richardjgowers@gmail.com>
2016-01-12 12:29:15 +0000

    Refactored MOL2 encoding slightly
    Uses TopologyGroups better

--- package/MDAnalysis/coordinates/MOL2.py
+++ package/MDAnalysis/coordinates/MOL2.py
@@ -247,33 +247,39 @@ class MOL2Writer(base.Writer):
     def encode_block(self, obj):
         traj = obj.universe.trajectory
 
-        # Make sure Universe has loaded bonds
-        obj.universe.bonds
+        # Need to remap atom indices to 1 based in this selection
+        mapping = {a: i for i, a in enumerate(obj.atoms, start=1)}
 
-        bonds = set()
-        for a in obj.atoms:
-            bonds.update(a.bonds)
-        bonds = sorted([(bond[0].id, bond[1].id, bond.order) for bond in bonds])
-        mapping = {a.id: i for i, a in enumerate(obj.atoms)}
-
-        atom_lines = ["{0:>4} {1:>4} {2:>13.4f} {3:>9.4f} {4:>9.4f} {5:>4} {6} {7} "
-                      "{8:>7.4f}".format(mapping[a.id] + 1, a.name,
-                                        float(a.pos[0]),
-                                        float(a.pos[1]),
-                                        float(a.pos[2]), a.type,
-                                        a.resid, a.resname,
-                                        a.charge) for a in obj.atoms]
-        atom_lines = ["@<TRIPOS>ATOM"] + atom_lines + ["\n"]
-        atom_lines = "\n".join(atom_lines)
-
-        bonds = [(atom1, atom2, order) for atom1, atom2, order in bonds if atom1 in mapping and atom2 in mapping]
-        bond_lines = ["{0:>5} {1:>5} {2:>5} {3:>2}"
-                      "".format(bid + 1,
-                                mapping[atom1] + 1,
-                                mapping[atom2] + 1,
-                                order) for bid, (atom1, atom2, order) in enumerate(bonds)]
-        bond_lines = ["@<TRIPOS>BOND"] + bond_lines + ["\n"]
+        # Grab only bonds between atoms in the obj
+        # ie none that extend out of it
+        bondgroup = obj.bonds.atomgroup_intersection(obj, strict=True)
+        bonds = sorted((b[0], b[1], b.order) for b in bondgroup)
+        bond_lines = ["@<TRIPOS>BOND"]
+        bond_lines.extend("{0:>5} {1:>5} {2:>5} {3:>2}"
+                          "".format(bid,
+                                    mapping[atom1],
+                                    mapping[atom2],
+                                    order)
+                          for bid, (atom1, atom2, order)in enumerate(
+                                  bonds, start=1))
+        bond_lines.append("\n")
         bond_lines = "\n".join(bond_lines)
+
+        atom_lines = ["@<TRIPOS>ATOM"]
+        atom_lines.extend("{0:>4} {1:>4} {2:>13.4f} {3:>9.4f} {4:>9.4f}"
+                          " {5:>4} {6} {7} {8:>7.4f}"
+                          "".format(mapping[a],
+                                    a.name,
+                                    a.position[0],
+                                    a.position[1],
+                                    a.position[2],
+                                    a.type,
+                                    a.resid,
+                                    a.resname,
+                                    a.charge)
+                          for a in obj.atoms)
+        atom_lines.append("\n")
+        atom_lines = "\n".join(atom_lines)
 
         try:
             substructure = traj.substructure[traj.frame]
@@ -284,7 +290,7 @@ class MOL2Writer(base.Writer):
 
         molecule = traj.molecule[traj.frame]
         check_sums = molecule[1].split()
-        check_sums[0], check_sums[1] = str(len(obj.atoms)), str(len(bonds))
+        check_sums[0], check_sums[1] = str(len(obj.atoms)), str(len(bondgroup))
         molecule[1] = "{0}\n".format(" ".join(check_sums))
         molecule = ["@<TRIPOS>MOLECULE\n"] + molecule
 
