c909cfe2ef21b91144cca7a66b5079da313a3820
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-01-30 19:46:14 +0000

    Unpacked the map iterator in CRD.py for Python 3 compatibility.

--- package/MDAnalysis/coordinates/CRD.py
+++ package/MDAnalysis/coordinates/CRD.py
@@ -64,9 +64,9 @@ class CRDReader(base.SingleFrameReader):
                 # process coordinates
                 try:
                     if extended:
-                        coords_list.append(np.array(map(float, line[45:100].split()[0:3])))
+                        coords_list.append(np.array(list(map(float, line[45:100].split()[0:3]))))
                     else:
-                        coords_list.append(np.array(map(float, line[20:50].split()[0:3])))
+                        coords_list.append(np.array(list(map(float, line[20:50].split()[0:3]))))
                 except:
                     raise ValueError("Check CRD format at line {0}: {1}"
                                      "".format(linenum, line.rstrip()))
