62fdf9927f3a9187df5670703b5a027f9c726338
Richard Gowers <richardjgowers@gmail.com>
2016-01-31 21:07:46 +0000

    Using memory views instead

--- package/MDAnalysis/lib/trans.pyx
+++ package/MDAnalysis/lib/trans.pyx
@@ -6,16 +6,15 @@ cimport numpy as np
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def residue_positions(
-        np.ndarray[np.float32_t, ndim=2] coords,
-        np.ndarray[np.int64_t, ndim=1] indices):
+        float[:, :] coords not None,
+        long[:] indices not None):
     cdef Py_ssize_t i, j, k
 
     cdef int n_items = indices.shape[0]
     cdef int n_res = np.unique(indices).shape[0]
 
-    cdef np.ndarray[np.int64_t] counter = np.zeros(n_res, dtype=np.int64)
-    cdef np.ndarray[np.float32_t, ndim=2] output = np.zeros((n_res, 3),
-                                                            dtype=np.float32)
+    cdef long[:] counter = np.zeros(n_res, dtype=np.int64)
+    cdef float[:, :] output = np.zeros((n_res, 3), dtype=np.float32)
 
     for i in range(n_items):
         j = indices[i]
@@ -27,4 +26,4 @@ def residue_positions(
         for k in range(3):
             output[i, k] /= counter[i]
 
-    return output
+    return np.asarray(output)
