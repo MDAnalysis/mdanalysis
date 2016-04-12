36eebf940a5f09b8a3c5e793497f326edf94ebeb
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-04-12 16:08:48 +0100

    Updated function names and docs in ten2sixteen.py.

--- package/MDAnalysis/migration/ten2sixteen.py
+++ package/MDAnalysis/migration/ten2sixteen.py
@@ -4,10 +4,10 @@ from lib2to3.main import main
 if __name__ == '__main__':
     sys.exit(main("fixes"))
 
-def ten2eleven(*files):
-    """Convert MDAnalysis 0.10.0 scripts to 0.11.0
+def ten2sixteen(*files):
+    """Convert MDAnalysis 0.10.0 scripts to 0.16.0
 
     Usage:
-       ten2eleven('myscript.py')
+       ten2sixteen('myscript.py')
     """
     main('MDAnalysis.migration.fixes', ['-w'] + list(files))
