e41711c3eebd205066aeaa0f186e5d0227bbf37e
John Detlefs <jdetle@gmail.com>
2016-05-29 17:15:11 -0700

    added tests, fixed logger issue
    style fixes, protection fixes, added docs
    
    fix of some broken areas
    
    fixed selections and updated tests accordingly
    
    switched order of imports [skip ci]

--- testsuite/MDAnalysisTests/analysis/test_diffusionmap.py
+++ testsuite/MDAnalysisTests/analysis/test_diffusionmap.py
@@ -14,9 +14,10 @@
 # J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
 #
 from __future__ import print_function
+import numpy as np
 import MDAnalysis
 import MDAnalysis.analysis.diffusionmap as diffusionmap
-from numpy.testing import (assert_almost_equal, assert_equal)
+from numpy.testing import (assert_almost_equal, assert_equal, raises)
 
 
 from MDAnalysisTests.datafiles import PDB, XTC
@@ -24,30 +25,47 @@ from MDAnalysisTests.datafiles import PDB, XTC
 
 class TestDiffusionmap(object):
     def __init__(self):
-        #slow 6s test
-        #u = MDAnalysis.Universe(PSF,DCD)
-        #eg,ev=diffusionmap.diffusionmap(u)
-        #assert_equal(self.eg.shape, (98,))
-        #assert_almost_equal(self.eg[0], 1.0)
-        #assert_almost_equal(self.eg[-1],0.03174182)
-        #assert_equal(self.ev.shape, (98,98))
-        #assert_almost_equal(self.ev[0,0], .095836037343022831)
-        #faster
-        u = MDAnalysis.Universe(PDB, XTC)
-        eg, ev = diffusionmap.diffusionmap(u, select = 'backbone', k = 5)
-        self.eg = eg
-        self.ev = ev
-       
+        # slow 6s test
+        # u = MDAnalysis.Universe(PSF,DCD)
+        # eg,ev=diffusionmap.diffusionmap(u)
+        # assert_equal(self.eg.shape, (98,))
+        # assert_almost_equal(self.eg[0], 1.0)
+        # assert_almost_equal(self.eg[-1],0.03174182)
+        # assert_equal(self.ev.shape, (98,98))
+        # assert_almost_equal(self.ev[0,0], .095836037343022831)
+        # faster
+        self.u = MDAnalysis.Universe(PDB, XTC)
+        self.dmap = diffusionmap.DiffusionMap(self.u, select='backbone', k=5)
+        self.dmap.run()
+        self.eigvals = self.dmap.eigenvalues
+        self.eigvects = self.dmap.eigenvectors
+        self.weights = np.ones((self.dmap.nframes, ))
 
     def test_eg(self):
-        assert_equal(self.eg.shape, (10, ))
-        assert_almost_equal(self.eg[0], 1.0, decimal=5)
-        assert_almost_equal(self.eg[-1], 0.0142, decimal = 3)
-
+        # number of frames is trajectory is now 10 vs. 98
+        assert_equal(self.eigvals.shape, (self.dmap.nframes, ))
+        assert_almost_equal(self.eigvals[0], 1.0, decimal=5)
+        assert_almost_equal(self.eigvals[-1], 0.006621, decimal=3)
 
     def test_ev(self):
-        assert_equal(self.ev.shape, (10, 10))
-        assert_almost_equal(self.ev[0, 0], -0.3019, decimal = 2)
+        assert_equal(self.eigvects.shape, (self.dmap.nframes, self.dmap.nframes))
+        assert_almost_equal(self.eigvects[0, 0], 0.3172, decimal=2)
 
+    def test_weights(self):
+        dmap2 = diffusionmap.DiffusionMap(self.u, select='backbone',
+                                          weights=self.weights, k=5)
+        dmap2.run()
+        assert_almost_equal(self.eigvals, dmap2.eigenvalues, decimal=5)
+        assert_almost_equal(self.eigvects, dmap2.eigenvectors, decimal=6)
 
+    @raises(ValueError)
+    def test_wrong_weights(self):
+        dmap2 = diffusionmap.DiffusionMap(self.u, select='backbone',
+                                          weights=np.ones((2,)),
+                                          k=5)
 
+    def test_constant_epsilon(self):
+        dmap3 = diffusionmap.DiffusionMap(self.u, select='backbone', k=5,
+                                          epsilon=16.12272861)
+        dmap3.run()
+        assert_almost_equal(self.eigvals, dmap3.eigenvalues, decimal=5)
