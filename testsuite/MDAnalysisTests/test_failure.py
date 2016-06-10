aff3e9af32f7751407e50ce778242356e86be07e
Jonathan Barnoud <jonathan@barnoud.net>
2016-06-10 15:10:49 +0200

    Plugin to display the open files upon test failure
    The plugin is enabled by default. It displays the path to the files that
    have a open file handler. To disable the plugin, run mda_nosetests with
    the --no-open-files option or with the NOSE_NO_MDA_FILES environment
    variable set.
    
    The commit also introduces a test that fails if the MDA_FAILURE_TEST
    environment variable is set. This test comes handy to test nose behavior
    upon failure.
    
    The open-files plugin requires psutil. That requirement has been added
    to setup.py.

---
+++ testsuite/MDAnalysisTests/test_failure.py
@@ -0,0 +1,26 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# MDAnalysis --- http://www.MDAnalysis.org
+# Copyright (c) 2006-2015 Naveen Michaud-Agrawal, Elizabeth J. Denning,
+# Oliver Beckstein and contributors (see AUTHORS for the full list)
+#
+# Released under the GNU Public Licence, v2 or any higher version
+#
+# Please cite your use of MDAnalysis in published work:
+#
+# N. Michaud-Agrawal, E. J. Denning, T. B. Woolf, and O. Beckstein.
+# MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
+# J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
+#
+
+import os
+
+
+def test_failure():
+    """Fail if the MDA_FAILURE_TEST environment variable is set.
+    """
+    # Have a file open to trigger an output from the open_files plugin.
+    f = open('./failure.txt', 'w')
+    if u'MDA_FAILURE_TEST' in os.environ:
+        assert False
