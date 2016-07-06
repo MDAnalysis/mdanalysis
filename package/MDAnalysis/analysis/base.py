095f9b0ad0fee4d8d61a1f9eaca9b3d389253165
Max Linke <max_linke@gmx.de>
2016-07-06 18:00:55 +0200

    Add progressmeter to AnalysisBase
    This also adds an initialization function to AnalysisBase. This is done
    now because we need to do more then just to setup the iteration.

--- package/MDAnalysis/analysis/base.py
+++ package/MDAnalysis/analysis/base.py
@@ -2,8 +2,8 @@
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 #
 # MDAnalysis --- http://www.MDAnalysis.org
-# Copyright (c) 2006-2015 Naveen Michaud-Agrawal, Elizabeth J. Denning, Oliver Beckstein
-# and contributors (see AUTHORS for the full list)
+# Copyright (c) 2006-2015 Naveen Michaud-Agrawal, Elizabeth J. Denning, Oliver
+# Beckstein and contributors (see AUTHORS for the full list)
 #
 # Released under the GNU Public Licence, v2 or any higher version
 #
@@ -24,7 +24,6 @@ classes.
 """
 from six.moves import range
 
-import numpy as np
 import logging
 from MDAnalysis.lib.log import ProgressMeter
 
@@ -32,39 +31,90 @@ logger = logging.getLogger(__name__)
 
 
 class AnalysisBase(object):
-    """Base class for defining multi frame analysis
+    """Base class for defining multi frame analysis, it is designed as a
+    template for creating multiframe analysis. This class will automatically
+    take care of setting up the trajectory reader for iterating and offers to
+    show a progress meter.
 
-    The analysis base class is designed as a template for creating
-    multiframe analysis.
+    To define a new Analysis, `AnalysisBase` needs to be subclassed
+    `_single_frame` must be defined. It is also possible to define
+    `_prepare` and `_conclude` for pre and post processing. See the example
+    below.
 
-    The class implements the following methods:
+    .. code-block:: python
+       class NewAnalysis(AnalysisBase):
+           def __init__(self, atomgroup, parameter, **kwargs):
+               super(NewAnalysis, self).__init__(atomgroup.universe.trajectory,
+                                                 **kwargs)
+               self._parameter = parameter
+               self._ag = atomgroup
 
-    _setup_frames(trajectory, start=None, stop=None, step=None)
-      Pass a Reader object and define the desired iteration pattern
-      through the trajectory
+           def _prepare(self):
+               # OPTIONAL
+               # Called before iteration on the trajectory has begun.
+               # Data structures can be set up at this time
+               self.result = []
 
-    run
-      The user facing run method.  Calls the analysis methods
-      defined below
+           def _single_frame(self):
+               # REQUIRED
+               # Called after the trajectory is moved onto each new frame.
+               # store result of `some_function` for a single frame
+               self.result.append(some_function(self._ag, self._parameter))
 
-    Your analysis can implement the following methods, which are
-    called from run:
+           def _conclude(self):
+               # OPTIONAL
+               # Called once iteration on the trajectory is finished.
+               # Apply normalisation and averaging to results here.
+               self.result = np.asarray(self.result) / np.sum(self.result)
 
-    _prepare
-      Called before iteration on the trajectory has begun.
-      Data structures can be set up at this time, however most
-      error checking should be done in the __init__
+    Afterwards the new analysis can be run like this.
 
-    _single_frame
-      Called after the trajectory is moved onto each new frame.
-
-    _conclude
-      Called once iteration on the trajectory is finished.
-      Apply normalisation and averaging to results here.
+    .. code-block:: python
+       na = NewAnalysis(u.select_atoms('name CA'), 35).run()
+       print(na.result)
 
     """
+    def __init__(self, trajectory, start=None,
+                 stop=None, step=None, quiet=True):
+        """
+        Parameters
+        ----------
+        trajectory : mda.Reader
+            A trajectory Reader
+        start : int, optional
+            start frame of analysis
+        stop : int, optional
+            stop frame of analysis
+        step : int, optional
+            number of frames to skip between each analysed frame
+        quiet : bool, optional
+            Turn off verbosity
+        """
+        self._setup_frames(trajectory, start, stop, step)
+        interval = int(self.n_frames // 100)
+        if interval == 0:
+            interval = 1
+        self._pm = ProgressMeter(self.nframes if self.nframes else 1,
+                                 interval=interval, quiet=quiet)
+        self._quiet = quiet
+
     def _setup_frames(self, trajectory, start=None,
                       stop=None, step=None):
+        """
+        Pass a Reader object and define the desired iteration pattern
+        through the trajectory
+
+        Parameters
+        ----------
+        trajectory : mda.Reader
+            A trajectory Reader
+        start : int, optional
+            start frame of analysis
+        stop : int, optional
+            stop frame of analysis
+        step : int, optional
+            number of frames to skip between each analysed frame
+        """
         self._trajectory = trajectory
         start, stop, step = trajectory.check_slice_indices(
             start, stop, step)
@@ -78,7 +128,7 @@ class AnalysisBase(object):
 
         Don't worry about normalising, just deal with a single frame.
         """
-        pass
+        raise NotImplementedError("Only implemented in child classes")
 
     def _prepare(self):
         """Set things up before the analysis loop begins"""
@@ -91,7 +141,7 @@ class AnalysisBase(object):
         """
         pass
 
-    def run(self, **kwargs):
+    def run(self):
         """Perform the calculation"""
         logger.info("Starting preparation")
         self._prepare()
@@ -101,5 +151,7 @@ class AnalysisBase(object):
             self._ts = ts
             # logger.info("--> Doing frame {} of {}".format(i+1, self.n_frames))
             self._single_frame()
+            self._pm.echo(self._frame_index)
         logger.info("Finishing up")
         self._conclude()
+        return self
