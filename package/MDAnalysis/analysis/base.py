054a9de26d29e1df9d424bf9d8f3a7d156d47465
Max Linke <kain88-de@users.noreply.github.com>
2016-07-14 11:05:02 +0200

    Analysis progmeter (#892)
    * PEP8 Changes
    
    * Add progressmeter to AnalysisBase
    
    This also adds an initialization function to AnalysisBase. This is done
    now because we need to do more then just to setup the iteration.
    
    * Adopt Contacts
    
    * Follow best practices for matplotlib
    
    Returning an axis object is considered to be better since the user is
    able to adjust the plot by himself if he wishes to do so.
    
    * Addopt Persistencelength
    
    * Adopt InterRDF
    
    * Adopt LinearDensity
    
    * Adopt AlignTrj
    
    * update changelog
    
    * Ensure old API still works
    
    Since user code might already exist we keep make sure the old API still
    works as well.

--- package/MDAnalysis/analysis/base.py
+++ package/MDAnalysis/analysis/base.py
@@ -2,8 +2,8 @@
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 #
 # MDAnalysis --- http://www.MDAnalysis.org
-# Copyright (c) 2006-2015 Naveen Michaud-Agrawal, Elizabeth J. Denning, Oliver Beckstein
-# and contributors (see AUTHORS for the full list)
+# Copyright (c) 2006-2015 Naveen Michaud-Agrawal, Elizabeth J. Denning, Oliver
+# Beckstein and contributors (see AUTHORS for the full list)
 #
 # Released under the GNU Public Licence, v2 or any higher version
 #
@@ -24,7 +24,6 @@ classes.
 """
 from six.moves import range
 
-import numpy as np
 import logging
 from MDAnalysis.lib.log import ProgressMeter
 
@@ -32,39 +31,85 @@ logger = logging.getLogger(__name__)
 
 
 class AnalysisBase(object):
-    """Base class for defining multi frame analysis
+    """Base class for defining multi frame analysis, it is designed as a
+    template for creating multiframe analysis. This class will automatically
+    take care of setting up the trajectory reader for iterating and offers to
+    show a progress meter.
 
-    The analysis base class is designed as a template for creating
-    multiframe analysis.
+    To define a new Analysis, `AnalysisBase` needs to be subclassed
+    `_single_frame` must be defined. It is also possible to define
+    `_prepare` and `_conclude` for pre and post processing. See the example
+    below.
 
-    The class implements the following methods:
+    .. code-block:: python
+       class NewAnalysis(AnalysisBase):
+           def __init__(self, atomgroup, parameter, **kwargs):
+               super(NewAnalysis, self).__init__(atomgroup.universe.trajectory,
+                                                 **kwargs)
+               self._parameter = parameter
+               self._ag = atomgroup
 
-    _setup_frames(trajectory, start=None, stop=None, step=None)
-      Pass a Reader object and define the desired iteration pattern
-      through the trajectory
+           def _prepare(self):
+               # OPTIONAL
+               # Called before iteration on the trajectory has begun.
+               # Data structures can be set up at this time
+               self.result = []
 
-    run
-      The user facing run method.  Calls the analysis methods
-      defined below
+           def _single_frame(self):
+               # REQUIRED
+               # Called after the trajectory is moved onto each new frame.
+               # store result of `some_function` for a single frame
+               self.result.append(some_function(self._ag, self._parameter))
 
-    Your analysis can implement the following methods, which are
-    called from run:
+           def _conclude(self):
+               # OPTIONAL
+               # Called once iteration on the trajectory is finished.
+               # Apply normalisation and averaging to results here.
+               self.result = np.asarray(self.result) / np.sum(self.result)
 
-    _prepare
-      Called before iteration on the trajectory has begun.
-      Data structures can be set up at this time, however most
-      error checking should be done in the __init__
+    Afterwards the new analysis can be run like this.
 
-    _single_frame
-      Called after the trajectory is moved onto each new frame.
-
-    _conclude
-      Called once iteration on the trajectory is finished.
-      Apply normalisation and averaging to results here.
+    .. code-block:: python
+       na = NewAnalysis(u.select_atoms('name CA'), 35).run()
+       print(na.result)
 
     """
+    def __init__(self, trajectory, start=None,
+                 stop=None, step=None, quiet=True):
+        """
+        Parameters
+        ----------
+        trajectory : mda.Reader
+            A trajectory Reader
+        start : int, optional
+            start frame of analysis
+        stop : int, optional
+            stop frame of analysis
+        step : int, optional
+            number of frames to skip between each analysed frame
+        quiet : bool, optional
+            Turn off verbosity
+        """
+        self._quiet = quiet
+        self._setup_frames(trajectory, start, stop, step)
+
     def _setup_frames(self, trajectory, start=None,
                       stop=None, step=None):
+        """
+        Pass a Reader object and define the desired iteration pattern
+        through the trajectory
+
+        Parameters
+        ----------
+        trajectory : mda.Reader
+            A trajectory Reader
+        start : int, optional
+            start frame of analysis
+        stop : int, optional
+            stop frame of analysis
+        step : int, optional
+            number of frames to skip between each analysed frame
+        """
         self._trajectory = trajectory
         start, stop, step = trajectory.check_slice_indices(
             start, stop, step)
@@ -72,13 +117,23 @@ class AnalysisBase(object):
         self.stop = stop
         self.step = step
         self.n_frames = len(range(start, stop, step))
+        interval = int(self.n_frames // 100)
+        if interval == 0:
+            interval = 1
+
+        # ensure _quiet is set when __init__ wasn't called, this is to not
+        # break pre 0.16.0 API usage of AnalysisBase
+        if not hasattr(self, '_quiet'):
+            self._quiet = True
+        self._pm = ProgressMeter(self.n_frames if self.n_frames else 1,
+                                 interval=interval, quiet=self._quiet)
 
     def _single_frame(self):
         """Calculate data from a single frame of trajectory
 
         Don't worry about normalising, just deal with a single frame.
         """
-        pass
+        raise NotImplementedError("Only implemented in child classes")
 
     def _prepare(self):
         """Set things up before the analysis loop begins"""
@@ -91,7 +146,7 @@ class AnalysisBase(object):
         """
         pass
 
-    def run(self, **kwargs):
+    def run(self):
         """Perform the calculation"""
         logger.info("Starting preparation")
         self._prepare()
@@ -101,5 +156,7 @@ class AnalysisBase(object):
             self._ts = ts
             # logger.info("--> Doing frame {} of {}".format(i+1, self.n_frames))
             self._single_frame()
+            self._pm.echo(self._frame_index)
         logger.info("Finishing up")
         self._conclude()
+        return self
--- testsuite/MDAnalysisTests/analysis/test_base.py
+++ testsuite/MDAnalysisTests/analysis/test_base.py
@@ -2,8 +2,8 @@
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
 #
 # MDAnalysis --- http://www.MDAnalysis.org
-# Copyright (c) 2006-2015 Naveen Michaud-Agrawal, Elizabeth J. Denning, Oliver Beckstein
-# and contributors (see AUTHORS for the full list)
+# Copyright (c) 2006-2015 Naveen Michaud-Agrawal, Elizabeth J. Denning, Oliver
+# Beckstein and contributors (see AUTHORS for the full list)
 #
 # Released under the GNU Public Licence, v2 or any higher version
 #
@@ -18,7 +18,7 @@ from __future__ import division
 from six.moves import range
 
 from numpy.testing import (
-    assert_, dec
+    assert_, dec, raises
 )
 
 import MDAnalysis as mda
@@ -30,17 +30,27 @@ from MDAnalysisTests import parser_not_found
 
 class FrameAnalysis(AnalysisBase):
     """Just grabs frame numbers of frames it goes over"""
-    def __init__(self, reader, start=None, stop=None, step=None):
+    def __init__(self, reader, **kwargs):
+        super(FrameAnalysis, self).__init__(reader, **kwargs)
         self.traj = reader
-        self._setup_frames(reader,
-                           start=start,
-                           stop=stop,
-                           step=step)
-
         self.frames = []
 
     def _single_frame(self):
         self.frames.append(self._ts.frame)
+
+
+class IncompleteAnalysis(AnalysisBase):
+    def __init__(self, reader, **kwargs):
+        super(IncompleteAnalysis, self).__init__(reader, **kwargs)
+
+
+class OldAPIAnalysis(AnalysisBase):
+    """for version 0.15.0"""
+    def __init__(self, reader, **kwargs):
+        self._setup_frames(reader, **kwargs)
+
+    def _single_frame(self):
+        pass
 
 
 class TestAnalysisBase(object):
@@ -54,29 +64,32 @@ class TestAnalysisBase(object):
         del self.u
 
     def test_default(self):
-        an = FrameAnalysis(self.u.trajectory)
+        an = FrameAnalysis(self.u.trajectory).run()
         assert_(an.n_frames == len(self.u.trajectory))
-
-        an.run()
         assert_(an.frames == list(range(len(self.u.trajectory))))
 
     def test_start(self):
-        an = FrameAnalysis(self.u.trajectory, start=20)
+        an = FrameAnalysis(self.u.trajectory, start=20).run()
         assert_(an.n_frames == len(self.u.trajectory) - 20)
-
-        an.run()
         assert_(an.frames == list(range(20, len(self.u.trajectory))))
 
     def test_stop(self):
-        an = FrameAnalysis(self.u.trajectory, stop=20)
+        an = FrameAnalysis(self.u.trajectory, stop=20).run()
         assert_(an.n_frames == 20)
-
-        an.run()
         assert_(an.frames == list(range(20)))
 
     def test_step(self):
-        an = FrameAnalysis(self.u.trajectory, step=20)
+        an = FrameAnalysis(self.u.trajectory, step=20).run()
         assert_(an.n_frames == 5)
-
-        an.run()
         assert_(an.frames == list(range(98))[::20])
+
+    def test_quiet(self):
+        a = FrameAnalysis(self.u.trajectory, quiet=False)
+        assert_(not a._quiet)
+
+    @raises(NotImplementedError)
+    def test_incomplete_defined_analysis(self):
+        IncompleteAnalysis(self.u.trajectory).run()
+
+    def test_old_api(self):
+        OldAPIAnalysis(self.u.trajectory).run()
