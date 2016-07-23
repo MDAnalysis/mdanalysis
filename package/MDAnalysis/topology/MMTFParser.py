337778c4f22ede041b256e7b67f308fa6c8d89d3
Richard Gowers <richardjgowers@gmail.com>
2016-07-23 11:12:12 +0100

    MMTF: Added reading segids (chains)

--- package/MDAnalysis/topology/MMTFParser.py
+++ package/MDAnalysis/topology/MMTFParser.py
@@ -3,21 +3,44 @@
 More docs here
 """
 import mmtf
-from six.moves import zip
+from six.moves import zip, range
 
 from . import base
 from ..core.AtomGroup import Atom
 
 class MMTFParser(base.TopologyReader):
+    """
+    Mapping of MMTF terms to MDAnalysis terms:
+      models -> frames
+      chains -> segments
+      groups -> residues
+      atoms  -> atoms
+    """
     format = 'MMTF'
 
     def parse(self):
         top = mmtf.parse(self.filename)
 
+        # TODO: What is the difference between these two?
+        #       Which is closest to segid?
+        #       Gets resolved in 363 anyway as we can use both fields
+        chain_id = 0
+        segid = top.chain_name_list[chain_id]
+        #chain_id = top.chain_id_list[chain_id]
+        chain_cum_sum = [sum(top.groups_per_chain[:i+1])
+                         for i in range(top.num_chains)]
+
         atoms = []
         atom_id = 0
         # Loop over groups in mmtf top object
         for i, group_id in enumerate(top.group_type_list):
+            # Check if we've jumped into the next chain
+            if i > chain_cum_sum[0]:
+                # Move on to next tally
+                chain_cum_sum.pop(0)
+                chain_id += 1
+                segid = top.chain_name_list[chain_id]
+
             # Select the corresponding group
             group = top.group_list[group_id]
             # Grab per Group data
@@ -29,7 +52,7 @@ class MMTFParser(base.TopologyReader):
             resname = group['groupName']
 
             # Unused group properties
-            sec_struc = top.sec_struc_list[group_id]
+            sec_struc = top.sec_struct_list[group_id]
             sequence = top.sequence_index_list[group_id]
             single_letter_code = group['singleLetterCode']
             chem_comp_type = group['chemCompType']
@@ -48,8 +71,6 @@ class MMTFParser(base.TopologyReader):
 
                 # TODO: Fix mass!
                 mass = 1.0
-                # TODO: Fix segid!
-                segid = 'FIXME'
 
                 atoms.append(Atom(
                     atom_id,
