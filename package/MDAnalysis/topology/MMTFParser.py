5a812d48c6cc159c0256f946bc37c3a54d3e9fcc
Richard Gowers <richardjgowers@gmail.com>
2016-07-23 10:55:50 +0100

    More MMTF work
    Added resnum reading
    
    Added many unused residue and atom proprties

--- package/MDAnalysis/topology/MMTFParser.py
+++ package/MDAnalysis/topology/MMTFParser.py
@@ -18,10 +18,22 @@ class MMTFParser(base.TopologyReader):
         atom_id = 0
         # Loop over groups in mmtf top object
         for i, group_id in enumerate(top.group_type_list):
+            # Select the corresponding group
             group = top.group_list[group_id]
-
+            # Grab per Group data
             resid = i + 1
+            resnum = top.ins_code_list[group_id]
+            # this field seems to be filled with '\x00' ie null
+            if resnum == '\x00':
+                resnum = None
             resname = group['groupName']
+
+            # Unused group properties
+            sec_struc = top.sec_struc_list[group_id]
+            sequence = top.sequence_index_list[group_id]
+            single_letter_code = group['singleLetterCode']
+            chem_comp_type = group['chemCompType']
+            
 
             for name, charge, element in zip(
                     group['atomNameList'],
@@ -30,6 +42,9 @@ class MMTFParser(base.TopologyReader):
                 altLoc = top.alt_loc_list[atom_id]
                 occupancy = top.occupancy_list[atom_id]
                 bfactor = top.b_factor_list[atom_id]
+
+                # atom id as defined in mmtf
+                mmtf_atom_id = top.atom_id_list[atom_id]
 
                 # TODO: Fix mass!
                 mass = 1.0
@@ -47,7 +62,7 @@ class MMTFParser(base.TopologyReader):
                     charge,
                     radius=None,
                     bfactor=bfactor,
-                    resnum=None,
+                    resnum=resnum,
                     serial=None,
                     altLoc=altLoc,
                     universe=self._u,
