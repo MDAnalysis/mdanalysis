ce1b3f1a81492a8406ad793910cc882a830a5d19
Sean Seyler <slseyler@asu.edu>
2016-10-07 16:08:37 -0700

    Added decorators to psa tests to skip when matplotlib/scipy not installed

--- testsuite/MDAnalysisTests/analysis/test_psa.py
+++ testsuite/MDAnalysisTests/analysis/test_psa.py
@@ -29,6 +29,10 @@ from MDAnalysisTests import parser_not_found, tempdir
 class TestPSAnalysis(TestCase):
     @dec.skipif(parser_not_found('DCD'),
                 'DCD parser not available. Are you using python 3?')
+    @dec.skipif(module_not_found('matplotlib'),
+                "Test skipped because matplotlib is not available.")
+    @dec.skipif(module_not_found('scipy'),
+                "Test skipped because scipy is not available.")
     def setUp(self):
         self.tmpdir = tempdir.TempDir()
         self.iu1 = np.triu_indices(3, k=1)
