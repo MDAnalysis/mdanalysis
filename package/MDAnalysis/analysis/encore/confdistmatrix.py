de0d0684c8b5e821bc8513aed7c5600c7b6ecb86
Max Linke <max_linke@gmx.de>
2016-12-31 14:45:34 +0100

    switch to joblib parallel processing
    This is included in scikit-learn. It does work stealing job balancing for us
    that helps to use the full processor power. The absolute biggest advantage of
    this is though that I can now include print/exception errors for debugging in
    the `conf_dist_function`.

--- package/MDAnalysis/analysis/encore/confdistmatrix.py
+++ package/MDAnalysis/analysis/encore/confdistmatrix.py
@@ -44,6 +44,8 @@ from datetime import datetime
 from time import sleep
 import logging
 
+from sklearn.externals.joblib import Parallel, delayed
+
 from ...core.universe import Universe
 
 from ..align import rotation_matrix
@@ -55,9 +57,9 @@ from .utils import TriangularMatrix, trm_indeces, \
 
 
 def conformational_distance_matrix(ensemble,
-    conf_dist_function, selection="",
-    superimposition_selection="", ncores=1, pairwise_align=True,
-    mass_weighted=True, metadata=True, *args, **kwargs):
+                                   conf_dist_function, selection="",
+                                   superimposition_selection="", ncores=1, pairwise_align=True,
+                                   mass_weighted=True, metadata=True, verbose=False):
     """
     Run the conformational distance matrix calculation.
     args and kwargs are passed to conf_dist_function.
@@ -160,73 +162,30 @@ def conformational_distance_matrix(ensemble,
         else:
             subset_masses = None
 
-    # matsize: number of elements of the triangular matrix, diagonal
-    # elements included.
-    matsize = framesn * (framesn + 1) / 2
-
-    # Calculate the number of matrix elements that each core has to
-    # calculate as equally as possible.
-    if ncores > matsize:
-        ncores = matsize
-    runs_per_worker = [matsize / int(ncores) for x in range(ncores)]
-    unfair_work = matsize % ncores
-    for i in range(unfair_work):
-        runs_per_worker[i] += 1
-
-    # Splice the matrix in ncores segments. Calculate the first and the
-    # last (i,j) matrix elements of the slices that will be assigned to
-    # each worker. Each of them will proceed in a column-then-row order
-    # (e.g. 0,0 1,0 1,1 2,0 2,1 2,2 ... )
-    i = 0
-    a = [0, 0]
-    b = [0, 0]
-    tasks_per_worker = []
-    for n,r in enumerate(runs_per_worker):
-        while i * (i - 1) / 2 < np.sum(runs_per_worker[:n + 1]):
-            i += 1
-        b = [i - 2,
-             np.sum(runs_per_worker[0:n + 1]) - (i - 2) * (i - 1) / 2 - 1]
-        tasks_per_worker.append((tuple(a), tuple(b)))
-        if b[0] == b[1]:
-            a[0] = b[0] + 1
-            a[1] = 0
-        else:
-            a[0] = b[0]
-            a[1] = b[1] + 1
-
     # Allocate for output matrix
+    matsize = framesn * (framesn + 1) / 2
     distmat = Array(c_float, matsize)
 
-    # Prepare progress bar stuff and run it
-    pbar = AnimatedProgressBar(end=matsize, width=80)
-    partial_counters = [RawValue('i', 0) for i in range(ncores)]
 
     # Initialize workers. Simple worker doesn't perform fitting,
     # fitter worker does.
-
-    workers = [Process(target=conf_dist_function, args=(
-        tasks_per_worker[i],
+    indices = trm_indeces((0, 0), (framesn - 1, framesn - 1))
+    Parallel(n_jobs=ncores, verbose=verbose)(delayed(conf_dist_function)(
+        element,
         rmsd_coordinates,
         distmat,
         masses,
         fitting_coordinates,
         subset_masses,
-        partial_counters[i],
-        args,
-        kwargs)) for i in range(ncores)]
+        masses) for element in indices)
 
-    # Start & join the workers
-    for w in workers:
-        w.start()
-    for w in workers:
-        w.join()
 
     # When the workers have finished, return a TriangularMatrix object
     return TriangularMatrix(distmat, metadata=metadata)
 
 
 def set_rmsd_matrix_elements(tasks, coords, rmsdmat, masses, fit_coords=None,
-                       fit_masses=None, pbar_counter=None, *args, **kwargs):
+                             fit_masses=None, pbar_counter=None, *args, **kwargs):
 
     '''
     RMSD Matrix calculator
@@ -264,48 +223,38 @@ def set_rmsd_matrix_elements(tasks, coords, rmsdmat, masses, fit_coords=None,
     fit_masses : numpy.array
         Array of atomic masses, having the same order as the
         fit_coords array
-
-    pbar_counter : multiprocessing.RawValue
-        Thread-safe shared value. This counter is updated at
-        every cycle and used to evaluate the progress of
-        each worker in a parallel calculation.
         '''
-
+    i, j = tasks
 
     if fit_coords is None and fit_masses is None:
-        for i, j in trm_indeces(tasks[0], tasks[1]):
-            summasses = np.sum(masses)
-            rmsdmat[(i + 1) * i / 2 + j] = PureRMSD(coords[i].astype(np.float64),
-                                                    coords[j].astype(np.float64),
-                                                    coords[j].shape[0],
-                                                    masses,
-                                                    summasses)
+        summasses = np.sum(masses)
+        rmsdmat[(i + 1) * i / 2 + j] = PureRMSD(coords[i].astype(np.float64),
+                                                coords[j].astype(np.float64),
+                                                coords[j].shape[0],
+                                                masses,
+                                                summasses)
 
     elif fit_coords is not None and fit_coords is not None:
-        for i, j in trm_indeces(tasks[0], tasks[1]):
-            summasses = np.sum(masses)
-            subset_weights = np.asarray(fit_masses) / np.mean(fit_masses)
-            com_i = np.average(fit_coords[i], axis=0,
-                            weights=fit_masses)
-            translated_i = coords[i] - com_i
-            subset1_coords = fit_coords[i] - com_i
-            com_j = np.average(fit_coords[j], axis=0,
-                            weights=fit_masses)
-            translated_j = coords[j] - com_j
-            subset2_coords = fit_coords[j] - com_j
-            rotamat = rotation_matrix(subset1_coords, subset2_coords,
-                                      subset_weights)[0]
-            rotated_i = np.transpose(np.dot(rotamat, np.transpose(translated_i)))
-            rmsdmat[(i + 1) * i / 2 + j] = PureRMSD(
-                rotated_i.astype(np.float64), translated_j.astype(np.float64),
-                coords[j].shape[0], masses, summasses)
-
+        summasses = np.sum(masses)
+        subset_weights = np.asarray(fit_masses) / np.mean(fit_masses)
+        com_i = np.average(fit_coords[i], axis=0,
+                           weights=fit_masses)
+        translated_i = coords[i] - com_i
+        subset1_coords = fit_coords[i] - com_i
+        com_j = np.average(fit_coords[j], axis=0,
+                           weights=fit_masses)
+        translated_j = coords[j] - com_j
+        subset2_coords = fit_coords[j] - com_j
+        rotamat = rotation_matrix(subset1_coords, subset2_coords,
+                                  subset_weights)[0]
+        rotated_i = np.transpose(np.dot(rotamat, np.transpose(translated_i)))
+        rmsdmat[(i + 1) * i / 2 + j] = PureRMSD(
+            rotated_i.astype(np.float64), translated_j.astype(np.float64),
+            coords[j].shape[0], masses, summasses)
     else:
         raise TypeError("Both fit_coords and fit_masses must be specified \
                         if one of them is given")
 
-    if pbar_counter is not None:
-        pbar_counter.value += 1
 
 def pbar_updater(pbar, pbar_counters, max_val, update_interval=0.2):
     '''Method that updates and prints the progress bar, upon polling
