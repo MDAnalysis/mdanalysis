7a768282151da0d7abb27b64d5ef2078b89b0484
Max Linke <max_linke@gmx.de>
2017-04-17 10:27:06 +0200

    update mathjax cdn
    The original cdn will be taken down on 30 April 2017

--- package/doc/sphinx/source/conf.py
+++ package/doc/sphinx/source/conf.py
@@ -60,8 +60,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
               'sphinx.ext.mathjax', 'sphinx.ext.viewcode',
               'sphinx.ext.napoleon', 'sphinx.ext.todo']
 
-
-mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
