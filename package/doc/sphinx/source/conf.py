5f2e0f11c1b2fd16b925c3b9f77baaad320f39f4
Oliver Beckstein <orbeckst@gmail.com>
2017-05-17 23:49:51 -0700

    removed Mock class from sphinx/conf.py
    Not needed.

--- package/doc/sphinx/source/conf.py
+++ package/doc/sphinx/source/conf.py
@@ -18,32 +18,6 @@ import platform
 # http://alabaster.readthedocs.io/en/latest/
 import alabaster
 
-class Mock(object):
-    __all__ = []
-
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(cls, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper():
-            mockType = type(name, (), {})
-            mockType.__module__ = __name__
-            return mockType
-        else:
-            return Mock()
-
-# for some reason, can't get the following to work in
-# my virtualenv
-# MOCK_MODULES = ['mmtf']
-# for mod_name in MOCK_MODULES:
-#     sys.modules[mod_name] = Mock()
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown
