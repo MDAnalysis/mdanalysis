afcd60632105b673532c135470157dd1cb1450a5
Oliver Beckstein <orbeckst@gmail.com>
2017-05-23 22:32:42 -0700

    changed URL for docs to new http://docs.mdanalysis.org
    - closes #1315
    - master docs were moved to their own repo https://github.com/MDAnalysis/docs and are
      served as GitHub pages
    - replaces obsolete https://pythonhosted.org/MDAnalysis
    - updated CHANGELOG (also added notes on #378)

--- package/doc/sphinx/source/conf.py
+++ package/doc/sphinx/source/conf.py
@@ -227,7 +227,7 @@ html_sidebars = {
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-html_use_opensearch = 'http://pythonhosted.org/MDAnalysis'
+html_use_opensearch = 'http://docs.mdanalysis.org'
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
 #html_file_suffix = None
