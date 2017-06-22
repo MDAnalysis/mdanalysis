647698732ee364ae1511ed2b525abcf2c8011371
Richard Gowers <richardjgowers@gmail.com>
2017-06-22 11:06:30 +0100

    Added pytest-raises
    Converted test_units to use parametrize
    
    Some hacking of test_distances to use pytest features
    
    Completed moving test_distances to pytest style
    
    rewrote test_transformations into pytest style

--- testsuite/MDAnalysisTests/utils/test_units.py
+++ testsuite/MDAnalysisTests/utils/test_units.py
@@ -21,15 +21,16 @@
 #
 from __future__ import unicode_literals, absolute_import
 import six
+import pytest
 
 import numpy as np
-from numpy.testing import assert_equal, assert_almost_equal, assert_raises,TestCase
+from numpy.testing import assert_equal, assert_almost_equal, assert_raises
 
 from MDAnalysis import units
 from MDAnalysis.core import flags
 
 
-class TestDefaultUnits(TestCase):
+class TestDefaultUnits(object):
     def test_length(self):
         assert_equal(flags['length_unit'], 'Angstrom',
                      u"The default length unit should be Angstrom (in core.flags)")
@@ -43,7 +44,8 @@ class TestDefaultUnits(TestCase):
                      u"The default behaviour should be to auto-convert Gromacs trajectories")
 
 
-class TestUnitEncoding(TestCase):
+
+class TestUnitEncoding(object):
     def test_unicode(self):
         try:
             assert_equal(units.lengthUnit_factor[u"\u212b"], 1.0)
@@ -68,9 +70,9 @@ class TestConstants(object):
         'calorie': 4.184,                      # J
         }
 
-    def test_constant(self):
-        for name, value in six.iteritems(self.constants_reference):
-            yield self.check_physical_constant, name, value
+    @pytest.mark.parametrize('name, value', constants_reference.items())
+    def test_constant(self, name, value):
+        self.check_physical_constant(name, value)
 
     @staticmethod
     def check_physical_constant(name, reference):
@@ -80,33 +82,49 @@ class TestConstants(object):
 class TestConversion(object):
     @staticmethod
     def _assert_almost_equal_convert(value, u1, u2, ref):
-        assert_almost_equal(units.convert(value, u1, u2), ref,
+        val = units.convert(value, u1, u2)
+        assert_almost_equal(val, ref,
                             err_msg="Conversion {0} --> {1} failed".format(u1, u2))
 
-    # generate individual test cases using nose's test generator mechanism
-    def test_length(self):
-        nm = 12.34567
-        A = nm * 10.
-        yield self._assert_almost_equal_convert, nm, 'nm', 'A', A
-        yield self._assert_almost_equal_convert, A, 'Angstrom', 'nm', nm
+    nm = 12.34567
+    A = nm * 10.
+    @pytest.mark.parametrize('quantity, unit1, unit2, ref', (
+        (nm, 'nm', 'A', A),
+        (A, 'Angstrom', 'nm', nm),
+    ))
+    def test_length(self, quantity, unit1, unit2, ref):
+        self._assert_almost_equal_convert(quantity, unit1, unit2, ref)
 
-    def test_time(self):
-        yield self._assert_almost_equal_convert, 1, 'ps', 'AKMA', 20.45482949774598
-        yield self._assert_almost_equal_convert, 1, 'AKMA', 'ps', 0.04888821
+    @pytest.mark.parametrize('quantity, unit1, unit2, ref', (
+        (1, 'ps', 'AKMA', 20.45482949774598),
+        (1, 'AKMA', 'ps', 0.04888821),
+    ))
+    def test_time(self, quantity, unit1, unit2, ref):
+        self._assert_almost_equal_convert(quantity, unit1, unit2, ref)
 
-    def test_energy(self):
-        yield self._assert_almost_equal_convert, 1, 'kcal/mol', 'kJ/mol', 4.184
-        yield self._assert_almost_equal_convert, 1, 'kcal/mol', 'eV', 0.0433641
+    @pytest.mark.parametrize('quantity, unit1, unit2, ref', (
+        (1, 'kcal/mol', 'kJ/mol', 4.184),
+        (1, 'kcal/mol', 'eV', 0.0433641),
+    ))
+    def test_energy(self, quantity, unit1, unit2, ref):
+        self._assert_almost_equal_convert(quantity, unit1, unit2, ref)
 
-    def test_force(self):
-        yield self._assert_almost_equal_convert, 1, 'kJ/(mol*A)', 'J/m', 1.66053892103219e-11
-        yield self._assert_almost_equal_convert, 2.5, 'kJ/(mol*nm)', 'kJ/(mol*A)', 0.25
-        yield self._assert_almost_equal_convert, 1, 'kcal/(mol*Angstrom)', 'kJ/(mol*Angstrom)', 4.184
+    @pytest.mark.parametrize('quantity, unit1, unit2, ref', (
+        (1, 'kJ/(mol*A)', 'J/m', 1.66053892103219e-11),
+        (2.5, 'kJ/(mol*nm)', 'kJ/(mol*A)', 0.25),
+        (1, 'kcal/(mol*Angstrom)', 'kJ/(mol*Angstrom)', 4.184),
+    ))
+    def test_force(self, quantity, unit1, unit2, ref):
+        self._assert_almost_equal_convert(quantity, unit1, unit2, ref)
 
-    def test_unit_unknown(self):
-        nm = 12.34567
-        assert_raises(ValueError, units.convert, nm, 'Stone', 'nm')
-        assert_raises(ValueError, units.convert, nm, 'nm', 'Stone')
+    @pytest.mark.parametrize('quantity, unit1, unit2, ref', (
+        pytest.mark.raises((nm, 'Stone', 'nm', None), exception=ValueError),
+        pytest.mark.raises((nm, 'nm', 'Stone', None), exception=ValueError),
+    ))
+    def test_unit_unknown(self, quantity, unit1, unit2, ref):
+        val = units.convert(quantity, unit1, unit2)
+        assert_almost_equal(val, ref,
+                            err_msg="Conversion {0} --> {1} failed".format(unit1, unit2))
 
     def test_unit_unconvertable(self):
         nm = 12.34567
