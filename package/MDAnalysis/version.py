47f956f430fa2554e61071507608b152acb253b1
Max Linke <kain88-de@users.noreply.github.com>
2017-06-24 12:47:35 +0200

    update version number to 0.17.0 (#1426)

--- package/MDAnalysis/version.py
+++ package/MDAnalysis/version.py
@@ -66,4 +66,4 @@ Data
 # e.g. with lib.log
 
 #: Release of MDAnalysis as a string, using `semantic versioning`_.
-__version__ = "0.16.2-dev0"  # NOTE: keep in sync with RELEASE in setup.py
+__version__ = "0.17.0-dev"  # NOTE: keep in sync with RELEASE in setup.py
