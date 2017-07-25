f93608f526dbeb59d443894ce57ba8a92477d8a0
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-07-25 19:50:04 +0530

    Cleanup

--- testsuite/MDAnalysisTests/analysis/test_distances.py
+++ testsuite/MDAnalysisTests/analysis/test_distances.py
@@ -34,7 +34,6 @@ from numpy.testing import assert_equal
 import numpy as np
 
 
-
 class TestContactMatrix(object):
     coord = np.array([[1, 1, 1],
                       [5, 5, 5],
@@ -59,23 +58,35 @@ class TestContactMatrix(object):
 
 
     def test_np(self):
-        contacts = MDAnalysis.analysis.distances.contact_matrix(self.coord, cutoff=1, returntype="numpy")
-        assert contacts.shape == self.shape, "wrong shape (should be {0})".format(self.shape)
+        contacts = MDAnalysis.analysis.distances.contact_matrix(
+            self.coord, cutoff=1, returntype="numpy"
+        )
+        assert contacts.shape == self.shape, \
+            "wrong shape (should be {0})".format(self.shape)
         assert_equal(contacts, self.res_no_pbc)
 
     def test_sparse(self):
-        contacts = MDAnalysis.analysis.distances.contact_matrix(self.coord, cutoff=1.5, returntype="sparse")
-        assert contacts.shape == self.shape, "wrong shape (should be {0})".format(self.shape)
+        contacts = MDAnalysis.analysis.distances.contact_matrix(
+            self.coord, cutoff=1.5, returntype="sparse"
+        )
+        assert contacts.shape == self.shape, \
+            "wrong shape (should be {0})".format(self.shape)
         assert_equal(contacts.toarray(), self.res_no_pbc)
 
     def test_box_numpy(self):
-        contacts = MDAnalysis.analysis.distances.contact_matrix(self.coord, box=self.box, cutoff=1)
-        assert contacts.shape == self.shape, "wrong shape (should be {0})".format(self.shape)
+        contacts = MDAnalysis.analysis.distances.contact_matrix(
+            self.coord, box=self.box, cutoff=1
+        )
+        assert contacts.shape == self.shape, \
+            "wrong shape (should be {0})".format(self.shape)
         assert_equal(contacts, self.res_pbc)
 
     def test_box_sparse(self):
-        contacts = MDAnalysis.analysis.distances.contact_matrix(self.coord, box=self.box, cutoff=1, returntype='sparse')
-        assert contacts.shape == self.shape, "wrong shape (should be {0})".format(self.shape)
+        contacts = MDAnalysis.analysis.distances.contact_matrix(
+            self.coord, box=self.box, cutoff=1, returntype='sparse'
+        )
+        assert contacts.shape == self.shape, \
+            "wrong shape (should be {0})".format(self.shape)
         assert_equal(contacts.toarray(), self.res_pbc)
 
 
@@ -84,8 +95,10 @@ class TestDist(object):
     ag = u.atoms[:20]
     u2 = MDAnalysis.Universe(GRO)
     ag2 = u2.atoms[:20]
-    ag2.positions = np.random.shuffle(ag2.positions)
-    expected = np.diag(scipy.spatial.distance.cdist(ag.positions, ag2.positions))
+    np.random.shuffle(ag2.positions)
+    expected = np.diag(scipy.spatial.distance.cdist(
+        ag.positions, ag2.positions)
+    )
 
     def test_pairwise_dist(self):
         '''Ensure that pairwise distances between atoms are
@@ -96,14 +109,17 @@ class TestDist(object):
     def test_pairwise_dist_offset_effect(self):
         '''Test that feeding in offsets to dist() doesn't alter
         pairwise distance matrix.'''
-        actual = MDAnalysis.analysis.distances.dist(self.ag, self.ag2, offset=229)[2]
+        actual = MDAnalysis.analysis.distances.dist(
+            self.ag, self.ag2, offset=229)[2]
         assert_equal(actual, self.expected)
 
 
     def test_offset_calculation(self):
         '''Test that offsets fed to dist() are correctly calculated.'''
-        actual = MDAnalysis.analysis.distances.dist(self.ag, self.ag2, offset=33)[:2]
-        assert_equal(actual, np.array([self.ag.atoms.resids + 33, self.ag2.atoms.resids + 33]))
+        actual = MDAnalysis.analysis.distances.dist(self.ag, self.ag2,
+                                                    offset=33)[:2]
+        assert_equal(actual, np.array([self.ag.atoms.resids + 33,
+                                       self.ag2.atoms.resids + 33]))
 
     def test_mismatch_exception(self):
         '''A ValueError should be raised if the two atomgroups
@@ -118,10 +134,12 @@ class TestBetween(object):
     ag2 = u.atoms[12:33]
     group = u.atoms[40:]
     distance = 5.9
-    distance_matrix_1 = scipy.spatial.distance.cdist(group.positions, ag.positions)
+    distance_matrix_1 = scipy.spatial.distance.cdist(group.positions,
+                                                     ag.positions)
     mask_1 = np.unique(np.where(distance_matrix_1 <= distance)[0])
     group_filtered = group[mask_1]
-    distance_matrix_2 = scipy.spatial.distance.cdist(group_filtered.positions, ag2.positions)
+    distance_matrix_2 = scipy.spatial.distance.cdist(group_filtered.positions,
+                                                     ag2.positions)
     mask_2 = np.unique(np.where(distance_matrix_2 <= distance)[0])
     expected = group_filtered[mask_2].indices
 
