ff6fadb89ef95d252c38136da283dded67f80799
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-07-27 22:52:15 +0530

    Cleanup

--- testsuite/MDAnalysisTests/analysis/test_leaflet.py
+++ testsuite/MDAnalysisTests/analysis/test_leaflet.py
@@ -26,43 +26,46 @@ import pytest
 from numpy.testing import assert_equal, assert_almost_equal
 import numpy as np
 
+from MDAnalysis.analysis.leaflet import LeafletFinder, optimize_cutoff
 from MDAnalysisTests.datafiles import Martini_membrane_gro
 
 
-class TestLeafletFinder(object):
-    @staticmethod
-    @pytest.fixture()
-    def universe():
-        return MDAnalysis.Universe(Martini_membrane_gro, Martini_membrane_gro)
+LIPID_HEAD_STRING = "name PO4"
 
-    @pytest.fixture()
-    def lipid_heads(self, universe):
-        return universe.select_atoms("name PO4")
 
-    lipid_head_string = "name PO4"
+@pytest.fixture()
+def universe():
+    return MDAnalysis.Universe(Martini_membrane_gro)
 
-    def test_leaflet_finder(self, universe, lipid_heads):
-        from MDAnalysis.analysis.leaflet import LeafletFinder
-        lfls = LeafletFinder(universe, lipid_heads, pbc=True)
-        top_heads, bottom_heads = lfls.groups()
-        # Make top be... on top.
-        if top_heads.center_of_geometry()[2] < bottom_heads.center_of_geometry()[2]:
-            top_heads,bottom_heads = (bottom_heads,top_heads)
-        assert_equal(top_heads.indices, np.arange(1,2150,12), err_msg="Found wrong leaflet lipids")
-        assert_equal(bottom_heads.indices, np.arange(2521,4670,12), err_msg="Found wrong leaflet lipids")
 
-    def test_string_vs_atomgroup_proper(self, universe, lipid_heads):
-        from MDAnalysis.analysis.leaflet import LeafletFinder
-        lfls_ag = LeafletFinder(universe, lipid_heads, pbc=True)
-        lfls_string = LeafletFinder(universe, self.lipid_head_string, pbc=True)
-        groups_ag = lfls_ag.groups()
-        groups_string = lfls_string.groups()
-        assert_equal(groups_string[0].indices, groups_ag[0].indices)
-        assert_equal(groups_string[1].indices, groups_ag[1].indices)
+@pytest.fixture()
+def lipid_heads(universe):
+    return universe.select_atoms(LIPID_HEAD_STRING)
 
-    def test_optimize_cutoff(self, universe, lipid_heads):
-        from MDAnalysis.analysis.leaflet import optimize_cutoff
-        cutoff, N = optimize_cutoff(universe, lipid_heads, pbc=True)
-        assert N == 2
-        assert_almost_equal(cutoff, 10.5, decimal=4)
+
+def test_leaflet_finder(universe, lipid_heads):
+    lfls = LeafletFinder(universe, lipid_heads, pbc=True)
+    top_heads, bottom_heads = lfls.groups()
+    # Make top be... on top.
+    if top_heads.center_of_geometry()[2] < bottom_heads.center_of_geometry()[2]:
+        top_heads,bottom_heads = (bottom_heads,top_heads)
+    assert_equal(top_heads.indices, np.arange(1,2150,12),
+                 err_msg="Found wrong leaflet lipids")
+    assert_equal(bottom_heads.indices, np.arange(2521,4670,12),
+                 err_msg="Found wrong leaflet lipids")
+
+
+def test_string_vs_atomgroup_proper(universe, lipid_heads):
+    lfls_ag = LeafletFinder(universe, lipid_heads, pbc=True)
+    lfls_string = LeafletFinder(universe, LIPID_HEAD_STRING, pbc=True)
+    groups_ag = lfls_ag.groups()
+    groups_string = lfls_string.groups()
+    assert_equal(groups_string[0].indices, groups_ag[0].indices)
+    assert_equal(groups_string[1].indices, groups_ag[1].indices)
+
+
+def test_optimize_cutoff(universe, lipid_heads):
+    cutoff, N = optimize_cutoff(universe, lipid_heads, pbc=True)
+    assert N == 2
+    assert_almost_equal(cutoff, 10.5, decimal=4)
 
