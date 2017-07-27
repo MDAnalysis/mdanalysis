925eb224dae51d886d05597624a84c41ac27dae9
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-07-27 23:19:37 +0530

    Cleanup

--- testsuite/MDAnalysisTests/analysis/test_gnm.py
+++ testsuite/MDAnalysisTests/analysis/test_gnm.py
@@ -33,119 +33,121 @@ import pytest
 from MDAnalysisTests.datafiles import GRO, XTC
 
 
-class TestGNM(object):
-    @staticmethod
-    @pytest.fixture()
-    def universe():
-        return MDAnalysis.Universe(GRO, XTC)
+@pytest.fixture()
+def universe():
+    return MDAnalysis.Universe(GRO, XTC)
 
-    def test_gnm(self, universe, tmpdir):
-        output = os.path.join(str(tmpdir), 'output.txt')
-        gnm = MDAnalysis.analysis.gnm.GNMAnalysis(universe, ReportVector=output)
-        gnm.run()
-        result = gnm.results
-        assert len(result) == 10
-        time, eigenvalues, eigenvectors = zip(*result)
-        assert_almost_equal(time, np.arange(0, 1000, 100), decimal=4)
-        assert_almost_equal(eigenvalues,
-          [ 2.0287113e-15, 4.1471575e-15, 1.8539533e-15, 4.3810359e-15,
-            3.9607304e-15, 4.1289113e-15, 2.5501084e-15, 4.0498182e-15,
-            4.2058769e-15, 3.9839431e-15])
 
-    def test_gnm_run_step(self, universe):
-        gnm = MDAnalysis.analysis.gnm.GNMAnalysis(universe)
-        gnm.run(step=3)
-        result = gnm.results
-        assert len(result) == 4
-        time, eigenvalues, eigenvectors = zip(*result)
-        assert_almost_equal(time, np.arange(0, 1200, 300), decimal=4)
-        assert_almost_equal(eigenvalues,
-          [ 2.0287113e-15, 4.3810359e-15, 2.5501084e-15, 3.9839431e-15])
+def test_gnm(universe, tmpdir):
+    output = os.path.join(str(tmpdir), 'output.txt')
+    gnm = MDAnalysis.analysis.gnm.GNMAnalysis(universe, ReportVector=output)
+    gnm.run()
+    result = gnm.results
+    assert len(result) == 10
+    time, eigenvalues, eigenvectors = zip(*result)
+    assert_almost_equal(time, np.arange(0, 1000, 100), decimal=4)
+    assert_almost_equal(eigenvalues,
+      [ 2.0287113e-15, 4.1471575e-15, 1.8539533e-15, 4.3810359e-15,
+        3.9607304e-15, 4.1289113e-15, 2.5501084e-15, 4.0498182e-15,
+        4.2058769e-15, 3.9839431e-15])
 
-    def test_generate_kirchoff(self, universe):
-        gnm = MDAnalysis.analysis.gnm.GNMAnalysis(universe)
-        gnm.run()
-        gen = gnm.generate_kirchoff()
-        assert_almost_equal(gen[0],
-          [ 7,-1,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0,-1,-1,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-    def test_closeContactGNMAnalysis(self, universe):
-        gnm = MDAnalysis.analysis.gnm.closeContactGNMAnalysis(universe, weights="size")
+def test_gnm_run_step(universe):
+    gnm = MDAnalysis.analysis.gnm.GNMAnalysis(universe)
+    gnm.run(step=3)
+    result = gnm.results
+    assert len(result) == 4
+    time, eigenvalues, eigenvectors = zip(*result)
+    assert_almost_equal(time, np.arange(0, 1200, 300), decimal=4)
+    assert_almost_equal(eigenvalues,
+      [ 2.0287113e-15, 4.3810359e-15, 2.5501084e-15, 3.9839431e-15])
 
-        gnm.run()
 
-        result = gnm.results
-        assert len(result) == 10
-        time, eigenvalues, eigenvectors = zip(*result)
-        assert_almost_equal(time, np.arange(0, 1000, 100), decimal=4)
-        assert_almost_equal(eigenvalues,
-          [ 0.1502614,  0.1426407,  0.1412389,  0.1478305,  0.1425449,
-            0.1563304,  0.156915 ,  0.1503619,  0.1572592,  0.1542063])
+def test_generate_kirchoff(universe):
+    gnm = MDAnalysis.analysis.gnm.GNMAnalysis(universe)
+    gnm.run()
+    gen = gnm.generate_kirchoff()
+    assert_almost_equal(gen[0],
+      [ 7,-1,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,-1,-1,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        gen = gnm.generate_kirchoff()
-        assert_almost_equal(gen[0],
-          [ 16.326744128018923, -2.716098853586913, -1.94736842105263, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, -0.05263157894736842, 0.0, 0.0, 0.0, -3.3541953679557905, 0.0, -1.4210526315789465, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, -1.0423368771244421, -1.3006649542861801, -0.30779350562554625, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.927172649945531, -0.7509392614826383,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, -2.263157894736841, -0.24333213169614382])
 
-    def test_closeContactGNMAnalysis_weights_None(self, universe):
-        gnm = MDAnalysis.analysis.gnm.closeContactGNMAnalysis(universe, weights=None)
+def test_closeContactGNMAnalysis(universe):
+    gnm = MDAnalysis.analysis.gnm.closeContactGNMAnalysis(universe, weights="size")
 
-        gnm.run()
+    gnm.run()
 
-        result = gnm.results
-        assert len(result) == 10
-        time, eigenvalues, eigenvectors = zip(*result)
-        assert_almost_equal(time, np.arange(0, 1000, 100), decimal=4)
-        assert_almost_equal(eigenvalues,
-          [ 2.4328739,  2.2967251,  2.2950061,  2.4110916,  2.3271343,
-            2.5213111,  2.5189955,  2.4481649,  2.5224835,  2.4824345])
+    result = gnm.results
+    assert len(result) == 10
+    time, eigenvalues, eigenvectors = zip(*result)
+    assert_almost_equal(time, np.arange(0, 1000, 100), decimal=4)
+    assert_almost_equal(eigenvalues,
+      [ 0.1502614,  0.1426407,  0.1412389,  0.1478305,  0.1425449,
+        0.1563304,  0.156915 ,  0.1503619,  0.1572592,  0.1542063])
 
-        gen = gnm.generate_kirchoff()
-        assert_almost_equal(gen[0],
-          [ 303.0, -58.0, -37.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0,
-            0.0, 0.0, 0.0, -67.0, 0.0, -27.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -17.0, -15.0,
-            -6.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, -14.0, -15.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -43.0, -3.0])
+    gen = gnm.generate_kirchoff()
+    assert_almost_equal(gen[0],
+      [ 16.326744128018923, -2.716098853586913, -1.94736842105263, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, -0.05263157894736842, 0.0, 0.0, 0.0, -3.3541953679557905, 0.0, -1.4210526315789465, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, -1.0423368771244421, -1.3006649542861801, -0.30779350562554625, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.927172649945531, -0.7509392614826383,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, -2.263157894736841, -0.24333213169614382])
 
-    def test_closeContactGNMAnalysis_default_weights_is_size(self, universe):
-        gnm = MDAnalysis.analysis.gnm.closeContactGNMAnalysis(universe)
-        assert gnm.weights == "size"
 
-    def test_closeContactGNMAnalysis_deprecated_MassWeight_False(self, universe):
-        gnm = MDAnalysis.analysis.gnm.closeContactGNMAnalysis(universe, MassWeight=False)
-        assert gnm.weights is None
+def test_closeContactGNMAnalysis_weights_None(universe):
+    gnm = MDAnalysis.analysis.gnm.closeContactGNMAnalysis(universe, weights=None)
 
-    def test_closeContactGNMAnalysis_deprecated_MassWeight_True(self, universe):
-        gnm = MDAnalysis.analysis.gnm.closeContactGNMAnalysis(universe, MassWeight=True)
-        assert gnm.weights == "size"
+    gnm.run()
+
+    result = gnm.results
+    assert len(result) == 10
+    time, eigenvalues, eigenvectors = zip(*result)
+    assert_almost_equal(time, np.arange(0, 1000, 100), decimal=4)
+    assert_almost_equal(eigenvalues,
+      [ 2.4328739,  2.2967251,  2.2950061,  2.4110916,  2.3271343,
+        2.5213111,  2.5189955,  2.4481649,  2.5224835,  2.4824345])
+
+    gen = gnm.generate_kirchoff()
+    assert_almost_equal(gen[0],
+      [ 303.0, -58.0, -37.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0,
+        0.0, 0.0, 0.0, -67.0, 0.0, -27.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -17.0, -15.0,
+        -6.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, -14.0, -15.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -43.0, -3.0])
+
+
+@pytest.mark.parametrize('MassWeight, expected_value', (
+    (None, 'size'),
+    (False, None),
+    (True, 'size')
+))
+def test_closeContactGNMAnalysis(universe, MassWeight, expected_value):
+    gnm = MDAnalysis.analysis.gnm.closeContactGNMAnalysis(universe,
+                                                          MassWeight=MassWeight)
+    assert gnm.weights == expected_value
