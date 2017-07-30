b307b2c2ae7703b172061b2c0a9beaedc13b021f
Utkarsh Bansal <bansalutkarsh3@gmail.com>
2017-07-30 16:56:11 +0530

    Pytest Style analysis/test_rdf.py (#1570)

--- testsuite/MDAnalysisTests/analysis/test_rdf.py
+++ testsuite/MDAnalysisTests/analysis/test_rdf.py
@@ -22,9 +22,7 @@
 from __future__ import absolute_import
 from six.moves import zip
 
-from unittest import TestCase
-
-from numpy.testing import assert_
+import pytest
 
 import MDAnalysis as mda
 from MDAnalysis.analysis.rdf import InterRDF
@@ -32,76 +30,63 @@ from MDAnalysis.analysis.rdf import InterRDF
 from MDAnalysisTests.datafiles import two_water_gro
 
 
-class TestInterRDF(TestCase):
-    def setUp(self):
-        self.u = mda.Universe(two_water_gro)
+@pytest.fixture(scope='module')
+def u():
+    return mda.Universe(two_water_gro)
 
-    def tearDown(self):
-        del self.u
 
-    def _linear_water(self):
-        """Set the positions of the two water molecules to be linear
+@pytest.fixture(scope='module')
+def sels(u):
+    for at, (x, y) in zip(u.atoms, zip([1] * 3 + [2] * 3, [2, 1, 3] * 2)):
+        at.position = x, y, 0.0
+    s1 = u.atoms.OW
+    s2 = u.atoms.HW1 + u.atoms.HW2
+    return s1, s2
 
-        y =   | 1   |  2  | 3   |
-        ------+-----------------|
-        x = 1 | HW1 - OW - HW2  |
-        ------+-----------------|
-        x = 2 | HW2 - OW - HW2  |
-        ------+-----------------|
 
-        """
-        # 2, 1, 3 because OW is first
-        for at, (x, y) in zip(self.u.atoms,
-                              zip([1] * 3 + [2] * 3, [2, 1, 3] * 2)):
-            at.position = x, y, 0.0
+def test_nbins(u):
+    s1 = u.atoms[:3]
+    s2 = u.atoms[3:]
+    rdf = InterRDF(s1, s2, nbins=412).run()
 
-    def _get_sels(self):
-        s1 = self.u.atoms.OW
-        s2 = self.u.atoms.HW1 + self.u.atoms.HW2
-        return s1, s2
+    assert len(rdf.bins) == 412
 
-    def test_nbins(self):
-        s1 = self.u.atoms[:3]
-        s2 = self.u.atoms[3:]
-        rdf = InterRDF(s1, s2, nbins=412).run()
 
-        assert_(len(rdf.bins) == 412)
+def test_range(u):
+    s1 = u.atoms[:3]
+    s2 = u.atoms[3:]
+    rmin, rmax = 1.0, 13.0
+    rdf = InterRDF(s1, s2, range=(rmin, rmax)).run()
 
-    def test_range(self):
-        s1 = self.u.atoms[:3]
-        s2 = self.u.atoms[3:]
-        rmin, rmax = 1.0, 13.0
-        rdf = InterRDF(s1, s2, range=(rmin, rmax)).run()
+    assert rdf.edges[0] == rmin
+    assert rdf.edges[-1] == rmax
 
-        assert_(rdf.edges[0] == rmin)
-        assert_(rdf.edges[-1] == rmax)
 
-    def test_count_sum(self):
-        # OW vs HW
-        # should see 8 comparisons in count
-        self._linear_water()
-        s1, s2 = self._get_sels()
-        rdf = InterRDF(s1, s2).run()
-        assert_(rdf.count.sum() == 8)
+def test_count_sum(sels):
+    # OW vs HW
+    # should see 8 comparisons in count
+    s1, s2 = sels
+    rdf = InterRDF(s1, s2).run()
+    assert rdf.count.sum() == 8
 
-    def test_count(self):
-        # should see two distances with 4 counts each
-        self._linear_water()
-        s1, s2 = self._get_sels()
-        rdf = InterRDF(s1, s2).run()
-        assert_(len(rdf.count[rdf.count == 4]) == 2)
 
-    def test_double_run(self):
-        # running rdf twice should give the same result
-        self._linear_water()
-        s1, s2 = self._get_sels()
-        rdf = InterRDF(s1, s2).run()
-        rdf.run()
-        assert_(len(rdf.count[rdf.count == 4]) == 2)
+def test_count(sels):
+    # should see two distances with 4 counts each
+    s1, s2 = sels
+    rdf = InterRDF(s1, s2).run()
+    assert len(rdf.count[rdf.count == 4]) == 2
 
-    def test_exclusion(self):
-        # should see two distances with 4 counts each
-        self._linear_water()
-        s1, s2 = self._get_sels()
-        rdf = InterRDF(s1, s2, exclusion_block=(1, 2)).run()
-        assert_(rdf.count.sum() == 4)
+
+def test_double_run(sels):
+    # running rdf twice should give the same result
+    s1, s2 = sels
+    rdf = InterRDF(s1, s2).run()
+    rdf.run()
+    assert len(rdf.count[rdf.count == 4]) == 2
+
+
+def test_exclusion(sels):
+    # should see two distances with 4 counts each
+    s1, s2 = sels
+    rdf = InterRDF(s1, s2, exclusion_block=(1, 2)).run()
+    assert rdf.count.sum() == 4
