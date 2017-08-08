979da77586ad837b3e8773e4cb2bd4892c871fbc
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-08-08 19:23:48 +0530

    Pytest Style topology/test_tprparser.py

--- testsuite/MDAnalysisTests/topology/test_tprparser.py
+++ testsuite/MDAnalysisTests/topology/test_tprparser.py
@@ -20,7 +20,9 @@
 # J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
 #
 from __future__ import absolute_import
-from numpy.testing import assert_, assert_array_equal
+
+import pytest
+from numpy.testing import assert_equal
 import functools
 
 import numpy as np
@@ -43,18 +45,13 @@ class TPRAttrs(ParserBase):
 
     def test_moltypes(self, top):
         moltypes = top.moltypes.values
-        assert_array_equal(moltypes, self.ref_moltypes)
-
-
+        assert_equal(moltypes, self.ref_moltypes)
 
 
 class TestTPR(TPRAttrs):
     """
     this test the data/adk_oplsaa.tpr which is of tpx version 58
     """
-
-    __test__ = True
-
     filename = TPR
     expected_n_atoms = 47681
     expected_n_residues = 11302
@@ -76,82 +73,56 @@ class TPRBase(TPRAttrs):
 
 # All these classes should be generated in a loop.
 class TestTPR400(TPRBase):
-
-    __test__ = True
-
     filename = TPR400
 
+
 class TestTPR402(TPRBase):
-
-    __test__ = True
-
     filename = TPR402
 
+
 class TestTPR403(TPRBase):
-
-    __test__ = True
-
     filename = TPR403
 
+
 class TestTPR404(TPRBase):
-
-    __test__ = True
-
     filename = TPR404
 
+
 class TestTPR405(TPRBase):
-
-    __test__ = True
-
     filename = TPR405
 
+
 class TestTPR406(TPRBase):
-
-    __test__ = True
-
     filename = TPR406
 
+
 class TestTPR407(TPRBase):
-
-    __test__ = True
-
     filename = TPR407
 
+
 class TestTPR450(TPRBase):
-
-    __test__ = True
-
     filename = TPR450
 
+
 class TestTPR451(TPRBase):
-
-    __test__ = True
-
     filename = TPR451
 
+
 class TestTPR452(TPRBase):
-
-    __test__ = True
-
     filename = TPR452
 
+
 class TestTPR453(TPRBase):
-
-    __test__ = True
-
     filename = TPR453
 
+
 class TestTPR454(TPRBase):
-
-    __test__ = True
-
     filename = TPR454
 
+
 class TestTPR455(TPRBase):
-
-    __test__ = True
-
     filename = TPR455
+
 
 class TPRDouble(TPRAttrs):
     expected_n_atoms = 21692
@@ -162,10 +133,8 @@ class TPRDouble(TPRAttrs):
                             + ['SOL'] * 4284,
                             dtype=object)
 
+
 class TestTPR455Double(TPRDouble):
-
-    __test__ = True
-
     filename = TPR455Double
 
 
@@ -181,46 +150,30 @@ class TPR46xBase(TPRAttrs):
 
 
 class TestTPR460(TPR46xBase):
-
-    __test__ = True
-
     filename = TPR460
 
+
 class TestTPR461(TPR46xBase):
-
-    __test__ = True
-
     filename = TPR461
 
 
 class TestTPR502(TPRBase):
-
-    __test__ = True
-
     filename = TPR502
 
+
 class TestTPR504(TPRBase):
-
-    __test__ = True
-
     filename = TPR504
 
+
 class TestTPR505(TPRBase):
-
-    __test__ = True
-
     filename = TPR505
 
+
 class TestTPR510(TPRBase):
-
-    __test__ = True
-
     filename = TPR510
 
-class TPR2016(TPRBase):
 
-    __test__ = True
-
+class TestTPR2016(TPRBase):
     filename = TPR2016
 
 
@@ -232,58 +185,87 @@ def _test_is_in_topology(name, elements, topology_path, topology_section):
     parser = MDAnalysis.topology.TPRParser.TPRParser(topology_path)
     top = parser.parse()
     for element in elements:
-        assert_(element in getattr(top, topology_section).values,
-                'Interaction type "{}" not found'.format(name))
+        assert element in getattr(top, topology_section).values, \
+            'Interaction type "{}" not found'.format(name)
 
 
-def test_all_bonds():
+@pytest.mark.parametrize('topology', (
+        TPR510_bonded,
+        TPR2016_bonded
+))
+@pytest.mark.parametrize('bond', (
+        ('BONDS', [(0, 1)]),
+        ('G96BONDS', [(1, 2)]),
+        ('MORSE', [(2, 3)]),
+        ('CUBICBONDS', [(3, 4)]),
+        ('CONNBONDS', [(4, 5)]),
+        ('HARMONIC', [(5, 6)]),
+        ('FENEBONDS', [(6, 7)]),
+        ('RESTRAINTPOT', [(7, 8)]),
+        ('TABBONDS', [(8, 9)]),
+        ('TABBONDSNC', [(9, 10)]),
+        ('CONSTR', [(10, 11)]),
+        ('CONSTRNC', [(11, 12)]),
+))
+def test_all_bonds(topology, bond):
     """Test that all bond types are parsed as expected"""
-    topologies = (TPR510_bonded, TPR2016_bonded)
-
-    bonds = {'BONDS':[(0, 1)], 'G96BONDS':[(1, 2)], 'MORSE':[(2, 3)],
-             'CUBICBONDS':[(3, 4)], 'CONNBONDS':[(4, 5)], 'HARMONIC':[(5, 6)],
-             'FENEBONDS':[(6, 7)], 'RESTRAINTPOT':[(7, 8)],
-             'TABBONDS':[(8, 9)], 'TABBONDSNC':[(9, 10)],
-             'CONSTR':[(10, 11)], 'CONSTRNC':[(11, 12)],}
     bond_type_in_topology = functools.partial(_test_is_in_topology,
                                               topology_section='bonds')
-    for topology in topologies:
-        for bond_type, elements in bonds.items():
-            yield (bond_type_in_topology, bond_type, elements, topology)
+    bond_type, elements = bond
+    bond_type_in_topology(bond_type, elements, topology)
 
 
-def test_all_angles():
-    topologies = (TPR510_bonded, TPR2016_bonded)
-    angles = {'ANGLES':[(0, 1, 2)], 'G96ANGLES':[(1, 2, 3)],
-              'CROSS_BOND_BOND':[(2, 3, 4)], 'CROSS_BOND_ANGLE':[(3, 4, 5)],
-              'UREY_BRADLEY':[(4, 5, 6)], 'QANGLES':[(5, 6, 7)],
-              'RESTRANGLES':[(6, 7, 8)], 'TABANGLES':[(7, 8, 9)],}
+@pytest.mark.parametrize('topology', (
+    TPR510_bonded,
+    TPR2016_bonded
+))
+@pytest.mark.parametrize('angle', (
+    ('ANGLES',[(0, 1, 2)]),
+    ('G96ANGLES',[(1, 2, 3)]),
+    ('CROSS_BOND_BOND',[(2, 3, 4)]),
+    ('CROSS_BOND_ANGLE',[(3, 4, 5)]),
+    ('UREY_BRADLEY',[(4, 5, 6)]),
+    ('QANGLES',[(5, 6, 7)]),
+    ('RESTRANGLES',[(6, 7, 8)]),
+    ('TABANGLES',[(7, 8, 9)]),
+))
+def test_all_angles(topology, angle):
     angle_type_in_topology = functools.partial(_test_is_in_topology,
                                                topology_section='angles')
-    for topology in topologies:
-        for angle_type, elements in angles.items():
-            yield (angle_type_in_topology, angle_type, elements, topology)
+    angle_type, elements = angle
+    angle_type_in_topology(angle_type, elements, topology)
 
 
-def test_all_dihedrals():
-    topologies = (TPR510_bonded, TPR2016_bonded)
-    dihs = {'PDIHS':[(0, 1, 2, 3), (1, 2, 3, 4), (7, 8, 9, 10)],
-            'RBDIHS':[(4, 5, 6, 7)], 'RESTRDIHS':[(8, 9, 10, 11)],
-            'CBTDIHS':[(9, 10, 11, 12)], 'FOURDIHS':[(6, 7, 8, 9)],
-            'TABDIHS':[(10, 11, 12, 13)],}
+@pytest.mark.parametrize('topology', (
+    TPR510_bonded,
+    TPR2016_bonded
+))
+@pytest.mark.parametrize('dih', (
+        ('PDIHS',[(0, 1, 2, 3), (1, 2, 3, 4), (7, 8, 9, 10)]),
+        ('RBDIHS',[(4, 5, 6, 7)]),
+        ('RESTRDIHS',[(8, 9, 10, 11)]),
+        ('CBTDIHS',[(9, 10, 11, 12)]),
+        ('FOURDIHS',[(6, 7, 8, 9)]),
+        ('TABDIHS',[(10, 11, 12, 13)]),
+))
+def test_all_dihedrals(topology, dih):
     dih_type_in_topology = functools.partial(_test_is_in_topology,
                                              topology_section='dihedrals')
-    for topology in topologies:
-        for dih_type, elements in dihs.items():
-            yield (dih_type_in_topology, dih_type, elements, topology)
+    dih_type, elements = dih
+    dih_type_in_topology(dih_type, elements, topology)
 
 
-def test_all_impropers():
-    topologies = (TPR510_bonded, TPR2016_bonded)
-    imprs = {'IDIHS':[(2, 3, 4, 5), (3, 4, 5, 6)], 'PIDIHS':[(5, 6, 7, 8)]}
+@pytest.mark.parametrize('topology', (
+    TPR510_bonded,
+    TPR2016_bonded
+))
+@pytest.mark.parametrize('impr', (
+    ('IDIHS', [(2, 3, 4, 5), (3, 4, 5, 6)]),
+    ('PIDIHS', [(5, 6, 7, 8)])
+))
+def test_all_impropers(topology, impr):
     impr_type_in_topology = functools.partial(_test_is_in_topology,
                                               topology_section='impropers')
-    for topology in topologies:
-        for impr_type, elements in imprs.items():
-            yield (impr_type_in_topology, impr_type, elements, topology)
 
+    impr_type, elements = impr
+    impr_type_in_topology(impr_type, elements, topology)
