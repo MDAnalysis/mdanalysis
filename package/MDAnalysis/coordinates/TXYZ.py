0acc0f93d47baea8c50d41765ba45366e5a4261b
Richard Gowers <richardjgowers@gmail.com>
2017-08-16 17:39:28 +0100

    fixed TXYZ.py rst bullet list

--- package/MDAnalysis/coordinates/TXYZ.py
+++ package/MDAnalysis/coordinates/TXYZ.py
@@ -25,10 +25,12 @@ TXYZ file format --- :mod:`MDAnalysis.coordinates.TXYZ`
 
 Coordinate reader for Tinker_ xyz files .txyz and trajectory .arc files
 Differences between Tinker format_ and normal xyz files: 
- - there is only one header line containing both the number of atoms and a comment
- - column 1 contains atom numbers (starting from 1)
- - column 6 contains atoms types 
- - the following columns indicate connectivity (atoms to which that particular atom is bonded, according to numbering in column 1)
+
+- there is only one header line containing both the number of atoms and a comment
+- column 1 contains atom numbers (starting from 1)
+- column 6 contains atoms types 
+- the following columns indicate connectivity (atoms to which that particular atom is
+  bonded, according to numbering in column 1)
 
 .. _format: http://chembytes.wikidot.com/tnk-tut00#toc2
 .. _Tinker: https://dasher.wustl.edu/tinker/
