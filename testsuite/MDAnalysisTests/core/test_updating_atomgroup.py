b2ac8887fd7cef70a7447962f76d5216d0c1a787
Utkarsh Bansal <bansalutkarsh3@gmail.com>
2017-09-04 10:44:02 +0530

    [Review Needed]Pytest Style core/test_updating_atomgroup.py (#1619)
    * Pytest Style TestUpdatingSelection, TestUpdatingSelectionNotraj
    
    * Pytest Style TestUAGCallCount, TestDynamicUAG
    
    * Cleanup
    
    * Indentation fixes
    
    * use builtin next method
    
    This is more future proof for a python3 only library in a couple of years.

--- testsuite/MDAnalysisTests/core/test_updating_atomgroup.py
+++ testsuite/MDAnalysisTests/core/test_updating_atomgroup.py
@@ -21,15 +21,9 @@
 #
 from __future__ import absolute_import
 
-from unittest import TestCase
-
 import numpy as np
 import pytest
-from numpy.testing import (
-    assert_,
-    assert_array_equal,
-    assert_equal,
-)
+from numpy.testing import assert_equal
 import mock
 
 
@@ -40,84 +34,106 @@ import MDAnalysis
 import MDAnalysis as mda
 
 
-class TestUpdatingSelection(TestCase):
-    def setUp(self):
-        self.u = mda.Universe(GRO, XTC)
-        self.ag = self.u.select_atoms(
-            "prop x < 5 and prop y < 5 and prop z < 5")
-        self.ag_updating = self.u.select_atoms(
-            "prop x < 5 and prop y < 5 and prop z < 5", updating=True)
-        self.ag_updating_compounded = self.u.select_atoms("around 2 group sele",
-                                    sele=self.ag, updating=True)
-        self.ag_updating_chained = self.u.select_atoms("around 2 group sele",
-                                    sele=self.ag_updating, updating=True)
-        self.ag_updating_chained2 = self.ag_updating.select_atoms("all",
-                                                                updating=True)
+class TestUpdatingSelection(object):
+    @pytest.fixture()
+    def u(self):
+        return mda.Universe(GRO, XTC)
 
-    def test_update(self):
-        assert_array_equal(self.ag_updating.indices, self.ag.indices)
-        target_idxs = np.array([ 4469,  4470,  4472,  6289,  6290,  6291,
+    @pytest.fixture()
+    def ag(self, u):
+        return u.select_atoms("prop x < 5 and prop y < 5 and prop z < 5")
+
+    @pytest.fixture()
+    def ag_updating(self, u):
+        return u.select_atoms("prop x < 5 and prop y < 5 and prop z < 5",
+                              updating=True)
+
+    @pytest.fixture()
+    def ag_updating_compounded(self, u, ag):
+        return u.select_atoms("around 2 group sele",
+                              sele=ag, updating=True)
+
+    @pytest.fixture()
+    def ag_updating_chained(self, u, ag_updating):
+        return u.select_atoms("around 2 group sele",
+                              sele=ag_updating, updating=True)
+
+    @pytest.fixture()
+    def ag_updating_chained2(self, ag_updating):
+        return ag_updating.select_atoms("all", updating=True)
+
+    def test_update(self, u, ag, ag_updating):
+        assert_equal(ag_updating.indices, ag.indices)
+        target_idxs = np.array([4469, 4470, 4472, 6289, 6290, 6291,
                                 6292, 31313, 31314, 31315, 31316, 34661,
                                 34663, 34664])
-        self.u.trajectory.next()
-        assert_equal(self.ag_updating._lastupdate, 0)
-        assert_(not self.ag_updating.is_uptodate)
-        assert_array_equal(self.ag_updating.indices, target_idxs)
-        assert_(self.ag_updating.is_uptodate)
-        self.ag_updating.is_uptodate = False
-        assert_(self.ag_updating._lastupdate is None)
+        next(u.trajectory)
+        assert_equal(ag_updating._lastupdate, 0)
+        assert ag_updating.is_uptodate is False
+        assert_equal(ag_updating.indices, target_idxs)
+        assert ag_updating.is_uptodate
+        ag_updating.is_uptodate = False
+        assert ag_updating._lastupdate is None
 
-    def test_compounded_update(self):
-        target_idxs0 = np.array([ 3650,  7406, 22703, 31426, 40357,
+    def test_compounded_update(self, u, ag_updating_compounded):
+        target_idxs0 = np.array([3650, 7406, 22703, 31426, 40357,
                                  40360, 41414])
-        target_idxs1 = np.array([ 3650,  8146, 23469, 23472, 31426,
+        target_idxs1 = np.array([3650, 8146, 23469, 23472, 31426,
                                  31689, 31692, 34326, 41414])
-        assert_array_equal(self.ag_updating_compounded.indices,
-                           target_idxs0)
-        self.u.trajectory.next()
-        assert_array_equal(self.ag_updating_compounded.indices,
-                           target_idxs1)
+        assert_equal(ag_updating_compounded.indices,
+                     target_idxs0)
+        next(u.trajectory)
+        assert_equal(ag_updating_compounded.indices,
+                     target_idxs1)
 
-    def test_chained_update(self):
-        target_idxs = np.array([ 4471,  7406, 11973, 11975, 34662, 44042])
-        assert_array_equal(self.ag_updating_chained.indices,
-                           self.ag_updating_compounded.indices)
-        self.u.trajectory.next()
-        assert_array_equal(self.ag_updating_chained.indices, target_idxs)
+    def test_chained_update(self, u, ag_updating_chained,
+                            ag_updating_compounded):
+        target_idxs = np.array([4471, 7406, 11973, 11975, 34662, 44042])
+        assert_equal(ag_updating_chained.indices,
+                     ag_updating_compounded.indices)
+        next(u.trajectory)
+        assert_equal(ag_updating_chained.indices, target_idxs)
 
-    def test_chained_update2(self):
-        assert_array_equal(self.ag_updating_chained2.indices,
-                           self.ag_updating.indices)
-        self.u.trajectory.next()
-        assert_array_equal(self.ag_updating_chained2.indices,
-                           self.ag_updating.indices)
+    def test_chained_update2(self, u, ag_updating, ag_updating_chained2):
+        assert_equal(ag_updating_chained2.indices,
+                     ag_updating.indices)
+        next(u.trajectory)
+        assert_equal(ag_updating_chained2.indices,
+                     ag_updating.indices)
 
-    def test_slice_is_static(self):
-        ag_static1 = self.ag_updating[:] 
-        ag_static2 = self.ag_updating.select_atoms("all") 
-        assert_array_equal(ag_static1.indices, self.ag.indices)
-        assert_array_equal(ag_static2.indices, self.ag.indices)
-        self.u.trajectory.next()
-        assert_array_equal(ag_static1.indices, self.ag.indices)
-        assert_array_equal(ag_static2.indices, self.ag.indices)
+    def test_slice_is_static(self, u, ag, ag_updating):
+        ag_static1 = ag_updating[:]
+        ag_static2 = ag_updating.select_atoms("all")
+        assert_equal(ag_static1.indices, ag.indices)
+        assert_equal(ag_static2.indices, ag.indices)
+        next(u.trajectory)
+        assert_equal(ag_static1.indices, ag.indices)
+        assert_equal(ag_static2.indices, ag.indices)
 
-    def test_kwarg_check(self):
+    def test_kwarg_check(self, u):
         with pytest.raises(TypeError):
-            self.u.select_atoms("group updating", {"updating": True})
+            u.select_atoms("group updating", {"updating": True})
 
 
-class TestUpdatingSelectionNotraj(TestCase):
-    def setUp(self):
-        self.u = mda.Universe(PSF)
-        self.ag = self.u.select_atoms("name N*")
-        self.ag_updating = self.u.select_atoms("name N*", updating=True)
+class TestUpdatingSelectionNotraj(object):
+    @pytest.fixture()
+    def u(self):
+        return mda.Universe(PSF)
 
-    def test_update(self):
-        assert_(self.ag_updating.is_uptodate)
-        assert_array_equal(self.ag_updating.indices, self.ag.indices)
-        assert_equal(self.ag_updating._lastupdate, -1)
-        self.ag_updating.is_uptodate = False
-        assert_(self.ag_updating._lastupdate is None)
+    @pytest.fixture()
+    def ag(self, u):
+        return u.select_atoms("name N*")
+
+    @pytest.fixture()
+    def ag_updating(self, u):
+        return u.select_atoms("name N*", updating=True)
+
+    def test_update(self, ag, ag_updating):
+        assert ag_updating.is_uptodate
+        assert_equal(ag_updating.indices, ag.indices)
+        assert_equal(ag_updating._lastupdate, -1)
+        ag_updating.is_uptodate = False
+        assert ag_updating._lastupdate is None
 
 
 class UAGReader(mda.coordinates.base.ReaderBase):
@@ -139,6 +155,7 @@ class UAGReader(mda.coordinates.base.ReaderBase):
     Whilst quite possible not the best data for molecular simulation,
     it does make for easy to write tests.
     """
+
     def __init__(self, n_atoms):
         super(UAGReader, self).__init__('UAGReader')
         self._auxs = {}
@@ -168,68 +185,68 @@ class UAGReader(mda.coordinates.base.ReaderBase):
         return self._read_next_frame
 
 
-class TestUAGCallCount(TestCase):
+class TestUAGCallCount(object):
     # make sure updates are only called when required!
     # 
     # these tests check that potentially expensive selection operations are only
     # done when necessary
-    def setUp(self):
-        self.u = u = make_Universe(('names',))
+    @pytest.fixture()
+    def u(self):
+        u = make_Universe(('names',))
         u.trajectory = UAGReader(125)
+        return u
 
-    def tearDown(self):
-        del self.u
-
-    @mock.patch.object(MDAnalysis.core.groups.UpdatingAtomGroup, 'update_selection',
-                       autospec=True, # required to make it get self when called
+    @mock.patch.object(MDAnalysis.core.groups.UpdatingAtomGroup,
+                       'update_selection',
+                       autospec=True,
+                       # required to make it get self when called
                        )
-    def test_updated_when_creating(self, mock_update_selection):
-        uag = self.u.select_atoms('name XYZ', updating=True)
+    def test_updated_when_creating(self, mock_update_selection, u):
+        uag = u.select_atoms('name XYZ', updating=True)
 
-        assert_(mock_update_selection.call_count == 1)
+        assert mock_update_selection.call_count == 1
 
-    def test_updated_when_next(self):
-        uag = self.u.select_atoms('name XYZ', updating=True)
+    def test_updated_when_next(self, u):
+        uag = u.select_atoms('name XYZ', updating=True)
 
         # Use mock.patch.object to start inspecting the uag update selection method
         # wraps= keyword makes it still function as normal, just we're spying on it now
         with mock.patch.object(uag, 'update_selection',
                                wraps=uag.update_selection) as mock_update:
-            self.u.trajectory.next()
-            assert_(mock_update.call_count == 0)
+            next(u.trajectory)
+            assert mock_update.call_count == 0
 
             # Access many attributes..
             pos = uag.positions
             names = uag.names
             # But check we only got updated once
-            assert_(mock_update.call_count == 1)
+            assert mock_update.call_count == 1
 
 
-class TestDynamicUAG(TestCase):
-    def setUp(self):
-        self.u = u = make_Universe(('names',))
+class TestDynamicUAG(object):
+    @pytest.fixture()
+    def u(self):
+        u = make_Universe(('names',))
         u.trajectory = UAGReader(125)
+        return u
 
-    def tearDown(self):
-        del self.u
+    def test_nested_uags(self, u):
+        bg = u.atoms[[3, 4]]
 
-    def test_nested_uags(self):
-        bg = self.u.atoms[[3, 4]]
+        uag1 = u.select_atoms('around 1.5 group bg', bg=bg, updating=True)
 
-        uag1 = self.u.select_atoms('around 1.5 group bg', bg=bg, updating=True)
+        uag2 = u.select_atoms('around 1.5 group uag', uag=uag1, updating=True)
 
-        uag2 = self.u.select_atoms('around 1.5 group uag', uag=uag1, updating=True)
-
-        for ts in self.u.trajectory:
+        for ts in u.trajectory:
             assert_equal(len(bg), 2)
             assert_equal(len(uag1), 2)  # around doesn't include bg, so 2
             assert_equal(len(uag2), 4)  # doesn't include uag1
 
-    def test_driveby(self):
-        uag = self.u.select_atoms('prop x < 5.5', updating=True)
+    def test_driveby(self, u):
+        uag = u.select_atoms('prop x < 5.5', updating=True)
 
         n_init = 6
-        for i, ts in enumerate(self.u.trajectory):
+        for i, ts in enumerate(u.trajectory):
             # should initially be 6 atoms with x < 5.5
             n_expected = max(n_init - i, 0)  # floors at 0
 
@@ -256,10 +273,11 @@ def test_representations():
     assert "selections 'bynum 1' + 'bynum 2'" in rep
     assert "another AtomGroup" in rep
 
+
 def test_empty_UAG():
     u = make_Universe()
 
     # technically possible to make a UAG without any selections..
     uag = mda.core.groups.UpdatingAtomGroup(u.atoms, (), '')
 
-    assert_(isinstance(uag, mda.core.groups.UpdatingAtomGroup))
+    assert isinstance(uag, mda.core.groups.UpdatingAtomGroup)
