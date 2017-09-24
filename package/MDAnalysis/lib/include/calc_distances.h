91577595cbd37e3dca5648b14575aefce16b4268
Max Linke <max_linke@gmx.de>
2017-09-24 23:22:33 +0200

    use explicit image search for triclinic cells

--- package/MDAnalysis/lib/include/calc_distances.h
+++ package/MDAnalysis/lib/include/calc_distances.h
@@ -2,6 +2,7 @@
 /* vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 */
 /*
   MDAnalysis --- http://mdanalysis.googlecode.com
+
   Copyright (c) 2006-2014 Naveen Michaud-Agrawal,
                 Elizabeth J. Denning, Oliver Beckstein,
                 and contributors (see AUTHORS for the full list)
@@ -42,43 +43,40 @@ static void minimum_image(double *x, float *box, float *inverse_box)
   }
 }
 
-static void minimum_image_triclinic(double *dx, coordinate* box, float* box_half)
+static void minimum_image_triclinic(double *dx, coordinate* box)
 {
   // Minimum image convention for triclinic systems, modelled after domain.cpp in LAMMPS
   // Assumes that there is a maximum separation of 1 box length (enforced in dist functions
   // by moving all particles to inside the box before calculating separations)
   // Requires a box
   // Assumes box having zero values for box[0][1], box[0][2] and box [1][2]
+  double dmin[3], rx[3], ry[3], rz[3];
+  double min = FLT_MAX;
 
-  // z
-  if (fabs(dx[2]) > box_half[2]) {
-    if (dx[2] < 0.0 ) {
-      dx[2] += box[2][2];
-      dx[1] += box[2][1];
-      dx[0] += box[2][0];
-    } else {
-      dx[2] -= box[2][2];
-      dx[1] -= box[2][1];
-      dx[0] -= box[2][0];
+  for (int x = -1; x < 2; ++x) {
+    rx[0] = dx[0] + box[0][0] * (float)x;
+    rx[1] = dx[1];
+    rx[2] = dx[2];
+    for (int y = -1; y < 2; ++y) {
+      ry[0] = rx[0] + box[1][0] * (float)y;
+      ry[1] = rx[1] + box[1][1] * (float)y;
+      ry[2] = rx[2];
+      for (int z = -1; z < 2; ++z) {
+        rz[0] = ry[0] + box[2][0] * (float)z;
+        rz[1] = ry[1] + box[2][1] * (float)z;
+        rz[2] = ry[2] + box[2][2] * (float)z;
+        const double d = rz[0]*rz[0] + rz[1]*rz[1] + rz[2] * rz[2];
+        if (d < min) {
+          for (i=0; i<3; ++i){
+            min = d;
+            dmin[i] = rz[i];
+          }
+        }
+      }
     }
   }
-  // y
-  if (fabs(dx[1]) > box_half[1]) {
-    if (dx[1] < 0.0) {
-      dx[1] += box[1][1];
-      dx[0] += box[1][0];
-    } else {
-      dx[1] -= box[1][1];
-      dx[0] -= box[1][0];
-    }
-  }
-  // x
-  if ( fabs(dx[0]) > box_half[0]) {
-    if (dx[0] < 0.0) {
-      dx[0] += box[0][0];
-    } else {
-      dx[0] -= box[0][0];
-    }
+  for (int i =0; i<3; ++i) {
+    dx[i] = dmin[i];
   }
 }
 
@@ -177,12 +175,8 @@ static void _calc_distance_array_triclinic(coordinate* ref, int numref,
 {
   int i, j;
   double dx[3];
-  float box_half[3], box_inverse[3];
+  float box_inverse[3];
   double rsq;
-
-  box_half[0] = 0.5 * box[0][0];
-  box_half[1] = 0.5 * box[1][1];
-  box_half[2] = 0.5 * box[2][2];
 
   box_inverse[0] = 1.0 / box[0][0];
   box_inverse[1] = 1.0 / box[1][1];
@@ -199,7 +193,7 @@ static void _calc_distance_array_triclinic(coordinate* ref, int numref,
       dx[0] = conf[j][0] - ref[i][0];
       dx[1] = conf[j][1] - ref[i][1];
       dx[2] = conf[j][2] - ref[i][2];
-      minimum_image_triclinic(dx, box, box_half);
+      minimum_image_triclinic(dx, box);
       rsq = (dx[0]*dx[0] + dx[1]*dx[1] + dx[2]*dx[2]);
       *(distances + i*numconf + j) = sqrt(rsq);
     }
@@ -273,11 +267,7 @@ static void _calc_self_distance_array_triclinic(coordinate* ref, int numref,
   int i, j, distpos;
   double dx[3];
   double rsq;
-  float box_half[3], box_inverse[3];
-
-  box_half[0] = 0.5 * box[0][0];
-  box_half[1] = 0.5 * box[1][1];
-  box_half[2] = 0.5 * box[2][2];
+  float box_inverse[3];
 
   box_inverse[0] = 1.0 / box[0][0];
   box_inverse[1] = 1.0 / box[1][1];
@@ -298,7 +288,7 @@ static void _calc_self_distance_array_triclinic(coordinate* ref, int numref,
       dx[0] = ref[j][0] - ref[i][0];
       dx[1] = ref[j][1] - ref[i][1];
       dx[2] = ref[j][2] - ref[i][2];
-      minimum_image_triclinic(dx, box, box_half);
+      minimum_image_triclinic(dx, box);
       rsq = (dx[0]*dx[0] + dx[1]*dx[1] + dx[2]*dx[2]);
       *(distances + distpos) = sqrt(rsq);
       distpos += 1;
@@ -381,12 +371,8 @@ static void _calc_bond_distance_triclinic(coordinate* atom1, coordinate* atom2,
 {
   int i;
   double dx[3];
-  float box_half[3], box_inverse[3];
+  float box_inverse[3];
   double rsq;
-
-  box_half[0] = 0.5 * box[0][0];
-  box_half[1] = 0.5 * box[1][1];
-  box_half[2] = 0.5 * box[2][2];
 
   box_inverse[0] = 1.0/box[0][0];
   box_inverse[1] = 1.0/box[1][1];
@@ -403,7 +389,7 @@ static void _calc_bond_distance_triclinic(coordinate* atom1, coordinate* atom2,
     dx[1] = atom1[i][1] - atom2[i][1];
     dx[2] = atom1[i][2] - atom2[i][2];
     // PBC time!
-    minimum_image_triclinic(dx, box, box_half);
+    minimum_image_triclinic(dx, box);
     rsq = (dx[0]*dx[0])+(dx[1]*dx[1])+(dx[2]*dx[2]);
     *(distances+i) = sqrt(rsq);
   }
@@ -491,11 +477,7 @@ static void _calc_angle_triclinic(coordinate* atom1, coordinate* atom2,
   int i;
   double rji[3], rjk[3];
   double x, y, xp[3];
-  float box_half[3], box_inverse[3];
-
-  box_half[0] = 0.5 * box[0][0];
-  box_half[1] = 0.5 * box[1][1];
-  box_half[2] = 0.5 * box[2][2];
+  float box_inverse[3];
 
   box_inverse[0] = 1.0/box[0][0];
   box_inverse[1] = 1.0/box[1][1];
@@ -512,12 +494,12 @@ static void _calc_angle_triclinic(coordinate* atom1, coordinate* atom2,
     rji[0] = atom1[i][0] - atom2[i][0];
     rji[1] = atom1[i][1] - atom2[i][1];
     rji[2] = atom1[i][2] - atom2[i][2];
-    minimum_image_triclinic(rji, box, box_half);
+    minimum_image_triclinic(rji, box);
 
     rjk[0] = atom3[i][0] - atom2[i][0];
     rjk[1] = atom3[i][1] - atom2[i][1];
     rjk[2] = atom3[i][2] - atom2[i][2];
-    minimum_image_triclinic(rjk, box, box_half);
+    minimum_image_triclinic(rjk, box);
 
     x = rji[0]*rjk[0] + rji[1]*rjk[1] + rji[2]*rjk[2];
 
@@ -639,11 +621,7 @@ static void _calc_dihedral_triclinic(coordinate* atom1, coordinate* atom2,
 {
   int i;
   double va[3], vb[3], vc[3];
-  float box_half[3], box_inverse[3];
-
-  box_half[0] = 0.5 * box[0][0];
-  box_half[1] = 0.5 * box[1][1];
-  box_half[2] = 0.5 * box[2][2];
+  float box_inverse[3];
 
   box_inverse[0] = 1.0/box[0][0];
   box_inverse[1] = 1.0/box[1][1];
@@ -662,17 +640,17 @@ static void _calc_dihedral_triclinic(coordinate* atom1, coordinate* atom2,
     va[0] = atom2[i][0] - atom1[i][0];
     va[1] = atom2[i][1] - atom1[i][1];
     va[2] = atom2[i][2] - atom1[i][2];
-    minimum_image_triclinic(va, box, box_half);
+    minimum_image_triclinic(va, box);
 
     vb[0] = atom3[i][0] - atom2[i][0];
     vb[1] = atom3[i][1] - atom2[i][1];
     vb[2] = atom3[i][2] - atom2[i][2];
-    minimum_image_triclinic(vb, box, box_half);
+    minimum_image_triclinic(vb, box);
 
     vc[0] = atom4[i][0] - atom3[i][0];
     vc[1] = atom4[i][1] - atom3[i][1];
     vc[2] = atom4[i][2] - atom3[i][2];
-    minimum_image_triclinic(vc, box, box_half);
+    minimum_image_triclinic(vc, box);
 
     _calc_dihedral_angle(va, vb, vc, angles + i);
   }
