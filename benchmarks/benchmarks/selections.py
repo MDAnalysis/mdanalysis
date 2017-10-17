b00f9c7ee0d9584c11d74a52404b27f760702173
Tyler Reddy <tyler.je.reddy@gmail.com>
2017-10-17 13:38:50 -0600

    TST: Added simple selection string asv benchmark code.

---
+++ benchmarks/benchmarks/selections.py
@@ -0,0 +1,33 @@
+from __future__ import division, absolute_import, print_function
+
+import MDAnalysis
+
+try:
+    from MDAnalysisTests.datafiles import GRO
+except:
+    pass
+
+class SimpleSelectionBench(object):
+    """Benchmarks for the various MDAnalysis
+    simple selection strings.
+    """
+    params = ('protein',
+              'backbone',
+              'nucleic',
+              'nucleicbackbone',
+              'resid 1:10',
+              'resnum 1:10',
+              'resname LYS',
+              'name CA'
+              'bynum 0:10')
+
+    param_names = ['selection_string']
+
+    def setup(self, selection_string):
+        self.u = MDAnalysis.Universe(GRO)
+
+    def time_simple_selections(self, selection_string):
+        """Benchmark simple selections on the protein-based
+        standard test GRO file.
+        """
+        self.u.select_atoms(selection_string)
