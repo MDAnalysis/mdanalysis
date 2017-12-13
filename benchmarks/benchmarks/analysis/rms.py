300c70665ae1109ad1ec1501583819daa41b394c
Tyler Reddy <tyler.je.reddy@gmail.com>
2017-12-13 15:32:37 -0700

    TST: Added rms.RMSD and rms.RMSF asv benchmark drafts.

--- benchmarks/benchmarks/analysis/rms.py
+++ benchmarks/benchmarks/analysis/rms.py
@@ -51,4 +51,58 @@ class SimpleRmsBench(object):
                  center=center,
                  superposition=superposition)
 
+class RmsdTrajBench(object):
+    """Benchmarks for MDAnalysis.analysis.rms.RMSD
+    """
 
+    # TODO: RMSD has many parameters / options,
+    # some of which are apparently still considered
+    # experimental -- we'll eventually want to
+    # benchmark more of these
+
+    params = (['all', 'backbone'],
+              [None, 'mass'])
+
+    param_names = ['select',
+                   'weights']
+
+    def setup(self, select, weights):
+        self.u = MDAnalysis.Universe(PSF, DCD)
+        self.RMSD_inst = rms.RMSD(atomgroup=self.u,
+                                  reference=None,
+                                  select=select,
+                                  weights=weights)
+
+    def time_RMSD(self, select, weights):
+        """Benchmark RMSD.run() method, which parses
+        over the entire trajectory.
+        """
+        self.RMSD_inst.run()
+
+
+class RmsfTrajBench(object):
+    """Benchmarks for MDAnalysis.analysis.rms.RMSF
+    """
+
+    params = ([100,500,2000],
+              [None, 3],
+              [None,'mass'])
+
+    param_names = ['n_atoms',
+                   'step',
+                   'weights']
+
+    def setup(self, n_atoms, step, weights):
+        self.u = MDAnalysis.Universe(PSF, DCD)
+        self.ag = self.u.atoms[:n_atoms]
+        self.RMSF_inst = rms.RMSF(atomgroup=self.ag,
+                                  start=None,
+                                  stop=None,
+                                  step=step,
+                                  weights=weights)
+
+    def time_RMSF(self, n_atoms, step, weights):
+        """Benchmark RMSF.run() method, which parses
+        over the entire trajectory.
+        """
+        self.RMSF_inst.run()
