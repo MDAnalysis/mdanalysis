bbe9fe540f8ca663bbb06e8122f6b6a5dc1d6d4c
ayushsuhane <ayush.suhane@gmail.com>
2018-02-28 12:12:48 -0800

    Changed the format of due.py to resolve the error in test

--- package/MDAnalysis/due.py
+++ package/MDAnalysis/due.py
@@ -59,7 +59,7 @@ except Exception as e:
     if type(e).__name__ != 'ImportError':
         import logging
         logging.getLogger("duecredit").error(
-            "Failed to import duecredit due to %s" % str(e))
+            "Failed to import duecredit due to %s",str(e))
     # Initiate due stub
     due = InactiveDueCreditCollector()
     BibTeX = Doi = Url = _donothing_func
