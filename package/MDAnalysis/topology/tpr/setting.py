b41fa2f4f0e6f3fdc1fb45973add4f747c40b54a
Jonathan Barnoud <jonathan@barnoud.net>
2018-04-09 15:08:01 +0200

    Bump TPX version

--- package/MDAnalysis/topology/tpr/setting.py
+++ package/MDAnalysis/topology/tpr/setting.py
@@ -40,7 +40,7 @@ from __future__ import absolute_import
 from six.moves import range
 
 #: Gromacs TPR file format versions that can be read by the TPRParser.
-SUPPORTED_VERSIONS = (58, 73, 83, 100, 103, 110)
+SUPPORTED_VERSIONS = (58, 73, 83, 100, 103, 110, 112)
 
 # Some constants
 STRLEN = 4096
