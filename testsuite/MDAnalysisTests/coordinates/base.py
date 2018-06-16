MDAnalysis/mdanalysis

commit 8572d796a95e92bbdaa827325a1ae82e1664dcba

BLD / MAINT: Improvements related to Windows compatibility
* adjustments to the build process, code base, and test suite
that improve Windows compatibility; many of these are code
modernizations (i.e., broader adoption of pytest) that serve
the dual purpose of improving compatibility

package/MDAnalysis/lib/formats/src/xdrfile.c
@@ -45,7 +45,14 @@
 #endif
 
 #include <sys/types.h>
-#include <unistd.h>
+
+#ifdef __unix__
+    #include <unistd.h>
+#endif
+
+#ifdef _WIN32
+    #include <io.h>
+#endif
 
 #ifdef HAVE_RPC_XDR_H
 #  include <rpc/rpc.h>
@@ -2598,7 +2605,13 @@ xdrstdio_putbytes (XDR *xdrs, char *addr, unsigned int len)
 static off_t
 xdrstdio_getpos (XDR *xdrs)
 {
+    #ifdef __unix__
     return ftello((FILE *) xdrs->x_private);
+    #endif
+
+    #ifdef _WIN32
+    return _ftelli64((FILE *) xdrs->x_private);
+    #endif
 }
 
 static int
@@ -2610,7 +2623,13 @@ xdrstdio_setpos (XDR *xdrs, off_t pos, int whence)
     /* We return errno relying on the fact that it is never set to 0 on
      * success, which means that if an error occurrs it'll never be the same
      * as exdrOK, and xdr_seek won't be confused.*/
+    #ifdef __unix__
 	return fseeko((FILE *) xdrs->x_private, pos, whence) < 0 ? errno : exdrOK;
+    #endif
+
+    #ifdef _WIN32
+	return _fseeki64((FILE *) xdrs->x_private, pos, whence) < 0 ? errno : exdrOK;
+    #endif
 }
 
 

package/MDAnalysis/analysis/encore/clustering/include/ap.h
@@ -23,8 +23,10 @@ int trmIndex(int, int);
 
 int sqmIndex(int, int, int);
 
-float min(float*, int);
+#ifndef _WIN32
+    float min(float*, int);
+    float max(float*, int);
+#endif
 
-float max(float*, int);
 
 int CAffinityPropagation(float*, int, float, int, int, int, long*);

package/MDAnalysis/analysis/encore/clustering/src/ap.c
@@ -33,6 +33,7 @@ inline int sqmIndex(int colsn, int row, int col) { // array index for square mat
 	return row*colsn + col;
 }
 
+#ifndef _WIN32
 float min(float * values, int length) { //array min
 	float min = values[0];
 	for (int i=1;i<length;i++) {
@@ -52,6 +53,7 @@ float max(float * values, int length) { //array max
 	}
 	return max;
 }
+#endif
 
 void printarray(float* array, int lenarray) { //print an array, for debug purposes
 	for (int i=0;i<lenarray;i++) {

package/MDAnalysis/analysis/encore/dimensionality_reduction/src/spe.c
@@ -26,7 +26,14 @@
 #include <math.h>
 #include <time.h>
 #include <sys/types.h>
-#include <unistd.h>
+
+#ifdef __unix__
+    #include <unistd.h>
+#endif
+
+#ifdef _WIN32
+    #include <io.h>
+#endif
 
 #define EPSILON 1e-8
 

package/MDAnalysis/coordinates/PDB.py
@@ -275,16 +275,27 @@ class PDBReader(base.ReaderBase):
         pdbfile = self._pdbfile = util.anyopen(filename)
 
         line = "magical"
+        # we handle windows file striding uniquely
+        # because file.tell() is not reliable on Windows
+        # for a text file and binary mode is not compatible
+        # with i.e., our use of anyopen for zip files
+        windows_overbyte = 0
         while line:
             # need to use readline so tell gives end of line
             # (rather than end of current chunk)
             line = pdbfile.readline()
+            # the tell() text opaque number striding is not portable
+            # to windows so compensate accordingly
+            if os.name == 'nt':
+                # for a text file mode these aren't necessarily
+                # bytes; var name for simplicity only
+                windows_overbyte += 1
 
             if line.startswith('MODEL'):
-                models.append(pdbfile.tell())
+                models.append(pdbfile.tell() - windows_overbyte)
             elif line.startswith('CRYST1'):
                 # remove size of line to get **start** of CRYST line
-                crysts.append(pdbfile.tell() - len(line))
+                crysts.append(pdbfile.tell() - len(line) - windows_overbyte)
             elif line.startswith('HEADER'):
                 # classification = line[10:50]
                 # date = line[50:59]

package/MDAnalysis/core/universe.py
@@ -101,7 +101,10 @@ import sys
 # A clean solution is therefore simply to disable os.fork() prior to importing
 # the uuid module and to re-enable it afterwards.
 import os
-if sys.version_info >= (3, 7):
+
+# Windows doesn't have os.fork so can ignore
+# the issue for that platform
+if sys.version_info >= (3, 7) or os.name == 'nt':
     import uuid
 else:
     _os_dot_fork, os.fork = os.fork, None

package/MDAnalysis/lib/c_distances.pyx
@@ -44,7 +44,7 @@ cdef extern from "calc_distances.h":
     void _calc_self_distance_array(coordinate* ref, int numref, double* distances, int distnum)
     void _calc_self_distance_array_ortho(coordinate* ref, int numref, float* box, double* distances, int distnum)
     void _calc_self_distance_array_triclinic(coordinate* ref, int numref, coordinate* box, double* distances, int distnum)
-    void _coord_transform(coordinate* coords, int numCoords, coordinate* box)
+    void _coord_transform(float* coords, int numCoords, float* box)
     void _calc_bond_distance(coordinate* atom1, coordinate* atom2, int numatom, double* distances)
     void _calc_bond_distance_ortho(coordinate* atom1, coordinate* atom2, int numatom, float*box, double* distances)
     void _calc_bond_distance_triclinic(coordinate* atom1, coordinate* atom2, int numatom, coordinate* box, double* distances)
@@ -125,13 +125,15 @@ def calc_self_distance_array_triclinic(numpy.ndarray ref,
                                         <coordinate*>box.data,
                                         <double*>result.data, distnum)
 
-def coord_transform(numpy.ndarray coords,
-                    numpy.ndarray box):
-    cdef int numcoords
+def coord_transform(float[:,:] coords,
+                    float[:,:] box):
+    cdef int numcoords, size
     numcoords = coords.shape[0]
+    size = coords.size
 
-    _coord_transform(<coordinate*> coords.data, numcoords,
-                     <coordinate*> box.data)
+    if size >  0:
+        _coord_transform( &coords[0,0], numcoords,
+                          &box[0,0])
 
 def calc_bond_distance(numpy.ndarray coords1,
                        numpy.ndarray coords2,

package/MDAnalysis/lib/formats/include/fastio.h
@@ -90,7 +90,7 @@ static int fio_open(const char *filename, int mode, fio_fd *fd) {
   access = FILE_ALL_ACCESS; /* XXX hack if above modes fail */
 #endif
 
-  sharing = 0;       /* disallow sharing with other processes  */
+  sharing = 1;       /* allow sharing with other processes  */
   security = NULL;   /* child processes don't inherit anything */
 
   /* since we never append, blow away anything that's already there */

commit 69a28e31fb222820764b1158cbb9c4170d73443e

fix #1942
Commit 8572d796a95e92bbdaa827325a1ae82e1664dcba broke XTC/TRR reading on macOS.
This fixes this issue. Only the development version 0.18.1-dev was affected;
the released 0.18.0 did NOT have this bug.

package/MDAnalysis/lib/formats/src/xdrfile.c
@@ -46,13 +46,12 @@
 
 #include <sys/types.h>
 
-#ifdef __unix__
-    #include <unistd.h>
+#ifdef _WIN32
+#  include <io.h>
+#else /* __unix__ */
+#  include <unistd.h>
 #endif
 
-#ifdef _WIN32
-    #include <io.h>
-#endif
 
 #ifdef HAVE_RPC_XDR_H
 #  include <rpc/rpc.h>
@@ -2605,12 +2604,10 @@ xdrstdio_putbytes (XDR *xdrs, char *addr, unsigned int len)
 static off_t
 xdrstdio_getpos (XDR *xdrs)
 {
-    #ifdef __unix__
-    return ftello((FILE *) xdrs->x_private);
-    #endif
-
     #ifdef _WIN32
     return _ftelli64((FILE *) xdrs->x_private);
+    #else /* __unix__ */
+    return ftello((FILE *) xdrs->x_private);
     #endif
 }
 
@@ -2623,13 +2620,13 @@ xdrstdio_setpos (XDR *xdrs, off_t pos, int whence)
     /* We return errno relying on the fact that it is never set to 0 on
      * success, which means that if an error occurrs it'll never be the same
      * as exdrOK, and xdr_seek won't be confused.*/
-    #ifdef __unix__
+    #ifdef _WIN32
+	return _fseeki64((FILE *) xdrs->x_private, pos, whence) < 0 ? errno : exdrOK;
+    #else /*  __unix__ */
 	return fseeko((FILE *) xdrs->x_private, pos, whence) < 0 ? errno : exdrOK;
     #endif
 
-    #ifdef _WIN32
-	return _fseeki64((FILE *) xdrs->x_private, pos, whence) < 0 ? errno : exdrOK;
-    #endif
+
 }
 
 

testsuite/MDAnalysisTests/coordinates/base.py
@@ -209,14 +209,14 @@ class BaseReaderTest(object):
         reader.add_auxiliary('lowf', ref.aux_lowf, dt=ref.aux_lowf_dt, initial_time=0, time_selector=None)
         reader.add_auxiliary('highf', ref.aux_highf, dt=ref.aux_highf_dt, initial_time=0, time_selector=None)
         return reader
-    
+
     @staticmethod
     @pytest.fixture()
     def transformed(ref):
         transformed = ref.reader(ref.trajectory)
         transformed.add_transformations(translate([1,1,1]), translate([0,0,0.33]))
         return transformed
-    
+
     def test_n_atoms(self, ref, reader):
         assert_equal(reader.n_atoms, ref.n_atoms)
 
@@ -346,15 +346,15 @@ class BaseReaderTest(object):
         for ts in reader[:-1]:
             pass
         assert_equal(reader.frame, 0)
-        
+
     def test_transformations_iter(self, ref, transformed):
         # test one iteration and see if the transformations are applied
         v1 = np.float32((1,1,1))
-        v2 = np.float32((0,0,0.33)) 
+        v2 = np.float32((0,0,0.33))
         for i, ts in enumerate(transformed):
             idealcoords = ref.iter_ts(i).positions + v1 + v2
             assert_array_almost_equal(ts.positions, idealcoords, decimal=ref.prec)
-    
+
     def test_transformations_2iter(self, ref, transformed):
         # Are the transformations applied and
         # are the coordinates "overtransformed"?
@@ -364,10 +364,10 @@ class BaseReaderTest(object):
         for i, ts in enumerate(transformed):
             idealcoords.append(ref.iter_ts(i).positions + v1 + v2)
             assert_array_almost_equal(ts.positions, idealcoords[i], decimal=ref.prec)
-            
+
         for i, ts in enumerate(transformed):
             assert_almost_equal(ts.positions, idealcoords[i], decimal=ref.prec)
-            
+
     def test_transformations_slice(self, ref, transformed):
         # Are the transformations applied when iterating over a slice of the trajectory?
         v1 = np.float32((1,1,1))
@@ -375,8 +375,8 @@ class BaseReaderTest(object):
         for i,ts in enumerate(transformed[2:3:1]):
             idealcoords = ref.iter_ts(ts.frame).positions + v1 + v2
             assert_array_almost_equal(ts.positions, idealcoords, decimal = ref.prec)
-            
-        
+
+
     def test_transformations_switch_frame(self, ref, transformed):
         # This test checks if the transformations are applied and if the coordinates
         # "overtransformed" on different situations
@@ -388,10 +388,10 @@ class BaseReaderTest(object):
             assert_array_almost_equal(transformed[0].positions, first_ideal, decimal = ref.prec)
             second_ideal = ref.iter_ts(1).positions + v1 + v2
             assert_array_almost_equal(transformed[1].positions, second_ideal, decimal = ref.prec)
-            
+
             # What if we comeback to the previous frame?
             assert_array_almost_equal(transformed[0].positions, first_ideal, decimal = ref.prec)
-            
+
             # How about we switch the frame to itself?
             assert_array_almost_equal(transformed[0].positions, first_ideal, decimal = ref.prec)
         else:
@@ -405,20 +405,20 @@ class BaseReaderTest(object):
         ideal_coords = ref.iter_ts(0).positions + v1 + v2
         transformed.rewind()
         assert_array_almost_equal(transformed[0].positions, ideal_coords, decimal = ref.prec)
-    
+
     def test_transformations_copy(self,ref,transformed):
         # this test checks if transformations are carried over a copy and if the
         # coordinates of the copy are equal to the original's
         v1 = np.float32((1,1,1))
         v2 = np.float32((0,0,0.33))
         new = transformed.copy()
-        assert_equal(transformed.transformations, new.transformations, 
+        assert_equal(transformed.transformations, new.transformations,
                      "transformations are not equal")
         for i, ts in enumerate(new):
             ideal_coords = ref.iter_ts(i).positions + v1 + v2
             assert_array_almost_equal(ts.positions, ideal_coords, decimal = ref.prec)
-            
-        
+
+
     def test_add_another_transformations_raises_ValueError(self, transformed):
         # After defining the transformations, the workflow cannot be changed
         with pytest.raises(ValueError):
@@ -438,6 +438,15 @@ class MultiframeReaderTest(BaseReaderTest):
         ts = reader[ref.jump_to_frame.frame]
         assert_timestep_almost_equal(ts, ref.jump_to_frame,
                                      decimal=ref.prec)
+
+    def test_frame_jump_issue1942(self, ref, reader):
+        """Test for issue 1942 (especially XDR on macOS)"""
+        reader.rewind()
+        try:
+            for ii in range(ref.n_frames + 2):
+                reader[0]
+        except StopIteration:
+            pytest.fail("Frame-seeking wrongly iterated (#1942)")
 
     def test_next_gives_second_frame(self, ref, reader):
         reader = ref.reader(ref.trajectory)
@@ -623,14 +632,14 @@ class BaseTimestepTest(object):
     unitcell = np.array([10., 11., 12., 90., 90., 90.])
     ref_volume = 1320.  # what the volume is after setting newbox
     uni_args = None
-        
+
     @pytest.fixture()
     def ts(self):
         ts = self.Timestep(self.size)
         ts.frame += 1
         ts.positions = self.refpos
         return ts
-        
+
     def test_getitem(self, ts):
         assert_equal(ts[1], self.refpos[1])
 
@@ -978,13 +987,13 @@ class BaseTimestepTest(object):
 
     def _check_npint_slice(self, name, ts):
         for integers in [np.byte, np.short, np.intc, np.int_, np.longlong,
-                         np.intp, np.int8, np.int16, np.int32, np.int64, 
+                         np.intp, np.int8, np.int16, np.int32, np.int64,
                          np.ubyte, np.ushort, np.uintc, np.ulonglong,
                          np.uintp, np.uint8, np.uint16, np.uint32, np.uint64]:
             sl = slice(1, 2, 1)
             ts2 = ts.copy_slice(slice(integers(1), integers(2), integers(1)))
             self._check_slice(ts, ts2, sl)
-        
+
     def _check_slice(self, ts1, ts2, sl):
         if ts1.has_positions:
             assert_array_almost_equal(ts1.positions[sl], ts2.positions)