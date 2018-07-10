fixed calculation errors and added plot function

package/MDAnalysis/analysis/dihedrals.py
@@ -12,6 +12,8 @@ from MDAnalysis.lib.log import ProgressMeter, _set_verbose
 from MDAnalysis.lib.util import asiterable, iterable, get_weights
 
 
+import numpy as np
+
 def dihedral_calc(atomgroups):
     """Calculates phi and psi angles for a list of AtomGroups over trajectory.
 
@@ -28,7 +30,7 @@ def dihedral_calc(atomgroups):
     """
 
     dihedrals = [atomgroup.dihedral for atomgroup in atomgroups]
-    angles = np.array([dih.value() for dih in dihedrals])
+    angles = [dih.value() for dih in dihedrals]
 
     return angles
 
@@ -54,7 +56,7 @@ class Ramachandran(AnalysisBase):
             super(Ramachandran, self).__init__(self.atomgroup.universe.trajectory,
                                        start=start, stop=stop, step=step,
                                        verbose=verbose)
-        return super(RMSF, self).run()
+        return super(Ramachandran, self).run()
 
     def _prepare(self):
         self.protein = self.atomgroup.universe.atoms.select_atoms("protein")
@@ -62,13 +64,28 @@ class Ramachandran(AnalysisBase):
         self.phi_atoms = [self.protein.residues[resid-1].phi_selection()
                           for resid in self.resids
                           if 1 < resid < len(self.protein.residues)]
-        self.psi_atoms = [self.protein.rseidues[resid-1].psi_selection()
+        self.psi_atoms = [self.protein.residues[resid-1].psi_selection()
                           for resid in self.resids
                           if 1 < resid < len(self.protein.residues)]
+        self.angles = []
 
     def _single_frame(self):
-        self.phi_angles = [dihedral_calc(atoms) for atoms in self.phi_atoms]
-        self.psi_angles = [dihedral_calc(atoms) for atoms in self.psi_atoms]
+        self.phi_angles = dihedral_calc(self.phi_atoms)
+        self.psi_angles = dihedral_calc(self.psi_atoms)
+        self.angles.append((self.phi_angles,self.psi_angles))
 
     def _conclude(self):
-        self.angles = np.array(self.phi_angles,self.psi_angles)
+        self.angles = np.array(self.angles)
+
+    def plot(self):
+        fig = plt.figure(figsize=(10,10))
+        ax1 = plt.subplot(111)
+        ax1.axis([-180,180,-180,180])
+        ax1.axhline(0, color='k', lw=1)
+        ax1.axvline(0, color='k', lw=1)
+        plt.xticks(np.arange(-180,181,60))
+        plt.yticks(np.arange(-180,181,60))
+        plt.xlabel(r"$\phi$ (deg)")
+        plt.ylabel(r"$\psi$ (deg)")
+        for angles in self.angles:
+            ax1.plot(angles[0],angles[1],'ks')