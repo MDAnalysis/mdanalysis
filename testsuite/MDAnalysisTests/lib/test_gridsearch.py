089eb09e815d3f240c43f1709dc3eeac9c936a7c
Sébastien Buchoux <sebastien.buchoux@gmail.com>
2018-07-16 11:45:11 +0200

    Unneeded code removed from c_gridsearch.pyx

--- testsuite/MDAnalysisTests/lib/test_gridsearch.py
+++ testsuite/MDAnalysisTests/lib/test_gridsearch.py
@@ -27,7 +27,7 @@ from numpy.testing import assert_equal, assert_allclose
 import numpy as np
 
 import MDAnalysis as mda
-from MDAnalysis.lib import grid
+from MDAnalysis.lib import nsgrid
 from MDAnalysis.lib.pkdtree import PeriodicKDTree
 
 from MDAnalysisTests.datafiles import GRO
@@ -39,102 +39,85 @@ def universe():
     return u
 
 
-@pytest.fixture
-def grid_results():
-    u = mda.Universe(GRO)
-    cutoff = 2
-    ref_pos = u.atoms.positions[13937]
-    return run_grid_search(u, ref_pos, cutoff)
-
-
-def run_grid_search(u, ref_pos, cutoff):
+def run_grid_search(u, ids, cutoff=3):
     coords = u.atoms.positions
 
     # Run grid search
-    searcher = grid.FastNS(u)
-    searcher.set_cutoff(cutoff)
-    searcher.set_coords(coords)
-    searcher.prepare()
+    searcher = grid.FastNS(u, cutoff, coords, debug=True)
 
-    return searcher.search(ref_pos)
-
-
-def run_search(universe, ref_id):
-    cutoff = 3
-    coords = universe.atoms.positions
-    ref_pos = coords[ref_id]
-
-    # Run pkdtree search
-    pkdt = PeriodicKDTree(universe.atoms.dimensions, bucket_size=10)
-    pkdt.set_coords(coords)
-    pkdt.search(ref_pos, cutoff)
-
-    results_pkdtree = pkdt.get_indices()
-    results_pkdtree.remove(ref_id)
-    results_pkdtree = np.array(results_pkdtree)
-    results_pkdtree.sort()
-
-    # Run grid search
-    results_grid = run_grid_search(universe, ref_pos, cutoff)
-    results_grid = results_grid.get_indices()[0]
-    results_grid.sort()
-
-    return results_pkdtree, results_grid
+    return searcher.search(ids, debug=False)
 
 
 def test_gridsearch(universe):
     """Check that pkdtree and grid search return the same results (No PBC needed)"""
 
     ref_id = 0
-    results_pkdtree, results_grid = run_search(universe, ref_id)
-    assert_equal(results_pkdtree, results_grid)
+    cutoff = 3
+    results = np.array([2, 3, 4, 5, 6, 7, 8, 9, 18, 19, 1211, 10862, 10865, 17582, 17585, 38342,
+                        38345]) - 1  # Atomid are from gmx select so there start from 1 and not 0. hence -1!
+
+    results_grid = run_grid_search(universe, ref_id, cutoff).get_indices()[0]
+
+    assert_equal(results, results_grid)
 
 
 def test_gridsearch_PBC(universe):
-    """Check that pkdtree and grid search return the same results (PBC needed)"""
+    """Check that pkdtree and grid search return the same results (No PBC needed)"""
 
     ref_id = 13937
-    results_pkdtree, results_grid = run_search(universe, ref_id)
-    assert_equal(results_pkdtree, results_grid)
+    results = np.array([4398, 4401, 13939, 13940, 13941, 17987, 23518, 23519, 23521, 23734,
+                        47451]) - 1  # Atomid are from gmx select so there start from 1 and not 0. hence -1!
+
+    results_grid = run_grid_search(universe, ref_id).get_indices()[0]
+
+    assert_equal(results, results_grid)
 
 
-def test_gridsearch_arraycoord(universe):
-    """Check the NS routine accepts a single bead coordinate as well as array of coordinates"""
-    cutoff = 2
-    ref_pos = universe.atoms.positions[:5]
-
-    results = [
-        np.array([2, 1, 4, 3]),
-        np.array([2, 0, 3]),
-        np.array([0, 1, 3]),
-        np.array([    2,     0,     1, 38341]),
-        np.array([ 6,  0,  5, 17])
-    ]
-
-    results_grid = run_grid_search(universe, ref_pos, cutoff).get_indices()
-
-    assert_equal(results_grid, results)
-
-
-def test_gridsearch_search_coordinates(grid_results):
-    """Check the NS routine can return coordinates instead of ids"""
-
-    results = np.array(
-        [
-            [40.32, 34.25, 55.9],
-            [0.61, 76.33, -0.56],
-            [0.48999998, 75.9, 0.19999999],
-            [-0.11, 76.19, 0.77]
-        ])
-
-    assert_allclose(grid_results.get_coordinates()[0], results)
-
-
-def test_gridsearch_search_distances(grid_results):
-    """Check the NS routine can return PBC distances from neighbors"""
-    results = np.array([0.096, 0.096, 0.015, 0.179]) * 10  # These distances were obtained using gmx distance
-    results.sort()
-
-    rounded_results = np.round(grid_results.get_distances()[0], 2)
-
-    assert_allclose(sorted(rounded_results), results)
+# def test_gridsearch_PBC(universe):
+#     """Check that pkdtree and grid search return the same results (PBC needed)"""
+#
+#     ref_id = 13937
+#     results_pkdtree, results_grid = run_search(universe, ref_id)
+#     assert_equal(results_pkdtree, results_grid)
+#
+#
+# def test_gridsearch_arraycoord(universe):
+#     """Check the NS routine accepts a single bead coordinate as well as array of coordinates"""
+#     cutoff = 2
+#     ref_pos = universe.atoms.positions[:5]
+#
+#     results = [
+#         np.array([2, 1, 4, 3]),
+#         np.array([2, 0, 3]),
+#         np.array([0, 1, 3]),
+#         np.array([    2,     0,     1, 38341]),
+#         np.array([ 6,  0,  5, 17])
+#     ]
+#
+#     results_grid = run_grid_search(universe, ref_pos, cutoff).get_indices()
+#
+#     assert_equal(results_grid, results)
+#
+#
+# def test_gridsearch_search_coordinates(grid_results):
+#     """Check the NS routine can return coordinates instead of ids"""
+#
+#     results = np.array(
+#         [
+#             [40.32, 34.25, 55.9],
+#             [0.61, 76.33, -0.56],
+#             [0.48999998, 75.9, 0.19999999],
+#             [-0.11, 76.19, 0.77]
+#         ])
+#
+#     assert_allclose(grid_results.get_coordinates()[0], results)
+#
+#
+# def test_gridsearch_search_distances(grid_results):
+#     """Check the NS routine can return PBC distances from neighbors"""
+#     results = np.array([0.096, 0.096, 0.015, 0.179]) * 10  # These distances were obtained using gmx distance
+#     results.sort()
+#
+#     rounded_results = np.round(grid_results.get_distances()[0], 2)
+#
+#     assert_allclose(sorted(rounded_results), results)
