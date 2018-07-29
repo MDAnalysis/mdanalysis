af62d15073fe71dad576ecdcfe81d415e42dfef1
Micaela Matta <micaela.matta@northwestern.edu>
2018-07-29 11:42:47 -0500

    added tinker writer

--- package/MDAnalysis/coordinates/TXYZ.py
+++ package/MDAnalysis/coordinates/TXYZ.py
@@ -84,20 +84,7 @@ class TXYZWriter(base.WriterBase):
             filename of trajectory file. If it ends with "gz" then the file
             will be gzip-compressed; if it ends with "bz2" it will be bzip2
             compressed.
-        n_atoms: int (optional)
-            Number of atoms in trajectory. By default assume that this is None
-            and that this file is used to store several different models
-            instead of a single trajectory. If a number is provided each
-            written TimeStep has to contain the same number of atoms.
-        atoms: str | list (optional)
-            Provide atom names: This can be a list of names or an
-            :class:`AtomGroup`.  If none is provided, atoms will
-            be called 'X' in the output. These atom names will be
-            used when a trajectory is written from raw
-            :class:`Timestep` objects which do not contain atom
-            information. If you write a :class:`AtomGroup` with
-            :meth:`XYZWriter.write` then atom information is taken
-            at each step and *atoms* is ignored.
+
         remark: str (optional)
             text following n_atoms ("molecule name"). By default writes MDAnalysis
             version
@@ -108,30 +95,12 @@ class TXYZWriter(base.WriterBase):
             self.convert_units = convert_units
         else:
             self.convert_units = flags['convert_lengths']
-        self.atomnames = self._get_atomnames(atoms)
+
         default_remark = "Written by {0} (release {1})".format(
             self.__class__.__name__, __version__)
         self.remark = default_remark if remark is None else remark
         # can also be gz, bz2
         self._txyz = util.anyopen(self.filename, 'wt')
-
-    def _get_atomnames(self, atoms):
-        """Return a list of atom names"""
-        # Default case
-        if atoms is None:
-            return itertools.cycle(('X',))
-        # Single atom name provided
-        elif isinstance(atoms, six.string_types):
-            return itertools.cycle((atoms,))
-        # List of atom names providded
-        elif isinstance(atoms, list):
-            return atoms
-        # AtomGroup or Universe, grab the names else default
-        # (AtomGroup.atoms just returns AtomGroup)
-        try:
-            return atoms.atoms.names
-        except (AttributeError, NoDataError):
-            return itertools.cycle(('X',))
 
     def close(self):
         """Close the trajectory file and finalize the writing"""
@@ -153,58 +122,26 @@ class TXYZWriter(base.WriterBase):
             The :class:`~MDAnalysis.core.groups.AtomGroup` or
             :class:`~MDAnalysis.core.universe.Universe` to write.
         """
-        # prepare the Timestep and extract atom names if possible
-        # (The way it is written it should be possible to write
-        # trajectories with frames that differ in atom numbers
-        # but this is not tested.)
-        try:
-            atoms = obj.atoms
-        except AttributeError:
-            if isinstance(obj, base.Timestep):
-                ts = obj
-            else:
-                raise TypeError("No Timestep found in obj argument")
+        # information needed:
+        # timestep if writing a trajectory
+        # atom numbers, names, positions, types, connectivity
+
+        atoms = obj.atoms
+        if not hasattr(atoms, 'bonds'):
+            raise NoDataError('TXYZWriter requires bond information! '
+                              'provide a topology file or use guess_bonds '
+                              'before writing the current trajectory in '
+                              'Tinker format.'
+
+        if self.n_atoms is None:
+            self.n_atoms = atoms.n_atoms
         else:
-            if hasattr(obj, 'universe'):
-                # For AtomGroup and children (Residue, ResidueGroup, Segment)
-                ts_full = obj.universe.trajectory.ts
-                if ts_full.n_atoms == atoms.n_atoms:
-                    ts = ts_full
-                else:
-                    # Only populate a time step with the selected atoms.
-                    ts = ts_full.copy_slice(atoms.indices)
-            elif hasattr(obj, 'trajectory'):
-                # For Universe only --- get everything
-                ts = obj.trajectory.ts
-            # update atom names
-            self.atomnames = self._get_atomnames(atoms)
-
-        self.write_next_timestep(ts)
-
-    def write_next_timestep(self, ts=None):
-        """Write coordinate information in *ts* to the trajectory"""
-        if ts is None:
-            if not hasattr(self, 'ts'):
-                raise NoDataError('TXYZWriter: no coordinate data to write to '
-                                  'trajectory file')
-            else:
-                ts = self.ts
-
-        if self.n_atoms is not None:
-            if self.n_atoms != ts.n_atoms:
+            if self.n_atoms != atoms.n_atoms:
                 raise ValueError('n_atoms keyword was specified indicating '
                                  'that this should be a trajectory of the '
                                  'same model. But the provided TimeStep has a '
                                  'different number ({}) then expected ({})'
                                  ''.format(ts.n_atoms, self.n_atoms))
-        else:
-            if (not isinstance(self.atomnames, itertools.cycle) and
-                len(self.atomnames) != ts.n_atoms):
-                logger.info('Trying to write a TimeStep with unkown atoms. '
-                            'Expected {}, got {}. Try using "write" if you are '
-                            'using "write_next_timestep" directly'.format(
-                                len(self.atomnames), ts.n_atoms))
-                self.atomnames = np.array([self.atomnames[0]] * ts.n_atoms)
 
         if self.convert_units:
             coordinates = self.convert_pos_to_native(
@@ -212,12 +149,17 @@ class TXYZWriter(base.WriterBase):
         else:
             coordinates = ts.positions
 
-        self._xyz.write("{0:d} frame {1}\n".format(ts.n_atoms, ts.frame))
-        self._xyz.write("{0:10.5f} {1:10.5f} {2:10.5f} {3:10.5f}
-                         {4:10.5f} {5:10.5f} {6:10.5f}\n".format(ts.dimensions))
-        for atom, (x, y, z), typ  in zip(self.atomnames, coordinates, self.atomtypes):
-            self._xyz.write("{0!s:>8}  {1:10.5f} {2:10.5f} {3:10.5f} {4}"
-                            "".format(atom, x, y, z, typ))
+        self._txyz.write("{0:d} frame {1} {2}\n".format(
+                                             n_atoms, ts.frame, default_remark))
+        self._txyz.write("{0:10.5f} {1:10.5f} {2:10.5f} "
+                         "{3:10.5f} {4:10.5f} {5:10.5f} \n".format(ts.dimensions))
+
+
+        for atom, (x, y, z)  in zip(self.atoms, coordinates):
+            self._txyz.write("{0} {1!s:>8} {2:10.5f} {3:10.5f} {4:10.5f} {5}"
+                .format(atom.ix+1, atom.name, x, y, z, atom.type)
+                b = " ".join(str(item) for item in (atom.bonded_atoms).indices + 1))
+
 
 class TXYZReader(base.ReaderBase):
     """Reads from a TXYZ file"""
