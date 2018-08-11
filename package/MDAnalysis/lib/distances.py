54c0700a6c0bc905ff93b61a9d427423b0294ac1
ayush <ayush.suhane@gmail.com>
2018-08-11 01:27:11 -0700

    changed determine_method

--- package/MDAnalysis/lib/distances.py
+++ package/MDAnalysis/lib/distances.py
@@ -541,7 +541,7 @@ def _determine_method(reference, configuration, max_cutoff, min_cutoff=None,
 
     if len(reference) < 10 or len(configuration) < 10:
         return methods['bruteforce']
-    elif len(reference)*len(configuration) > 1e7:
+    elif len(reference)*len(configuration) > 1e8:
         # CAUTION : for large datasets, shouldnt go into 'bruteforce'
         # in any case. Arbitrary number, but can be characterized
         return methods['nsgrid']
