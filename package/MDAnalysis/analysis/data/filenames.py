825ee05423085521e6a718b3527be9b723409280
Oliver Beckstein <orbeckst@gmail.com>
2018-08-16 11:08:09 -0700

    tests and updated docs for analysis.data (#2047)
    - add docs explaining data and showing example use
    - simple test for loading all data in analysis.data.filenames (analysis/test_data.py)
    - replaced assert_array_equal with assert_equal in utils/test_datafiles.py, which I used
      as a template

--- package/MDAnalysis/analysis/data/filenames.py
+++ package/MDAnalysis/analysis/data/filenames.py
@@ -19,8 +19,7 @@
 # MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
 # J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
 #
-"""
-Analysis data files
+"""Analysis data files
 ===================
 
 :mod:`MDAnalysis.analysis.data` contains data files that are used as part of
@@ -37,11 +36,38 @@ Data files
 
    Reference Ramachandran histogram for :class:`MDAnalysis.analysis.dihedrals.Ramachandran`.
    The data were calculated on a data set of 500 PDB structures taken from [Lovell2003]_.
+   This is a numpy array in the :math:`\phi` and :math:`psi` backbone dihedral angles.
+
+   Load and plot it with ::
+
+      import numpy as np
+      import matplotlib.pyplot as plt
+      from MDAnalysis.analysis.data.filenames import Rama_ref
+      X, Y = np.meshgrid(np.arange(-180, 180, 4), np.arange(-180, 180, 4))
+      Z = np.load(Rama_ref)
+      ax.contourf(X, Y, Z, levels=[1, 17, 15000])
+
+   The given levels will draw contours that contain 90% and 99% of the data
+   points. See the :ref:`Ramachandran plot figure <figure-ramachandran>` as an
+   example.
 
 .. data:: Janin_ref
 
-   Reference Ramachandran histogram for :class:`MDAnalysis.analysis.dihedrals.Ramachandran`.
+   Reference Janin histogram for :class:`MDAnalysis.analysis.dihedrals.Janin`.
    The data were calculated on a data set of 500 PDB structures taken from [Lovell2003]_.
+   This is a numpy array in the :math:`\chi_1` and :math:`chi_2` sidechain dihedral angles.
+
+   Load and plot it with ::
+
+      import numpy as np
+      import matplotlib.pyplot as plt
+      from MDAnalysis.analysis.data.filenames import Janin_ref
+      X, Y = np.meshgrid(np.arange(-180, 180, 4), np.arange(-180, 180, 4))
+      Z = np.load(Janin_ref)
+      ax.contourf(X, Y, Z, levels=[1, 6, 600])
+
+   The given levels will draw contours that contain 90% and 98% of the
+   data. See the :ref:`Janin plot figure <figure-janin>` as an example.
 
 """
 from __future__ import absolute_import
