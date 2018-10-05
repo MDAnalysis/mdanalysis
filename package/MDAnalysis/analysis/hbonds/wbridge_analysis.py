3337e25ba79371dcd5074abb52ca21f571aae15c
zhiyi wu <zhiyi.wu@msdtc.ox.ac.uk>
2018-10-05 14:09:49 +0100

    fixed test

--- package/MDAnalysis/analysis/hbonds/wbridge_analysis.py
+++ package/MDAnalysis/analysis/hbonds/wbridge_analysis.py
@@ -463,7 +463,7 @@ Classes
       of a water bridge existence.
 
 """
-from __future__ import print_function, absolute_import
+from __future__ import print_function, absolute_import, division
 
 from collections import defaultdict
 import logging
