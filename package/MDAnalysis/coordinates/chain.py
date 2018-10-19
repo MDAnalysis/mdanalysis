37072c333183196f5f5156a37cf6b092b2f7ab0c
Philip Loche <ploche@physik.fu-berlin.de>
2018-10-19 17:32:11 +0200

    Limit output of __repr__ for ChainReader class
    The output of __repr__ for the ChainReader class
    is limited to 3 trajectories. For more only the first
    one is printed.

--- package/MDAnalysis/coordinates/chain.py
+++ package/MDAnalysis/coordinates/chain.py
@@ -189,6 +189,8 @@ class ChainReader(base.ProtoReader):
        frames and individual :attr:`dt`.
     .. versionchanged:: 0.19.0
        added ``continuous`` trajectory option
+    .. versionchanged:: 0.19.0
+       limit output of __repr__
 
     """
     format = 'CHAIN'
@@ -575,10 +577,16 @@ class ChainReader(base.ProtoReader):
             yield ts
 
     def __repr__(self):
-        return ("<{clsname} {fname} with {nframes} frames of {natoms} atoms>"
+        if len(self.filenames) > 3:
+            fnames = "{fname} and {nfanmes} more".format(
+                    fname=os.path.basename(self.filenames[0]), 
+                    nfanmes=len(self.filenames) - 1)
+        else:
+            fnames = ", ".join([os.path.basename(fn) for fn in self.filenames])
+        return ("<{clsname} containing {fname} with {nframes} frames of {natoms} atoms>"
                 "".format(
                     clsname=self.__class__.__name__,
-                    fname=[os.path.basename(fn) for fn in self.filenames],
+                    fname=fnames,
                     nframes=self.n_frames,
                     natoms=self.n_atoms))
 
