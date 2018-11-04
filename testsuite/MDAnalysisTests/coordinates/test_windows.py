c8e329abfb445758529124501a0e7d4a1e3ff83f
richardjgowers <richardjgowers@gmail.com>
2018-11-04 17:27:18 -0600

    other format tests

--- testsuite/MDAnalysisTests/coordinates/test_windows.py
+++ testsuite/MDAnalysisTests/coordinates/test_windows.py
@@ -31,8 +31,9 @@ line ending files.
 
 from __future__ import absolute_import
 
+import numpy as np
 import pytest
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_equal
 from MDAnalysisTests.datafiles import (
     WIN_PDB_multiframe,
     WIN_ARC,
@@ -40,8 +41,67 @@ from MDAnalysisTests.datafiles import (
     WIN_TRJ,
     WIN_LAMMPSDUMP,
 )
-
+from MDAnalysisTests.coordinates.test_trj import TestTRJReader
+from MDAnalysisTests.coordinates.test_dlpoly import TestDLPolyHistory
+from MDAnalysisTests.coordinates.test_lammps import TestLammpsDumpReader
 import MDAnalysis as mda
+
+
+class TestWinLammpsDump(TestLammpsDumpReader):
+    @pytest.fixture
+    def u(self):
+        return mda.Universe(WIN_LAMMPSDUMP, format='LAMMPSDUMP')
+
+
+class TestWinPDB(object):
+    @staticmethod
+    @pytest.fixture(scope='class')
+    def multiverse():
+        return mda.Universe(WIN_PDB_multiframe, guess_bonds=True)
+
+    def test_n_frames(self, multiverse):
+        assert_equal(multiverse.trajectory.n_frames, 24,
+                     "Wrong number of frames read from PDB muliple model file")
+    def test_rewind(self, multiverse):
+        u = multiverse
+        u.trajectory[11]
+        assert_equal(u.trajectory.ts.frame, 11,
+                     "Failed to forward to 11th frame (frame index 11)")
+        u.trajectory.rewind()
+        assert_equal(u.trajectory.ts.frame, 0,
+                     "Failed to rewind to 0th frame (frame index 0)")
+
+    def test_iteration(self, multiverse):
+        u = multiverse
+        frames = []
+        for frame in u.trajectory:
+            pass
+        # should rewind after previous test
+        # problem was: the iterator is NoneType and next() cannot be called
+        for ts in u.trajectory:
+            frames.append(ts)
+        assert_equal(
+            len(frames), u.trajectory.n_frames,
+            "iterated number of frames %d is not the expected number %d; "
+            "trajectory iterator fails to rewind" %
+            (len(frames), u.trajectory.n_frames))
+
+    def test_slice_iteration(self, multiverse):
+        u = multiverse
+        frames = []
+        for ts in u.trajectory[4:-2:4]:
+            frames.append(ts.frame)
+        assert_equal(np.array(frames),
+                     np.arange(u.trajectory.n_frames)[4:-2:4],
+                     err_msg="slicing did not produce the expected frames")
+
+
+class TestWinDLPolyHistory(TestDLPolyHistory):
+    f = WIN_DLP_HISTORY
+
+
+class TestWinTRJ(TestTRJReader):
+    trajectory_file = WIN_TRJ
 
 
 class TestWinARC(object):
