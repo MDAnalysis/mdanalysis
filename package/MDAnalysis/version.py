26d194ea6246101526d4af11bddf7e7664a312b1
richardjgowers <richardjgowers@gmail.com>
2018-11-06 09:58:41 -0600

    started version 0.19.3

--- package/MDAnalysis/version.py
+++ package/MDAnalysis/version.py
@@ -66,4 +66,4 @@ Data
 # e.g. with lib.log
 
 #: Release of MDAnalysis as a string, using `semantic versioning`_.
-__version__ = "0.19.2"  # NOTE: keep in sync with RELEASE in setup.py
+__version__ = "0.19.3-dev"  # NOTE: keep in sync with RELEASE in setup.py
