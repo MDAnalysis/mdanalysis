99bdd08b4cd8ffa329cbe1c75969f3f03f1b94bf
richardjgowers <richardjgowers@gmail.com>
2019-02-16 11:24:45 -0600

    added more formats to multiprocessing tests
    and broke everything

--- testsuite/MDAnalysisTests/parallelism/test_multiprocessing.py
+++ testsuite/MDAnalysisTests/parallelism/test_multiprocessing.py
@@ -7,32 +7,62 @@ import pytest
 
 import MDAnalysis as mda
 from MDAnalysisTests.datafiles import (
-    PSF, DCD
+    PSF, DCD,
+    GRO, XTC,
+    PDB,
+    XYZ,
 )
 
 from numpy.testing import assert_equal
 
 
-@pytest.fixture
-def u():
-    return mda.Universe(PSF, DCD)
+@pytest.fixture(params=[
+    (PSF, DCD),
+    (GRO, XTC),
+    (PDB,),
+    (XYZ,),
+])
+def u(request):
+    if len(request.param) == 1:
+        f = request.param
+        return mda.Universe(f)
+    else:
+        top, trj = request.param
+        return mda.Universe(top, trj)
 
-
+# Define target functions here
+# inside test functions doesn't work
 def cog(u, ag, frame_id):
     u.trajectory[frame_id]
 
     return ag.center_of_geometry()
 
 
-def test_multiprocess_COM(u):
+def getnames(u, ix):
+    # Check topology stuff works
+    return u.atoms[ix].name
+
+
+def test_multiprocess_COG(u):
     ag = u.atoms[10:20]
 
     ref = np.array([cog(u, ag, i)
                     for i in range(4)])
 
     p = multiprocessing.Pool(2)
-
     res = np.array([p.apply(cog, args=(u, ag, i))
                     for i in range(4)])
+    p.close()
+    assert_equal(ref, res)
+
+
+def test_multiprocess_names(u):
+    ref = [getnames(u, i)
+           for i in range(10)]
+
+    p = multiprocessing.Pool(2)
+    res = [p.apply(getnames, args=(u, i))
+                   for i in range(10)]
+    p.close()
 
     assert_equal(ref, res)
