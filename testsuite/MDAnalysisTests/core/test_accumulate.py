67d9ebf8c4524226ed8180a0e8c241ff0f7a10d3
Philip Loche <ploche@physik.fu-berlin.de>
2019-02-19 20:19:12 +0100

    streamlined tests

--- testsuite/MDAnalysisTests/core/test_accumulate.py
+++ testsuite/MDAnalysisTests/core/test_accumulate.py
@@ -30,33 +30,29 @@ from MDAnalysis.exceptions import DuplicateWarning, NoDataError
 from MDAnalysisTests.datafiles import PSF, DCD, TPR_xvf, TRR_xvf, GRO
 import pytest
 
-universe = mda.Universe(PSF, DCD)
-universe_molfrag = mda.Universe(TPR_xvf, TRR_xvf)
-levellist = ('atoms', 'residues', 'segments')
-
-
+levelist=('atoms', 'residues', 'segments')
+        
 class TestAccumulate(object):
     """Tests the functionality of *Group.accumulate()."""
+    @pytest.fixture(params=levelist)
+    def group(self, request):
+        u = mda.Universe(PSF, DCD)
+        return getattr(u, request.param)
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_accumulate_str_attribute(self, level):
-        group = getattr(universe, level)
-        assert_almost_equal(
-            group.accumulate("masses"), np.sum(group.atoms.masses))
+    def test_accumulate_str_attribute(self, group):
+        assert_almost_equal(group.accumulate("masses"), np.sum(group.atoms.masses))
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_accumulate_different_func(self, level):
-        group = getattr(universe, level)
+    def test_accumulate_different_func(self, group):
         assert_almost_equal(
             group.accumulate("masses", function=np.prod),
             np.prod(group.atoms.masses))
 
     @pytest.mark.parametrize('universe, name, compound',
-                             ((universe, 'resids', 'residues'),
-                              (universe, 'segids', 'segments'),
-                              (universe_molfrag, 'molnums', 'molecules'),
-                              (universe_molfrag, 'fragindices', 'fragments')))
-    @pytest.mark.parametrize('level', levellist)
+                             ((mda.Universe(PSF, DCD), 'resids', 'residues'),
+                              (mda.Universe(PSF, DCD), 'segids', 'segments'),
+                              (mda.Universe(TPR_xvf, TRR_xvf), 'molnums', 'molecules'),
+                              (mda.Universe(TPR_xvf, TRR_xvf), 'fragindices', 'fragments')))
+    @pytest.mark.parametrize('level', levelist)
     def test_accumulate_str_attribute_compounds(self, universe, name, compound,
                                                 level):
         group = getattr(universe, level)
@@ -64,100 +60,83 @@ class TestAccumulate(object):
         vals = group.accumulate("masses", compound=compound)
         assert_almost_equal(vals, ref, decimal=5)
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_accumulate_wrongname(self, level):
-        group = getattr(universe, level)
+    def test_accumulate_wrongname(self, group):
         with pytest.raises(AttributeError):
             group.accumulate("foo")
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_accumulate_wrongcomponent(self, level):
-        group = getattr(universe, level)
+    def test_accumulate_wrongcomponent(self, group):
         with pytest.raises(ValueError):
             group.accumulate("masses", compound="foo")
 
-    @pytest.mark.parametrize('level', levellist)
+    @pytest.mark.parametrize('level', levelist)
     def test_accumulate_nobonds(self, level):
         group = getattr(mda.Universe(GRO), level)
         with pytest.raises(NoDataError):
             group.accumulate("masses", compound="fragments")
 
-    @pytest.mark.parametrize('level', levellist)
+    @pytest.mark.parametrize('level', levelist)
     def test_accumulate_nomolnums(self, level):
         group = getattr(mda.Universe(GRO), level)
         with pytest.raises(NoDataError):
             group.accumulate("masses", compound="molecules")
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_accumulate_array_attribute(self, level):
-        group = getattr(universe, level)
+    def test_accumulate_array_attribute(self, group):
         a = np.ones((len(group.atoms), 10))
         assert_equal(group.accumulate(a), np.sum(a, axis=0))
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_accumulate_array_attribute_wrongshape(self, level):
-        group = getattr(universe, level)
+    def test_accumulate_array_attribute_wrongshape(self, group):
         with pytest.raises(ValueError):
             group.accumulate(np.ones(len(group.atoms) - 1))
 
     @pytest.mark.parametrize('universe, name, compound',
-                             ((universe, 'resids', 'residues'),
-                              (universe, 'segids', 'segments'),
-                              (universe_molfrag, 'molnums', 'molecules'),
-                              (universe_molfrag, 'fragindices', 'fragments')))
-    @pytest.mark.parametrize('level', levellist)
+                             ((mda.Universe(PSF, DCD), 'resids', 'residues'),
+                              (mda.Universe(PSF, DCD), 'segids', 'segments'),
+                              (mda.Universe(TPR_xvf, TRR_xvf), 'molnums', 'molecules'),
+                              (mda.Universe(TPR_xvf, TRR_xvf), 'fragindices', 'fragments')))
+    @pytest.mark.parametrize('level', levelist)
     def test_accumulate_array_attribute_compounds(self, universe, name,
                                                   compound, level):
         group = getattr(universe, level)
         ref = [np.ones((len(a), 10)).sum(axis=0) for a in group.atoms.groupby(name).values()]
         assert_equal(group.accumulate(np.ones((len(group.atoms), 10)), compound=compound), ref)
 
-
 class TestTotals(object):
     """Tests the functionality of *Group.total*() like total_mass
     and total_charge.
     """
+    @pytest.fixture(params=levelist)
+    def group(self, request):
+        u = mda.Universe(PSF, DCD)
+        return getattr(u, request.param)
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_total_charge(self, level):
-        group = getattr(universe, level)
+    def test_total_charge(self, group):
         assert_almost_equal(group.total_charge(), -4.0, decimal=4)
 
     @pytest.mark.parametrize('name, compound',
                              (('resids', 'residues'), ('segids', 'segments'),
                               ('fragindices', 'fragments')))
-    @pytest.mark.parametrize('level', levellist)
-    def test_total_charge_compounds(self, name, compound, level):
-        group = getattr(universe, level)
+    def test_total_charge_compounds(self, group, name, compound):
         ref = [sum(a.charges) for a in group.atoms.groupby(name).values()]
         assert_almost_equal(group.total_charge(compound=compound), ref)
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_total_charge_duplicates(self, level):
-        group = getattr(universe, level)
+    def test_total_charge_duplicates(self, group):
         group2 = group + group[0]
         ref = group.total_charge() + group[0].charge
         with pytest.warns(DuplicateWarning) as w:
             assert_almost_equal(group2.total_charge(), ref)
             assert len(w) == 1
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_total_mass(self, level):
-        group = getattr(universe, level)
+    def test_total_mass(self, group):
         assert_almost_equal(group.total_mass(), 23582.043)
 
     @pytest.mark.parametrize('name, compound',
                              (('resids', 'residues'), ('segids', 'segments'),
                               ('fragindices', 'fragments')))
-    @pytest.mark.parametrize('level', levellist)
-    def test_total_mass_compounds(self, name, compound, level):
-        group = getattr(universe, level)
+    def test_total_mass_compounds(self, group, name, compound):
         ref = [sum(a.masses) for a in group.atoms.groupby(name).values()]
         assert_almost_equal(group.total_mass(compound=compound), ref)
 
-    @pytest.mark.parametrize('level', levellist)
-    def test_total_mass_duplicates(self, level):
-        group = getattr(universe, level)
+    def test_total_mass_duplicates(self, group):
         group2 = group + group[0]
         ref = group.total_mass() + group2[0].mass
         with pytest.warns(DuplicateWarning) as w:
