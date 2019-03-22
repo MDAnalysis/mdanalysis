94ccf13f14f81cf59458d87d008c5e28775847d1
Fenil Suchak <fenu1998@gmail.com>
2019-03-22 20:15:33 +0530

    fixed redundant

--- package/MDAnalysis/core/groups.py
+++ package/MDAnalysis/core/groups.py
@@ -3080,11 +3080,7 @@ class AtomGroup(GroupBase):
         # Try and select a Class using get_ methods (becomes `writer`)
         # Once (and if!) class is selected, use it in with block
         try:
-            if file_format is None: #If this case then get_writer_for guesses format from filename
-                pass
-            else:
-                format = file_format
-            writer = get_writer_for(filename, format=format, multiframe=multiframe)
+            writer = get_writer_for(filename, format=file_format, multiframe=multiframe)
         except (ValueError, TypeError):
             pass
         else:
