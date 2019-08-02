7449a38ebdaa80f7bf4df400df7855d83a5a7037
Irfan Alibay <IAlibay@users.noreply.github.com>
2019-08-02 20:38:33 +0100

    Parsing negative values in ATOMIC_NUMBER records of AMBER topologies (#2307)
    * Negative values in the ATOMIC_NUMBER records are now read by guessing the atom element using `guessers.guess_atom_element`.
    * Switch for dummy atoms ("DUMMY" as element)
    * DUMMY atm guess + tests
    * Changelog update
    * Tests for new TOPParser warnings
    * fixes #2306
    * element guesses  in the Amber parser now output user warnings (for #2222)

--- testsuite/MDAnalysisTests/datafiles.py
+++ testsuite/MDAnalysisTests/datafiles.py
@@ -96,6 +96,7 @@ __all__ = [
     "PFncdf_Top", "PFncdf_Trj", # Amber ncdf with Positions and Forces
     "PRMcs", # Amber (format, Issue 1331)
     "PRMNCRST", # Amber ncrst with positions/forces/velocities
+    "PRMNEGATIVE", # Amber negative ATOMIC_NUMBER (Issue 2306)
     "PRMErr1", "PRMErr2", "PRMErr3", # Amber TOP files to check raised errors
     "PQR",  # PQR v1
     "PQR_icodes",  # PQR v2 with icodes
@@ -344,6 +345,8 @@ NCDFtruncoct =  resource_filename(__name__, 'data/Amber/tz2.truncoct.nc')
 PRMcs = resource_filename(__name__, 'data/Amber/chitosan.prmtop')
 
 PRMNCRST = resource_filename(__name__, 'data/Amber/ace_mbondi3.parm7')
+
+PRMNEGATIVE = resource_filename(__name__, 'data/Amber/ace_mbondi3.negative.parm7')
 
 PRMErr1 = resource_filename(__name__, 'data/Amber/ace_mbondi3.error1.parm7')
 PRMErr2 = resource_filename(__name__, 'data/Amber/ace_mbondi3.error2.parm7')
