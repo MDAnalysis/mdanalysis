f1253ba0076d45f414fa6347c1ee744f7801a319
richardjgowers <richardjgowers@gmail.com>
2019-08-28 22:49:36 +0100

    started version 0.20.1

--- package/setup.py
+++ package/setup.py
@@ -73,7 +73,7 @@ else:
     from commands import getoutput
 
 # NOTE: keep in sync with MDAnalysis.__version__ in version.py
-RELEASE = "0.20.0"
+RELEASE = "0.20.1-dev"
 
 is_release = 'dev' not in RELEASE
 
