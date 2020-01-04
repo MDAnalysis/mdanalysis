4e0c1028c197c3576dfb6d7a82368601da5b009e
Jonathan Barnoud <jonathan@barnoud.net>
2020-01-04 10:50:09 +0000

    Convert atom types to string in TXYZ
    Fixes #2435

--- package/MDAnalysis/topology/TXYZParser.py
+++ package/MDAnalysis/topology/TXYZParser.py
@@ -90,7 +90,7 @@ class TXYZParser(TopologyReaderBase):
 
             atomids = np.zeros(natoms, dtype=np.int)
             names = np.zeros(natoms, dtype=object)
-            types = np.zeros(natoms, dtype=np.int)
+            types = np.zeros(natoms, dtype=object)
             bonds = []
             # Find first atom line, maybe there's box information
             fline = inf.readline()
@@ -110,7 +110,7 @@ class TXYZParser(TopologyReaderBase):
                 line = line.split()
                 atomids[i]= line[0]
                 names[i] = line[1]
-                types[i] = line[5]
+                types[i] = str(line[5])
                 bonded_atoms = line[6:]
                 for other_atom in bonded_atoms:
                     other_atom = int(other_atom) - 1
