935087023a5a3c9281279be432f27d9c7ec1d307
Oliver Beckstein <orbeckst@gmail.com>
2020-02-07 13:50:49 -0700

    include *.coor files in tests via setup.py

--- testsuite/setup.py
+++ testsuite/setup.py
@@ -176,6 +176,7 @@ if __name__ == '__main__':
                          'data/*.gsd',
                          'data/windows/*',
                          'data/*.itp',
+                         'data/*.coor',
                         ],
           },
           install_requires=[
