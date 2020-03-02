aa29d3b44c4066ca99239d9cb85c6c11d67c19b5
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2020-03-02 21:43:08 +1100

    fill out framework to calculate msd

--- package/MDAnalysis/analysis/msd.py
+++ package/MDAnalysis/analysis/msd.py
@@ -48,54 +48,93 @@ logger = logging.getLogger("MDAnalysis.analysis.contacts")
 class MeanSquaredDisplacement(AnalysisBase):
 
 
-    def __init__(self, u, selection, t0, delta_t, msd_type='xyz')
+    def __init__(self, u, selection, t0, delta_t, msd_type='xyz', position_treatment='atom', mass_weighted=False)
                  kwargs=None, **basekwargs):
         self.u = u
         super(MeanSquaredDisplacement, self).__init__(self.u.trajectory, **basekwargs)
 
-        self.fraction_kwargs = kwargs if kwargs is not None else {}
-        self.select = selection
+        self.selection = selection
         self.t0 = t0
         self.delta_t = delta_t
         self.msd_type = msd_type
+        self.position_treatment = position_treatment
+        self.mass_weighted = mass_weighted
+
         self.dim_fac = 0
+        self.atoms = None
+        self.timeseries = None
 
         
     def _prepare(self):
         parse_msd_type()
+        check_masses()
         select_reference_positions()
-    
-    def select_reference_positions():
         
-    
+    def check_masses(self):
+        self.atoms = u.select_atoms(self.selection)
+        if (mass_weighted or position_treatment == 'com'):
+           
+            #TODO check that all the atoms have masses
+        else:
+            pass
+        
+    def select_reference_positions(self):
+        if self.position_treatment == 'atom'
+            self.timeseries = atoms #TODO work out timeseries
+        elif self.position_treatment == 'com':
+            
+            self.timeseries = calculate_com(atoms) #TODO work out timeseries
+        else:
+            raise ValueError('invalid position_treatment specified')
+
     def parse_msd_type(self):
+
         if self.msd_type = 'xyz':
             self._dim = [0,1,2]
             self.dim_fac = 3.0
+            
         elif self.msd_type = 'xy':
             self._dim = [0,1]
             self.dim_fac = 2.0
+
         elif self.md_type = 'xz':
             self._dime = [0,2]
             self.dim_fac = 2.0
+
         elif self.msd_type = 'yz':
             self._dim = [1,2]
             self.dim_fac = 2.0
+
         elif self.msd_type = 'x':
             self._dim = [0]
             self.dim_fac = 1.0
+
         elif self.msd_type = 'y':
             self._dim = [1]
             self.dim_fac = 1.0
+
         elif self.msd_type = 'z':
             self._dim = [2]
             self.dim_fac = 1.0
+
         else:
             raise ValueError('invalid msd_type specified')
         
 
     
     def _run():
+
+    
+    def one_time_point(self,):
+        for i in self.timeseries:
+            displ_sq_sum = 0.0
+            for j in self.n_atoms:
+                displ = np.abs(r[self._dim]-r0[self._dim])
+                displ_sq = displ*displ
+                displ_sq_sum += displ_sq
+            1.0/self.n_atoms
+
+
 
         
 
