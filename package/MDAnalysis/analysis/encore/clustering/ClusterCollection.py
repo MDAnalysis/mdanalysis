cae2332ac775b85f84b4eed9c21b401740d70ee0
shfrz <39633713+shfrz@users.noreply.github.com>
2020-03-09 21:48:32 +0530

    Update ClusterCollection.py

--- package/MDAnalysis/analysis/encore/clustering/ClusterCollection.py
+++ package/MDAnalysis/analysis/encore/clustering/ClusterCollection.py
@@ -185,10 +185,12 @@ class ClusterCollection(object):
         """
         idn = 0
         if elements is None:
+            self.size = 0
             self.clusters = None
+            self.elements = np.array([])
             return
 
-        if not len(set((type(el) for el in range(elements)))) == 1:
+        if not len(set((type(el) for el in self.elements))) == 1:
             raise TypeError("all the elements must have the same type")
         self.clusters = []
         elements_array = np.array(elements)
