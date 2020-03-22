17b832c4cade3bd667853b591ae76626f3cc3612
shfrz <shahfaraaz91@gmail.com>
2020-03-22 21:55:22 +0530

    test_update_util_2

--- testsuite/MDAnalysisTests/lib/test_util.py
+++ testsuite/MDAnalysisTests/lib/test_util.py
@@ -221,9 +221,10 @@ class TestGeometryFunctions(object):
         assert_equal(mdamath.normal(vec1, vec2), value)
         # add more non-trivial tests
 
+    @pytest.fixture()
     def test_angle(self, a):
       a = np.array([0.1, 0, 0.2])
-    	assert_equal(mdamath.angle(a, -a), mdamath.angle(a**0.5, -(a**0.5)))
+      assert mdamath.angle(a, -a) == mdamath.angle(a**0.5, -(a**0.5))
             
     def test_stp(self):
         assert mdamath.stp(self.e1, self.e2, self.e3) == 1.0
