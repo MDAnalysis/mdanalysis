29513880d4e5486158bbad3ff8969c419192df80
Rocco Meli <r.meli@bluemail.ch>
2020-04-25 11:47:02 +0200

    merge test into single one

--- testsuite/MDAnalysisTests/core/test_atomselections.py
+++ testsuite/MDAnalysisTests/core/test_atomselections.py
@@ -1101,20 +1101,18 @@ class TestICodeSelection(object):
             u.select_atoms('resid 10A-12')
 
 
-def test_same_icode():
+@pytest.mark.parametrize(
+    "selection",
+    [
+        "same residue as ", "same resid as", "byres"
+    ]
+)
+def test_similarity_selection_icodes(selection):
+
     u = mda.Universe(PDB_icodes)
 
     # Select residues 162 and 163A
-    sel = u.select_atoms("same residue as (around 2.0 resid 163)")
-
-    assert len(sel.atoms) == 11
-
-
-def test_byres_icode():
-    u = mda.Universe(PDB_icodes)
-
-    # Select residues 162 and 163A
-    sel = u.select_atoms("byres (around 2.0 resid 163)")
+    sel = u.select_atoms(selection + "(around 2.0 resid 163)")
 
     assert len(sel.atoms) == 11
 
