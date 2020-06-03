e9f4cf4eb7cfa705ab27c5979d52765ac1ce2014
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2020-06-03 13:49:17 +1000

    authroship note

--- testsuite/MDAnalysisTests/analysis/test_msd.py
+++ testsuite/MDAnalysisTests/analysis/test_msd.py
@@ -20,6 +20,8 @@
 # MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
 # J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
 #
+# This module written by Hugo MacDermott-Opeskin 2020
+
 from __future__ import division, absolute_import, print_function
 
 
