4f50ba73aa33d9bf052358bc41f6b21f19cbb48b
richard <richard@nextmovesoftware.com>
2020-06-10 09:05:59 +0100

    bumped version number to 1.0.0

--- testsuite/MDAnalysisTests/__init__.py
+++ testsuite/MDAnalysisTests/__init__.py
@@ -98,7 +98,7 @@ import pytest
 logger = logging.getLogger("MDAnalysisTests.__init__")
 
 # keep in sync with RELEASE in setup.py
-__version__ = "0.20.2-dev0"
+__version__ = "1.0.0"
 
 
 # Do NOT import MDAnalysis at this level. Tests should do it themselves.
