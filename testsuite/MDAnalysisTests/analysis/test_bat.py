389e534e21a216d6931fd7f7f0315a6fafc58c28
daveminh <dminh@iit.edu>
2020-06-17 05:32:42 -0400

    Integrated some suggestions from code review

--- testsuite/MDAnalysisTests/analysis/test_bat.py
+++ testsuite/MDAnalysisTests/analysis/test_bat.py
@@ -24,7 +24,6 @@ from __future__ import absolute_import
 
 import numpy as np
 from numpy.testing import assert_equal, assert_almost_equal
-import matplotlib
 import pytest
 
 import MDAnalysis as mda
