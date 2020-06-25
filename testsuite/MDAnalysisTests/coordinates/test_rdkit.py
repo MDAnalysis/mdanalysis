eb5d3658caacf0670dcfc04162255a10989bd3a7
Cédric Bouysset <cedric.bouysset@unice.fr>
2020-06-25 18:29:55 +0200

    more tests on topology + pep8

--- testsuite/MDAnalysisTests/coordinates/test_rdkit.py
+++ testsuite/MDAnalysisTests/coordinates/test_rdkit.py
@@ -24,24 +24,34 @@ import warnings
 
 import pytest
 import MDAnalysis as mda
+from MDAnalysis.topology.guessers import guess_atom_element
 import numpy as np
 from numpy.testing import (assert_equal,
                            assert_almost_equal)
 
 from MDAnalysisTests.datafiles import mol2_molecule, PDB_full
 
-Chem = pytest.importorskip("rdkit.Chem")
-AllChem = pytest.importorskip("rdkit.Chem.AllChem")
+try:
+    from rdkit import Chem
+    from rdkit.Chem import AllChem
+except ImportError:
+    rdkit_installed = False
+else:
+    rdkit_installed = True
+
 
 def mol2_mol():
     return Chem.MolFromMol2File(mol2_molecule, removeHs=False)
 
+
 def smiles_mol():
-    mol = Chem.MolFromSmiles("CCO")
+    mol = Chem.MolFromSmiles("CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
     mol = Chem.AddHs(mol)
     cids = AllChem.EmbedMultipleConfs(mol, numConfs=3)
     return mol
 
+
+@pytest.mark.skipif(rdkit_installed is False, reason="requires RDKit")
 class TestRDKitReader(object):
     @pytest.mark.parametrize("rdmol, n_frames", [
         (mol2_mol(), 1),
@@ -51,7 +61,7 @@ class TestRDKitReader(object):
         universe = mda.Universe(rdmol)
         assert universe.trajectory.n_frames == n_frames
         expected = np.array([
-            conf.GetPositions() for conf in rdmol.GetConformers()], 
+            conf.GetPositions() for conf in rdmol.GetConformers()],
             dtype=np.float32)
         assert_equal(expected, universe.trajectory.coordinate_array)
 
@@ -65,7 +75,7 @@ class TestRDKitReader(object):
             assert len(w) == 1
             assert "No coordinates found" in str(
                 w[-1].message)
-        expected = np.empty((1,u.atoms.n_atoms,3), dtype=np.float32)
+        expected = np.empty((1, u.atoms.n_atoms, 3), dtype=np.float32)
         expected[:] = np.nan
         assert_equal(u.trajectory.coordinate_array, expected)
 
@@ -73,10 +83,11 @@ class TestRDKitReader(object):
         universe = mda.Universe(mol2_mol())
         mol2 = mda.Universe(mol2_molecule)
         assert universe.trajectory.n_frames == mol2.trajectory.n_frames
-        assert_equal(universe.trajectory.ts.positions, 
+        assert_equal(universe.trajectory.ts.positions,
                      mol2.trajectory.ts.positions)
-        
 
+
+@pytest.mark.skipif(rdkit_installed is False, reason="requires RDKit")
 class TestRDKitConverter(object):
     @pytest.fixture
     def pdb(self):
@@ -84,7 +95,13 @@ class TestRDKitConverter(object):
 
     @pytest.fixture
     def mol2(self):
-        return mda.Universe(mol2_molecule)
+        u = mda.Universe(mol2_molecule)
+        # add elements
+        elements = np.array([
+            guess_atom_element(x).capitalize() for x in u.atoms.types
+        ], dtype=object)
+        u.add_TopologyAttr('elements', elements)
+        return u
 
     @pytest.mark.parametrize("sel_str", [
         "resid 1",
@@ -97,20 +114,68 @@ class TestRDKitConverter(object):
         umol = sel.convert_to("RDKIT")
         atom = umol.GetAtomWithIdx(0)
         mi = atom.GetMonomerInfo()
-        
+
         for mda_attr, rd_attr in mda.coordinates.RDKit.RDATTRIBUTES.items():
             if mda_attr == "occupancy":
                 mda_attr = "occupancie"
+            elif mda_attr == "segindex":
+                mda_attr = "segindice"
             rd_value = getattr(mi, "Get%s" % rd_attr)()
             mda_value = getattr(sel, "%ss" % mda_attr)[0]
             if mda_attr == "name":
                 rd_value = rd_value.strip()
-            elif mda_attr == "segid":
-                rd_value = np.base_repr(rd_value, 36)
             assert rd_value == mda_value
 
     def test_identical_topology_mol2(self, mol2):
-        # no chirality check
+        """Check stereochemistry on atoms and bonds (but not yet)"""
         rdmol = mol2_mol()
         umol = mol2.atoms.convert_to("RDKIT")
+        assert rdmol.HasSubstructMatch(umol, useChirality=False)
+        assert umol.HasSubstructMatch(rdmol, useChirality=False)
+
+    def test_identical_topology(self):
+        rdmol = smiles_mol()
+        u = mda.Universe(rdmol)
+        umol = u.atoms.convert_to("RDKIT")
         assert rdmol.HasSubstructMatch(umol) and umol.HasSubstructMatch(rdmol)
+
+    def test_raise_requires_elements(self):
+        u = mda.Universe(mol2_molecule)
+        with pytest.raises(AttributeError) as e:
+            u.atoms.convert_to("RDKIT")
+            assert "`elements` attribute is required for the RDKitConverter" in str(
+                e.value)
+
+    def test_warn_guess_bonds(self, pdb):
+        pdb.delete_bonds(pdb.bonds)
+        ag = pdb.select_atoms("resnum 101 and segid A")
+        pdb.delete_bonds(ag.bonds)
+        with warnings.catch_warnings(record=True) as w:
+            # Cause all warnings to always be triggered.
+            warnings.simplefilter("always")
+            # trigger warning
+            ag.convert_to("RDKIT")
+            assert len(w) == 1
+            assert "No `bonds` attribute in this AtomGroup" in str(
+                w[-1].message)
+
+    @pytest.mark.parametrize("idx", [0, 10, 42])
+    def test_other_attributes(self, mol2, idx):
+        mol = mol2.atoms.convert_to("RDKIT")
+        rdprops = mol.GetAtomWithIdx(idx).GetPropsAsDict()
+        for prop in ["charge", "segid", "type"]:
+            rdprop = rdprops["_MDAnalysis_%s" % prop]
+            mdaprop = getattr(mol2.atoms[idx], prop)
+            assert rdprop == mdaprop
+
+
+class TestRequiresRDKit(object):
+    def test_converter_requires_rdkit(self):
+        if rdkit_installed:
+            pass
+        else:
+            u = mda.Universe(mol2_molecule)
+            with pytest.raises(ImportError) as e:
+                u.atoms.convert_to("RDKIT")
+                assert "RDKit is required for the RDKitConverter" in str(
+                    e.value)
