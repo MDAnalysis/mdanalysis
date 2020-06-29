68dc7ca94e277f00e6a0e4e66235a145faa710a8
Cédric Bouysset <cedric.bouysset@unice.fr>
2020-06-29 17:53:41 +0200

    test mda to rdkit MonomerInfo conversion

--- package/MDAnalysis/coordinates/RDKit.py
+++ package/MDAnalysis/coordinates/RDKit.py
@@ -291,25 +291,21 @@ def _add_mda_attr_to_rdkit(attr, value, mi):
     mi : rdkit.Chem.rdchem.AtomPDBResidueInfo
         MonomerInfo object that will store the relevant atom attributes
     """
-    try:  # get value in MDA atom
-        value = getattr(atom, attr)
-    except AttributeError:
-        pass
-    else:
-        if isinstance(value, np.generic):
-            # convert numpy types to python standard types
-            value = value.item()
-        if attr == "name":
-            # RDKit needs the name to be properly formated for a
-            # PDB file (1 letter elements start at col 14)
-            name = re.findall('(\D+|\d+)', value)
-            if len(name) == 2:
-                symbol, number = name
-            else:
-                symbol, number = name[0], ""
-            value = "{:>2}".format(symbol) + "{:<2}".format(number)
-        # set attribute value in RDKit MonomerInfo
-        getattr(mi, "Set%s" % rdattr)(value)
+    if isinstance(value, np.generic):
+        # convert numpy types to python standard types
+        value = value.item()
+    if attr == "names":
+        # RDKit needs the name to be properly formated for a
+        # PDB file (1 letter elements start at col 14)
+        name = re.findall('(\D+|\d+)', value)
+        if len(name) == 2:
+            symbol, number = name
+        else:
+            symbol, number = name[0], ""
+        value = "{:>2}{:<2}".format(symbol, number)
+    # set attribute value in RDKit MonomerInfo
+    rdattr = RDATTRIBUTES[attr]
+    getattr(mi, "Set%s" % rdattr)(value)
 
 
 def _infer_bo_and_charges(mol, border_atom_indices):
