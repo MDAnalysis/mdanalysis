a87057bc53050a97564dbf26a6cfa4c5ec76a39f
Edis <ejakupov@asu.edu>
2020-07-07 02:26:08 -0700

    Removed masses from H5MDReader and adding first iteration of test_h5md.py

--- package/MDAnalysis/coordinates/H5MD.py
+++ package/MDAnalysis/coordinates/H5MD.py
@@ -1,18 +1,19 @@
 import sys
 import os
 import pyh5md
+import numpy as np
 import MDAnalysis as mda
 from MDAnalysis.coordinates import base, core
 
 class Timestep(base.Timestep):
     """H5MD Timestep
-    
+
     """
     order = 'C'
-    
+
     def _init_unitcell(self):
         return np.zeros((3, 3), dtype=np.float32)
-    
+
     @property
     def dimensions(self):
         """unitcell dimensions (*A*, *B*, *C*, *alpha*, *beta*, *gamma*)
@@ -21,24 +22,26 @@ class Timestep(base.Timestep):
         angles are in degrees.
 
         Setting dimensions will populate the underlying native format description (triclinic box vectors)
-        If `edges<https://nongnu.org/h5md/h5md.html#simulation-box>`_ is a matrix, 
-        the box is of triclinic shape with the edge vectors given by the rows of the matrix.) 
+        If `edges<https://nongnu.org/h5md/h5md.html#simulation-box>`_ is a matrix,
+        the box is of triclinic shape with the edge vectors given by the rows of the matrix.)
         """
         return core.triclinic_box(*self._unitcell) # vectors are the rows
 
     @dimensions.setter
     def dimensions(self, box):
         self._unitcell[:] = core.triclinic_vectors(box)
-    
+
 class H5MDReader(base.ReaderBase):
     """Reader for the H5MD format.
+    
+    At the moment, all data is read directly from file without unit conversion.
 
     """
     format = 'H5MD'
     units = {'time': None, 'length': None}
     # need to add units
     _Timestep = Timestep
-    
+
     def __init__(self, filename, **kwargs):
         """
         Parameters
@@ -55,18 +58,18 @@ class H5MDReader(base.ReaderBase):
         self.n_atoms = pyh5md.element(self._trajectory, 'n_atoms').value[()]
         self.ts = self._Timestep(self.n_atoms, **self._ts_kwargs)
         self._read_next_timestep()
-        
-        
+
+
     def open_trajectory(self) :
         """opens the trajectory file using pyh5md module"""
-        self._frame = -1 
+        self._frame = -1
         self._file = pyh5md.File(self.filename, 'r')
         self._trajectory = self._file.particles_group('trajectory')
 
     def close(self):
         """close reader"""
         self._file.close()
-    
+
     @property
     def n_frames(self):
         """number of frames in trajectory"""
@@ -74,7 +77,7 @@ class H5MDReader(base.ReaderBase):
 
     def _reopen(self):
         """reopen trajectory"""
-        self.close() 
+        self.close()
         self.open_trajectory()
 
     def _read_frame(self, frame):
@@ -90,7 +93,7 @@ class H5MDReader(base.ReaderBase):
         # sets the Timestep object
         ts.frame = frame
         ts.data['step'] = pyh5md.element(self._trajectory, 'positions').step[frame]
-        # will add more data 
+        # will add more data
 
         # set frame box dimensions
         # set triclinic box vectors
@@ -105,7 +108,7 @@ class H5MDReader(base.ReaderBase):
                 " topology!"%(frame, n_atoms_now, self.n_atoms))
         else :
             ts.positions = frame_positions
-            
+
         # set particle velocities
         frame_velocities = pyh5md.element(self._trajectory, 'velocities').value[frame, :]
         n_atoms_now = frame_velocities.shape[0]
@@ -115,7 +118,7 @@ class H5MDReader(base.ReaderBase):
                 " topology!"%(frame, n_atoms_now, self.n_atoms))
         else :
             ts.velocities = frame_velocities
-            
+
         # set particle forces
         frame_forces = pyh5md.element(self._trajectory, 'forces').value[frame, :]
         n_atoms_now = frame_forces.shape[0]
@@ -125,17 +128,7 @@ class H5MDReader(base.ReaderBase):
                 " topology!"%(frame, n_atoms_now, self.n_atoms))
         else :
             ts.forces = frame_forces
-            
-        # set particle masses
-        frame_masses = pyh5md.element(self._trajectory, 'masses').value[()]
-        n_atoms_now = frame_masses.shape
-        if n_atoms_now != self.n_atoms :
-            raise ValueError("Frame %d has %d atoms but the initial frame has %d"
-                " atoms. MDAnalysis in unable to deal with variable"
-                " topology!"%(frame, n_atoms_now, self.n_atoms))
-        else :
-            ts.masses = frame_masses
-            
+
         return ts
 
     def _read_next_timestep(self) :
---
+++ testsuite/MDAnalysisTests/coordinates/test_h5md.py
@@ -0,0 +1,105 @@
+import pytest
+from numpy.testing import assert_almost_equal, assert_array_equal
+import numpy as np
+import MDAnalysis as mda
+from MDAnalysisTests.datafiles import H5MD_xvf, TPR_xvf
+from MDAnalysis.coordinates import H5MD
+
+
+@pytest.fixture
+def h5md_universe():
+    return mda.Universe(TPR_xvf, H5MD_xvf)
+
+def test_h5md_n_frames(h5md_universe):
+    assert len(h5md_universe.trajectory) == 3
+
+def test_h5md_positions(h5md_universe):
+    # first timestep tests
+    h5md_universe.trajectory[0]
+    assert_almost_equal(h5md_universe.atoms.positions[0],
+                        [32.309906, 13.77798 , 14.372463],
+                        decimal=6)
+    assert_almost_equal(h5md_universe.atoms.positions[42],
+                        [28.116928, 19.405945, 19.647358],
+                        decimal=6)
+    assert_almost_equal(h5md_universe.atoms.positions[10000],
+                        [44.117805, 50.442093, 23.299038],
+                        decimal=6)
+
+    # second timestep tests
+    h5md_universe.trajectory[1]
+    assert_almost_equal(h5md_universe.atoms.positions[0],
+                        [30.891968, 13.678971, 13.6000595],
+                        decimal=6)
+    assert_almost_equal(h5md_universe.atoms.positions[42],
+                        [27.163246, 19.846561, 19.3582],
+                        decimal=6)
+    assert_almost_equal(h5md_universe.atoms.positions[10000],
+                        [45.869278, 5.0342298, 25.460655],
+                        decimal=6)
+
+    # third timestep tests
+    h5md_universe.trajectory[2]
+    assert_almost_equal(h5md_universe.atoms.positions[0],
+                        [31.276512, 13.89617, 15.015897],
+                        decimal=6)
+    assert_almost_equal(h5md_universe.atoms.positions[42],
+                        [28.567991, 20.56532, 19.40814],
+                        decimal=6)
+    assert_almost_equal(h5md_universe.atoms.positions[10000],
+                        [39.713223,  6.127234, 18.284992],
+                        decimal=6)
+
+
+def test_h5md_dimensions(h5md_universe):
+    # first timestep
+    ts = h5md_universe.trajectory[0]
+    assert_almost_equal(ts.dimensions,
+                        [52.763, 52.763, 52.763, 90., 90., 90.],
+                        decimal=6)
+    # second timestep
+    ts = h5md_universe.trajectory[1]
+    assert_almost_equal(ts.dimensions,
+                        [52.807877, 52.807877, 52.807877, 90., 90., 90.],
+                        decimal=6)
+    # third timestep
+    ts = h5md_universe.trajectory[2]
+    assert_almost_equal(ts.dimensions,
+                        [52.839806, 52.839806, 52.839806, 90., 90., 90.],
+                        decimal=6)
+
+def test_h5md_data_step(h5md_universe):
+    assert h5md_universe.trajectory[0].data['step'] == 0
+    assert h5md_universe.trajectory[1].data['step'] == 1
+    assert h5md_universe.trajectory[2].data['step'] == 2
+
+def test_rewind(h5md_universe):
+    h5md_universe.trajectory.rewind()
+    assert h5md_universe.trajectory.ts.frame == 0
+
+def test_next(h5md_universe):
+    h5md_universe.trajectory.rewind()
+    h5md_universe.trajectory.next()
+    assert h5md_universe.trajectory.ts.frame == 1
+
+def test_jump_last_frame(h5md_universe):
+    h5md_universe.trajectory[-1]
+    assert h5md_universe.trajectory.ts.frame == 2
+
+@pytest.mark.parametrize("start, stop, step", ((0, 2, 1),
+                                               (1, 2, 1)))
+def test_slice(h5md_universe, start, stop, step):
+    frames = [h5md_universe.trajectory.ts.frame for ts in h5md_universe.trajectory[start:stop:step]]
+    assert_array_equal(frames, np.arange(start, stop, step))
+
+
+@pytest.mark.parametrize("array_like", [list, np.array])
+def test_array_like(h5md_universe, array_like):
+    array = array_like([0, 2])
+    frames = [h5md_universe.trajectory.ts.frame for ts in h5md_universe.trajectory[array]]
+    assert_array_equal(frames, array)
+
+@pytest.mark.parametrize("indices", ([0, 1, 2, 1, 2, 2, 0]))
+def test_list_indices(h5md_universe, indices):
+    frames = [h5md_universe.trajectory.ts.frame for ts in h5md_universe.trajectory[indices]]
+    assert_array_equal(frames, indices)
