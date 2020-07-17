90c2c8135b57d6a2fb2133e27ef3290127d3c7ae
Jonathan Barnoud <jonathan@barnoud.net>
2020-07-17 07:06:48 +0100

    Spring cleaning for the TPR parser (#2840)
    * Remove python 2 compatibility hacks in TPR parser
    
    The TPXUnpacker class and its children had hacks to work on python 2.
    This commit removes these hacks has support for that version of python
    is now dropped.
    
    * Use f-strings throughout the TPR parser
    
    * Remove useless alias
    
    * Remove redundant version test in TPR perser
    
    The TPR parser supports file generated from gromacs 4.0 (tpx version 58)
    and newer. There were checks about older version that were either always
    true or always false. Because some of these checks were hidding
    erroneous code, this commit factor them out.
    
    * Remove useless alias
    
    * Use f-strings in TPR parser
    
    * Fix a spelling error
    
    * Use f-strings
    
    * Remove unused iparams from TPR parser
    
    The do_iparams function in the TPR parser reads the parameters of the
    various interactions in the TPR file. As we do not use these parameters,
    he function does not return anything. We use to store this absence of
    return value (None). This commit removes this.
    
    * Remove redundant version check
    
    * Add forgotten TPR tests
    
    * Improve test coverage
    
    * Add tests for gromacs 2020 in double precision
    
    * Add comment
    
    * Cover virtual site code in TPR parser
    
    * Improve TPR virtual sites tests
    
    * Test more interactions in TPR parser
    
    * Rename TPR_VS_2020 to account for its content
    
    The TPR containing he virtual sites example does now contain all the
    possible interactions. It is now called EXTRA rather than VS to account
    for that change.
    
    * Increase test coverage in TPR parser
    
    * Increase TPR test coverage
    
    * Remove more dead code in TPR parser
    
    * Update changelog for #2840

--- package/MDAnalysis/topology/tpr/obj.py
+++ package/MDAnalysis/topology/tpr/obj.py
@@ -40,7 +40,7 @@ TpxHeader = namedtuple(
         "bIr", "bTop", "bX", "bV", "bF", "bBox", "sizeOfTprBody"])
 Box = namedtuple("Box", "size rel v")
 Mtop = namedtuple("Mtop", "nmoltype moltypes nmolblock")
-Params = namedtuple("Params", "atnr ntypes functype reppow fudgeQQ iparams")
+Params = namedtuple("Params", "atnr ntypes functype reppow fudgeQQ")
 Atom = namedtuple("Atom", ["m", "q", "mB", "qB", "tp", "typeB", "ptype", "resind", "atomnumber"])
 Atoms = namedtuple("Atoms", "atoms nr nres type typeB atomnames resnames")
 Ilist = namedtuple("Ilist", "nr ik, iatoms")
@@ -62,8 +62,11 @@ class MoleculeKind(object):
         self.acceptors = acceptors
 
     def __repr__(self):
-        return "Molecule: {0:<20s} #atoms: {1:<10d} #residues: {2:<10d}".format(
-            self.name, self.number_of_atoms(), self.number_of_residues())
+        return (
+            f"Molecule: {self.name:<20s} "
+            f"#atoms: {self.number_of_atoms():<10d} "
+            f"#residues: {self.number_of_residues():<10d}"
+        )
 
     def number_of_atoms(self):
         return len(self.atomkinds)
@@ -111,10 +114,17 @@ class AtomKind(object):
         self.charge = charge
 
     def __repr__(self):
-        return \
-            ("< AtomKind: id {0:6d}, name {1:5s}, type {2:10s}, resid {3:6d}, resname {4:4s}, mass {5:8.4f}, "
-             "charge {6:12.3f} >".format(self.id, self.name, self.type, self.resid,
-                                         self.resname, self.mass, self.charge))
+        return (
+            f"< AtomKind: "
+            f"id {self.id:6d}, "
+            f"name {self.name:5s}, "
+            f"type {self.type:10s}, "
+            f"resid {self.resid:6d}, "
+            f"resname {self.resname:4s}, "
+            f"mass {self.mass:8.4f}, "
+            f"charge {6:12.3f} "
+            ">"
+        )
 
 
 class InteractionKind(object):
