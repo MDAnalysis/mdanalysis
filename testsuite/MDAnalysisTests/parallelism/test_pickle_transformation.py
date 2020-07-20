1c5bbdbdd353fbaf7fa380aada9ea07e3e8d35ee
Yuxuan Zhuang <yuxuan.zhuang@dbb.su.se>
2020-07-20 13:57:33 +0200

    pep

--- testsuite/MDAnalysisTests/parallelism/test_pickle_transformation.py
+++ testsuite/MDAnalysisTests/parallelism/test_pickle_transformation.py
@@ -20,9 +20,6 @@
 # MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
 # J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
 #
-import multiprocessing
-
-import numpy as np
 import pytest
 import pickle
 from numpy.testing import assert_equal
@@ -37,6 +34,7 @@ from MDAnalysis.transformations.wrap import wrap, unwrap
 
 from MDAnalysisTests.datafiles import TPR, XTC
 
+
 @pytest.fixture()
 def test_u():
     u = mda.Universe(TPR, XTC)
@@ -46,25 +44,28 @@ def test_u():
 u = mda.Universe(TPR, XTC)
 ag = u.atoms[0:10]
 
+
 @pytest.fixture(params=[
     (fit_translation, [ag, ag]),
     (fit_rot_trans, [ag, ag]),
     (PositionAverager, [3]),
-    (rotateby, [90, [0,0,1], [1,2,3]]),
-    (translate,[[1,2,3]]),
-    (center_in_box,[ag]),
-    (wrap,[u.atoms]),
-    (unwrap,[u.atoms])
+    (rotateby, [90, [0, 0, 1], [1, 2, 3]]),
+    (translate, [[1, 2, 3]]),
+    (center_in_box, [ag]),
+    (wrap, [u.atoms]),
+    (unwrap, [u.atoms])
 ])
 def transformation(request):
     transform = request.param[0](*request.param[1])
     return transform
+
 
 def test_transformation_pickle(transformation, test_u):
     ref_result = transformation(test_u.trajectory[5]).positions
     transformation_p = pickle.loads(pickle.dumps(transformation))
     result = transformation_p(test_u.trajectory[5]).positions
     assert_equal(ref_result, result)
+
 
 @pytest.mark.skip(reason="`Universe` cannot be picked now")
 def test_add_transformation_pickle(transformation, test_u):
@@ -73,4 +74,3 @@ def test_add_transformation_pickle(transformation, test_u):
     test_u.trajectory[0]
     for u_ts, u_p_ts in zip(test_u.trajectory[:5], test_u_p.trajectory[:5]):
         assert_equal(u_ts.positions, u_p_ts.positions)
-
