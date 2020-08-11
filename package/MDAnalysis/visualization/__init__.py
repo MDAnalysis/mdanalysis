1bd797f1be907d4eaf49b16deab82304184c86a7
Cédric Bouysset <cedric.bouysset@unice.fr>
2020-08-11 22:34:53 +0200

    Capitalize RDKit module

--- package/MDAnalysis/visualization/__init__.py
+++ package/MDAnalysis/visualization/__init__.py
@@ -23,6 +23,6 @@
 
 from . import streamlines
 from . import streamlines_3D
-from . import rdkit
+from . import RDKit
 
-__all__ = ['streamlines', 'streamlines_3D', 'rdkit']
+__all__ = ['streamlines', 'streamlines_3D', 'RDKit']
