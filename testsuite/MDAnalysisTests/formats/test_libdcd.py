MDAnalysis/mdanalysis

commit 563995c78c30a59404b37e4832f5c2a774ebba19

Serialize FileIO and TextIOWrapper and Universe (#2723)
* Fixes #2878
* basic approach: composition instead of inheritance for pickling Universe (which was
   tested in PR #2704 (which was derived from PR #2140))
* Changes made in this Pull Request:
    - add new classes and pickle_open function to picklable_file_io.py
    - add new picklable `BufferedReader`, `FileIO`, and `TextIOWrapper` classes.
    - implement `__getstate__`, `__setstate__` to `Universe` and `BaseReader`
    - fix DCD, XDR pickle issue
    - modified gsd and ncdf to be picklabel
    - modified ChainReader to be picklabel
    - ensure chemfiles is picklable
* add tests (MultiFrameReader will test for serializability)
* update CHANGELOG
* update docs

Note: This merge squashed 120 commits. See PR #2723 for the full history with 420 comments.

package/MDAnalysis/lib/formats/libdcd.pyx
@@ -261,8 +261,8 @@ cdef class DCDFile:
             return
 
         current_frame = state[1]
-        self.seek(current_frame)
-
+        self.seek(current_frame - 1)
+        self.current_frame = current_frame
 
     def tell(self):
         """

package/MDAnalysis/lib/formats/libmdaxdr.pyx
@@ -306,7 +306,8 @@ cdef class _XDRFile:
 
         # where was I
         current_frame = state[1]
-        self.seek(current_frame)
+        self.seek(current_frame - 1)
+        self.current_frame = current_frame
 
     def seek(self, frame):
         """Seek to Frame.

package/MDAnalysis/coordinates/DLPoly.py
@@ -32,6 +32,7 @@ import numpy as np
 
 from . import base
 from . import core
+from ..lib import util
 
 _DLPOLY_UNITS = {'length': 'Angstrom', 'velocity': 'Angstrom/ps', 'time': 'ps'}
 
@@ -149,7 +150,7 @@ class HistoryReader(base.ReaderBase):
         super(HistoryReader, self).__init__(filename, **kwargs)
 
         # "private" file handle
-        self._file = open(self.filename, 'r')
+        self._file = util.anyopen(self.filename, 'r')
         self.title = self._file.readline().strip()
         self._levcfg, self._imcon, self.n_atoms = np.int64(self._file.readline().split()[:3])
         self._has_vels = True if self._levcfg > 0 else False

package/MDAnalysis/coordinates/GSD.py
@@ -44,12 +44,19 @@ Classes
 .. autoclass:: GSDReader
    :inherited-members:
 
+.. autoclass:: GSDPicklable
+   :members:
+
+.. autofunction:: gsd_pickle_open
+
 """
 import numpy as np
-import os
+import gsd
+import gsd.fl
 import gsd.hoomd
 
 from . import base
+
 
 class GSDReader(base.ReaderBase):
     """Reader for the GSD format.
@@ -69,6 +76,10 @@ class GSDReader(base.ReaderBase):
 
 
         .. versionadded:: 0.17.0
+        .. versionchanged:: 2.0.0
+            Now use a picklable :class:`gsd.hoomd.HOOMDTrajectory`--
+            :class:`GSDPicklable`
+
         """
         super(GSDReader, self).__init__(filename, **kwargs)
         self.filename = filename
@@ -77,10 +88,10 @@ class GSDReader(base.ReaderBase):
         self.ts = self._Timestep(self.n_atoms, **self._ts_kwargs)
         self._read_next_timestep()
 
-    def open_trajectory(self) :
+    def open_trajectory(self):
         """opens the trajectory file using gsd.hoomd module"""
         self._frame = -1
-        self._file = gsd.hoomd.open(self.filename,mode='rb')
+        self._file = gsd_pickle_open(self.filename, mode='rb')
 
     def close(self):
         """close reader"""
@@ -97,7 +108,7 @@ class GSDReader(base.ReaderBase):
         self.open_trajectory()
 
     def _read_frame(self, frame):
-        try :
+        try:
             myframe = self._file[frame]
         except IndexError:
             raise IOError from None
@@ -111,20 +122,145 @@ class GSDReader(base.ReaderBase):
 
         # set frame box dimensions
         self.ts.dimensions = myframe.configuration.box
-        for i in range(3,6) :
-            self.ts.dimensions[i] = np.arccos(self.ts.dimensions[i]) * 180.0 / np.pi
+        self.ts.dimensions[3:] = np.rad2deg(np.arccos(self.ts.dimensions[3:]))
 
         # set particle positions
         frame_positions = myframe.particles.position
         n_atoms_now = frame_positions.shape[0]
-        if n_atoms_now != self.n_atoms :
+        if n_atoms_now != self.n_atoms:
             raise ValueError("Frame %d has %d atoms but the initial frame has %d"
                 " atoms. MDAnalysis in unable to deal with variable"
                 " topology!"%(frame, n_atoms_now, self.n_atoms))
-        else :
+        else:
             self.ts.positions = frame_positions
         return self.ts
 
-    def _read_next_timestep(self) :
+    def _read_next_timestep(self):
         """read next frame in trajectory"""
         return self._read_frame(self._frame + 1)
+
+
+class GSDPicklable(gsd.hoomd.HOOMDTrajectory):
+    """Hoomd GSD file object (read-only) that can be pickled.
+
+    This class provides a file-like object (as by :func:`gsd.hoomd.open`,
+    namely :class:`gsd.hoodm.HOOMDTrajectory`) that, unlike file objects,
+    can be pickled. Only read mode is supported.
+
+    When the file is pickled, filename and mode of :class:`gsd.fl.GSDFile` in
+    the file are saved. On unpickling, the file is opened by filename.
+    This means that for a successful unpickle, the original file still has to
+    be accessible with its filename.
+
+    Note
+    ----
+    Open hoomd GSD files with `gsd_pickle_open`.
+    After pickling, the current frame is reset. `universe.trajectory[i]` has
+    to be used to return to its original frame.
+
+    Parameters
+    ----------
+    file: :class:`gsd.fl.GSDFile`
+        File to access.
+
+    Example
+    -------
+    ::
+
+        gsdfileobj = gsd.fl.open(name=filename,
+                                     mode='rb',
+                                     application='gsd.hoomd '+gsd.__version__,
+                                     schema='hoomd',
+                                     schema_version=[1, 3])
+        file = GSDPicklable(gsdfileobj)
+        file_pickled = pickle.loads(pickle.dumps(file))
+
+    See Also
+    ---------
+    :func:`MDAnalysis.lib.picklable_file_io.FileIOPicklable`
+    :func:`MDAnalysis.lib.picklable_file_io.BufferIOPicklable`
+    :func:`MDAnalysis.lib.picklable_file_io.TextIOPicklable`
+    :func:`MDAnalysis.lib.picklable_file_io.GzipPicklable`
+    :func:`MDAnalysis.lib.picklable_file_io.BZ2Picklable`
+
+
+    .. versionadded:: 2.0.0
+    """
+    def __getstate__(self):
+        return self.file.name, self.file.mode
+
+    def __setstate__(self, args):
+        gsd_version = gsd.__version__
+        schema_version = [1, 4] if gsd_version >= '1.9.0' else [1, 3]
+        gsdfileobj = gsd.fl.open(name=args[0],
+                                 mode=args[1],
+                                 application='gsd.hoomd ' + gsd_version,
+                                 schema='hoomd',
+                                 schema_version=schema_version)
+        self.__init__(gsdfileobj)
+
+
+def gsd_pickle_open(name, mode='rb'):
+    """Open hoomd schema GSD file with pickle function implemented.
+
+    This function returns a GSDPicklable object. It can be used as a
+    context manager, and replace the built-in :func:`gsd.hoomd.open` function
+    in read mode that only returns an unpicklable file object.
+
+    Schema version will depend on the version of gsd module.
+
+    Note
+    ----
+    Can be only used with read mode.
+
+    Parameters
+    ----------
+    name : str
+        a filename given a text or byte string.
+    mode: {'r', 'rb'} (optional)
+        'r', 'rb':  open for reading;
+
+    Returns
+    -------
+    stream-like object: GSDPicklable
+
+    Raises
+    ------
+    ValueError
+        if `mode` is not one of the allowed read modes
+
+    Examples
+    -------
+    open as context manager::
+
+        with gsd_pickle_open('filename') as f:
+            line = f.readline()
+
+    open as function::
+
+        f = gsd_pickle_open('filename')
+        line = f.readline()
+        f.close()
+
+    See Also
+    --------
+    :func:`MDAnalysis.lib.util.anyopen`
+    :func:`MDAnalysis.lib.picklable_file_io.pickle_open`
+    :func:`MDAnalysis.lib.picklable_file_io.bz2_pickle_open`
+    :func:`MDAnalysis.lib.picklable_file_io.gzip_pickle_open`
+    :func:`gsd.hoomd.open`
+
+
+    .. versionadded:: 2.0.0
+    """
+    gsd_version = gsd.__version__
+    schema_version = [1, 4] if gsd_version >= '1.9.0' else [1, 3]
+    if mode not in {'r', 'rb'}:
+        raise ValueError("Only read mode ('r', 'rb') "
+                         "files can be pickled.")
+    gsdfileobj = gsd.fl.open(name=name,
+                             mode=mode,
+                             application='gsd.hoomd ' + gsd_version,
+                             schema='hoomd',
+                             schema_version=schema_version)
+    return GSDPicklable(gsdfileobj)

package/MDAnalysis/coordinates/TRJ.py
@@ -85,6 +85,8 @@ those and will raise a :exc:`NotImplementedError` if anything else is detected.
 .. autoclass:: NCDFWriter
    :members:
 
+.. autoclass:: NCDFPicklable
+   :members:
 
 .. _ascii-trajectories:
 
@@ -158,7 +160,6 @@ import logging
 import MDAnalysis
 from . import base
 from ..lib import util
-
 logger = logging.getLogger("MDAnalysis.coordinates.AMBER")
 
 
@@ -450,6 +451,9 @@ class NCDFReader(base.ReaderBase):
     .. versionchanged:: 1.0.0
        Support for reading `degrees` units for `cell_angles` has now been
        removed (Issue #2327)
+    .. versionchanged:: 2.0.0
+       Now use a picklable :class:`scipy.io.netcdf.netcdf_file`--
+       :class:`NCDFPicklable`.
 
     """
 
@@ -469,8 +473,8 @@ class NCDFReader(base.ReaderBase):
 
         super(NCDFReader, self).__init__(filename, **kwargs)
 
-        self.trjfile = scipy.io.netcdf.netcdf_file(self.filename,
-                                                   mmap=self._mmap)
+        self.trjfile = NCDFPicklable(self.filename,
+                                     mmap=self._mmap)
 
         # AMBER NetCDF files should always have a convention
         try:
@@ -1075,3 +1079,56 @@ class NCDFWriter(base.WriterBase):
         if self.trjfile is not None:
             self.trjfile.close()
             self.trjfile = None
+
+
+class NCDFPicklable(scipy.io.netcdf.netcdf_file):
+    """NetCDF file object (read-only) that can be pickled.
+
+    This class provides a file-like object (as returned by
+    :class:`scipy.io.netcdf.netcdf_file`) that,
+    unlike standard Python file objects,
+    can be pickled. Only read mode is supported.
+
+    When the file is pickled, filename and mmap of the open file handle in
+    the file are saved. On unpickling, the file is opened by filename,
+    and the mmap file is loaded.
+    This means that for a successful unpickle, the original file still has to
+    be accessible with its filename.
+
+    Parameters
+    ----------
+    filename : str or file-like
+        a filename given a text or byte string.
+    mmap : None or bool, optional
+        Whether to mmap `filename` when reading. True when `filename`
+        is a file name, False when `filename` is a file-like object.
+
+    Example
+    -------
+    ::
+
+        f = NCDFPicklable(NCDF)
+        print(f.variables['coordinates'].data)
+        f.close()
+
+    can also be used as context manager::
+
+        with NCDFPicklable(NCDF) as f:
+            print(f.variables['coordinates'].data)
+
+    See Also
+    ---------
+    :class:`MDAnalysis.lib.picklable_file_io.FileIOPicklable`
+    :class:`MDAnalysis.lib.picklable_file_io.BufferIOPicklable`
+    :class:`MDAnalysis.lib.picklable_file_io.TextIOPicklable`
+    :class:`MDAnalysis.lib.picklable_file_io.GzipPicklable`
+    :class:`MDAnalysis.lib.picklable_file_io.BZ2Picklable`
+
+
+    .. versionadded:: 2.0.0
+    """
+    def __getstate__(self):
+        return self.filename, self.use_mmap
+
+    def __setstate__(self, args):
+        self.__init__(args[0], mmap=args[1])

package/MDAnalysis/coordinates/base.py
@@ -226,6 +226,7 @@ class Timestep(object):
 
          create a timestep object with space for n_atoms
 
+
     .. versionchanged:: 0.11.0
        Added :meth:`from_timestep` and :meth:`from_coordinates` constructor
        methods.
@@ -233,6 +234,9 @@ class Timestep(object):
        :attr:`n_atoms` now a read only property.
        :attr:`frame` now 0-based instead of 1-based.
        Attributes `status` and `step` removed.
+    .. versionchanged:: 2.0.0
+       Timestep now can be (un)pickled. Weakref for Reader
+       will be dropped.
     """
     order = 'F'
 
@@ -299,7 +303,6 @@ class Timestep(object):
 
         # set up aux namespace for adding auxiliary data
         self.aux = Namespace()
-
 
     @classmethod
     def from_timestep(cls, other, **kwargs):
@@ -381,6 +384,22 @@ class Timestep(object):
 
         return ts
 
+    def __getstate__(self):
+        #  The `dt` property is lazy loaded.
+        #  We need to load it once from the `_reader` (if exists)
+        #  attached to this timestep to get the dt value.
+        #  This will help to (un)pickle a `Timestep` without pickling `_reader`
+        #  and retain its dt value.
+        self.dt
+
+        state = self.__dict__.copy()
+        state.pop('_reader', None)
+
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
     def _init_unitcell(self):
         """Create custom datastructure for :attr:`_unitcell`."""
         # override for other Timesteps
@@ -442,7 +461,7 @@ class Timestep(object):
             return self._pos[atoms]
         else:
             raise TypeError
-    
+
     def __getattr__(self, attr):
         # special-case timestep info
         if attr in ('velocities', 'forces', 'positions'):
@@ -1400,6 +1419,9 @@ class ProtoReader(IOBase, metaclass=_Readermeta):
 
     .. versionchanged:: 0.11.0
        Frames now 0-based instead of 1-based
+    .. versionchanged:: 2.0.0
+       Now supports (un)pickle. Upon unpickling,
+       the current timestep is retained by reconstrunction.
     """
 
     #: The appropriate Timestep class, e.g.
@@ -2060,6 +2082,9 @@ class ProtoReader(IOBase, metaclass=_Readermeta):
 
         return ts
 
+    def __setstate__(self, state):
+        self.__dict__ = state
+        self[self.ts.frame]
 
 
 class ReaderBase(ProtoReader):

package/MDAnalysis/coordinates/chain.py
@@ -211,6 +211,9 @@ class ChainReader(base.ProtoReader):
        added ``continuous`` trajectory option
     .. versionchanged:: 0.19.0
        limit output of __repr__
+    .. versionchanged:: 2.0.0
+       Now ChainReader can be (un)pickled. Upon unpickling,
+       current timestep is retained.
 
     """
     format = 'CHAIN'
@@ -413,6 +416,25 @@ class ChainReader(base.ProtoReader):
         # local frame index f in trajectory i (frame indices are 0-based)
         f = k - self._start_frames[i]
         return i, f
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        #  save ts temporarily otherwise it will be changed during rewinding.
+        state['ts'] = self.ts.__deepcopy__()
+
+        #  the ts.frame of each reader is set to the chained frame index during
+        #  iteration, thus we need to rewind the readers that have been used.
+        #  PR #2723
+        for reader in state['readers'][:self.__active_reader_index + 1]:
+            reader.rewind()
+
+        #  retrieve the current ts
+        self.ts = state['ts']
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.ts.frame = self.__current_frame
 
     # methods that can change with the current reader
     def convert_time_from_native(self, t):

package/MDAnalysis/coordinates/chemfiles.py
@@ -37,8 +37,8 @@ Classes
 
 .. autoclass:: ChemfilesWriter
 
+.. autoclass:: ChemfilesPicklable
 """
-import numpy as np
 from distutils.version import LooseVersion
 import warnings
 
@@ -48,6 +48,14 @@ try:
     import chemfiles
 except ImportError:
     HAS_CHEMFILES = False
+
+    # Allow building documentation even if chemfiles is not installed
+    import imp
+
+    class MockTrajectory:
+        pass
+    chemfiles = imp.new_module("chemfiles")
+    chemfiles.Trajectory = MockTrajectory
 else:
     HAS_CHEMFILES = True
 
@@ -132,7 +140,7 @@ class ChemfilesReader(base.ReaderBase):
         if isinstance(self.filename, chemfiles.Trajectory):
             self._file = self.filename
         else:
-            self._file = chemfiles.Trajectory(self.filename, 'r', self._format)
+            self._file = ChemfilesPicklable(self.filename, 'r', self._format)
 
     def close(self):
         """close reader"""
@@ -381,3 +389,71 @@ class ChemfilesWriter(base.WriterBase):
             topology.add_bond(bond.atoms[0].ix, bond.atoms[1].ix)
 
         return topology
+
+
+class ChemfilesPicklable(chemfiles.Trajectory):
+    """Chemfiles file object (read-only) that can be pickled.
+
+    This class provides a file-like object (as returned by
+    :class:`chemfiles.Trajectory`) that,
+    unlike standard Python file objects,
+    can be pickled. Only read mode is supported.
+
+    When the file is pickled, path, mode, and format of the file handle
+    are saved. On unpickling, the file is opened by path with mode,
+    and saved format.
+    This means that for a successful unpickle, the original file still has
+    to be accessible with its filename.
+
+    Note
+    ----
+    Can only be used with reading ('r') mode.
+    Upon pickling, the current frame is reset. `universe.trajectory[i]` has
+    to be used to return to its original frame.
+
+    Parameters
+    ----------
+    filename : str
+        a filename given a text or byte string.
+    mode : 'r' , optional
+        only 'r' can be used for pickling.
+    format : '', optional
+        guessed from the file extension if empty.
+
+    Example
+    -------
+    ::
+
+        f = ChemfilesPicklable(XYZ, 'r', '')
+        print(f.read())
+        f.close()
+
+    can also be used as context manager::
+
+        with ChemfilesPicklable(XYZ) as f:
+            print(f.read())
+
+    See Also
+    ---------
+    :class:`MDAnalysis.lib.picklable_file_io.FileIOPicklable`
+    :class:`MDAnalysis.lib.picklable_file_io.BufferIOPicklable`
+    :class:`MDAnalysis.lib.picklable_file_io.TextIOPicklable`
+    :class:`MDAnalysis.lib.picklable_file_io.GzipPicklable`
+    :class:`MDAnalysis.lib.picklable_file_io.BZ2Picklable`
+
+
+    .. versionadded:: 2.0.0
+    """
+    def __init__(self, path, mode="r", format=""):
+        if mode != 'r':
+            raise ValueError("Only read mode ('r') "
+                             "files can be pickled.")
+        super().__init__(path=path,
+                         mode=mode,
+                         format=format)
+
+    def __getstate__(self):
+        return self.path, self._Trajectory__mode, self._Trajectory__format
+
+    def __setstate__(self, args):
+        self.__init__(*args)

commit 23074c64f5de2795756c44eb16e488d4f1d253f0

fix DCD/XDR pickling on last frame (#2911)
- Fixes #2878
- Changes made in this Pull Request:
    - add more comprehensive tests for pickling low-level dcd and xdr files
    - fix a bug that was introduced in #2723 (wrong seeking in DCD and XDR, note: this bug was only in develop, never in released code, see discussion in PR #2908)
- update CHANGELOG
- maybe backport in #2908

package/MDAnalysis/lib/formats/libdcd.pyx
@@ -252,7 +252,7 @@ cdef class DCDFile:
                 self.__getstate__())
 
     def __getstate__(self):
-        return self.is_open, self.current_frame
+        return self.is_open, self.current_frame, self.n_frames
 
     def __setstate__(self, state):
         is_open = state[0]
@@ -261,8 +261,20 @@ cdef class DCDFile:
             return
 
         current_frame = state[1]
-        self.seek(current_frame - 1)
-        self.current_frame = current_frame
+        if current_frame < self.n_frames:
+            self.seek(current_frame)
+        elif current_frame == self.n_frames:
+            #  cannot seek to self.n_frames (a.k.a. len(DCDFile));
+            #  instead, we seek to the previous frame and read next. 
+            #  which is the state of the file when we need to serialize
+            #  at the end of the trajectory.
+            self.seek(current_frame - 1)
+            _ = self.read()
+        else:             # pragma: no cover
+            raise RuntimeError("Invalid frame number {} > {} -- this should"
+                               "not happen.".format(current_frame,
+                                                    self.n_frames)
+                              )
 
     def tell(self):
         """

package/MDAnalysis/lib/formats/libmdaxdr.pyx
@@ -306,8 +306,20 @@ cdef class _XDRFile:
 
         # where was I
         current_frame = state[1]
-        self.seek(current_frame - 1)
-        self.current_frame = current_frame
+        if current_frame < self.offsets.size:
+            self.seek(current_frame)
+        elif current_frame == self.offsets.size:
+            #  cannot seek to self.offsets.size (a.k.a len(_XDRFile));
+            #  instead, we seek to the previous frame and read next. 
+            #  which is the state of the file when we need to serialize
+            #  at the end of the trajectory.
+            self.seek(current_frame - 1)
+            _ = self.read()
+        else:       # pragma: no cover
+            raise RuntimeError("Invalid frame number {} > {} -- this should"
+                               "not happen.".format(current_frame,
+                                                    self.offsets.size)
+                              )
 
     def seek(self, frame):
         """Seek to Frame.

testsuite/MDAnalysisTests/formats/test_libdcd.py
@@ -83,23 +83,59 @@ def dcd():
         yield dcd
 
 
+def _assert_compare_readers(old_reader, new_reader):
+    #  same as next(old_reader)
+    frame = old_reader.read()
+    #  same as next(new_reader)
+    new_frame = new_reader.read()
+
+    assert old_reader.fname == new_reader.fname
+    assert old_reader.tell() == new_reader.tell()
+    assert_almost_equal(frame.xyz, new_frame.xyz)
+    assert_almost_equal(frame.unitcell, new_frame.unitcell)
+
+
 def test_pickle(dcd):
+    mid = len(dcd) // 2
+    dcd.seek(mid)
+    new_dcd = pickle.loads(pickle.dumps(dcd))
+    _assert_compare_readers(dcd, new_dcd)
+
+
+def test_pickle_last(dcd):
+    #  This is the file state when DCDReader is in its last frame.
+    #  (Issue #2878)
+
     dcd.seek(len(dcd) - 1)
-    dump = pickle.dumps(dcd)
-    new_dcd = pickle.loads(dump)
+    _ = dcd.read()
+    new_dcd = pickle.loads(pickle.dumps(dcd))
 
     assert dcd.fname == new_dcd.fname
     assert dcd.tell() == new_dcd.tell()
+    with pytest.raises(StopIteration):
+        new_dcd.read()
 
 
 def test_pickle_closed(dcd):
     dcd.seek(len(dcd) - 1)
     dcd.close()
-    dump = pickle.dumps(dcd)
-    new_dcd = pickle.loads(dump)
+    new_dcd = pickle.loads(pickle.dumps(dcd))
 
     assert dcd.fname == new_dcd.fname
     assert dcd.tell() != new_dcd.tell()
+
+
+def test_pickle_after_read(dcd):
+    _ = dcd.read()
+    new_dcd = pickle.loads(pickle.dumps(dcd))
+    _assert_compare_readers(dcd, new_dcd)
+
+
+def test_pickle_immediately(dcd):
+    new_dcd = pickle.loads(pickle.dumps(dcd))
+
+    assert dcd.fname == new_dcd.fname
+    assert dcd.tell() == new_dcd.tell()
 
 
 @pytest.mark.parametrize("new_frame", (10, 42, 21))

testsuite/MDAnalysisTests/formats/test_libmdaxdr.py
@@ -25,7 +25,7 @@ import pickle
 import numpy as np
 
 from numpy.testing import (assert_almost_equal, assert_array_almost_equal,
-                           assert_array_equal)
+                           assert_array_equal, assert_equal)
 
 from MDAnalysis.lib.formats.libmdaxdr import TRRFile, XTCFile
 
@@ -127,23 +127,52 @@ class TestCommonAPI(object):
             with pytest.raises(IOError):
                 f.read()
 
+    @staticmethod
+    def _assert_compare_readers(old_reader, new_reader):
+        frame = old_reader.read()
+        new_frame = new_reader.read()
+
+        assert old_reader.fname == new_reader.fname
+        assert old_reader.tell() == new_reader.tell()
+
+        assert_equal(old_reader.offsets, new_reader.offsets)
+        assert_almost_equal(frame.x, new_frame.x)
+        assert_almost_equal(frame.box, new_frame.box)
+        assert frame.step == new_frame.step
+        assert_almost_equal(frame.time, new_frame.time)
+
     def test_pickle(self, reader):
+        mid = len(reader) // 2
+        reader.seek(mid)
+        new_reader = pickle.loads(pickle.dumps(reader))
+        self._assert_compare_readers(reader, new_reader)
+
+    def test_pickle_last_frame(self, reader):
+        #  This is the file state when XDRReader is in its last frame.
+        #  (Issue #2878)
         reader.seek(len(reader) - 1)
-        dump = pickle.dumps(reader)
-        new_reader = pickle.loads(dump)
+        _ = reader.read()
+        new_reader = pickle.loads(pickle.dumps(reader))
 
         assert reader.fname == new_reader.fname
         assert reader.tell() == new_reader.tell()
-        assert_almost_equal(reader.offsets, new_reader.offsets)
+        with pytest.raises(StopIteration):
+            new_reader.read()
 
     def test_pickle_closed(self, reader):
         reader.seek(len(reader) - 1)
         reader.close()
-        dump = pickle.dumps(reader)
-        new_reader = pickle.loads(dump)
+        new_reader = pickle.loads(pickle.dumps(reader))
 
         assert reader.fname == new_reader.fname
         assert reader.tell() != new_reader.tell()
+
+    def test_pickle_immediately(self, reader):
+        new_reader = pickle.loads(pickle.dumps(reader))
+
+        assert reader.fname == new_reader.fname
+        assert reader.tell() == new_reader.tell()
+
 
 
 @pytest.mark.parametrize("xdrfile, fname, offsets",