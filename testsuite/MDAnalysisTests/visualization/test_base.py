ed438369804945abeeae3a5f6e94780daa2dcca3
Cédric Bouysset <cedric.bouysset@unice.fr>
2020-08-21 18:57:33 +0200

    add test for base class

---
+++ testsuite/MDAnalysisTests/visualization/test_base.py
@@ -0,0 +1,56 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# MDAnalysis --- https://www.mdanalysis.org
+# Copyright (c) 2006-2017 The MDAnalysis Development Team and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# Released under the GNU Public Licence, v2 or any higher version
+#
+# Please cite your use of MDAnalysis in published work:
+#
+# R. J. Gowers, M. Linke, J. Barnoud, T. J. E. Reddy, M. N. Melo, S. L. Seyler,
+# D. L. Dotson, J. Domanski, S. Buchoux, I. M. Kenney, and O. Beckstein.
+# MDAnalysis: A Python package for the rapid analysis of molecular dynamics
+# simulations. In S. Benthall and S. Rostrup editors, Proceedings of the 15th
+# Python in Science Conference, pages 102-109, Austin, TX, 2016. SciPy.
+# doi: 10.25080/majora-629e541a-00e
+#
+# N. Michaud-Agrawal, E. J. Denning, T. B. Woolf, and O. Beckstein.
+# MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
+# J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
+#
+
+import numpy as np
+from numpy.testing import assert_equal
+import pytest
+import MDAnalysis as mda
+from MDAnalysis.visualization.base import FormatterBase
+
+
+class Dummy(FormatterBase):
+    format = "dummy"
+
+
+class TestFormatterBase:
+    @pytest.fixture
+    def dummy(self):
+        return Dummy()
+
+    def test_add(self, dummy):
+        dummy_func = lambda: None
+        dummy.add_repr(int, "foo", dummy_func)
+        assert mda._FORMATTERS["dummy"][(int, "foo")] is dummy_func
+    
+    def test_reset_repr(self, dummy):
+        dummy_func = lambda: None
+        dummy.add_repr(int, "foo", dummy_func)
+        dummy.reset_repr(int, "foo")
+        assert (int, "foo") not in mda._FORMATTERS["dummy"].keys()
+
+    def test_reset_all_repr(self, dummy):
+        dummy_func = lambda: None
+        dummy.add_repr(int, "foo", dummy_func)
+        dummy.add_repr(float, "bar", dummy_func)
+        dummy.reset_all_repr()
+        assert mda._FORMATTERS["dummy"] == {}
