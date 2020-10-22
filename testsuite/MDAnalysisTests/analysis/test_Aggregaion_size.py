e87b4ca07ce202c523e82f0e00d76dcb131e00ed
aligh92 <alighamartale@gmail.com>
2020-10-21 23:28:57 -0230

    .xtc file has been replace to solve potential problem

--- testsuite/MDAnalysisTests/analysis/test_Aggregaion_size.py
+++ testsuite/MDAnalysisTests/analysis/test_Aggregaion_size.py
@@ -18,44 +18,44 @@ def atomgroup():
 
 
 def test_monomer_on_off(atomgroup):
-    ag_size_no_monomer_off = AggregationSize(atomgroup, number_of_mol=30,
+    ag_size_no_monomer_off = AggregationSize(atomgroup, number_of_mol=50,
                                              aggregation_criteria="Closest",
                                              no_monomer=False).run()
     assert_almost_equal(ag_size_no_monomer_off.results[:, 1],
-                        [1.4286, 2.7273, 2.7273, 3.75, 3.3333, 4.2857],
+                        [1.92308, 3.125, 7.1429, 8.3333, 6.25, 7.1429],
                         decimal=4)
 
 
 def test_agregation_closest(atomgroup):
-    ag_size = AggregationSize(atomgroup, number_of_mol=30,
+    ag_size = AggregationSize(atomgroup, number_of_mol=50,
                               aggregation_criteria="Closest",
                               no_monomer=True).run()
     assert_almost_equal(ag_size.results[:, 1],
-                        [2.2857, 3.7143, 4.1667, 5.4, 4, 4.2857], decimal=4)
+                        [3, 3.2667, 7.1429, 9.8, 7, 7.1429], decimal=4)
 
 
 def test_agregation_com(atomgroup):
-    ag_size = AggregationSize(atomgroup, number_of_mol=30,
+    ag_size = AggregationSize(atomgroup, number_of_mol=50,
                               aggregation_criteria="COM",
                               no_monomer=True).run()
-    assert_almost_equal(ag_size.results[:, 2], [2, 3.4211, 3.1, 3, 3, 3],
+    assert_almost_equal(ag_size.results[:, 2], [2, 2.95, 3.5909, 3.1304, 3.7917, 4.6522],
                         decimal=4)
 
 
 def test_agregation_specific(atomgroup):
-    ag_size = AggregationSize(atomgroup, number_of_mol=30,
+    ag_size = AggregationSize(atomgroup, number_of_mol=50,
                               aggregation_criteria="Specific",
                               no_monomer=True, atom_1="C1",
                               atom_2="C1").run()
     assert_almost_equal(ag_size.results[:, 3],
-                        [2, 3.871, 3.2807, 3.3226, 4, 3.8052], decimal=4)
+                        [2, 3.6972, 5.1977, 3.0840, 5.7573, 8.5312], decimal=4)
 
 
 def test_gyr_on_off(atomgroup):
-    ag_size_gyr_on = AggregationSize(atomgroup, number_of_mol=30,
+    ag_size_gyr_on = AggregationSize(atomgroup, number_of_mol=50,
                                      aggregation_criteria="Closest",
                                      no_monomer=True,
                                      ave_gyr_calc=True).run()
     assert_almost_equal(ag_size_gyr_on.results[:, 4],
-                        [8.1545, 10.2749, 10.7043, 11.229, 10.4413, 10.1946],
+                        [9.7073, 8.7964, 11.8574, 12.1666, 11.5719, 10.9102],
                         decimal=4)
