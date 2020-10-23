5ac87b6e0cd731b328bd0e80d2442462c18a3145
Oliver Beckstein <orbeckst@gmail.com>
2020-10-23 13:40:51 -0700

    removed funcsigs (not needed for Py >= 3.6)

--- package/MDAnalysis/core/topologyattrs.py
+++ package/MDAnalysis/core/topologyattrs.py
@@ -42,13 +42,7 @@ import numbers
 import numpy as np
 import warnings
 import textwrap
-
-# inspect.signature was added in python 3.3, earlier versions require
-# funcsigs as backport.
-try:
-    from inspect import signature as inspect_signature
-except ImportError:
-    from funcsigs import signature as inspect_signature
+from inspect import signature as inspect_signature
 
 from ..lib.util import (cached, convert_aa_code, iterable, warn_if_not_unique,
                         unique_int_1d)
