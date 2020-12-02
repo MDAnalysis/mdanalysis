9742cfa4c46de6c3740896db86df6a86236b58b6
Irfan Alibay <IAlibay@users.noreply.github.com>
2020-12-02 18:56:39 +0000

    Adds basic GH Actions CI workflow (#3040)
    Fixes #3036 
    
    ## Work done in this PR
    - Adds a continuous integration workflow based on github actions.
    - Disables previous continuous integration workflow based on TravisCI.
    - Fixes minor Visibledeprecation warnings with the hole2 tests.

--- testsuite/MDAnalysisTests/analysis/test_hole2.py
+++ testsuite/MDAnalysisTests/analysis/test_hole2.py
@@ -488,7 +488,7 @@ class TestHoleAnalysisLong(BaseTestHole):
             assert key == rmsd
 
         idx = np.argsort(op)
-        arr = np.array(list(hole.profiles.values()))
+        arr = np.array(list(hole.profiles.values()), dtype=object)
         for op_prof, arr_prof in zip(profiles.values(), arr[idx]):
             assert op_prof is arr_prof
 
@@ -504,7 +504,7 @@ class TestHoleAnalysisLong(BaseTestHole):
             assert key == rmsd
 
         idx = np.argsort(op)
-        arr = np.array(list(hole.profiles.values()))
+        arr = np.array(list(hole.profiles.values()), dtype=object)
         for op_prof, arr_prof in zip(profiles.values(), arr[idx]):
             assert op_prof is arr_prof
 
@@ -528,7 +528,7 @@ class TestHoleAnalysisLong(BaseTestHole):
 
         idx = np.argsort(op[:n_frames])
         values = list(hole.profiles.values())[:n_frames]
-        arr = np.array(values)
+        arr = np.array(values, dtype=object)
         for op_prof, arr_prof in zip(profiles.values(), arr[idx]):
             assert op_prof is arr_prof
 
