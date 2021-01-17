c7caaef0b1092b3b2345b296aaf378e7102caeec
Aditya Kamath <adtykamath@gmail.com>
2021-01-17 16:51:35 -0500

    Minor change to \ break line

--- package/MDAnalysis/analysis/density.py
+++ package/MDAnalysis/analysis/density.py
@@ -849,5 +849,4 @@ class Density(Grid):
             grid_type = 'density'
         else:
             grid_type = 'histogram'
-        return '<Density ' + grid_type + ' with ' + \
-            str(self.grid.shape) + ' bins>'
+            return f"<Density {grid_type} with {self.grid.shape} bins>"
