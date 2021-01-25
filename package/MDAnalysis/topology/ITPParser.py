26806fc939a16490dcb1a77bae36c7356103c733
Aditya Kamath <adtykamath@gmail.com>
2021-01-25 10:59:20 -0500

    Slight change to ITPParser

--- package/MDAnalysis/topology/ITPParser.py
+++ package/MDAnalysis/topology/ITPParser.py
@@ -265,10 +265,9 @@ class GmxTopIterator:
             current_file = self.current_file
 
         try:
-            path = path.name
+            path = os.path.abspath(path.name)
         except AttributeError:
             pass
-        path = os.path.abspath(path)
         current_dir = os.path.dirname(current_file)
         dir_path = os.path.join(current_dir, path)
         if os.path.exists(dir_path):
