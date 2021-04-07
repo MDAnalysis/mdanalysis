4478fbafe1d5a3dbc7afa7b6a234279c47da9264
Paarth Thadani <paarth.thadani15@gmail.com>
2021-04-07 15:13:55 +0530

    Updated docstrings

--- package/MDAnalysis/analysis/encore/clustering/ClusteringMethod.py
+++ package/MDAnalysis/analysis/encore/clustering/ClusteringMethod.py
@@ -195,7 +195,8 @@ if sklearn:
                 (default is 50). Parameter in the Affinity Propagation for
                 clustering.
 
-            **kwargs (optional): Other keyword arguments are passed to :class:`sklearn.cluster.AffinityPropagation`.
+            **kwargs : optional
+                Other keyword arguments are passed to :class:`sklearn.cluster.AffinityPropagation`.
 
             """
             self.ap = \
