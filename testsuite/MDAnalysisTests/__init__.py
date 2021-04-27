42ce76a13d4fd8b8635a98d3f2a24b84ea71ab81
IAlibay <irfan.alibay@gmail.com>
2021-04-27 05:00:21 +0100

    Prep for release 1.1.0

--- testsuite/MDAnalysisTests/__init__.py
+++ testsuite/MDAnalysisTests/__init__.py
@@ -98,7 +98,7 @@ import pytest
 logger = logging.getLogger("MDAnalysisTests.__init__")
 
 # keep in sync with RELEASE in setup.py
-__version__ = "1.0.2-dev0"
+__version__ = "1.1.0"
 
 
 # Do NOT import MDAnalysis at this level. Tests should do it themselves.
