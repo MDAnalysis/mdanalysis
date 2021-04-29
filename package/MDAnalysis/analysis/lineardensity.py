9ac4c4ca6e8421414941e997f3f7c3eeffebefff
Philip Loche <ploche@physik.fu-berlin.de>
2021-04-29 16:43:47 +0200

    Consistent indention

--- package/MDAnalysis/analysis/lineardensity.py
+++ package/MDAnalysis/analysis/lineardensity.py
@@ -100,9 +100,9 @@ class LinearDensity(AnalysisBase):
 
     .. code-block:: python
 
-        ldens = LinearDensity(selection)
-        ldens.run()
-        print(ldens.results.x.pos)
+       ldens = LinearDensity(selection)
+       ldens.run()
+       print(ldens.results.x.pos)
 
 
     .. versionadded:: 0.14.0
@@ -118,132 +118,132 @@ class LinearDensity(AnalysisBase):
        Changed `selection` keyword to `select`
 
     .. versionchanged:: 2.0.0
-       Results are now instances of 
+       Results are now instances of
        :class:`~MDAnalysis.core.analysis.Results` allowing access
        via key and attribute.
     """
 
     def __init__(self, select, grouping='atoms', binsize=0.25, **kwargs):
-        super(LinearDensity, self).__init__(select.universe.trajectory,
-                                            **kwargs)
-        # allows use of run(parallel=True)
-        self._ags = [select]
-        self._universe = select.universe
+       super(LinearDensity, self).__init__(select.universe.trajectory,
+                                          **kwargs)
+       # allows use of run(parallel=True)
+       self._ags = [select]
+       self._universe = select.universe
 
-        self.binsize = binsize
+       self.binsize = binsize
 
-        # group of atoms on which to compute the COM (same as used in
-        # AtomGroup.wrap())
-        self.grouping = grouping
+       # group of atoms on which to compute the COM (same as used in
+       # AtomGroup.wrap())
+       self.grouping = grouping
 
-        # Initiate result instances
-        self.results.x = Results(dim=0)
-        self.results.y = Results(dim=1)
-        self.results.z = Results(dim=2)
-        # Box sides
-        self.dimensions = self._universe.dimensions[:3]
-        self.volume = np.prod(self.dimensions)
-        # number of bins
-        bins = (self.dimensions // self.binsize).astype(int)
+       # Initiate result instances
+       self.results.x = Results(dim=0)
+       self.results.y = Results(dim=1)
+       self.results.z = Results(dim=2)
+       # Box sides
+       self.dimensions = self._universe.dimensions[:3]
+       self.volume = np.prod(self.dimensions)
+       # number of bins
+       bins = (self.dimensions // self.binsize).astype(int)
 
-        # Here we choose a number of bins of the largest cell side so that
-        # x, y and z values can use the same "coord" column in the output file
-        self.nbins = bins.max()
-        slices_vol = self.volume / bins
+       # Here we choose a number of bins of the largest cell side so that
+       # x, y and z values can use the same "coord" column in the output file
+       self.nbins = bins.max()
+       slices_vol = self.volume / bins
 
-        self.keys = ['pos', 'pos_std', 'char', 'char_std']
+       self.keys = ['pos', 'pos_std', 'char', 'char_std']
 
-        # Initialize results array with zeros
-        for dim in self.results:
-            idx = self.results[dim]['dim']
-            self.results[dim]['slice volume'] = slices_vol[idx]
-            for key in self.keys:
-                self.results[dim][key] = np.zeros(self.nbins)
+       # Initialize results array with zeros
+       for dim in self.results:
+           idx = self.results[dim]['dim']
+           self.results[dim]['slice volume'] = slices_vol[idx]
+       for key in self.keys:
+              self.results[dim][key] = np.zeros(self.nbins)
 
-        # Variables later defined in _prepare() method
-        self.masses = None
-        self.charges = None
-        self.totalmass = None
+       # Variables later defined in _prepare() method
+       self.masses = None
+       self.charges = None
+       self.totalmass = None
 
     def _prepare(self):
-        # group must be a local variable, otherwise there will be
-        # issues with parallelization
-        group = getattr(self._ags[0], self.grouping)
+       # group must be a local variable, otherwise there will be
+       # issues with parallelization
+       group = getattr(self._ags[0], self.grouping)
 
-        # Get masses and charges for the selection
-        try:  # in case it's not an atom
-            self.masses = np.array([elem.total_mass() for elem in group])
-            self.charges = np.array([elem.total_charge() for elem in group])
-        except AttributeError:  # much much faster for atoms
-            self.masses = self._ags[0].masses
-            self.charges = self._ags[0].charges
+       # Get masses and charges for the selection
+       try:  # in case it's not an atom
+           self.masses = np.array([elem.total_mass() for elem in group])
+           self.charges = np.array([elem.total_charge() for elem in group])
+       except AttributeError:  # much much faster for atoms
+           self.masses = self._ags[0].masses
+           self.charges = self._ags[0].charges
 
-        self.totalmass = np.sum(self.masses)
+       self.totalmass = np.sum(self.masses)
 
     def _single_frame(self):
-        self.group = getattr(self._ags[0], self.grouping)
-        self._ags[0].wrap(compound=self.grouping)
+       self.group = getattr(self._ags[0], self.grouping)
+       self._ags[0].wrap(compound=self.grouping)
 
-        # Find position of atom/group of atoms
-        if self.grouping == 'atoms':
-            positions = self._ags[0].positions  # faster for atoms
-        else:
-            # COM for res/frag/etc
-            positions = np.array([elem.centroid() for elem in self.group])
+       # Find position of atom/group of atoms
+       if self.grouping == 'atoms':
+           positions = self._ags[0].positions  # faster for atoms
+       else:
+       # COM for res/frag/etc
+           positions = np.array([elem.centroid() for elem in self.group])
 
-        for dim in ['x', 'y', 'z']:
-            idx = self.results[dim]['dim']
+       for dim in ['x', 'y', 'z']:
+           idx = self.results[dim]['dim']
 
-            key = 'pos'
-            key_std = 'pos_std'
-            # histogram for positions weighted on masses
-            hist, _ = np.histogram(positions[:, idx],
-                                   weights=self.masses,
-                                   bins=self.nbins,
-                                   range=(0.0, max(self.dimensions)))
+       key = 'pos'
+       key_std = 'pos_std'
+       # histogram for positions weighted on masses
+       hist, _ = np.histogram(positions[:, idx],
+                              weights=self.masses,
+                              bins=self.nbins,
+                              range=(0.0, max(self.dimensions)))
 
-            self.results[dim][key] += hist
-            self.results[dim][key_std] += np.square(hist)
+       self.results[dim][key] += hist
+       self.results[dim][key_std] += np.square(hist)
 
-            key = 'char'
-            key_std = 'char_std'
-            # histogram for positions weighted on charges
-            hist, _ = np.histogram(positions[:, idx],
-                                   weights=self.charges,
-                                   bins=self.nbins,
-                                   range=(0.0, max(self.dimensions)))
+       key = 'char'
+       key_std = 'char_std'
+       # histogram for positions weighted on charges
+       hist, _ = np.histogram(positions[:, idx],
+                              weights=self.charges,
+                              bins=self.nbins,
+                              range=(0.0, max(self.dimensions)))
 
-            self.results[dim][key] += hist
-            self.results[dim][key_std] += np.square(hist)
+       self.results[dim][key] += hist
+       self.results[dim][key_std] += np.square(hist)
 
     def _conclude(self):
-        k = 6.022e-1  # divide by avodagro and convert from A3 to cm3
+       k = 6.022e-1  # divide by avodagro and convert from A3 to cm3
 
-        # Average results over the  number of configurations
-        for dim in ['x', 'y', 'z']:
-            for key in ['pos', 'pos_std', 'char', 'char_std']:
-                self.results[dim][key] /= self.n_frames
-            # Compute standard deviation for the error
-            self.results[dim]['pos_std'] = np.sqrt(self.results[dim][
-                'pos_std'] - np.square(self.results[dim]['pos']))
-            self.results[dim]['char_std'] = np.sqrt(self.results[dim][
-                'char_std'] - np.square(self.results[dim]['char']))
+       # Average results over the  number of configurations
+       for dim in ['x', 'y', 'z']:
+           for key in ['pos', 'pos_std', 'char', 'char_std']:
+              self.results[dim][key] /= self.n_frames
+           # Compute standard deviation for the error
+           self.results[dim]['pos_std'] = np.sqrt(self.results[dim][
+                     'pos_std'] - np.square(self.results[dim]['pos']))
+           self.results[dim]['char_std'] = np.sqrt(self.results[dim][
+                     'char_std'] - np.square(self.results[dim]['char']))
 
-        for dim in ['x', 'y', 'z']:
-            norm = k * self.results[dim]['slice volume']
-            for key in self.keys:
-                 self.results[dim][key] /= norm
+       for dim in ['x', 'y', 'z']:
+           norm = k * self.results[dim]['slice volume']
+       for key in self.keys:
+           self.results[dim][key] /= norm
 
     def _add_other_results(self, other):
-        # For parallel analysis
-        results = self.results
-        for dim in ['x', 'y', 'z']:
-            key = 'pos'
-            key_std = 'pos_std'
-            results[dim][key] += other[dim][key]
-            results[dim][key_std] += other[dim][key_std]
+       # For parallel analysis
+       results = self.results
+       for dim in ['x', 'y', 'z']:
+           key = 'pos'
+           key_std = 'pos_std'
+           results[dim][key] += other[dim][key]
+           results[dim][key_std] += other[dim][key_std]
 
-            key = 'char'
-            key_std = 'char_std'
-            results[dim][key] += other[dim][key]
-            results[dim][key_std] += other[dim][key_std]
+           key = 'char'
+           key_std = 'char_std'
+           results[dim][key] += other[dim][key]
+           results[dim][key_std] += other[dim][key_std]
