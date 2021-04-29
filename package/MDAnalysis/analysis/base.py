8d67b9586434f1d2d7df0430b21168d8554499a6
Philip Loche <ploche@physik.fu-berlin.de>
2021-04-29 15:07:07 +0200

    Better docs
    Co-authored-by: Oliver Beckstein <orbeckst@gmail.com>

--- package/MDAnalysis/analysis/base.py
+++ package/MDAnalysis/analysis/base.py
@@ -44,7 +44,7 @@ class Results(dict):
     r"""Container object for storing results.
 
     Results are dictionaries that provide two ways in which can values be accessed:
-    `results["value_key"]`, or by an attribute, `results.value_key`. 
+    by dict key ``results["value_key"]`` or by object attribute, ``results.value_key``. 
     They store all results obatined from an analysis after the ``run`` call.
 
     The current implementation is similar to the `Bunch` class in `sklearn`.
