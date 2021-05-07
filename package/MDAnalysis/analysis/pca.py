91941165dd9f175f34ba502a245d56c38404cedb
Philip Loche <ploche@physik.fu-berlin.de>
2021-05-08 01:07:52 +0200

    PEP8

--- package/MDAnalysis/analysis/pca.py
+++ package/MDAnalysis/analysis/pca.py
@@ -91,8 +91,8 @@ Inspect the components to determine the principal components you would like
 to retain. The choice is arbitrary, but I will stop when 95 percent of the
 variance is explained by the components. This cumulated variance by the
 components is conveniently stored in the one-dimensional array attribute
-``results.cumulated_variance``. The value at the ith index of `results.cumulated_variance`
-is the sum of the variances from 0 to i.
+``results.cumulated_variance``. The value at the ith index of
+``results.cumulated_variance`` is the sum of the variances from 0 to i.
 
 .. code-block:: python
 
