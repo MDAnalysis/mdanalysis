947f497db7757bdf2dbd50b120aceedc5403a8c7
Irfan Alibay <IAlibay@users.noreply.github.com>
2021-05-13 14:52:31 +0100

    Apply suggestions from code review

--- testsuite/MDAnalysisTests/analysis/test_lineardensity.py
+++ testsuite/MDAnalysisTests/analysis/test_lineardensity.py
@@ -22,7 +22,6 @@
 #
 import MDAnalysis as mda
 import numpy as np
-import pytest
 
 from MDAnalysisTests.datafiles import waterPSF, waterDCD
 from MDAnalysis.analysis.lineardensity import LinearDensity
@@ -38,11 +37,3 @@ def test_serial():
                           0., 0., 0., 0.])
     ld = LinearDensity(selection, binsize=5).run()
     assert_almost_equal(xpos, ld.results['x']['pos'])
-
-
-def test_results_warning():
-    universe = mda.Universe(waterPSF, waterDCD)
-    msg = ("The structure of the `results` dictionary will change in "
-            "MDAnalysis version 2.0.")
-    with pytest.warns(DeprecationWarning, match=msg):
-        LinearDensity(universe.atoms).run()
