dcbe40966886749106d9395eac7ec94f092d7cda
IAlibay <irfan.alibay@gmail.com>
2021-05-22 23:10:46 +0100

    bump up release to 2.0.0-beta.1
    drop .1 to make tests not fail

--- testsuite/setup.py
+++ testsuite/setup.py
@@ -87,7 +87,7 @@ if sys.version_info[:2] < (3, 6):
 
 if __name__ == '__main__':
     # this must be in-sync with MDAnalysis
-    RELEASE = "2.0.0-dev0"
+    RELEASE = "2.0.0-beta"
     with open("README") as summary:
         LONG_DESCRIPTION = summary.read()
 
