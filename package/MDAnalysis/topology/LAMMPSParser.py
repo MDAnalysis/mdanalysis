7ec8a62ce19050d08cabe845a1e22adcb59cfdcc
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-07-15 19:49:12 +1000

    add versionchanged

--- package/MDAnalysis/topology/LAMMPSParser.py
+++ package/MDAnalysis/topology/LAMMPSParser.py
@@ -586,7 +586,7 @@ class DATAParser(TopologyReaderBase):
 class LammpsDumpParser(TopologyReaderBase):
     """Parses Lammps ascii dump files in 'atom' format.
     Sets all masses to 1.0.
-
+    .. versionchanged:: 2.0.0
     .. versionadded:: 0.19.0
     """
     format = 'LAMMPSDUMP'
