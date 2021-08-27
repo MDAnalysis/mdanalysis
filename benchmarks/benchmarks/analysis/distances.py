5ae0947b7e5b26cfdfdaf25c9b8289287b130e3e
miro-astore <miro.astore@gmail.com>
2021-08-27 18:04:09 +1000

    Added tests of different PBC conditions and fixed failing distances benchmarks
    The benchmarked distances functions now all test 3 PBC conditions:
    None, triclinic, orthogonal.
    
    Separated matrix contacts functions and simple distance functions into
    DistanceBench and ContactsBench classes to deal with time complexity issues.

--- benchmarks/benchmarks/analysis/distances.py
+++ benchmarks/benchmarks/analysis/distances.py
@@ -1,6 +1,12 @@
 import MDAnalysis
 import numpy as np
 
+
+try:
+    from MDAnalysis.lib import mdamath
+except:
+    pass
+
 try:
     from MDAnalysisTests.datafiles import GRO
 except:
@@ -18,10 +24,46 @@ class DistancesBench(object):
 
     # TODO: eventually we should include box / pbc
     # unit cell information in the benchmarks
-    params = (10, 100, 1000, 10000)
-    param_names = ['num_atoms']
+    temp_universe = MDAnalysis.Universe(GRO)
+    u = MDAnalysis.Universe(GRO)
+    tri_clinic_dimensions = u.dimensions
 
-    def setup(self, num_atoms):
+
+    params = ([10, 100, 1000, 10000], [None, 'orthogonal', 'triclinic'])
+    param_names = [ 'num_atoms','pbc_type']
+
+    def setup(self, num_atoms, pbc_type):
+
+        if pbc_type == None: 
+            self.u = MDAnalysis.Universe(GRO)
+            self.box_dims = None
+
+        elif pbc_type == 'orthogonal': 
+            self.u = MDAnalysis.Universe(GRO)
+            box_shape =  self.u.dimensions
+            basis_vectors = mdamath.triclinic_vectors(box_shape)
+
+            #this will calculate the furthest point from the origin on the unit cell.
+            furthest_point = np.sum(basis_vectors, axis = 0)
+            #get full size of cube and also deal with certain distance functions requiring float32
+            orthogonal_box_size = np.float32(np.max(furthest_point))
+            angle=np.float32(90.0)
+            
+            #make dummy cubic box, must be np.array, does not accept list
+            self.box_dims = np.array([orthogonal_box_size,orthogonal_box_size,orthogonal_box_size,angle,angle,angle])
+
+        elif pbc_type == 'triclinic': 
+            self.u = MDAnalysis.Universe(GRO)
+            print(self.u.dimensions)
+            self.box_dims = self.u.dimensions
+
+        else:
+            raise Exception("You must specify 'orthogonal', 'triclinic', or None for the box type")
+
+        self.ag1 = self.u.atoms[:num_atoms]
+        self.ag2 = self.u.atoms[num_atoms: 2 * num_atoms]
+        self.ag3 = self.u.atoms[-num_atoms:]
+
         np.random.seed(17809)
         self.coords_1 = np.random.random_sample((num_atoms, 3)).astype(np.float32)
         np.random.seed(9008716)
@@ -31,23 +73,19 @@ class DistancesBench(object):
         self.array_shape_1D = int(num_atoms * (num_atoms - 1) / 2.)
         self.allocated_array_1D = np.empty(self.array_shape_1D,
                                            dtype=np.float64)
-        self.u = MDAnalysis.Universe(GRO)
-        self.ag1 = self.u.atoms[:num_atoms]
-        self.ag2 = self.u.atoms[num_atoms: 2 * num_atoms]
-        self.ag3 = self.u.atoms[-num_atoms:]
 
-    def time_distance_array(self, num_atoms):
+    def time_distance_array(self, num_atoms, pbc_type):
         """Benchmark calculation of all distances
         between two numpy arrays of coordinates,
         using default arguments to distance_array.
         """
         distances.distance_array(reference=self.coords_1,
                                  configuration=self.coords_2,
-                                 box=None,
+                                 box=self.box_dims,
                                  result=None,
                                  backend='serial')
 
-    def time_distance_array_pre_allocated(self, num_atoms):
+    def time_distance_array_pre_allocated(self, num_atoms, pbc_type):
         """Benchmark calculation of all distances
         between two numpy arrays of coordinates,
         using distance_array with a preallocated
@@ -55,32 +93,119 @@ class DistancesBench(object):
         """
         distances.distance_array(reference=self.coords_1,
                                  configuration=self.coords_2,
-                                 box=None,
+                                 box=self.box_dims,
                                  result=self.allocated_array_2D,
                                  backend='serial')
 
-    def time_self_distance_array(self, num_atoms):
+    def time_self_distance_array(self, num_atoms, pbc_type):
         """Benchmark calculation of all distances
         within a single numpy array of coordinates
         using default parameters to self_distance_array.
         """
         distances.self_distance_array(reference=self.coords_1,
-                                      box=None,
+                                      box=self.box_dims,
                                       result=None,
                                       backend='serial')
 
-    def time_self_distance_array_pre_allocated(self, num_atoms):
+    def time_self_distance_array_pre_allocated(self, num_atoms, pbc_type):
         """Benchmark calculation of all distances
         within a single numpy array of coordinates
         using self_distance_array with preallocated
         result array.
         """
         distances.self_distance_array(reference=self.coords_1,
-                                      box=None,
+                                      box=self.box_dims,
                                       result=self.allocated_array_1D,
                                       backend='serial')
 
-    def time_contact_matrix(self, num_atoms):
+
+    def time_dist(self, num_atoms, box_dims):
+        """Benchmark calculation of distances between
+        atoms in two atomgroups with no offsets
+        to resids.
+        """
+        distances.dist(A=self.ag1,
+                       B=self.ag2,
+                       box=self.box_dims,
+                       offset=0)
+
+    def time_dist_offsets(self, num_atoms, box_dims):
+        """Benchmark calculation of distances between
+        atoms in two atomgroups with offsets
+        to resids.
+        """
+        distances.dist(A=self.ag1,
+                       B=self.ag2,
+                       box=self.box_dims,
+                       offset=20)
+
+    def time_between(self, num_atoms,box_dims):
+        """Benchmark determination of subgroup
+        of atomgroup that is within a specific
+        distance of two other atomgroups.
+        """
+        distances.between(group=self.ag3,
+                          A=self.ag1,
+                          B=self.ag2, 
+                          distance=15.0)
+
+class ContactsBench(object):
+    """Benchmarks for MDAnalysis.analysis.distances
+    functions.
+    """
+
+    # TODO: eventually we should include box / pbc
+    # unit cell information in the benchmarks
+    temp_universe = MDAnalysis.Universe(GRO)
+    u = MDAnalysis.Universe(GRO)
+    tri_clinic_dimensions = u.dimensions
+
+
+    params = ([10, 100, 1000], [None, 'orthogonal', 'triclinic'])
+    param_names = [ 'num_atoms','pbc_type']
+
+    def setup(self, num_atoms, pbc_type):
+
+        if pbc_type == None: 
+            self.u = MDAnalysis.Universe(GRO)
+            self.box_dims = None
+
+        elif pbc_type == 'orthogonal': 
+            self.u = MDAnalysis.Universe(GRO)
+            box_shape =  self.u.dimensions
+            basis_vectors = mdamath.triclinic_vectors(box_shape)
+
+            #this will calculate the furthest point from the origin on the unit cell.
+            furthest_point = np.sum(basis_vectors, axis = 0)
+            #get full size of cube and also deal with certain distance functions requiring float32
+            orthogonal_box_size = np.float32(np.max(furthest_point))
+            angle=np.float32(90.0)
+            
+            #make dummy cubic box, must be np.array, does not accept list
+            self.box_dims = np.array([orthogonal_box_size, orthogonal_box_size, orthogonal_box_size, angle, angle, angle])
+
+        elif pbc_type == 'triclinic': 
+            self.u = MDAnalysis.Universe(GRO)
+            print(self.u.dimensions)
+            self.box_dims = self.u.dimensions
+        else:
+            raise Exception("You must specify 'orthogonal', 'triclinic', or None for the box type")
+
+        self.ag1 = self.u.atoms[:num_atoms]
+        self.ag2 = self.u.atoms[num_atoms: 2 * num_atoms]
+        self.ag3 = self.u.atoms[-num_atoms:]
+
+        np.random.seed(17809)
+        self.coords_1 = np.random.random_sample((num_atoms, 3)).astype(np.float32)
+        np.random.seed(9008716)
+        self.coords_2 = np.random.random_sample((num_atoms, 3)).astype(np.float32)
+        self.allocated_array_2D = np.empty((num_atoms, num_atoms),
+                                            dtype=np.float64)
+        self.array_shape_1D = int(num_atoms * (num_atoms - 1) / 2.)
+        self.allocated_array_1D = np.empty(self.array_shape_1D,
+                                           dtype=np.float64)
+
+    def time_contact_matrix(self, num_atoms, pbc_type):
         """Benchmark calculation of contacts within
         a single numpy array using the default arguments
         to contact_matrix.
@@ -88,9 +213,9 @@ class DistancesBench(object):
         distances.contact_matrix(coord=self.coords_1,
                                  cutoff=15.0,
                                  returntype='numpy',
-                                 box=None)
+                                 box=self.box_dims)
 
-    def time_contact_matrix_sparse(self, num_atoms):
+    def time_contact_matrix_sparse(self, num_atoms, box_dims):
         """Benchmark calculation of contacts within
         a single numpy array using the slower reduced
         memory implementation of contact_matrix intended
@@ -99,32 +224,5 @@ class DistancesBench(object):
         distances.contact_matrix(coord=self.coords_1,
                                  cutoff=15.0,
                                  returntype='sparse',
-                                 box=None)
+                                 box=self.box_dims)
 
-    def time_dist(self, num_atoms):
-        """Benchmark calculation of distances between
-        atoms in two atomgroups with no offsets
-        to resids.
-        """
-        distances.dist(A=self.ag1,
-                       B=self.ag2,
-                       offset=0)
-
-    def time_dist_offsets(self, num_atoms):
-        """Benchmark calculation of distances between
-        atoms in two atomgroups with offsets
-        to resids.
-        """
-        distances.dist(A=self.ag1,
-                       B=self.ag2,
-                       offset=20)
-
-    def time_between(self, num_atoms):
-        """Benchmark determination of subgroup
-        of atomgroup that is within a specific
-        distance of two other atomgroups.
-        """
-        distances.between(group=self.ag3,
-                          A=self.ag1,
-                          B=self.ag2,
-                          distance=15.0)
