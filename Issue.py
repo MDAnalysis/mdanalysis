63459199ead4bc516248e2ce069469ddf8da144d
SanjanaSogimatt <sanjanasogimatt@gmail.com>
2021-10-10 12:56:29 +0530

    initial commit

---
+++ Issue.py
@@ -0,0 +1,15 @@
+#issue easy inverse index array
+from typing import List
+
+
+#List=input(list)
+#def inverse(list):
+ #   
+  #      res=list[::-1]
+   #     print(res)
+#l=inverse(List)
+#print(l)
+
+srr=[2,3,4,5,67,8]
+result=list(reversed(srr))
+print(result)
