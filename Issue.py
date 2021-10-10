bee226ad4df9c63191c2cf0530bc40b5a126d195
SanjanaSogimatt <sanjanasogimatt@gmail.com>
2021-10-10 13:17:54 +0530

    used a function

--- Issue.py
+++ Issue.py
@@ -1,6 +1,6 @@
 #issue easy inverse index array
 from typing import List
-
+import array
 
 #List=input(list)
 #def inverse(list):
@@ -9,7 +9,7 @@ from typing import List
    #     print(res)
 #l=inverse(List)
 #print(l)
-
-srr=[2,3,4,5,67,8]
-result=list(reversed(srr))
-print(result)
+def inverse_array(list):
+    srr=[2,3,4,5,67,8]
+    result=list(reversed(srr))
+    print(result)
