f959caa5a44bc4b3adc2442bc24eaff6b94fde17
Sanjana <87171143+SanjanaSogimatt@users.noreply.github.com>
2021-10-10 20:21:48 +0700

    Update file
    Fixed errors.

--- Issue.py
+++ Issue.py
@@ -1,15 +1,19 @@
 #issue easy inverse index array
 from typing import List
 import array
+#method 01
+List=input(list)
+def inverse(list):
+   
+    res=list[::-1]
+    print(res)
+l=inverse(List)
+print(l)
 
-#List=input(list)
-#def inverse(list):
- #   
-  #      res=list[::-1]
-   #     print(res)
-#l=inverse(List)
-#print(l)
+#method 02
 def inverse_array(list):
     srr=[2,3,4,5,67,8]
     result=list(reversed(srr))
     print(result)
+    
+    
