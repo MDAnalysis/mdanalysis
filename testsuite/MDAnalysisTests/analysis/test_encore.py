4436a07fa95817e091df21065df94ce9a482b238
pranjii <pranjiii20@gmail.com>
2021-12-11 02:56:23 +0530

    #3467

--- testsuite/MDAnalysisTests/analysis/test_encore.py
+++ testsuite/MDAnalysisTests/analysis/test_encore.py
@@ -395,6 +395,11 @@ inconsistent results")
         assert stdev < stdev_upper_bound, "Unexpected standard deviation for" \
                                            " bootstrapped samples in Dim. reduction Ensemble imilarity"
 
+    def test_covariance_matrix(self,
+                                ens1, ens2):
+        covariance_of_matrix = encore.covariance.covariance_matrix(ensemble = ens1, reference = ens2)
+        assert covariance_of_matrix is np.zeros(len(ens1), 3)
+
 class TestEncoreClustering(object):
     @pytest.fixture(scope='class')
     def ens1_template(self):
