74ad71c434f6a97162854ab3085455b69f4ab4b7
Jonathan Barnoud <jonathan@barnoud.net>
2022-02-03 09:28:01 +0100

    Remove trailing spaces

--- testsuite/MDAnalysisTests/core/test_groups.py
+++ testsuite/MDAnalysisTests/core/test_groups.py
@@ -326,7 +326,7 @@ class TestGroupSlicing(object):
 
         assert a.ix == ref
         assert isinstance(a, singular)
-    
+ 
     def test_none_getitem(self, group):
         with pytest.raises(TypeError):
             group[None]
