500148e6cbc6ce6a61a72db7b1384f190da6d72a
aya9alaa <ayam.alaa09@gmail.com>
2022-03-19 05:34:15 +0200

    pep8 specs2

--- testsuite/MDAnalysisTests/analysis/test_encore.py
+++ testsuite/MDAnalysisTests/analysis/test_encore.py
@@ -430,9 +430,6 @@ class TestEncoreClustering(object):
             ens2_template.trajectory.timeseries(order='fac'),
             format=mda.coordinates.memory.MemoryReader)
 
-    @pytest.fixture()
-    def empty_cluster(self):
-        return encore.Cluster()
 
     def test_clustering_one_ensemble(self, ens1):
         cluster_collection = encore.cluster(ens1)
@@ -561,18 +558,19 @@ class TestEncoreClustering(object):
         cluster.add_metadata('test', metadata)
         assert np.all(cluster.metadata['test'] == metadata), \
                      "Cluster metadata isn't as expected"
- 
-    def test_empty_Cluster(self, empty_cluster):
+
+    def test_empty_Cluster(self ):
+        empty_cluster = encore.Cluster()
         assert empty_cluster.size == 0 
         assert np.size(empty_cluster.elements) == 0 
         assert empty_cluster.centroid is None
         assert bool(empty_cluster.metadata) == False
-     
+
     def test_centroid_not_in_elements(self):
-        error_message = "Centroid of cluster not found in the element list"
+        error_message="Centroid of cluster not found in the element list"
         with pytest.raises(LookupError, match = error_message):
              encore.Cluster([38, 39, 40, 41, 42, 43], 99)
-       
+   
 
 class TestEncoreClusteringSklearn(object):
     """The tests in this class were duplicated from the affinity propagation
