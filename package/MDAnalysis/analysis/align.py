bf859c8d2173b8e2abc2c4fa7100a5a8272aa9e2
JoseAntonioSiguenza <80292371+JoseAntonioSiguenza@users.noreply.github.com>
2022-03-27 18:16:47 -0500

    update PEP8speaks comments

--- package/MDAnalysis/analysis/align.py
+++ package/MDAnalysis/analysis/align.py
@@ -507,11 +507,11 @@ def alignto(mobile, reference, select=None, weights=None,
         if mobile.universe == reference.universe:
             # raise a warning when passing atom groups from same Universe
             wmsg = ("The reference and mobile selections are atom groups"
-                   "from the same universe, the reference selection will" 
-                   "be transformed. If it is not the behavior you intend," 
-                   "try setting the subselection argument to 'all'."
-                   "`subselection = 'all' is deprecated in 2.1.0 but it"
-                   "will become the default behavior in version 3.0.0.")
+                    "from the same universe, the reference selection will" 
+                    "be transformed. If it is not the behavior you intend," 
+                    "try setting the subselection argument to 'all'."
+                    "`subselection = 'all' is deprecated in 2.1.0 but it"
+                    "will become the default behavior in version 3.0.0.")
             warnings.warn(wmsg, DeprecationWarning)
     elif isinstance(subselection, str):
         # select mobile_atoms from string
