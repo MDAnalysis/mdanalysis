cc5e25037b6d43b225913b523604953cd39d4a78
Mingyi Xue <2415309890@qq.com>
2022-04-03 01:27:41 -0500

    fix PEP8 issur

--- package/MDAnalysis/topology/MOL2Parser.py
+++ package/MDAnalysis/topology/MOL2Parser.py
@@ -163,14 +163,14 @@ class MOL2Parser(TopologyReaderBase):
                                      " unpacked: atom_id atom_name x y z"
                                      " atom_type [subst_id[subst_name"
                                      " [charge [status_bit]]]]".format(
-                        os.path.basename(self.filename)))
+                                         os.path.basename(self.filename)))
                 aid, name, x, y, z, atom_type = columns[:6]
                 for i in range(6, len(columns)):
                     opt_values[i-6] = columns[i]
                 for i in range(len(columns), 9):
                     warnings.warn("Not enough values to unpack."
                                   " {0} has been given value {1}.".format(
-                        opt_fields[i-6], opt_values[i-6]))
+                                      opt_fields[i-6], opt_values[i-6]))
                 resid, resname, charge = opt_values
             ids.append(aid)
             names.append(name)
