1d9e4d445dc802b124e042a3ec3c1515107c3a21
manishsaini6421 <manishsaini6421@gmail.com>
2022-04-08 04:51:07 +0530

    moved hole2.hole docstring to hole2._init_  and updated package/doc/sphinx/source/documentation_pages/analysis/hole2.rst

--- package/MDAnalysis/analysis/hole2/__init__.py
+++ package/MDAnalysis/analysis/hole2/__init__.py
@@ -21,7 +21,7 @@
 # J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
 #
 
-"""HOLE Analysis --- :mod:`MDAnalysis.analysis.hole2.hole`
+"""HOLE Analysis --- :mod:`MDAnalysis.analysis.hole2`
 =====================================================================================
 
 :Author: Lily Wang
