c0a4f610656baf08294d01ebff2fafc859887cfc
Aditi Tripathi <61978541+aditi2906@users.noreply.github.com>
2022-04-18 18:53:39 +0530

    spelling errors corrected accross various files (#3646)
    * spelling errors corrected accross various files
    
    * linting errors fixed
    
    * fixed errors in previous commit
    
    * Revert "fixed errors in previous commit"
    
    This reverts commit 5580846dcee9495a4ac58b7e84fb1cf0475dd4a7.
    
    * fixed previous commit issues
    
    * fixing styling errors

--- package/MDAnalysis/auxiliary/__init__.py
+++ package/MDAnalysis/auxiliary/__init__.py
@@ -339,7 +339,7 @@ The following attributes are inherited from
       Method to use in calculating representative auxiliary value for a 
       timestep. Default is 'closest'.
   ``cutoff``
-      Cutoff (in ps) for ignoring auxiliary steps when calculating 
+      Cutoff (in ps) for ignoring auxiliary steps when calculating
       representative value(s) for a timestep.
   ``auxstep``
       The :class:`~MDAnalysis.auxiliary.base.AuxStep` object to store data for
@@ -348,10 +348,10 @@ The following attributes are inherited from
   ``n_steps``
       Total number of auxiliary steps.
   ``frame_data``
-      `data` from each auxiliary step assigned to the last-read trajectory 
+      `data` from each auxiliary step assigned to the last-read trajectory
       timestep.
   ``frame_rep``
-      Represenatative value(s) of auxiliary data for last-read trajectory 
+      Representative value(s) of auxiliary data for last-read trajectory
       timestep.
 
 The following are stored in ``auxstep`` but may be accessed from the auxiliary
@@ -436,7 +436,7 @@ The following methods are inherited from
 
   ``next_nonempty_frame(ts)``
     Return the frame number of the next trajectory frame (after the current
-    auxiliary time) for which a represenatative auxiliary value can be
+    auxiliary time) for which a representative auxiliary value can be
     calculated (i.e., there is at least one assigned auxiliary step within
     ``cutoff``).
 
@@ -453,12 +453,12 @@ The following methods are inherited from
   ``__del__()``
     Calls ``close()``.
 
-  ``get_descrtiption``
+  ``get_description``
     Get the values of the attributes required for replicating an auxiliary (as
     listed in ``required_attrs``) and return as a dictionary.
 
   ``__eq__``
-    Check for equality by checking each of the attributes requried for 
+    Check for equality by checking each of the attributes required for
     replicating an auxiliary (as listed in ``required_attrs``) are equal.
 
 Each AuxReader must subclass :class:`~MDAnalysis.auxiliary.base.AuxReader`
@@ -466,11 +466,11 @@ and addionally define:
 
   ``__init__(auxdata, **kwargs)``
     Additional processing of *kwargs* and any necessary initial processing of 
-    *auxdata*. Must :func:`super` through  
+    *auxdata*. Must :func:`super` through
     :class:`~MDAnalysis.auxiliary.base.AuxReader`.
 
   ``_read_next_step()``
-    Read the data from the next auxiliary step and update ``auxstep`` as 
+    Read the data from the next auxiliary step and update ``auxstep`` as
     appropriate. Raise `StopIteration` when attempting to read past last step.
 
   ``_go_to_step(i)``
@@ -494,15 +494,15 @@ define/overwrite the following:
 
   ``close()``
 
-For convinience, when reading auxiliary data from an open file, step at a time, 
-:class:`~MDAnalysis.auxiliary.base.AuxFileReader` 
+For convenience, when reading auxiliary data from an open file, step at a time,
+:class:`~MDAnalysis.auxiliary.base.AuxFileReader`
 extends :class:`~MDAnalysis.auxiliary.base.AuxReader` by providing the
 following (these may be overwritten by subclasses as appropriate):
 
   ``__init__(auxfile, **kwargs)``
     Open *auxfile* and any additional processing of *kwargs*.
 
-  ``_restart()``, 
+  ``_restart()``,
     Seek to the start of ``auxfile``
 
   ``close()``
