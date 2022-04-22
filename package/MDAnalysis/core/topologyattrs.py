f1786b61f0958006009c4a1929a29a49a84a78f5
lordfme <fortmax120@gmail.com>
2022-04-22 14:00:43 +0100

    Added the _check_dtype wrapper for enforcing data type compliance for TopologyAttr instances

--- package/MDAnalysis/core/topologyattrs.py
+++ package/MDAnalysis/core/topologyattrs.py
@@ -117,6 +117,36 @@ def _check_length(func):
     return wrapper
 
 
+def _check_dtype(func):
+    """ Wrapper which validates the data type for the `values` argument to the
+    __setitem__ method of the :class:`TopologyAttr` class.
+
+    Iterates through `values` if it's a list and assets that all its items match
+    the data type of the `group` class's `dtype` attribute.
+    """
+    _ERROR_MSG = "Setting {group} {attr_name} to wrong data type. " \
+                 "Must be of type `{expected_dtype}`"
+    # Eg "Setting Atom name to wrong data type. Must be of type `str`"
+
+    @functools.wraps(func)
+    def wrapper(obj, group, values):
+        iter_values = values if isinstance(values, (list, np.ndarray)) else [values]
+
+        if isinstance(obj, TopologyAttr) and obj.dtype:
+            attr_name = obj.attrname if isinstance(values, list) else obj.singular
+            expected_dtype = str if obj.dtype is object else obj.dtype
+            if not all(map(lambda v: isinstance(v, expected_dtype), iter_values)):
+                raise TypeError(_ERROR_MSG.format(
+                    group=group.__class__.__name__,
+                    attr_name=attr_name,
+                    expected_dtype=expected_dtype.__name__
+                ))
+
+        return func(obj, group, values)
+
+    return wrapper
+
+
 def _wronglevel_error(attr, group):
     """Generate an error for setting attr at wrong level
 
@@ -459,6 +489,7 @@ class TopologyAttr(object, metaclass=_TopologyAttrMeta):
         elif isinstance(group, (Segment, SegmentGroup)):
             return self.get_segments(group)
 
+    @_check_dtype
     def __setitem__(self, group, values):
         if isinstance(group, (Atom, AtomGroup)):
             return self.set_atoms(group, values)
