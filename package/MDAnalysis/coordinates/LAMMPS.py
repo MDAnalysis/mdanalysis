efca264b68e2238fd8a207dbc25f97e0afefdca8
Philipp Stärk <philipp.staerk@t-online.de>
2022-05-20 10:21:15 +0200

    Fix the no additional columns case.

--- package/MDAnalysis/coordinates/LAMMPS.py
+++ package/MDAnalysis/coordinates/LAMMPS.py
@@ -539,6 +539,8 @@ class DumpReader(base.ReaderBase):
 
         if additional_columns:
             self._additional_columns = additional_columns
+        else:
+            self._additional_columns = []
 
         self._cache = {}
 
