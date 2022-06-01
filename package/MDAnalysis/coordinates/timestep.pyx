5f709eb23ac1049f87cd1cb55b5b396173ff5476
hmacdope <hugomacdermott@gmail.com>
2022-06-01 16:05:37 +1000

    move decls of C level vars to pxd file

--- package/MDAnalysis/coordinates/timestep.pyx
+++ package/MDAnalysis/coordinates/timestep.pyx
@@ -183,29 +183,6 @@ cdef class Timestep:
 
     order = 'C'
 
-    # cdef uint64_t _n_atoms
-    # cdef public int64_t  frame
-
-
-    # # info for numpy C API
-    # cdef int _typenum
-    # cdef cnp.npy_intp _particle_dependent_dim[2]
-
-    # cdef bool _has_positions
-    # cdef bool _has_velocities
-    # cdef bool _has_forces
-
-    # # these have to be public for testing
-    # cdef public cnp.ndarray _unitcell
-    # cdef public cnp.ndarray _pos
-    # cdef public cnp.ndarray _velocities
-    # cdef public cnp.ndarray _forces
-
-    # # python objects
-    # cdef object _dtype
-    # cdef public dict data
-    # cdef public object  _reader
-    # cdef public object aux
 
     def __cinit__(self, uint64_t n_atoms, dtype=np.float32, **kwargs):
         """Initialise C++ level parameters of a Timestep
