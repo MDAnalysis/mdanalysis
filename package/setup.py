7bf94a9202dfc8d2b37df2e8a2de05119aedd65d
Oliver Beckstein <orbeckst@gmail.com>
2022-06-11 16:49:12 -0700

    deprecate extras_require "AMBER" target for 2.4

--- package/setup.py
+++ package/setup.py
@@ -640,7 +640,7 @@ if __name__ == '__main__':
           # you might prefer to use the version available through your
           # packaging system
           extras_require={
-              'AMBER': [           # REMOVE for 3.0.0, use 'extra_formats'
+              'AMBER': [           # REMOVE for 2.4.0, use 'extra_formats'
                   'netCDF4>=1.0',  # for fast AMBER writing, also needs HDF5
               ],
               'extra_formats': [   # additional file formats
