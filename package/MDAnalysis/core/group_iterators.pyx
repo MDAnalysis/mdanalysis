b39e3998810ce589cadfab3e4b1f551d6559a49a
hmacdope <hugomacdermott@gmail.com>
2022-06-15 11:11:34 +1000

    add docs

--- package/MDAnalysis/core/group_iterators.pyx
+++ package/MDAnalysis/core/group_iterators.pyx
@@ -27,17 +27,30 @@
 Compiled helpers for group iteration --- :mod:`MDAnalysis.core.group_iterators`
 ===============================================================================
 
-Helpers
+This module contains Cython extension types that allow various low level
+routines to use groups such as a :class:`AtomGroup` or a NumPy
+:class:`numpy.ndarray` interchangeably. In general this module is primarily of
+interest to developers and should be modified with caution.
 
+The Cython extension types in this module are lightweight wrappers around the 
+C++ classes defined in `MDAnalysis/lib/include/iterators.h`. These C++ classes
+provide a homogenous interface to lower level functions for fast iteration
+of groups and arrays and are exposed as the `_iterator` attribute.
+See the documentation in `MDAnalysis/lib/include/iterators.h` for more
+information.
+
+The classes in this module are primarily responsible for hooking any data
+required for iteration onto the `._iterator` C++ class instance which provides
+the iteration interface.
 """
 
+import numpy as np
 from .groups import AtomGroup
 from ..libmda cimport iterators
 from libcpp.vector cimport vector
 from libc.stdint cimport int64_t
 cimport cython
 cimport numpy as cnp
-import numpy as np
 cnp.import_array()
 
 
@@ -55,9 +68,9 @@ cdef class AtomGroupIterator:
     def __cinit__(self, ag):
         self._iterator = iterators._AtomGroupIterator(ag.n_atoms)
         self._coord_view = ag.universe.trajectory.ts.positions
-        self._iterator.ptr = &self._coord_view[0,0]
+        self._iterator.ptr = &self._coord_view[0, 0]
         self._iterator.copy_ix( < int64_t*>cnp.PyArray_DATA(ag.ix_array))
-    
+
     def print_coords(self):
         print(np.asarray(self._coord_view))
 
@@ -65,14 +78,14 @@ cdef class AtomGroupIterator:
     def ix(self):
         cdef cnp.npy_intp dims[1]
         dims[0] = self._iterator.n_atoms
-        cdef cnp.ndarray arr 
-        arr =  _to_numpy_from_spec(self, 1, dims, cnp.NPY_INT64, self._iterator.ix.data())
+        cdef cnp.ndarray arr
+        arr = _to_numpy_from_spec(
+            self, 1, dims, cnp.NPY_INT64, self._iterator.ix.data())
         return arr
 
     @property
     def n_atoms(self):
         return self._iterator.n_atoms
-    
 
 
 cdef class ArrayIterator:
@@ -86,11 +99,10 @@ cdef class ArrayIterator:
                 "input array has incorrect second dimension, must be 3")
         self._iterator = iterators._ArrayIterator(arr.shape[0])
         self._coord_view = arr
-        self._iterator.ptr = &self._coord_view[0,0]
+        self._iterator.ptr = &self._coord_view[0, 0]
 
     def print_coords(self):
         print(np.asarray(self._coord_view))
-
 
     @property
     def n_atoms(self):
