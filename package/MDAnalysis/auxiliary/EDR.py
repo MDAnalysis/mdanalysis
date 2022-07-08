f24d296f0c637618d17a880243aeb52614adce42
bjarne <bjarnefeddersen@gmx.net>
2022-07-08 14:48:46 +0100

    initial file created, test working on different computers

---
+++ package/MDAnalysis/auxiliary/EDR.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  8 14:47:19 2022
+
+@author: bjarne
+"""
+
+import numbers
+import os
+import numpy as np
+from . import base
+from ..lib.util import anyopen
+import panedrlite as panedr
