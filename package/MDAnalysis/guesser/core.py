60b24c1a74712bb5339019d8f98fa46f7493254e
Aya Alaa <a.m.a09@outlook.com>
2022-07-23 05:17:54 +0200

    Update core.py

--- package/MDAnalysis/guesser/core.py
+++ package/MDAnalysis/guesser/core.py
@@ -6,11 +6,8 @@ from .DefaultGuesser import DefaultGuesser
 GUESSERS = {'DEFAULT': DefaultGuesser}
 
 
-<<<<<<< Updated upstream
-def get_guesser(atoms, context):
-=======
+
 def get_guesser(a, context):
->>>>>>> Stashed changes
     """get an appropiate guesser to the universe and pass
        the atomGroup of the universe to the guesser
 
@@ -20,22 +17,12 @@ def get_guesser(a, context):
     context: string or Guesser class
     Returns
     -------
-    TGuesser class
+    Guesser class
     """
     if isinstance(context, GuesserBase):
-<<<<<<< Updated upstream
-        context.setAtoms(atoms)
         return context
     try:
         guesser = GUESSERS[context.upper()]
-        guesser.setAtoms(atoms)
-=======
-        context.setAtoms(a)
-        return context
-    try:
-        guesser = GUESSERS[context.upper()](a)
-
->>>>>>> Stashed changes
     except KeyError:
         raise TypeError("Unidentified guesser type {0}".format(context))
     return guesser
