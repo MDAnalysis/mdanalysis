10b8c038b81228ea809b48ffe23125be24de78ca
aya9aladdin <a.M.A09@outlook.com>
2022-08-05 06:37:40 +0200

    Update tables.py

--- package/MDAnalysis/guesser/tables.py
+++ package/MDAnalysis/guesser/tables.py
@@ -307,9 +307,9 @@ TABLE_VDWRADII = r"""
 # Van der Waals radii taken from
 # [1] Bondi, A. (1964). "Van der Waals Volumes and Radii".
 #     J. Phys. Chem. 68 (3): 441-451. doi:10.1021/j100785a001.
-# [2] Rowland and Taylor (1996). "Intermolecular Nonbonded Contact Distances in 
+# [2] Rowland and Taylor (1996). "Intermolecular Nonbonded Contact Distances in
 #                                 Organic Crystal Structures:
-#                                 Comparison with Distances Expected from 
+#                                 Comparison with Distances Expected from
 #                                 van der Waals Radii".
 #     J. Phys. Chem., 1996, 100 (18), 7384.7391. doi:10.1021/jp953141+.
 # [3] Mantina, et al. (2009). "Consistent van der Waals Radii
@@ -388,13 +388,13 @@ Z2SYMB = {1: 'H', 2: 'He',
           35: 'Br', 36: 'Kr', 37: 'Rb', 42: 'Mo', 47: 'Ag', 48: 'Cd', 49: 'In',
           43: 'Tc', 44: 'Ru', 45: 'Rh', 46: 'Pd', 50: 'Sn', 56: 'Ba', 57: 'La',
           51: 'Sb', 52: 'Te', 53: 'I', 54: 'Xe', 55: 'Cs', 58: 'Ce', 65: 'Tb',
-          59: 'Pr', 60: 'Nd', 61: 'Pm', 62: 'Sm', 63: 'Eu', 64: 'Gd',  66: 'Dy',
+          59: 'Pr', 60: 'Nd', 61: 'Pm', 62: 'Sm', 63: 'Eu', 64: 'Gd', 66: 'Dy',
           67: 'Ho', 68: 'Er', 69: 'Tm', 70: 'Yb', 71: 'Lu', 72: 'Hf', 73: 'Ta',
-          74: 'W',  76: 'Os', 77: 'Ir', 78: 'Pt', 80: 'Hg', 81: 'Tl', 82: 'Pb',
-          75: 'Re', 79: 'Au', 85: 'At', 86: 'Rn', 87: 'Fr', 88: 'Ra',  97: 'Bk',
+          74: 'W', 76: 'Os', 77: 'Ir', 78: 'Pt', 80: 'Hg', 81: 'Tl', 82: 'Pb',
+          75: 'Re', 79: 'Au', 85: 'At', 86: 'Rn', 87: 'Fr', 88: 'Ra', 97: 'Bk',
           83: 'Bi', 84: 'Po', 89: 'Ac', 90: 'Th', 95: 'Am', 96: 'Cm',
           91: 'Pa', 92: 'U', 93: 'Np', 94: 'Pu', 98: 'Cf', 104: 'Rf', 105: 'Db',
-          99: 'Es', 100: 'Fm', 101: 'Md', 102: 'No', 103: 'Lr', 111: 'Rg', 
+          99: 'Es', 100: 'Fm', 101: 'Md', 102: 'No', 103: 'Lr', 111: 'Rg',
           106: 'Sg', 107: 'Bh', 108: 'Hs', 109: 'Mt', 110: 'Ds', 112: 'Cn',
           113: 'Nh', 114: 'Fl', 115: 'Mc', 116: 'Lv', 117: 'Ts', 118: 'Og'}
 
