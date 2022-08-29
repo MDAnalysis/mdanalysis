7b6b2f2f9bbd5a669856908c97123d42301806c4
IAlibay <ialibay@mdanalysis.org>
2022-08-29 19:03:56 +0100

    bump up versions

--- testsuite/setup.py
+++ testsuite/setup.py
@@ -87,7 +87,7 @@ if sys.version_info[:2] < (3, 8):
 
 if __name__ == '__main__':
     # this must be in-sync with MDAnalysis
-    RELEASE = "2.3.0-dev0"
+    RELEASE = "2.3.0"
     with open("README") as summary:
         LONG_DESCRIPTION = summary.read()
 
