1ec74456a2b4d8378be68f0a2b7d164a4c35c716
Hugo MacDermott <hugomacdermott@gmail.com>
2023-01-08 12:18:41 +1100

    darker

--- package/MDAnalysis/lib/_distopia.py
+++ package/MDAnalysis/lib/_distopia.py
@@ -36,24 +36,38 @@ except ImportError:
 else:
     HAS_DISTOPIA = True
 
-from .c_distances import calc_bond_distance_triclinic as _calc_bond_distance_triclinic_serial
+from .c_distances import (
+    calc_bond_distance_triclinic as _calc_bond_distance_triclinic_serial,
+)
 import warnings
 import numpy as np
 
-def calc_bond_distance_ortho(coords1, coords2: np.ndarray,
-                             box:  np.ndarray, results: np.ndarray) -> None:
 
-    results = distopia.calc_bonds_ortho_float(coords1, coords2, box[:3], results=results.astype(np.float32))
+def calc_bond_distance_ortho(
+    coords1, coords2: np.ndarray, box: np.ndarray, results: np.ndarray
+) -> None:
+    results = distopia.calc_bonds_ortho_float(
+        coords1, coords2, box[:3], results=results.astype(np.float32)
+    )
     # upcast is currently required, change for 3.0, see #3927
     results = results.astype(np.float64)
 
-def calc_bond_distance(coords1: np.ndarray , coords2: np.ndarray, results: np.ndarray) -> None:
-    results = distopia.calc_bonds_no_box_float(coords1, coords2, results=results.astype(np.float32))
+
+def calc_bond_distance(
+    coords1: np.ndarray, coords2: np.ndarray, results: np.ndarray
+) -> None:
+    results = distopia.calc_bonds_no_box_float(
+        coords1, coords2, results=results.astype(np.float32)
+    )
     # upcast is currently required, change for 3.0, see #3927
     results = results.astype(np.float64)
 
-def calc_bond_distance_triclinic(coords1: np.ndarray, coords2: np.ndarray,
-                                 box: np.ndarray, results: np.ndarray) -> None:
+
+def calc_bond_distance_triclinic(
+    coords1: np.ndarray, coords2: np.ndarray, box: np.ndarray, results: np.ndarray
+) -> None:
     # redirect to serial backend
-    warnings.warn("distopia does not support triclinic boxes, using serial backend instead.")
+    warnings.warn(
+        "distopia does not support triclinic boxes, using serial backend instead."
+    )
     _calc_bond_distance_triclinic_serial(coords1, coords2, box, results)
