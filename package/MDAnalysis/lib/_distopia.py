e91d4e87fa5fb834f7b2a5e8b20b278553fca957
Hugo MacDermott <hugomacdermott@gmail.com>
2023-01-08 11:47:25 +1100

    finalise distopia stub

--- package/MDAnalysis/lib/_distopia.py
+++ package/MDAnalysis/lib/_distopia.py
@@ -24,6 +24,35 @@
 """Stub module for distopia --- :mod:`MDAnalysis.analysis.distopia`
 ===================================================================
 
-This module is a stub to provide  distopia distance functions to `distances.py` 
+This module is a stub to provide distopia distance functions to `distances.py`
+as a selectable backend.
 """
 
+# check for distopia
+try:
+    import distopia
+except ImportError:
+    HAS_DISTOPIA = False
+else:
+    HAS_DISTOPIA = True
+
+from .c_distances import calc_bond_distance_triclinic as _calc_bond_distance_triclinic_serial
+import warnings
+
+def calc_bond_distance_ortho(numpy.ndarray coords1, numpy.ndarray coords2,
+                             numpy.ndarray box, numpy.ndarray results) -> None:
+
+    results = distopia.calc_bonds_ortho_float(coords1, coords2, box[:3], results=results.astype(np.float32))
+    # upcast is currently required, change for 3.0, see #3927
+    results = results.astype(np.float64)
+
+def calc_bond_distance(numpy.ndarray coords1, numpy.ndarray coords2, numpy.ndarray results) -> None:
+    results = distopia.calc_bonds_no_box_float(coords1, coords2, results=results.astype(np.float32))
+    # upcast is currently required, change for 3.0, see #3927
+    results = results.astype(np.float64)
+
+def calc_bond_distance_triclinic(numpy.ndarray coords1, numpy.ndarray coords2,
+                             numpy.ndarray box, numpy.ndarray results) -> None:
+    # redirect to serial backend
+    warnings.warn("distopia does not support triclinic boxes, using serial backend instead.")
+    _calc_bond_distance_triclinic_serial(coords1, coords2, box, results)
