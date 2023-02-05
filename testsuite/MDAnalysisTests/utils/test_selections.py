b9ae257d86d0d5c3005231b963eb207e11bead19
IAlibay <ialibay@mdanalysis.org>
2023-02-05 21:50:49 +0000

    remove TestCase as unused import

--- testsuite/MDAnalysisTests/utils/test_selections.py
+++ testsuite/MDAnalysisTests/utils/test_selections.py
@@ -29,7 +29,7 @@ from io import StringIO
 import re
 
 import numpy as np
-from numpy.testing import TestCase, assert_equal, assert_array_equal
+from numpy.testing import assert_equal, assert_array_equal
 
 from MDAnalysis.tests.datafiles import PSF, DCD
 
