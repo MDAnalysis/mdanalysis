11a3ff4a350bde54efe0515778385fecc5622860
pegerto.fernandez <pegerto.fernandez@creditkarma.com>
2023-02-13 10:49:51 +0000

    lint fixes

--- package/MDAnalysis/analysis/accesibility.py
+++ package/MDAnalysis/analysis/accesibility.py
@@ -1,9 +1,9 @@
 import numpy as np
-import math
 
 from scipy.spatial import KDTree
-from .base import AnalysisBase
 from numpy import arange, pi, sin, cos, arccos
+from .base import AnalysisBase
+
 
 ATOMIC_RADI = {
     "H": 1.200,
@@ -28,13 +28,13 @@ ATOMIC_RADI = {
     "I": 1.980,
     "HG": 1.550,
 }
-    
+
 def atomic_radi(e):
     """van der Waals radii""" 
-    DEFAULT_ATOMIC_RADI = 2.
-    
+    DEFAULT_ATOMIC_RADI = 2. 
     return ATOMIC_RADI[e]  if e in ATOMIC_RADI  else DEFAULT_ATOMIC_RADI
-        
+
+
 class SASA(AnalysisBase):
     r""" Calculation of solvent accesible surface areas using Shrake-Rupley "rolling ball'
          algorithm 
@@ -57,26 +57,28 @@ class SASA(AnalysisBase):
         self._twice_max_radi =  (max(list(ATOMIC_RADI.values())) + probe_radius) * 2
         self._probe_radius = probe_radius
 
+
     def _compute_sphere(self):
         """ Fibonacci lattice to evently distribute points in the sphere.
         """
-        goldenRatio = (1 + 5**0.5)/2
+        golden_ratio = (1 + 5**0.5)/2
         i = arange(0, self._n_points)
-        theta = 2 *pi * i / goldenRatio
+        theta = 2 *pi * i / golden_ratio
         phi = arccos(1 - 2*(i+0.5)/ self._n_points)
-        x, y, z = cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi);   
-        return np.transpose(np.array([x,y,z]))
-        
+        x_points, y_points, z_points = cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi)   
+        return np.transpose(np.array([x_points,y_points,z_points]))
+
+
     def _compute(self):
         asas = np.zeros(len(self._ag.atoms), dtype=np.double)
-                        
+
         for atom in self._ag.atoms:
             r_w = atomic_radi(atom.type)
             raddi = r_w + self._probe_radius
             # translate and transform the sphere to the centroid
             sphere = np.array(self._sphere, copy=True) * raddi + atom.position
             neighbours = self._atom_neighbours.query_ball_point(atom.position, self._twice_max_radi)
-            
+
             kdt_sphere = KDTree(sphere, 10)
             intersect = set()
             for n in neighbours:
@@ -85,13 +87,15 @@ class SASA(AnalysisBase):
                 n_raddi = atomic_radi(self._ag.atoms[n].type) + self._probe_radius
                 cut = kdt_sphere.query_ball_point(self._ag.atoms[n].position, n_raddi)
                 intersect |= set(cut)
-            
+        
             points = self._n_points - len(intersect)
-            totalSurefaceArea = raddi * raddi * 4 * np.pi
-            areaPerPoint = totalSurefaceArea / self._n_points
-            asas[atom.index] = points * areaPerPoint
+            total_surface_area = raddi * raddi * 4 * np.pi
+            area_per_point = total_surface_area / self._n_points
+            asas[atom.index] = points * area_per_point
         return asas
-            
+
 
     def atoms(self):
+        """ returns accessible surface area in A**2 for 
+        """
         return self._compute()
