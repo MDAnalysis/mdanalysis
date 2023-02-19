14b724ecb34394c45897093b907adf9f040b547f
Josh Vermaas <vermaasj@msu.edu>
2023-02-19 09:50:26 -0800

    Add self to AUTHORS, add to CHANGELOG, make style happier.

--- package/MDAnalysis/transformations/nojump.py
+++ package/MDAnalysis/transformations/nojump.py
@@ -41,7 +41,7 @@ class NoJump(TransformationBase):
     """
     Returns transformed coordinates for the given timestep so that an atom
     does not move more than half the periodic box size, and will not jump
-    across the periodic boundary. The algorithm used is based on Kulke & 
+    across the periodic boundary. The algorithm used is based on Kulke &
     Vermaas 2022, 10.1021/acs.jctc.2c00327, equation B8 for non-orthogonal
     systems.
 
@@ -54,12 +54,12 @@ class NoJump(TransformationBase):
     .. code-block:: python
 
         transformation = NoJump()
-        u.trajectory.add_transformations(transformation)   
+        u.trajectory.add_transformations(transformation)
         for ts in u.trajectory:
             print(ts.positions)
 
     In this case, ``ts.positions`` will return the NoJump unwrapped trajectory.
-    To reverse the process, wrap the coordinates again.   
+    To reverse the process, wrap the coordinates again. 
 
     Returns
     -------
@@ -67,13 +67,15 @@ class NoJump(TransformationBase):
 
     """
 
-    def __init__(self, check_continuity=True,
-                 max_threads=None,
-                 #NoJump transforms are inherently unparallelizable, since 
-                 #it depends on the previous frame's unwrapped coordinates
-                 parallelizable=False):
-        super().__init__(max_threads=max_threads,
-                         parallelizable=parallelizable)
+    def __init__(
+        self,
+        check_continuity=True,
+        max_threads=None,
+        # NoJump transforms are inherently unparallelizable, since
+        # it depends on the previous frame's unwrapped coordinates
+        parallelizable=False,
+    ):
+        super().__init__(max_threads=max_threads, parallelizable=parallelizable)
         self.Lprevinverse = np.eye(3)
         self.prevwrap = None
         self.prev = None
@@ -81,27 +83,27 @@ class NoJump(TransformationBase):
         self.check_c = check_continuity
 
     def _transform(self, ts):
-        if (self.prev is None):
+        if self.prev is None:
             self.prev = ts.positions.copy()
             self.prevwrap = ts.positions.copy()
             self.Lprevinverse = np.linalg.inv(ts.triclinic_dimensions)
             self.old_frame = ts.frame
             return ts
         if self.check_c and np.abs(self.old_frame - ts.frame) != 1:
-            warnings.warn("NoJump transform is only accurate when positions"
-                          "do not move by more than half a box length."
-                          "Currently jumping between frames with a step of more than 1."
-                          "This might be fine, but depending on the trajectory stride,"
-                          "this might be inaccurate.",
-                          Warning)
-        #Remember that @ is a shorthand for matrix multiplication.
-        #np.matmul(a, b) is equivalent to a @ b
+            warnings.warn(
+                "NoJump transform is only accurate when positions"
+                "do not move by more than half a box length."
+                "Currently jumping between frames with a step of more than 1."
+                "This might be fine, but depending on the trajectory stride,"
+                "this might be inaccurate.",
+                Warning,
+            )
+        # Remember that @ is a shorthand for matrix multiplication.
+        # np.matmul(a, b) is equivalent to a @ b
         alpha = ts.triclinic_dimensions @ self.Lprevinverse
         Linverse = np.linalg.inv(ts.triclinic_dimensions)
         current = ts.positions.copy()
-        ts.positions = (alpha @ self.prev.T).T 
-        ts.positions += current - (alpha @ self.prevwrap.T).T 
-        ts.positions -= (ts.triclinic_dimensions @ np.round(Linverse @ current.T - self.Lprevinverse @ self.prevwrap.T)).T
+        ts.positions = (alpha @ self.prev.T).T + current - (alpha @ self.prevwrap.T).T - (ts.triclinic_dimensions @ np.round(Linverse @ current.T - self.Lprevinverse @ self.prevwrap.T)).T
         self.Lprevinverse = Linverse
         self.prevwrap = current
         self.prev = ts.positions.copy()
