a484becfeae084ae7f995e44959f1c1662e99111
Josh Vermaas <vermaasj@msu.edu>
2023-02-26 15:50:14 -0500

    Apply zero dimensions

--- testsuite/MDAnalysisTests/transformations/test_nojump.py
+++ testsuite/MDAnalysisTests/transformations/test_nojump.py
@@ -176,6 +176,6 @@ def test_notinvertible(nojump_universe):
     with pytest.raises(mda.exceptions.NoDataError):
         u = nojump_universe
         u.dimensions = [0, 0, 0, 90, 90, 90]
-        workflow = [NoJump()]
+        workflow = [mda.transformations.boxdimensions.set_dimensions(dim),NoJump()]
         u.trajectory.add_transformations(*workflow)
         transformed_coordinates = u.trajectory.timeseries()[0]
