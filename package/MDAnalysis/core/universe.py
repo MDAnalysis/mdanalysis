084c27bb537cc110815eed41b3da277874dd9fc3
Ahmed Salah <72524423+AHMED-salah00@users.noreply.github.com>
2023-03-14 17:30:24 +0200

    Update package/MDAnalysis/core/universe.py
    Co-authored-by: Ian Kenney <ianmkenney@users.noreply.github.com>

--- package/MDAnalysis/core/universe.py
+++ package/MDAnalysis/core/universe.py
@@ -321,7 +321,7 @@ class Universe(object):
     """
     def __init__(self, topology=None, *coordinates, all_coordinates=False,
                  format=None, topology_format=None, transformations=None,
-                 guess_bonds=False, vdwradii=None,fudge_factor=0.55,
+                 guess_bonds=False, vdwradii=None, fudge_factor=0.55,
                  lower_bound=0.1, in_memory=False,
                  in_memory_step=1, **kwargs):
 
