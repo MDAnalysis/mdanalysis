a624920965764b372d25d776cdff66ac46746b6b
IAlibay <ialibay@mdanalysis.org>
2023-03-15 02:15:45 +0000

    get debugging info

--- maintainer/ci/darker-outcomes.py
+++ maintainer/ci/darker-outcomes.py
@@ -97,6 +97,8 @@ def get_action_url(repo, pr, run_id, workflow_name, job_name):
     linters = [wf for wf in repo.get_workflows()
                if wf.name == workflow_name][0]
 
+    print(linters)
+    print(run_id)
     # Extract the gh action run
     run = [r for r in linters.get_runs(branch=pr.head.ref)
            if r.id == run_id][0]
