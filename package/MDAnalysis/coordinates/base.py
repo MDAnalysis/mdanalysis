999aec6e2f31b49eb7eb50887f4b3f670b4eec64
IAlibay <ialibay@mdanalysis.org>
2023-03-28 15:22:35 +0100

    revert breaking code

--- package/MDAnalysis/coordinates/base.py
+++ package/MDAnalysis/coordinates/base.py
@@ -145,18 +145,6 @@ from ..auxiliary import _AUXREADERS
 from ..lib.util import asiterable, Namespace, store_init_arguments
 from ..lib.util import NamedStream
 
-class aClassWithSomeStrangeCasingAndAlsoAVeryLongNameThatShouldntBeUsedAnywhereElse(object):
-    def __init__(self):
-        pass
-    def nospacesmethod(self):
-        pass
-
-
-    def toomanyspacesmethod(self):
-        pass
-
-    def WhyAreYouNotFailing(self):
-        pass
 
 class FrameIteratorBase(object):
     """
