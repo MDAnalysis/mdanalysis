ea25af03bf4c9c3771b212a0195af1b521df9447
IAlibay <ialibay@mdanalysis.org>
2023-04-09 09:39:32 +0100

    fix errors by enforcing tempdir removal before teardown

--- testsuite/MDAnalysisTests/coordinates/test_xdr.py
+++ testsuite/MDAnalysisTests/coordinates/test_xdr.py
@@ -889,17 +889,14 @@ class _GromacsReader_offsets(object):
         assert_equal(os.path.exists(XDR.offsets_filename(filename,
                                                     ending='.lock')), False)
 
-        # pre-teardown permission fix for windows
+        # pre-teardown permission fix - leaving permission blocked dir
+        # is problematic on py3.9 + Windows it seems.
         if os.name == 'nt':
             subprocess.call(f"icacls {tmpdir} /grant Users:W", shell=True)
         else:
             os.chmod(str(tmpdir), 0o777)
 
-        def remove_readonly(func, path, _):
-            os.chmod(path, stat.S_IWRITE)
-            func(path)
-
-        shutil.rmtree(tmpdir, onerror=remove_readonly)
+        shutil.rmtree(tmpdir)
 
     def test_offset_lock_created(self):
         assert os.path.exists(XDR.offsets_filename(self.filename,
