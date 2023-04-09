30baa14fbb3f6f4a820f0945118bf7d867ac8e2e
Irfan Alibay <IAlibay@users.noreply.github.com>
2023-04-09 22:21:57 +0100

    Fix permission errors w/ readonly tmpdirs and pytest 7.3 (#4122)
    * Fix Pytest 7.3 PermissionErrors for read-only tmpdirs (Issue #4123)

--- testsuite/MDAnalysisTests/coordinates/test_xdr.py
+++ testsuite/MDAnalysisTests/coordinates/test_xdr.py
@@ -23,14 +23,12 @@
 import pytest
 from unittest.mock import patch
 
-import errno
-import numpy as np
 import os
-from os.path import split
 import shutil
 import subprocess
 from pathlib import Path
 
+import numpy as np
 from numpy.testing import (assert_equal,
                            assert_almost_equal,
                            assert_allclose)
@@ -889,6 +887,17 @@ class _GromacsReader_offsets(object):
         # check the lock file is not created as well.
         assert_equal(os.path.exists(XDR.offsets_filename(filename,
                                                     ending='.lock')), False)
+
+        # pre-teardown permission fix - leaving permission blocked dir
+        # is problematic on py3.9 + Windows it seems. See issue
+        # [4123](https://github.com/MDAnalysis/mdanalysis/issues/4123)
+        # for more details.
+        if os.name == 'nt':
+            subprocess.call(f"icacls {tmpdir} /grant Users:W", shell=True)
+        else:
+            os.chmod(str(tmpdir), 0o777)
+
+        shutil.rmtree(tmpdir)
 
     def test_offset_lock_created(self):
         assert os.path.exists(XDR.offsets_filename(self.filename,
