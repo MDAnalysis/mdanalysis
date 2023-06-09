7bb2e4e2506a4613ef1756ca833a432330b9f537
Egor Marin <marin@phystech.edu>
2023-06-09 14:39:38 +0300

    Add scheduler to rdf tests

--- testsuite/MDAnalysisTests/analysis/test_rdf.py
+++ testsuite/MDAnalysisTests/analysis/test_rdf.py
@@ -46,58 +46,58 @@ def sels(u):
     return s1, s2
 
 
-def test_nbins(u):
+def test_nbins(u, scheduler):
     s1 = u.atoms[:3]
     s2 = u.atoms[3:]
-    rdf = InterRDF(s1, s2, nbins=412).run()
+    rdf = InterRDF(s1, s2, nbins=412).run(scheduler=scheduler)
 
     assert len(rdf.results.bins) == 412
 
 
-def test_range(u):
+def test_range(u, scheduler):
     s1 = u.atoms[:3]
     s2 = u.atoms[3:]
     rmin, rmax = 1.0, 13.0
-    rdf = InterRDF(s1, s2, range=(rmin, rmax)).run()
+    rdf = InterRDF(s1, s2, range=(rmin, rmax)).run(scheduler=scheduler)
 
     assert rdf.results.edges[0] == rmin
     assert rdf.results.edges[-1] == rmax
 
 
-def test_count_sum(sels):
+def test_count_sum(sels, scheduler):
     # OW vs HW
     # should see 8 comparisons in count
     s1, s2 = sels
-    rdf = InterRDF(s1, s2).run()
+    rdf = InterRDF(s1, s2).run(scheduler=scheduler)
     assert rdf.results.count.sum() == 8
 
 
-def test_count(sels):
+def test_count(sels, scheduler):
     # should see two distances with 4 counts each
     s1, s2 = sels
-    rdf = InterRDF(s1, s2).run()
+    rdf = InterRDF(s1, s2).run(scheduler=scheduler)
     assert len(rdf.results.count[rdf.results.count == 4]) == 2
 
 
-def test_double_run(sels):
+def test_double_run(sels, scheduler):
     # running rdf twice should give the same result
     s1, s2 = sels
-    rdf = InterRDF(s1, s2).run()
-    rdf.run()
+    rdf = InterRDF(s1, s2).run(scheduler=scheduler)
+    rdf.run(scheduler=scheduler)
     assert len(rdf.results.count[rdf.results.count == 4]) == 2
 
 
-def test_exclusion(sels):
+def test_exclusion(sels, scheduler):
     # should see two distances with 4 counts each
     s1, s2 = sels
-    rdf = InterRDF(s1, s2, exclusion_block=(1, 2)).run()
+    rdf = InterRDF(s1, s2, exclusion_block=(1, 2)).run(scheduler=scheduler)
     assert rdf.results.count.sum() == 4
 
 
 @pytest.mark.parametrize("attr", ("rdf", "bins", "edges", "count"))
-def test_rdf_attr_warning(sels, attr):
+def test_rdf_attr_warning(sels, attr, scheduler):
     s1, s2 = sels
-    rdf = InterRDF(s1, s2).run()
+    rdf = InterRDF(s1, s2).run(scheduler=scheduler)
     wmsg = f"The `{attr}` attribute was deprecated in MDAnalysis 2.0.0"
     with pytest.warns(DeprecationWarning, match=wmsg):
         getattr(rdf, attr) is rdf.results[attr]
@@ -107,17 +107,17 @@ def test_rdf_attr_warning(sels, attr):
     ("density", 1.956823),
     ("rdf", 244602.88385),
     ("none", 4)])
-def test_norm(sels, norm, value):
+def test_norm(sels, norm, value, scheduler):
     s1, s2 = sels
-    rdf = InterRDF(s1, s2, norm=norm).run()
+    rdf = InterRDF(s1, s2, norm=norm).run(scheduler=scheduler)
     assert_allclose(max(rdf.results.rdf), value)
 
 
 @pytest.mark.parametrize("norm, norm_required", [
     ("Density", "density"), (None, "none")])
-def test_norm_values(sels, norm, norm_required):
+def test_norm_values(sels, norm, norm_required, scheduler):
     s1, s2 = sels
-    rdf = InterRDF(s1, s2, norm=norm).run()
+    rdf = InterRDF(s1, s2, norm=norm).run(scheduler=scheduler)
     assert rdf.norm == norm_required
 
 
