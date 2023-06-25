f975ecf52c7da7da2c8c2770fc30edec80cf68a2
Egor Marin <marin@phystech.edu>
2023-06-25 20:40:25 +0300

    dask and multiprocessing scheduler for test_density.py

--- package/MDAnalysis/analysis/density.py
+++ package/MDAnalysis/analysis/density.py
@@ -488,6 +488,9 @@ class DensityAnalysis(AnalysisBase):
                           parameters={'isDensity': False})
         density.make_density()
         self.results.density = density
+    
+    def _parallel_conclude(self):
+        self._grid = np.array([obj._grid for obj in self._remote_results]).sum(axis=0)
 
     @property
     def density(self):
