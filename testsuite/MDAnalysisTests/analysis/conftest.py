c5bd0b5922012881f3ddd49c34cb3d122605cc64
Egor Marin <marin@phystech.edu>
2023-06-25 19:32:24 +0300

    Add fixtures to test combinations of dask and multiprocessing

--- testsuite/MDAnalysisTests/analysis/conftest.py
+++ testsuite/MDAnalysisTests/analysis/conftest.py
@@ -2,11 +2,13 @@ import pytest
 
 @pytest.fixture(params=[None, 
                        'dask:1', 'dask:2', 
+                       'multiprocessing:1', 'multiprocessing:2'
                        ], 
                 scope='module')
-def scheduler(request):
+def schedulers_all(request):
     """
     Fixture for testing all possible schedulers.
+
     If scheduler raises an exception, you should explicitly check it
     using `with pytest.raises(NotImplementedError)`
     """
@@ -17,9 +19,29 @@ def scheduler(request):
         n_workers = int(n_workers)
         return {'scheduler':scheduler, 'n_workers':n_workers}
 
-@pytest.fixture
-def localscheduler(request):
+@pytest.fixture(params=[None])
+def scheduler_only_current_process(request):
     """
-    Fixture for testing only local, i.e. before-dask, codepath.
+    Fixture for testing only current process.
+    Made to allow for easier replacement of fixture in the future
+    upon implementation of parallelization for certain classes.
+
+    If scheduler raises an exception, you should explicitly check it
+    using `with pytest.raises(NotImplementedError)`
     """
     return {'scheduler':None}
+
+@pytest.fixture(params=[None, 'dask:1', 'dask:2'])
+def scheduler_current_or_dask(request):
+    """
+    Fixture for testing dask or current process (because multiprocessing is worse with serialization).
+
+    If scheduler raises an exception, you should explicitly check it
+    using `with pytest.raises(NotImplementedError)`
+    """
+    if request.param is None:
+        return {'scheduler':None}
+    else:
+        scheduler, n_workers = request.param.split(':')    
+        n_workers = int(n_workers)
+        return {'scheduler':scheduler, 'n_workers':n_workers}
