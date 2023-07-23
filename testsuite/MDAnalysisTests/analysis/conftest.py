48f094c8a5a6506d4e8166538e0cbcac47bbb62b
Egor Marin <marin@phystech.edu>
2023-07-24 00:17:43 +0300

    FINALLY implement working one-time dask cluster setup in kwargs of a client

--- testsuite/MDAnalysisTests/analysis/conftest.py
+++ testsuite/MDAnalysisTests/analysis/conftest.py
@@ -1,47 +1,98 @@
 import pytest
+import functools
+import importlib
+import multiprocessing
+from MDAnalysis.analysis.base import Client, AnalysisBase, AnalysisFromFunction
+from MDAnalysisTests.analysis.test_base import (
+    FrameAnalysis,
+    IncompleteAnalysis,
+    OldAPIAnalysis,
+)
 
-@pytest.fixture(params=[None, 
-                       'dask:1', 'dask:2', 
-                       'multiprocessing:1', 'multiprocessing:2'
-                       ], 
-                scope='module')
-def schedulers_all(request):
-    """
-    Fixture for testing all possible schedulers.
+import importlib
+from MDAnalysis.analysis.base import Client
 
-    If scheduler raises an exception, you should explicitly check it
-    using `with pytest.raises(NotImplementedError)`
-    """
-    if request.param is None:
-        return {'scheduler':None}
-    else:
-        scheduler, n_workers = request.param.split(':')    
-        n_workers = int(n_workers)
-        return {'scheduler':scheduler, 'n_workers':n_workers}
 
-@pytest.fixture(params=[None], scope='module')
-def scheduler_only_current_process(request):
-    """
-    Fixture for testing only current process.
-    Made to allow for easier replacement of fixture in the future
-    upon implementation of parallelization for certain classes.
+def is_installed(modulename):
+    """Check if modulename is present"""
+    if modulename == "local":
+        return True
+    try:
+        importlib.import_module(modulename)
+        return True
+    except ImportError:
+        return False
 
-    If scheduler raises an exception, you should explicitly check it
-    using `with pytest.raises(NotImplementedError)`
-    """
-    return {'scheduler':None}
 
-@pytest.fixture(params=[None, 'dask:1', 'dask:2'], scope='module')
-def scheduler_current_or_dask(request):
-    """
-    Fixture for testing dask or current process (because multiprocessing is worse with serialization).
+@pytest.fixture
+def list_of_all_subclasses():
+    return [
+        AnalysisBase,
+        FrameAnalysis,
+        IncompleteAnalysis,
+        OldAPIAnalysis,
+    ]
 
-    If scheduler raises an exception, you should explicitly check it
-    using `with pytest.raises(NotImplementedError)`
-    """
-    if request.param is None:
-        return {'scheduler':None}
-    else:
-        scheduler, n_workers = request.param.split(':')    
-        n_workers = int(n_workers)
-        return {'scheduler':scheduler, 'n_workers':n_workers}
+
+def create_fixture_params_for(cls: type):
+    possible_backends = cls.available_backends
+    installed_backends = [b for b in possible_backends if is_installed(b)]
+
+    params = [
+        pytest.param(
+            {"backend": backend, "n_workers": nproc},
+        )
+        for backend in installed_backends
+        for nproc in range(1, 3)
+        if backend != "dask.distributed"
+    ]
+
+    if is_installed("dask.distributed"):
+        params.extend([pytest.param({"client": get_running_dask_client()})])
+
+    return params
+
+
+def get_running_dask_client():
+    # the solution found here: https://stackoverflow.com/questions/59070260/dask-client-detect-local-default-cluster-already-running
+    # note: the used API is non-public and can change any time
+    import dask
+
+    return dask.distributed.client._get_global_client()
+
+
+def dask_client():
+    from dask import distributed
+
+    lc = distributed.LocalCluster(n_workers=2, processes=True)
+    client = distributed.Client(lc)
+    return client
+
+
+@pytest.fixture(scope="session", params=(1, 2, multiprocessing.cpu_count()))
+def setup_dask_distributed(tmpdir_factory, request):
+    import dask
+
+    client = dask.distributed.client._get_global_client()
+    if client is None:
+        with tmpdir_factory.mktemp("dask_cluster").as_cwd():
+            lc = dask.distributed.LocalCluster(n_workers=request.param, processes=True)
+            client = dask.distributed.Client(lc)
+    yield client
+    client.close()
+    lc.close()
+
+
+@pytest.fixture(params=create_fixture_params_for(FrameAnalysis))
+def client_FrameAnalysis(request):
+    return request.param
+
+
+@pytest.fixture(params=create_fixture_params_for(IncompleteAnalysis))
+def client_IncompleteAnalysis(request):
+    return request.param
+
+
+@pytest.fixture(params=create_fixture_params_for(AnalysisFromFunction))
+def client_AnalysisFromFunction(request):
+    return request.param
