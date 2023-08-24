16a3b01c83fc156d3443b0b03e2b0ca137e73cbd
Ian Kenney <ianmichaelkenney@gmail.com>
2023-08-24 16:25:58 -0700

    Added converters base module to sphinx build

--- package/MDAnalysis/converters/base.py
+++ package/MDAnalysis/converters/base.py
@@ -21,9 +21,8 @@
 # J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
 #
 
-"""\
-Converters
-----------
+""" Base classes --- :mod:`MDAnalysis.converters.base`
+======================================================
 
 Converters output information to other libraries.
 
