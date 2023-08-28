d6730218b8934468af7b7237b9ab589e32f35785
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-08-28 09:40:13 +0100

    Remove added set_variable_dimensions class
    Incorportate the functionality into set_dimensions

--- package/MDAnalysis/transformations/boxdimensions.py
+++ package/MDAnalysis/transformations/boxdimensions.py
@@ -29,7 +29,6 @@ Set dimensions of the simulation box, either to a constant vector across
 all timesteps or to a specified vector at each frame.
 
 .. autoclass:: set_dimensions
-.. autoclass:: set_variable_dimensions
 """
 import numpy as np
 
@@ -42,12 +41,13 @@ class set_dimensions(TransformationBase):
 
     Timestep dimensions are modified in place.
 
-    Example
-    -------
+    Examples
+    --------
 
     e.g. set simulation box dimensions to a vector containing unit cell
     dimensions [*a*, *b*, *c*, *alpha*, *beta*, *gamma*], lengths *a*,
     *b*, *c* are in the MDAnalysis length unit (Å), and angles are in degrees.
+    The same dimensions will be used for every frame in the trajectory.
 
     .. code-block:: python
 
@@ -55,61 +55,9 @@ class set_dimensions(TransformationBase):
         transform = mda.transformations.boxdimensions.set_dimensions(dim)
         u.trajectory.add_transformations(transform)
 
-    Parameters
-    ----------
-    dimensions: iterable of floats
-        vector that contains unit cell lengths and angles.
-        Expected shapes are (6, 0) or (1, 6)
-
-    Returns
-    -------
-    :class:`~MDAnalysis.coordinates.timestep.Timestep` object
-    """
-
-    def __init__(self,
-                 dimensions,
-                 max_threads=None,
-                 parallelizable=True):
-        super().__init__(max_threads=max_threads,
-                         parallelizable=parallelizable)
-        self.dimensions = dimensions
-
-        try:
-            self.dimensions = np.asarray(self.dimensions, np.float32)
-        except ValueError:
-            errmsg = (f'{self.dimensions} cannot be converted into '
-                       'np.float32 numpy.ndarray')
-            raise ValueError(errmsg)
-        try:
-            self.dimensions = self.dimensions.reshape(6, )
-        except ValueError:
-            errmsg = (f'{self.dimensions} array does not have valid box '
-                       'dimension shape.\nSimulation box dimensions are '
-                       'given by an float array of shape (6, ), '
-                       ' containing 3 lengths and 3 angles: '
-                       '[a, b, c, alpha, beta, gamma]')
-            raise ValueError(errmsg)
-
-    def _transform(self, ts):
-        ts.dimensions = self.dimensions
-        return ts
-
-
-class set_variable_dimensions(TransformationBase):
-    """
-    Set simulation box dimensions that may vary over time.
-    This is useful for creating NPT trajectories.
-
-    Timestep dimensions are modified in place.
-
-    Example
-    -------
-
-    e.g. set simulation box dimensions to a vector containing unit cell
+    Or e.g. set simulation box dimensions to a vector containing unit cell
     dimensions [*a*, *b*, *c*, *alpha*, *beta*, *gamma*] at the first frame,
     and [*2a*, *2b*, *2c*, *alpha*, *beta*, *gamma*] at the second frame.
-    Lengths *a*, *b*, *c* are in the MDAnalysis length unit (Å), and angles
-    are in degrees.
 
     .. code-block:: python
 
@@ -122,16 +70,19 @@ class set_variable_dimensions(TransformationBase):
 
     Parameters
     ----------
-    dimensions: iterable of floats
+    dimensions: iterable of floats or two-dimensional np.typing.NDArrayLike
         vector that contains unit cell lengths and angles.
-        Expected shape is (N, 6), where N is the number of frames
-        in the trajectory.
+        Expected shapes are (6, 0) or (1, 6) or (N, 6), where
+        N is the number of frames in the trajectory. If shape is (6, 0) or
+        (1, 6), the same dimensions will be used at every frame in the
+        trajectory.
 
     Returns
     -------
     :class:`~MDAnalysis.coordinates.timestep.Timestep` object
 
-    .. versionadded:: 2.7.0
+    .. versionchanged:: 2.7.0
+       Added the option to set varying box dimensions (i.e. an NPT trajectory).
     """
 
     def __init__(self,
@@ -153,14 +104,16 @@ class set_variable_dimensions(TransformationBase):
         except ValueError:
             errmsg = (f'{self.dimensions} array does not have valid box '
                        'dimension shape.\nSimulation box dimensions are '
-                       'given by an float array of shape (N_frames, 6), '
-                       ' containing 3 lengths and 3 angles at each frame: '
+                       'given by an float array of shape (6, 0), (1, 6), '
+                       'or (N, 6) where N is the number of frames in the '
+                       'trajectory and the dimension vector(s) containing '
+                       '3 lengths and 3 angles: '
                        '[a, b, c, alpha, beta, gamma]')
             raise ValueError(errmsg)
 
     def _transform(self, ts):
         try:
-            ts.dimensions = self.dimensions[ts.frame]
+            ts.dimensions = self.dimensions[0] if self.dimensions.shape[0] == 1 else self.dimensions[ts.frame]
         except IndexError as e:
             raise NoDataError(f"Dimensions array has no data for frame {ts.frame}") from e
         return ts
