bc885c30d1ee91674759c741ad30db37877c7a54
pgbarletta <pbarletta@gmail.com>
2023-09-05 13:27:50 -0300

    Document chainID reading

--- package/MDAnalysis/topology/TPRParser.py
+++ package/MDAnalysis/topology/TPRParser.py
@@ -113,7 +113,7 @@ Development notes
 The TPR reader is a pure-python implementation of a basic TPR
 parser. Currently the following sections of the topology are parsed:
 
-* Atoms: number, name, type, resname, resid, segid, mass, charge, element
+* Atoms: number, name, type, resname, resid, segid, chainID, mass, charge, element
   [residue, segment, radius, bfactor, resnum, moltype]
 * Bonds
 * Angles
