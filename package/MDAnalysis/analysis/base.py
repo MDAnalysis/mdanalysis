Start implementing correct result sizes in separate computation groups

package/MDAnalysis/analysis/base.py
@@ -368,23 +368,21 @@ class AnalysisBase(object):
         # if verbose unchanged, use class default
         verbose = getattr(self, "_verbose", False) if verbose is None else verbose
 
-        logger.info("Starting preparation")
-        logger.info("Starting analysis loop over %d trajectory frames", self.n_frames)
+        frames = indexed_frames[:, 1]
+        print(frames)
 
+        logger.info("Starting preparation")
+        self._setup_frames(trajectory=self._trajectory, frames=frames)
+        self._prepare()
         if len(indexed_frames) == 0:  # if `frames` were empty in `run` or `stop=0`
             return self
 
-        frame_indices, frames = (
-            indexed_frames[:, 0],
-            indexed_frames[:, 1],
-        )
         trajectory = self._trajectory[frames]
         for idx, ts in enumerate(ProgressBar(trajectory, verbose=verbose, **progressbar_kwargs)):
-            i = frame_indices[idx]
-            self._frame_index = i
+            self._frame_index = idx
             self._ts = ts
-            self.frames[i] = ts.frame
-            self.times[i] = ts.time
+            self.frames[idx] = ts.frame
+            self.times[idx] = ts.time
             self._single_frame()
         logger.info("Finishing up")
         return self
@@ -591,10 +589,11 @@ class AnalysisBase(object):
             start=start, stop=stop, step=step, frames=frames, n_parts=n_parts
         )
 
+        import os
         # gather all remote objects
         remote_objects: list["AnalysisBase"] = executor.apply(worker_func, computation_groups)
-        self.frames = np.array([obj.frames for obj in remote_objects]).sum(axis=0)
-        self.times = np.array([obj.times for obj in remote_objects]).sum(axis=0)
+        self.frames = np.hstack([obj.frames for obj in remote_objects])
+        self.times = np.hstack([obj.times for obj in remote_objects])
 
         # aggregate results
         remote_results = [obj.results for obj in remote_objects]

package/MDAnalysis/analysis/rms.py
@@ -682,7 +682,7 @@ class RMSD(AnalysisBase):
         self._mobile_coordinates64 = self.mobile_atoms.positions.copy().astype(np.float64)
 
     def _get_aggregator(self):
-        return ResultsGroup(lookup={'rmsd': ResultsGroup.ndarray_sum})
+        return ResultsGroup(lookup={'rmsd': ResultsGroup.ndarray_vstack})
 
     def _single_frame(self):
         mobile_com = self.mobile_atoms.center(self.weights_select).astype(np.float64)