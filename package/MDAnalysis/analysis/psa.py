fdc9ce1be542f30318ee630368a12c3def463a66
IAlibay <ialibay@mdanalysis.org>
2023-12-29 16:12:54 +0000

    Remove tests, add warning

--- package/MDAnalysis/analysis/psa.py
+++ package/MDAnalysis/analysis/psa.py
@@ -68,4 +68,4 @@ wmsg = ('Deprecation in version 2.8.0:\n'
         'MDAnalysis.analysis.psa is deprecated in favour of the MDAKit '
         'PathSimAnalysis (https://github.com/MDAnalysis/PathSimAnalysis) '
         'and will be removed in MDAnalysis version 3.0.0')
-warnings.warn(wmsg)
+warnings.warn(wmsg, category=DeprecationWarning)
