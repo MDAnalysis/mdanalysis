86e8a67781687b641c933bf4d57710b0bb11957c
JureCerar <jure.cerar92.@gmail.com>
2024-01-09 00:46:03 +0100

    Added tests for errors and warnings

--- testsuite/MDAnalysisTests/analysis/test_sasa.py
+++ testsuite/MDAnalysisTests/analysis/test_sasa.py
@@ -27,6 +27,7 @@ from MDAnalysis.analysis.sasa import SASA, RSASA
 from MDAnalysis.transformations import unwrap
 from MDAnalysisTests.datafiles import TPR, XTC, GRO
 
+import logging
 import numpy as np
 from numpy.testing import assert_almost_equal
 
@@ -91,31 +92,68 @@ def analytical_co(co, radii_dict):
 class TestSASA(object):
 
     def test_sasa_analytical(self, co, analytical_co, radii_dict):
+        """Compare to analytical result"""
         R = SASA(co, n_dots=1_024, probe_radius=0, radii_dict=radii_dict).run()
         assert_almost_equal(R.results.area, analytical_co, decimal=1)
 
     def test_sasa_residue(self, residue, radii_dict):
+        """Test for a single residue"""
         R = SASA(residue, radii_dict=radii_dict).run()
 
     @pytest.mark.parametrize('n_dots', [1, 1000])
     def test_sasa_dots(self, residue, n_dots):
+        """Test for different number of dots"""
         R = SASA(residue, n_dots=n_dots).run()
 
     @pytest.mark.parametrize('probe_radius', [0., 2.])
     def test_sasa_probe_radius(self, residue, probe_radius):
+        """Test for different probe radius"""
         R = SASA(residue, probe_radius=probe_radius).run()
 
     def test_sasa_single_frame(self, protein, radii_dict):
+        """Test calculation on a single frame"""
         R = SASA(protein, radii_dict=radii_dict).run()
 
     def test_sasa(self, traj, radii_dict):
+        """Test calculation on a trajectory"""
         # 2 frames are stressful enough
         R = SASA(traj, radii_dict=radii_dict).run(step=5)
 
+    def test_sasa_updating_atom_group(self, protein):
+        """Error if UpdatingAtomGroup"""
+        updating = protein.select_atoms("prop x < 1", updating=True)
+        with pytest.raises(TypeError):
+            R = SASA(updating)
+
+    def test_sasa_no_element_warning(self, residue, caplog):
+        """Check if warning is logged if unknown element"""
+        unknown = residue.copy()
+        unknown.atoms[0].element = "X"
+        R = SASA(unknown)
+        assert any(
+            record.levelno == logging.WARNING for record in caplog.records)
+
+    def test_sasa_wrong_probe_radius(self, protein):
+        """Error if probe radius less than 0"""
+        with pytest.raises(ValueError):
+            R = SASA(protein, probe_radius=-1.0)
+
+    def test_sasa_wrong_n_dots(self, protein):
+        """Error if number of dots less than 1"""
+        with pytest.raises(ValueError):
+            R = SASA(protein, n_dots=0)
+
+    def test_sasa_no_elements_attribute(self, protein):
+        """Error if AtomGroup has no elements attribute"""
+        no_elements = protein.copy()
+        no_elements.universe.del_TopologyAttr("elements")
+        with pytest.raises(ValueError):
+            R = SASA(no_elements)
 
 class TestSASA_r(object):
 
     def test_rsasa_analytical(self, protein, co, analytical_co, radii_dict):
+        """Compare to analytical result"""
         # HACK: Initiate class but call private method on CO data instead
         RS = RSASA(protein, n_dots=1_024,
                    probe_radius=0, radii_dict=radii_dict)
@@ -123,28 +161,70 @@ class TestSASA_r(object):
         assert_almost_equal(area, analytical_co, decimal=1)
 
     def test_rsasa_residue(self, residue, radii_dict):
+        """Test for a single residue"""
         RS = RSASA(residue.atoms, radii_dict=radii_dict).run()
         assert RS.results.relative_area == 1.0
 
     @pytest.mark.parametrize('n_dots', [1, 1000])
     def test_rsasa_dots(self, residue, n_dots):
+        """Test for different number of dots"""
         RS = RSASA(residue, n_dots=n_dots).run()
         assert all(0 <= a <= 1 for a in RS.results.relative_area)
 
     @pytest.mark.parametrize('probe_radius', [0., 2.])
     def test_rsasa_probe_radius(self, residue, probe_radius):
+        """Test for different probe radius"""
         RS = RSASA(residue, probe_radius=probe_radius).run()
         assert all(0 <= a <= 1 for a in RS.results.relative_area)
 
     def test_rsasa_subsele(self, protein):
+        """Test for a sub-selection"""
         RS = RSASA(protein, subsele="not backbone").run()
         assert all(0 <= a <= 1 for a in RS.results.relative_area)
 
     def test_rsasa_single_frame(self, protein, radii_dict):
+        """Test calculation on a single frame"""
         RS = RSASA(protein, radii_dict=radii_dict).run()
         assert all(0 <= a <= 1 for a in RS.results.relative_area)
 
     def test_rsasa(self, traj, radii_dict):
+        """Test calculation on a trajectory"""
         # 2 frames are stressful enough
         RS = RSASA(traj, radii_dict=radii_dict).run(step=5)
         assert all(0 <= a <= 1 for a in RS.results.relative_area)
+
+    def test_rsasa_updating_atom_group(self, protein):
+        """Error if UpdatingAtomGroup"""
+        updating = protein.select_atoms("prop x < 1", updating=True)
+        with pytest.raises(TypeError):
+            RS = RSASA(updating).run()
+
+    def test_rsasa_no_element_warning(self, residue, caplog):
+        """Check if warning is logged if unknown element"""
+        unknown = residue.copy()
+        unknown.atoms[0].element = "X"
+        RS = RSASA(unknown)
+        assert any(
+            record.levelno == logging.WARNING for record in caplog.records)
+
+    def test_rsasa_wrong_probe_radius(self, protein):
+        """Error if probe radius less than 0"""
+        with pytest.raises(ValueError):
+            RS = RSASA(protein, probe_radius=-1.0).run()
+
+    def test_rsasa_wrong_n_dots(self, protein):
+        """Error if number of dots less than 1"""
+        with pytest.raises(ValueError):
+            RS = RSASA(protein, n_dots=0).run()
+
+    def test_rsasa_no_elements_attribute(self, protein):
+        """Error if AtomGroup has no elements attribute"""
+        no_elements = protein.copy()
+        no_elements.universe.del_TopologyAttr("elements")
+        with pytest.raises(ValueError):
+            RS = RSASA(no_elements).run()
+
+    def test_rsasa_no_bonds_attribute(self, co):
+        """Error if AtomGroup has no bonds attribute"""
+        with pytest.raises(ValueError):
+            RS = RSASA(co).run()
