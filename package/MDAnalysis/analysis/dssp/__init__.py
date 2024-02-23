ac6f063fb46bf3bd47c495d861089f5837fa2c1a
Egor Marin <marinegor@fastmail.com>
2024-02-23 18:57:58 +0100

    Remove unnecessary get_hbond_map import

--- package/MDAnalysis/analysis/dssp/__init__.py
+++ package/MDAnalysis/analysis/dssp/__init__.py
@@ -1,8 +1,7 @@
 __all__ = [
     "DSSP",
-    "get_hbond_map",
     "assign",
     "translate",
 ]
 
-from .dssp import DSSP, get_hbond_map, assign, translate
+from .dssp import DSSP, assign, translate
