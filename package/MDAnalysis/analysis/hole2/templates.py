8c69fc013124416a3d2d0cce88cc5aa04a63ed47
IAlibay <ialibay@mdanalysis.org>
2024-02-23 05:56:01 +0000

    update for external mdahole2 mdakit

--- package/MDAnalysis/analysis/hole2/templates.py
+++ package/MDAnalysis/analysis/hole2/templates.py
@@ -32,108 +32,21 @@
 
 Templates used in :mod:`MDAnalysis.analysis.hole2.hole`
 """
+import warnings
 
-exe_err = ('HOLE binary {name} not found. {name} must be on the '
-           'PATH, or the path must provided with the keyword '
-           'argument: {kw}')
+from mdahole2.analysis.templates import (
+    exe_err,
+    IGNORE_RESIDUES,
+    SIMPLE2_RAD,
+    hole_input,
+    hole_lines,
+    vmd_script_array,
+    vmd_script_function,
 
-IGNORE_RESIDUES = ["SOL", "WAT", "TIP", "HOH", "K  ", "NA ", "CL "]
+)
 
-
-#: Built-in HOLE radii (based on ``simple.rad`` from the HOLE_ distribution):
-#: van der Waals radii are AMBER united atom from Weiner et al. (1984), JACS, vol 106 pp765-768.
-#: *Simple* - Only use one value for each element C O H etc.
-#: Added radii for K+, NA+, CL- (Pauling hydration radius from Hille 2002).
-#: The data file can be written with the convenience function :func:`write_simplerad2`.
-SIMPLE2_RAD = r"""
-remark: Time-stamp: <2005-11-21 13:57:55 oliver> [OB]
-remark: van der Waals radii: AMBER united atom
-remark: from Weiner et al. (1984), JACS, vol 106 pp765-768
-remark: Simple - Only use one value for each element C O H etc.
-remark: van der Waals radii
-remark: general last
-VDWR C??? ??? 1.85
-VDWR O??? ??? 1.65
-VDWR S??? ??? 2.00
-VDWR N??? ??? 1.75
-VDWR H??? ??? 1.00
-VDWR H?   ??? 1.00
-VDWR P??? ??? 2.10
-remark: ASN, GLN polar H (odd names for these atoms in xplor)
-VDWR E2?  GLN 1.00
-VDWR D2?  ASN 1.00
-remark: amber lone pairs on sulphurs
-VDWR LP?? ??? 0.00
-remark: for some funny reason it wants radius for K even though
-remark: it is on the exclude list
-remark: Use Pauling hydration radius (Hille 2001) [OB]
-VDWR K?   ??? 1.33
-VDWR NA?  ??? 0.95
-VDWR CL?  ??? 1.81
-remark: funny hydrogens in gA structure [OB]
-VDWR 1H?  ??? 1.00
-VDWR 2H?  ??? 1.00
-VDWR 3H?  ??? 1.00
-remark: need bond rad for molqpt option
-BOND C??? 0.85
-BOND N??? 0.75
-BOND O??? 0.7
-BOND S??? 1.1
-BOND H??? 0.5
-BOND P??? 1.0
-BOND ???? 0.85
-"""
-
-hole_input = """
-! Input file for Oliver Smart's HOLE program
-! written by MDAnalysis.analysis.hole2.hole
-! filename = {filename}
-COORD  {coordinates}
-RADIUS {radius}
-SPHPDB {sphpdb}
-SAMPLE {sample:f}
-ENDRAD {end_radius:f}
-IGNORE {ignore}
-SHORTO {output_level:d}
-"""
-
-hole_lines = {
-    'cpoint': 'CPOINT {:.10f} {:.10f} {:.10f}\n',
-    'cvect': 'CVECT {:.10f} {:.10f} {:.10f}\n',
-    'random_seed': 'RASEED {}\n',
-    'dcd': 'CHARMD {dcd}\nCHARMS {iniskip:d} {step:d}\n',
-}
-
-vmd_script_array = """\
-set no_water_color {no_water_color}
-set one_water_color {one_water_color}
-set double_water_color {double_water_color}
-array set triangles {{}}
-"""
-
-vmd_script_function = r"""
-global vmd_frame;
-trace add variable vmd_frame([molinfo top]) write drawFrame
-
-proc drawFrame { name element op } {
-            global vmd_frame triangles no_water_color one_water_color double_water_color;
-            set frame $vmd_frame([molinfo top])
-            draw delete all;
-
-            draw color $no_water_color;
-            foreach shape [lindex $triangles($frame) 0] {
-                draw trinorm {*}$shape
-            }
-            draw color $one_water_color;
-            foreach shape [lindex $triangles($frame) 1] {
-                draw trinorm {*}$shape
-            }
-            draw color $double_water_color;
-            foreach shape [lindex $triangles($frame) 2] {
-                draw trinorm {*}$shape
-            }
-        }
-
-drawFrame 0 0 0
-"""
-
+wmsg = ("Deprecation in version 2.8.0\n"
+        "MDAnalysis.analysis.hole2 is deprecated in favour of the "
+        "MDAKit madahole2 (https://www.mdanalysis.org/mdahole2/) "
+        "and will be removed in MDAnalysis version 3.0.0")
+warnings.warn(wmsg, category=DeprecationWarning)
