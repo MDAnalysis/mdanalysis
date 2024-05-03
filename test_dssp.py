f50f5c39d0455d58a3674727b9f7f19e0bf31109
Oliver Beckstein <orbeckst@gmail.com>
2024-05-03 15:15:52 -0700

    Delete test_dssp.py
    manual test a top level, should not be in source

--- test_dssp.py
+++
@@ -1,4 +0,0 @@
-from MDAnalysis.analysis.dssp import DSSP
-import MDAnalysis as mda
-u = mda.Universe('large_data/NhaA_non_water.gro', 'large_data/NhaA_non_water.xtc')
-DSSP(u, guess_hydrogens=False).run(stop=20)
