6682d6e42c3992d8e16631248f2412ded1658072
Egor Marin <marinegor@fastmail.com>
2024-05-22 22:59:45 +0200

    Remove inherited docs

--- package/MDAnalysis/topology/MMCIFParser.py
+++ package/MDAnalysis/topology/MMCIFParser.py
@@ -1,42 +1,6 @@
-# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
-#
-# MDAnalysis --- https://www.mdanalysis.org
-# Copyright (c) 2006-2017 The MDAnalysis Development Team and contributors
-# (see the file AUTHORS for the full list of names)
-#
-# Released under the GNU Public Licence, v2 or any higher version
-#
-# Please cite your use of MDAnalysis in published work:
-#
-# R. J. Gowers, M. Linke, J. Barnoud, T. J. E. Reddy, M. N. Melo, S. L. Seyler,
-# D. L. Dotson, J. Domanski, S. Buchoux, I. M. Kenney, and O. Beckstein.
-# MDAnalysis: A Python package for the rapid analysis of molecular dynamics
-# simulations. In S. Benthall and S. Rostrup editors, Proceedings of the 15th
-# Python in Science Conference, pages 102-109, Austin, TX, 2016. SciPy.
-# doi: 10.25080/majora-629e541a-00e
-#
-# N. Michaud-Agrawal, E. J. Denning, T. B. Woolf, and O. Beckstein.
-# MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
-# J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
-# TODO
-
 """
 MMCIF Topology Parser # TODO
 ===================
-
-.. versionadded:: 0.9.1
-
-Reads an xyz file and pulls the atom information from it.  Because
-xyz only has atom name information, all information about residues
-and segments won't be populated.
-
-Classes
--------
-
-.. autoclass:: XYZParser
-   :members:
-
 """
 
 import numpy as np
@@ -62,16 +26,6 @@ from ..core.topologyattrs import (
 
 
 class MMCIFParser(TopologyReaderBase):
-    """Parse a list of atoms from an XYZ file.
-
-    Creates the following attributes:
-     - Atomnames
-     - TODO
-
-
-    .. versionadded:: 2.8.0
-    """
-
     format = "MMCIF"
 
     def parse(self, **kwargs):
