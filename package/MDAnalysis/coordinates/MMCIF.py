3de856511e46afffd3f25f3d97f01c6b65be4232
Egor Marin <marinegor@fastmail.com>
2024-09-30 14:45:00 +0200

    Start adding documentation

--- package/MDAnalysis/coordinates/MMCIF.py
+++ package/MDAnalysis/coordinates/MMCIF.py
@@ -15,7 +15,18 @@ except ImportError:
 logger = logging.getLogger("MDAnalysis.coordinates.MMCIF")
 
 
-def get_Coordinates(model: gemmi.Model) -> np.ndarray:
+def get_coordinates(model: gemmi.Model) -> np.ndarray:
+    """Get coordinates of all atoms in the `gemmi.Model` object.
+
+    Parameters
+    ----------
+    model
+        input `gemmi.Model`, e.g. `gemmi.read_structure('file.cif')[0]`
+
+    Returns
+    -------
+        np.ndarray, shape [n, 3], where `n` is the number of atoms in the structure.
+    """
     return np.array(
         [[*at.pos.tolist()] for chain in model for res in chain for at in res]
     )
@@ -33,14 +44,17 @@ class MMCIFReader(base.SingleFrameReaderBase):
             warnings.warn(
                 f"File {self.filename} has {len(structure)} models, but only the first one will be read"
             )
+        if len(structure) > 1:
+            warnings.warn(
+                "MMCIF model {self.filename} contains {len(model)=} different models, "
+                "but only the first one will be used to assign the topology"
+            )  # TODO: if the structures represent timestamps, can parse them with :func:`get_coordinates`.
+
         model = structure[0]
-        coords = get_Coordinates(model)
+        coords = get_coordinates(model)
         self.n_atoms = len(coords)
         self.ts = self._Timestep.from_coordinates(coords, **self._ts_kwargs)
         self.ts.frame = 0
-
-    def Writer(self, filename, n_atoms=None, **kwargs):
-        raise NotImplementedError
 
     def close(self):
         pass
