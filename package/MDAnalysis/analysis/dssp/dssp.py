aa344ac31dd8865541e501d797b38bc32995e3df
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-09-30 10:55:32 +0200

    Update package/MDAnalysis/analysis/dssp/dssp.py
    Co-authored-by: Yuxuan Zhuang <yuzhuang@stanford.edu>

--- package/MDAnalysis/analysis/dssp/dssp.py
+++ package/MDAnalysis/analysis/dssp/dssp.py
@@ -277,7 +277,6 @@ class DSSP(AnalysisBase):
 
     .. versionadded:: 2.8.0
 
-    .. versionchanged:: 2.8.0
        Enabled **parallel execution** with the ``multiprocessing`` and ``dask``
        backends; use the new method :meth:`get_supported_backends` to see all
        supported backends.
