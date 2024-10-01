0d61248879d5630945510cd0fb2fb3c6a4e29aee
Egor Marin <marinegor@fastmail.com>
2024-10-01 23:15:45 +0200

    Add documentation for trajectory and topology parsers

--- package/MDAnalysis/coordinates/MMCIF.py
+++ package/MDAnalysis/coordinates/MMCIF.py
@@ -33,13 +33,29 @@ def get_coordinates(model: gemmi.Model) -> np.ndarray:
 
 
 class MMCIFReader(base.SingleFrameReaderBase):
-    """Reads from an MMCIF file"""
+    """Reads from an MMCIF file using ``gemmi`` library as a backend.
+
+    Notes
+    -----
+
+    If the structure represents an ensemble, only the first structure in the ensemble
+    is read here (and a warning is thrown). Also, if the structure has a placeholder "CRYST1"
+    record (1, 1, 1, 90, 90, 90), it's set to ``None`` instead.
+
+    .. versionadded:: 2.8.0
+    """
 
     format = "MMCIF"
     units = {"time": None, "length": "Angstrom"}
 
     def _read_first_frame(self):
         structure = gemmi.read_structure(self.filename)
+        cell_dims = np.array(
+            [
+                getattr(structure.cell, name)
+                for name in ("a", "b", "c", "alpha", "beta", "gamma")
+            ]
+        )
         if len(structure) > 1:
             warnings.warn(
                 f"File {self.filename} has {len(structure)} models, but only the first one will be read"
@@ -54,6 +70,15 @@ class MMCIFReader(base.SingleFrameReaderBase):
         coords = get_coordinates(model)
         self.n_atoms = len(coords)
         self.ts = self._Timestep.from_coordinates(coords, **self._ts_kwargs)
+        if np.allclose(cell_dims, np.array([1.0, 1.0, 1.0, 90.0, 90.0, 90.0])):
+            warnings.warn(
+                "1 A^3 CRYST1 record,"
+                " this is usually a placeholder."
+                " Unit cell dimensions will be set to None."
+            )
+            self.ts.dimensions = None
+        else:
+            self.ts.dimensions = cell_dims
         self.ts.frame = 0
 
     def close(self):
--- package/MDAnalysis/topology/MMCIFParser.py
+++ package/MDAnalysis/topology/MMCIFParser.py
@@ -58,13 +58,15 @@ def _into_idx(arr: list) -> list[int]:
     Returns
     -------
         list[int] -- array where these elements are replaced with their unique indices, in order of appearance.
+
+    .. versionadded:: 2.8.0
     """
     return [idx for idx, (_, group) in enumerate(itertools.groupby(arr)) for _ in group]
 
 
 def get_Atomattrs(model: gemmi.Model) -> tuple[list[AtomAttr], np.ndarray]:
     """Extract all attributes that are subclasses of :class:`..core.topologyattrs.AtomAttr` from a  ``gemmi.Model`` object,
-    and a `segidx` index.
+    and a `residx` index with indices of all atoms in residues.
 
     Parameters
     ----------
@@ -79,6 +81,8 @@ def get_Atomattrs(model: gemmi.Model) -> tuple[list[AtomAttr], np.ndarray]:
     ------
     ValueError
         if any of the records is neither 'ATOM' nor 'HETATM'
+
+    .. versionadded:: 2.8.0
     """
     (
         altlocs,  # at.altloc
@@ -162,10 +166,24 @@ def get_Atomattrs(model: gemmi.Model) -> tuple[list[AtomAttr], np.ndarray]:
 
 
 def get_Residueattrs(model: gemmi.Model) -> tuple[list[ResidueAttr], np.ndarray]:
+    """Extract all attributes that are subclasses of :class:`..core.topologyattrs.ResidueAttr` from a  ``gemmi.Model`` object,
+    and a `segidx` index witn indices of all residues in segments.
+
+    Parameters
+    ----------
+    model
+        input `gemmi.Model`, e.g. `gemmi.read_structure('file.cif')[0]`
+
+    Returns
+    -------
+        tuple[list[ResidueAttr], np.ndarray] -- first element is list of all extracted attributes, second element is `segidx`
+
+    .. versionadded:: 2.8.0
+    """
     (
-        icodes,  # res.seqid.icode
-        resids,  # res.seqid.num
-        resnames,  # res.name
+        icodes,  # residue.seqid.icode
+        resids,  # residue.seqid.num
+        resnames,  # residue.name
         segidx,  # chain.name
         resnums,
     ) = map(
@@ -174,34 +192,73 @@ def get_Residueattrs(model: gemmi.Model) -> tuple[list[ResidueAttr], np.ndarray]
             zip(
                 *[
                     (
-                        res.seqid.icode,
-                        res.seqid.num,
-                        res.name,
+                        residue.seqid.icode,
+                        residue.seqid.num,
+                        residue.name,
                         chain.name,
-                        res.seqid.num,
+                        residue.seqid.num,
                     )
                     for chain in model
-                    for res in chain
+                    for residue in chain
                 ]
             )
         ),
     )
     segidx = np.array(_into_idx(segidx))
     attrs = [
-        Resnums(resnums),  # res.seqid.num
-        ICodes(icodes),  # res.seqid.icode
-        Resids(resids),  # res.seqid.num
-        Resnames(resnames),  # res.name
+        Resnums(resnums),
+        ICodes(icodes),
+        Resids(resids),
+        Resnames(resnames),
     ]
     return attrs, segidx
 
 
-def get_Segmentattrs(model: gemmi.Model) -> list[SegmentAttr]:
+def get_Segmentattrs(model: gemmi.Model) -> SegmentAttr:
+    """Extract all attributes that are subclasses of :class:`..core.topologyattrs.SegmentAttr` from a  ``gemmi.Model`` object.
+
+    Parameters
+    ----------
+    model
+        input `gemmi.Model`, e.g. `gemmi.read_structure('file.cif')[0]`
+
+    Returns
+    -------
+        list[SegmentAttr] -- list of all extracted attributes
+
+    .. versionadded:: 2.8.0
+    """
     segids = [chain.name for chain in model]
     return [Segids(segids)]
 
 
 class MMCIFParser(TopologyReaderBase):
+    """Parser that obtains a list of atoms from a standard MMCIF/PDBx file using ``gemmi`` library (https://github.com/project-gemmi/gemmi).
+
+    Creates the following Attributes (if present):
+        - :class:`..core.topologyattrs.AtomAttr` subclasses:
+            - :class:`..core.topologyattrs.AltLocs`
+            - :class:`..core.topologyattrs.Atomids`
+            - :class:`..core.topologyattrs.Atomnames`
+            - :class:`..core.topologyattrs.Atomtypes`
+            - :class:`..core.topologyattrs.ChainIDs`
+            - :class:`..core.topologyattrs.Elements`
+            - :class:`..core.topologyattrs.FormalCharges`
+            - :class:`..core.topologyattrs.Masses`
+            - :class:`..core.topologyattrs.Occupancies`
+            - :class:`..core.topologyattrs.RecordTypes`
+            - :class:`..core.topologyattrs.Tempfactors`
+        - :class:`..core.topologyattrs.ResidueAttr` subclasses:
+            - :class:`..core.topologyattrs.Resnums`
+            - :class:`..core.topologyattrs.ICodes`
+            - :class:`..core.topologyattrs.Resids`
+            - :class:`..core.topologyattrs.Resnames`
+        - :class:`..core.topologyattrs.SegmentAttr` subclasses:
+            - :class:`..core.topologyattrs.Segids`
+
+    .. versionadded:: 2.8.0
+    """
+
     format = "MMCIF"
 
     def parse(self, **kwargs):
@@ -226,6 +283,7 @@ class MMCIFParser(TopologyReaderBase):
 
         attrs = atomAttrs + residAttrs + segmentAttrs
 
+        # due to the list(map(...)) construction, all elements in array have equal lengths
         n_atoms = len(atomAttrs[0])
         n_residues = len(residAttrs[0])
         n_segments = len(segmentAttrs[0])
