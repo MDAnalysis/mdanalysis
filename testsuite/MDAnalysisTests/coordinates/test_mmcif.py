4fc3a783886383e38ad95b456c5a72d61da3558c
Egor Marin <marinegor@fastmail.com>
2024-10-02 19:52:03 +0200

    Write simple tests

--- testsuite/MDAnalysisTests/coordinates/test_mmcif.py
+++ testsuite/MDAnalysisTests/coordinates/test_mmcif.py
@@ -1,6 +1,4 @@
 import glob
-import os
-from io import StringIO
 
 import MDAnalysis as mda
 import numpy as np
@@ -34,11 +32,28 @@ def test_works_without_explicit_format(mmcif_filename):
     [
         (f"{MMCIF_FOLDER}/1YJP.cif", 59, 66),
         (f"{MMCIF_FOLDER}/1YJP.cif.gz", 59, 66),
-        (f"{MMCIF_FOLDER}/7ETN.cif", 70, 70),
-        (f"{MMCIF_FOLDER}/7ETN.cif.gz", 70, 70),
+        (f"{MMCIF_FOLDER}/7ETN.cif", 150, 150),
+        (f"{MMCIF_FOLDER}/7ETN.cif.gz", 150, 150),
     ],
 )
 def test_n_atoms(mmcif_filename, natoms_protein, natoms_total):
     u = mda.Universe(mmcif_filename)
     assert len(u.atoms) == natoms_total
-    assert len(u.select("protein").atoms) == natoms_protein
+    assert len(u.select_atoms("protein").atoms) == natoms_protein
+
+
+@pytest.mark.parametrize(
+    "mmcif_filename,cell",
+    [
+        (
+            f"{MMCIF_FOLDER}/1YJP.cif.gz",
+            np.array([21.937, 4.866, 23.477, 90.00, 107.08, 90.00]),
+        ),
+        (
+            f"{MMCIF_FOLDER}/7ETN.cif.gz",
+            np.array([5.264, 24.967, 20.736, 90.00, 94.85, 90.00]),
+        ),
+    ],
+)
+def test_cell(mmcif_filename, cell):
+    assert np.allclose(mda.Universe(mmcif_filename).coord._unitcell, cell)
