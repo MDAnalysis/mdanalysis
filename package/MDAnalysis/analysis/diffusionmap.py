b8a03ceaab9f11f8f54eb3e88e0134e29e4d10a2
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-10-20 00:17:12 +0200

    Update diffusionmap.py

--- package/MDAnalysis/analysis/diffusionmap.py
+++ package/MDAnalysis/analysis/diffusionmap.py
@@ -234,7 +234,7 @@ class DistanceMatrix(AnalysisBase):
     .. versionchanged:: 2.8.0
          :class:`DistanceMatrix` is now correctly works with `frames=...`
          parameter (#4432) by iterating over `self._sliced_trajectory`.
-         Enabled **parallel execution** with the ``multiprocessing`` and 
+         Enabled **parallel execution** with the ``multiprocessing`` and
          ``dask`` backends; use the new method :meth:`get_supported_backends`
          to see all supported backends.
     """
