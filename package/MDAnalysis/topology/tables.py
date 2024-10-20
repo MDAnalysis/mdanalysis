e791a008b7cb5ff0a0c57b04374097141b895381
IAlibay <IAlibay@users.noreply.github.com>
2024-10-20 10:43:25 +0100

    Add tables back

---
+++ package/MDAnalysis/topology/tables.py
@@ -0,0 +1,21 @@
+import warnings
+from MDAnalysis.guesser.tables import (
+    kv2dict,
+    TABLE_ATOMELEMENTS,
+    atomelements,
+    elements,
+    TABLE_MASSES,
+    masses,
+    TABLE_VDWRADII,
+    vdwradii,
+    Z2SYMB,
+    SYMB2Z,
+    SYBYL2SYMB,
+)
+
+wmsg = ("Deprecated in version 2.8.0\n"
+        "MDAnalysis.topology.tables has been moved to "
+        "MDAnalysis.guesser.tables. This import point "
+        "will be removed in MDAnalysis version 3.0.0")
+warnings.warn(wmsg, category=DeprecationWarning)
+
