927d7a0f3e41f95f5fe49af2628155de47172098
Egor Marin <marinegor@fastmail.com>
2024-10-25 11:17:18 +0000

    add black instead of ruff

--- testsuite/MDAnalysisTests/topology/test_mmcif.py
+++ testsuite/MDAnalysisTests/topology/test_mmcif.py
@@ -35,7 +35,10 @@ def test_chains(mmcif_filename, n_chains):
 @pytest.mark.parametrize(
     "mmcif_filename,sequence",
     [
-        (f"{MMCIF_FOLDER}/1YJP.cif", ["GLY", "ASN", "ASN", "GLN", "GLN", "ASN", "TYR"]),
+        (
+            f"{MMCIF_FOLDER}/1YJP.cif",
+            ["GLY", "ASN", "ASN", "GLN", "GLN", "ASN", "TYR"],
+        ),
         (
             f"{MMCIF_FOLDER}/1YJP.cif.gz",
             ["GLY", "ASN", "ASN", "GLN", "GLN", "ASN", "TYR"],
@@ -47,6 +50,7 @@ def test_chains(mmcif_filename, n_chains):
 def test_sequence(mmcif_filename, sequence):
     u = mda.Universe(mmcif_filename)
     in_structure = [
-        str(res.resname) for res in u.select_atoms("protein and chainid A").residues
+        str(res.resname)
+        for res in u.select_atoms("protein and chainid A").residues
     ]
     assert in_structure == sequence, ":".join(in_structure)
