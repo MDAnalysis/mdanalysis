07df13cddadb317fe15a22f5ea7e9af140d7daa4
IAlibay <ialibay@mdanalysis.org>
2024-11-11 17:15:02 +0000

    Finalize v2.8.0 release

--- testsuite/setup.py
+++ testsuite/setup.py
@@ -74,7 +74,7 @@ class MDA_SDist(sdist.sdist):
 
 if __name__ == '__main__':
     # this must be in-sync with MDAnalysis
-    RELEASE = "2.8.0-dev0"
+    RELEASE = "2.8.0"
 
     setup(
         version=RELEASE,
