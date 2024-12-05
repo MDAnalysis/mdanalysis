fd8163a1bde4f76f3a0b308828a8710f2f025d93
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-12-05 02:19:55 +0100

    Update test_atomicdistances.py

--- testsuite/MDAnalysisTests/analysis/test_atomicdistances.py
+++ testsuite/MDAnalysisTests/analysis/test_atomicdistances.py
@@ -119,7 +119,8 @@ class TestAtomicDistances(object):
         '''Ensure that pairwise distances between atoms are
         correctly calculated without PBCs.'''
         pairwise_no_pbc = ad.AtomicDistances(
-            ad_ag1, ad_ag2, pbc=False).run()
+            ad_ag1, ad_ag2, pbc=False
+        ).run()
         actual = pairwise_no_pbc.results
 
         # compare with expected values from dist()
