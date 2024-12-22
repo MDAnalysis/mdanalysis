c0274e36435c53f3cd889f80510011afc7711c6f
Rocco Meli <r.meli@bluemail.ch>
2024-12-22 23:01:21 +0100

    fmt misc

--- benchmarks/benchmarks/analysis/rms.py
+++ benchmarks/benchmarks/analysis/rms.py
@@ -1,7 +1,7 @@
 import MDAnalysis
 
 try:
-    from MDAnalysisTests.datafiles import PSF, DCD
+    from MDAnalysisTests.datafiles import DCD, PSF
 except:
     pass
 
@@ -10,18 +10,12 @@ try:
 except:
     pass
 
-class SimpleRmsBench(object):
-    """Benchmarks for MDAnalysis.analysis.rms.rmsd
-    """
 
-    params = ([100, 500, 2000],
-              [None, [1.0, 0.5]],
-              [False, True],
-              [False, True])
-    param_names = ['num_atoms',
-                   'weights',
-                   'center',
-                   'superposition']
+class SimpleRmsBench(object):
+    """Benchmarks for MDAnalysis.analysis.rms.rmsd"""
+
+    params = ([100, 500, 2000], [None, [1.0, 0.5]], [False, True], [False, True])
+    param_names = ["num_atoms", "weights", "center", "superposition"]
 
     def setup(self, num_atoms, weights, center, superposition):
         # mimic rmsd docstring example code
@@ -43,33 +37,32 @@ class SimpleRmsBench(object):
         its docstring example code along with several possible
         permutations of parameters.
         """
-        rms.rmsd(a=self.A,
-                 b=self.B,
-                 weights=weights,
-                 center=center,
-                 superposition=superposition)
+        rms.rmsd(
+            a=self.A,
+            b=self.B,
+            weights=weights,
+            center=center,
+            superposition=superposition,
+        )
+
 
 class RmsdTrajBench(object):
-    """Benchmarks for MDAnalysis.analysis.rms.RMSD
-    """
+    """Benchmarks for MDAnalysis.analysis.rms.RMSD"""
 
     # TODO: RMSD has many parameters / options,
     # some of which are apparently still considered
     # experimental -- we'll eventually want to
     # benchmark more of these
 
-    params = (['all', 'backbone'],
-              [None, 'mass'])
+    params = (["all", "backbone"], [None, "mass"])
 
-    param_names = ['select',
-                   'weights']
+    param_names = ["select", "weights"]
 
     def setup(self, select, weights):
         self.u = MDAnalysis.Universe(PSF, DCD)
-        self.RMSD_inst = rms.RMSD(atomgroup=self.u,
-                                  reference=None,
-                                  select=select,
-                                  weights=weights)
+        self.RMSD_inst = rms.RMSD(
+            atomgroup=self.u, reference=None, select=select, weights=weights
+        )
 
     def time_RMSD(self, select, weights):
         """Benchmark RMSD.run() method, which parses
@@ -79,22 +72,16 @@ class RmsdTrajBench(object):
 
 
 class RmsfTrajBench(object):
-    """Benchmarks for MDAnalysis.analysis.rms.RMSF
-    """
+    """Benchmarks for MDAnalysis.analysis.rms.RMSF"""
 
-    params = ([100,500,2000],
-              [None, 3],
-              [None,'mass'])
+    params = ([100, 500, 2000], [None, 3], [None, "mass"])
 
-    param_names = ['n_atoms',
-                   'step',
-                   'weights']
+    param_names = ["n_atoms", "step", "weights"]
 
     def setup(self, n_atoms, step, weights):
         self.u = MDAnalysis.Universe(PSF, DCD)
         self.ag = self.u.atoms[:n_atoms]
-        self.RMSF_inst = rms.RMSF(atomgroup=self.ag,
-                                  weights=weights)
+        self.RMSF_inst = rms.RMSF(atomgroup=self.ag, weights=weights)
 
     def time_RMSF(self, n_atoms, step, weights):
         """Benchmark RMSF.run() method, which parses
