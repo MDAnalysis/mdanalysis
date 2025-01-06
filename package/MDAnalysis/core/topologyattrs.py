89d0b1785f2f9d3cd105b60b8212cfad15464076
Oliver Beckstein <orbeckst@gmail.com>
2025-01-06 16:43:01 -0700

    Update package/MDAnalysis/core/topologyattrs.py

--- package/MDAnalysis/core/topologyattrs.py
+++ package/MDAnalysis/core/topologyattrs.py
@@ -259,7 +259,9 @@ def _build_stub(method_name, method, attribute_name):
             attribute_name
         )
     )
-    # The original docstring is arrumed to be formatted with black
+    # The original docstring is assumed to be formatted with black
+    # (i.e., *all text* lines are already indented, the first line 
+    # is a triple quote followed by newline)
     stub_method.__doc__ = textwrap.dedent(method.__doc__) + "\n\n" + annotation
     stub_method.__name__ = method_name
     stub_method.__signature__ = inspect_signature(method)
