e68fccecfe5788a5b861dc5775531b58093b0dd1
Egor Marin <e.marin@enpicom.com>
2025-02-19 10:01:23 +0100

    Add documentation for mmcif coords

--- package/MDAnalysis/coordinates/MMCIF.py
+++ package/MDAnalysis/coordinates/MMCIF.py
@@ -1,3 +1,71 @@
+"""MMCIF structure files in MDAnalysis --- :mod:`MDAnalysis.coordinates.MMCIF`
+==========================================================================
+
+MDAnalysis reads coordinates from MMCIF (macromolecular Crystallographic Information File) files
+using the gemmi library as a backend. MMCIF is a more modern and flexible alternative to the PDB format,
+capable of storing detailed structural and experimental data about biological macromolecules.
+
+MMCIF files use a structured, tabular format with key-value pairs to store both coordinate and atom information.
+The format supports multiple models/frames, though this implementation currently only reads the first model
+and provides warning messages for multi-model files.
+
+Basic usage
+-----------
+
+Reading an MMCIF file is straightforward:
+
+    .. code-block:: python
+
+        import MDAnalysis as mda
+        u = mda.Universe("structure.cif")
+
+
+The reader will automatically detect if the structure contains placeholder unit cell information
+(usually it's the case for cryoEM structures, and cell parameters are (1, 1, 1, 90, 90, 90))
+and set dimensions to None in that case.
+
+Capabilities
+------------
+
+The MMCIF reader implementation uses the gemmi library to parse files and extract coordinates
+and unit cell information. Currently only reading capability is supported, with the following
+features:
+
+- Single frame/model reading
+- Unit cell dimensions detection
+- Support for compressed .cif.gz files
+- Automatic handling of placeholder unit cells for cryoEM structures
+
+Examples
+--------
+
+Basic structure loading::
+
+    .. code-block:: python
+        # Load structure from MMCIF
+        u = mda.Universe("structure.cif")
+
+        # or from cif.gz file
+        u = mda.Universe("structure.cif.gz")
+
+
+The coordinates are stored in Angstroms.
+
+Classes
+-------
+
+.. autoclass:: MMCIFReader
+   :members:
+   :inherited-members:
+
+See Also
+--------
+- wwPDB MMCIF Resources: http://mmcif.wwpdb.org
+- Gemmi library documentation: https://gemmi.readthedocs.io
+
+.. versionadded:: 2.9.0
+"""
+
 import logging
 import warnings
 
@@ -57,11 +125,11 @@ class MMCIFReader(base.SingleFrameReaderBase):
             ]
         )
         if len(structure) > 1:
-            warnings.warn(
+            warnings.warn(  # FIXME: add tests for this
                 f"File {self.filename} has {len(structure)} models, but only the first one will be read"
             )
         if len(structure) > 1:
-            warnings.warn(
+            warnings.warn(  # FIXME: add tests for this
                 "MMCIF model {self.filename} contains {len(model)=} different models, "
                 "but only the first one will be used to assign the topology"
             )  # TODO: if the structures represent timestamps, can parse them with :func:`get_coordinates`.
@@ -71,15 +139,12 @@ class MMCIFReader(base.SingleFrameReaderBase):
         self.n_atoms = len(coords)
         self.ts = self._Timestep.from_coordinates(coords, **self._ts_kwargs)
         if np.allclose(cell_dims, np.array([1.0, 1.0, 1.0, 90.0, 90.0, 90.0])):
-            warnings.warn(
+            warnings.warn(  # FIXME: add tests for this
                 "1 A^3 CRYST1 record,"
                 " this is usually a placeholder."
                 " Unit cell dimensions will be set to None."
             )
-            self.ts.dimensions = None
+            self.ts.dimensions = None  # FIXME: add tests for this
         else:
             self.ts.dimensions = cell_dims
         self.ts.frame = 0
-
-    def close(self):
-        pass
