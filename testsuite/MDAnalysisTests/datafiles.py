53c51f4931acf2139ff56ece923e98cb3887c92b
Egor Marin <marinegor@fastmail.com>
2025-02-23 14:37:36 +0100

    format datafiles with black

--- testsuite/MDAnalysisTests/datafiles.py
+++ testsuite/MDAnalysisTests/datafiles.py
@@ -380,7 +380,7 @@ __all__ = [
     "SURFACE_PDB",  # 111 FCC lattice topology for NSGrid bug #2345
     "SURFACE_TRR",  # full precision coordinates for NSGrid bug #2345
     "DSSP",  # DSSP test suite
-    "MMCIF", # MMCIF test suite
+    "MMCIF",  # MMCIF test suite
 ]
 
 from importlib import resources
