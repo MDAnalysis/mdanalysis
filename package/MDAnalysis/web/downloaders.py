8c1a19608e57669fea6bae2374638f195126690f
Joshua Raphael Uy <jauy1@asu.edu>
2025-03-05 14:49:28 -0700

    refactored cache logic

--- package/MDAnalysis/web/downloaders.py
+++ package/MDAnalysis/web/downloaders.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-import tempfile
 from abc import ABC, abstractmethod
+import tempfile
 
 import requests
 from MDAnalysis.core.universe import Universe
@@ -13,15 +13,18 @@ class FileDownloadPDBError(Exception):
         message -- explanation of the error
     """
 
-    def __init__(self,
+    def __init__(self, 
         message="There was an error downloading the file from the Protein Data Bank. PDB or format for PDB code may not be available.",
         ):
-  
-        super().__init__(self.message)
+
+        super().__init__(message)
 
 
 class BaseDownloader(ABC):
     """Abstract Base Class for all Downloaders. Not meant to be directly initalized!"""
+
+    def __str__(self):
+        return f"Metadata: id={self.id}, file_format={self.file_format}, "
 
     def __init__(self, id, file_format):
         """Declares attributes meant to be manipulated by child classes"""
@@ -29,15 +32,14 @@ class BaseDownloader(ABC):
         # Attributes are meant to get updated by child instances 
         self.id = id
         self.file_format = file_format
+        
         self._file = None
 
     @abstractmethod
     def download():
+        """This should be be implelemented setting self._file to a file like object"""
         pass
 
-    def __str__(self):
-        return f"Metadata: id={self.id}, file_format={self.file_format}, "
- 
     def convert_to_universe(self, **kwargs):
         """Converts Downloaded file to a Universe"""
 
@@ -47,8 +49,7 @@ class BaseDownloader(ABC):
         try:
             return Universe(self._file.name, topology_format=self.file_format.upper(), **kwargs)
         finally:
-            self._file.close() #  Mark temporary file for deletion
-
+            self._file.close() 
 
 class PdbDownloader(BaseDownloader):
     """Class to handle download PDBs from the RCSB"""
@@ -56,41 +57,51 @@ class PdbDownloader(BaseDownloader):
     def __init__(self, PDB_ID, file_format="pdb"):
         super().__init__(PDB_ID, file_format)
 
-    def download(self, cache_path=None, timeout=None):
-        """Downloads files from the RCSB"""
+        self._download = False
 
-        ### Start writing file to disk
+    def _open_file(self, cache_path):
+        """Sets private file-like attribute _file to a physical file on disk"""
+
         # create temporary file to save pdb file
         if cache_path is None: 
             self._file = tempfile.NamedTemporaryFile(mode='w+t')
+            self._download = True
 
         # Create/Parse download cache
         else: 
             named_file_path = Path(cache_path) /  f"{self.id}.{self.file_format}"
-
+            
             # Found Cache, so don't download anything and open existing file
             if named_file_path.exists() and named_file_path.is_file():
-                self._file = open(named_file_path,"r")            
-                return self
+                self._file = open(named_file_path,"r")                      
             
             else: # No cache found, so create Cache
                 self._file = open(named_file_path, 'w+t')
-        ###
+                self._download = True
+
+
+    def download(self, cache_path=None, timeout=None):
+        """Downloads files from the RCSB"""
+
+        # Sets self._file correctly
+        self._open_file(cache_path)
 
         # Downloading file into temporary file or cached it into a pernament file
-        try:
-            r = requests.get(f"https://files.rcsb.org/download/{self.id}.{self.file_format}",
-                             timeout=timeout)
-            r.raise_for_status()
-            self._file.write(r.text)
-        except requests.HTTPError:
-            # Clean Up files in case of download failure
-            self._file.close()
-            named_file_path.unlink() 
-            raise FileDownloadPDBError
-        finally:
-            # Closes File safely if saving to cache
-            if cache_path is not None:
-                self._file.close()
-                
+        if self._download:
+            try:
+                r = requests.get(f"https://files.rcsb.org/download/{self.id}.{self.file_format}",
+                                timeout=timeout)
+                r.raise_for_status()
+                self._file.write(r.text)
+            except requests.HTTPError:
+                print(self._file)
+                print(self._file.name)
+                # Clean Up files in case of download failure
+                #Path(str(self._file)).unlink()
+                raise FileDownloadPDBError
+            finally:
+                # Closes File safely if saving to cache
+                if cache_path is not None:
+                    self._file.close()
+                    
         return self
