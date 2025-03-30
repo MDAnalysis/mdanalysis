e8d83d67c794fba2a0c5cbf1d8c97774326a0d18
Yu-Yuan (Stuart) Yang - from connectin, AFLab <yu-yuan.yang@qmul.ac.uk>
2025-03-30 15:32:09 +0100

    udpate doc

--- package/MDAnalysis/coordinates/PDB.py
+++ package/MDAnalysis/coordinates/PDB.py
@@ -259,7 +259,7 @@ class PDBReader(base.ReaderBase):
        Tempfactors (aka bfactors) are now read into the ts.data dictionary each
        frame.  Occupancies are also read into this dictionary.
     .. versionchanged:: 2.10.0
-       segID is read from 73-76 instead of 67-72.
+       segID is read from 73-76 instead of 67-76.
     """
     format = ['PDB', 'ENT']
     units = {'time': None, 'length': 'Angstrom'}
