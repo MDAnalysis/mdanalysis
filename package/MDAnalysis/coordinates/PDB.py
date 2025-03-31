96ac6f5010fb7ddebb7d6e607f0c4589633fbcbb
Yu-Yuan (Stuart) Yang - from connectin, AFLab <yu-yuan.yang@qmul.ac.uk>
2025-03-31 10:03:51 +0100

    PDB note less

--- package/MDAnalysis/coordinates/PDB.py
+++ package/MDAnalysis/coordinates/PDB.py
@@ -261,10 +261,6 @@ class PDBReader(base.ReaderBase):
     .. versionchanged:: 2.5.0
        Tempfactors (aka bfactors) are now read into the ts.data dictionary each
        frame.  Occupancies are also read into this dictionary.
-    .. versionchanged:: 2.10.0
-       segID is read from 73-76 instead of 67-72.
-       The `force_chainids_to_segids` keyword was added to allow the user to
-       prioritize the chain ID as the segment ID when reading PDB files.
     """
     format = ['PDB', 'ENT']
     units = {'time': None, 'length': 'Angstrom'}
