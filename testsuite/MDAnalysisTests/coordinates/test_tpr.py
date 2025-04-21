f13576f6c2c988e7b06f97d945c26addd62b780b
Tyler Reddy <tyler.je.reddy@gmail.com>
2025-04-21 11:26:33 -0600

    ENH: PR 4873 revisions
    * Add support for velocity and coordinate reading from `.tpr`
    files that are in tpx generation `122` (GROMACS 2021). Includes
    two regression tests.

--- testsuite/MDAnalysisTests/coordinates/test_tpr.py
+++ testsuite/MDAnalysisTests/coordinates/test_tpr.py
@@ -6,6 +6,8 @@ from MDAnalysisTests.datafiles import (
     TPR2023,
     TPR2022RC1,
     TPR2022RC1_bonded,
+    TPR2021,
+    TPR2021_bonded,
     TPR_xvf_2024_4,
     TPR_NNPOT_2025_0,
 )
@@ -92,6 +94,22 @@ from numpy.testing import assert_allclose, assert_equal
         ),
         (
             TPR2022RC1_bonded,  # tpx 127
+            [4.446, 4.659, 2.384],
+            [4.446, 4.659, 2.384],
+            (14, 3),
+            np.zeros(3),
+            np.zeros(3),
+        ),
+        (
+            TPR2021,  # tpx 122
+            [3.25000e-01, 1.00400e00, 1.03800e00],
+            [-2.56000e-01, 1.37300e00, 3.59800e00],
+            (2263, 3),
+            np.zeros(3),
+            np.zeros(3),
+        ),
+        (
+            TPR2021_bonded,  # tpx 122
             [4.446, 4.659, 2.384],
             [4.446, 4.659, 2.384],
             (14, 3),
