3ceca5ceae17f0377031386b8da0e4a82984529b
Sirsha Ganguly <ganguly.sirsha@ufl.edu>
2025-08-01 10:50:04 -0400

    Indentation edit

--- package/MDAnalysis/analysis/msd.py
+++ package/MDAnalysis/analysis/msd.py
@@ -299,13 +299,13 @@ class EinsteinMSD(AnalysisBase):
     results.msds_by_particle : :class:`numpy.ndarray`
         The MSD of each individual particle with respect to constant lag-time or
         unique Δt intervals.
-        - for `non_linear=False`: a 2D array of shape (n_lagtimes, n_atoms)
-        - for `non_linear=True`: a 2D array of shape (n_delta_t_values, n_atoms)
+            for `non_linear=False`: a 2D array of shape (n_lagtimes, n_atoms)
+            for `non_linear=True`: a 2D array of shape (n_delta_t_values, n_atoms)
     results.delta_t_values : :class:`numpy.ndarray`
-        - Array of unique Δt (time differences) at which time-averaged MSD values are
+        Array of unique Δt (time differences) at which time-averaged MSD values are
         computed (for `non_linear=True`).
-        - For `non_linear=False` it is Null array. Time differences are same as the lag-times.
-          (To access: `lagtimes = np.arange(nframes)*timestep`)
+        For `non_linear=False` it is Null array. Time differences are same as the lag-times.
+        (To access: `lagtimes = np.arange(nframes)*timestep`).
 
         .. versionadded:: 2.10.0
 
