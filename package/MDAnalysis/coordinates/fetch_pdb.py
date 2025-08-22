cecd57050a7619a011e0aa0a0440e5dc19962001
Joshua Raphael Uy <jauy1@asu.edu>
2025-08-22 09:59:17 -0700

    fetch_pdb() now returns paths instead of universes

--- package/MDAnalysis/coordinates/fetch_pdb.py
+++ package/MDAnalysis/coordinates/fetch_pdb.py
@@ -5,20 +5,20 @@ from MDAnalysis.core.universe import Universe
 
 def fetch_pdb(PDB_IDS=None,
             cache_path=None,
-            progress_bar=False,
+            progressbar=False,
             file_format="pdb.gz",
             ):
+    # note, progress_bar required tdqm. make an optional depedency if not already in
     
     ## Have to do this approach instead of Pooch.retrieve in order to prevent known hash warning from showing up
-    
-    # Handles the case if input is a string
+
+    # Handles the case if input arguement is a string (i.e. only one pdb)
     if isinstance(PDB_IDS, str):
         PDB_IDS = (PDB_IDS,)
 
     # Handles multiple tuples and lists
     registry_dictionary = {f'{PDB_ID}.{file_format}': None for PDB_ID in PDB_IDS}
-    filepath_list = []
-    universe_list = []
+    
     
     downloader = pooch.create(
         path=cache_path,
@@ -26,22 +26,19 @@ def fetch_pdb(PDB_IDS=None,
         registry=registry_dictionary
     )
 
-    # Make so that in the case of passing one string- the output is just one universe
-    for file_name in registry_dictionary.keys():
-        filepath_list.append(downloader.fetch(fname=file_name, progressbar=progress_bar))
-
-    for path in filepath_list:
-        universe_list.append(Universe(path))
-        
-    return universe_list
+    print(registry_dictionary.items())
 
 
+    if len(PDB_IDS) == 1: # Ensures only one Path is returned in the case of one PDB_ID input argument
+        for file_name in registry_dictionary.keys():
+            return downloader.fetch(fname=file_name, progressbar=progressbar)
+    else:
+        filepath_list = []
+        for file_name in registry_dictionary.keys():
+            filepath_list.append(downloader.fetch(fname=file_name, progressbar=progressbar))
+        return filepath_list
 
 
 
-u = fetch_pdb(PDB_IDS='1AKE', cache_path='~/tmp', progress_bar=True)
-
-print(u)
-
-
-print(fetch_pdb(PDB_IDS=['1AKE', '4BWZ'] , cache_path='~/tmp', progress_bar=True))
+print(fetch_pdb(PDB_IDS='1AKE', cache_path='~/tmp', progressbar=True))
+print(fetch_pdb(PDB_IDS=['1AKE', '4BWZ'] , cache_path='~/tmp', progressbar=True))
