215ee434b58233b70f3a3022ff2948ab9a3c26c9
Joshua Raphael Uy <jauy1@asu.edu>
2025-08-22 10:12:22 -0700

    cleaned up return logic with Syntactic sugar

--- package/MDAnalysis/coordinates/fetch_pdb.py
+++ package/MDAnalysis/coordinates/fetch_pdb.py
@@ -8,17 +8,16 @@ def fetch_pdb(PDB_IDS=None,
             progressbar=False,
             file_format="pdb.gz",
             ):
-    # note, progress_bar required tdqm. make an optional depedency if not already in
     
     ## Have to do this approach instead of Pooch.retrieve in order to prevent known hash warning from showing up
-
-    # Handles the case if input arguement is a string (i.e. only one pdb)
+    
+    # Handles the case if input is a string
     if isinstance(PDB_IDS, str):
         PDB_IDS = (PDB_IDS,)
 
     # Handles multiple tuples and lists
     registry_dictionary = {f'{PDB_ID}.{file_format}': None for PDB_ID in PDB_IDS}
-    
+  
     
     downloader = pooch.create(
         path=cache_path,
@@ -26,23 +25,10 @@ def fetch_pdb(PDB_IDS=None,
         registry=registry_dictionary
     )
 
-<<<<<<< HEAD
     if len(PDB_IDS) == 1:
         return downloader.fetch(fname=tuple(registry_dictionary.keys())[0], progressbar=progressbar)
-=======
-    print(registry_dictionary.items())
-
-
-    if len(PDB_IDS) == 1: # Ensures only one Path is returned in the case of one PDB_ID input argument
-        for file_name in registry_dictionary.keys():
-            return downloader.fetch(fname=file_name, progressbar=progressbar)
->>>>>>> parent of 03638c8ef (Cleaned up return logic with syntactic sugar)
     else:
-        filepath_list = []
-        for file_name in registry_dictionary.keys():
-            filepath_list.append(downloader.fetch(fname=file_name, progressbar=progressbar))
-        return filepath_list
-
+        return [downloader.fetch(fname=file_name, progressbar=progressbar) for file_name in registry_dictionary.keys()]
 
 
 print(fetch_pdb(PDB_IDS='1AKE', cache_path='~/tmp', progressbar=True))
