7d021d330a0b5f74b30a7a6ab53b17a46230baad
zhangc <2023024681@m.scnu.edu.cn>
2025-10-14 01:38:04 +0800

    update benchmark for InterRDF_s

--- benchmarks/benchmarks/analysis/rdf.py
+++ benchmarks/benchmarks/analysis/rdf.py
@@ -12,13 +12,13 @@ except:
 
 
 class SimpleRdfBench(object):
-    """Benchmarks for MDAnalysis.analysis.rdf"""
+    """Benchmarks for MDAnalysis.analysis.rdf.InterRDF"""
 
-    params = ([20, 75, 200], [[0, 5], [0, 15], [0, 20]], [1, 100, 1000, 10000], [1, 3, 9])
+    params = ([20, 75, 200], [[0, 5], [0, 15], [0, 20]], [1, 100, 1000, 10000])
 
-    param_names = ["nbins", "range_val", "natoms", "npairs"]
+    param_names = ["nbins", "range_val", "natoms"]
 
-    def setup(self, nbins, range_val, natoms, npairs):
+    def setup(self, nbins, range_val, natoms):
 
         self.sel_str = "name OW"
 
@@ -34,14 +34,33 @@ class SimpleRdfBench(object):
 
         self.rdf = InterRDF(g1=self.sel, g2=self.sel, nbins=nbins, range=range_val)
 
-        ags = [[self.sel, self.sel]] * npairs
-        self.rdf_s = InterRDF_s(self.u, ags, nbins=nbins, range=range_val)
-
     def time_interrdf(self, nbins, range_val, natoms):
         """Benchmark a full trajectory parse
         by MDAnalysis.analysis.rdf.InterRDF
         """
         self.rdf.run()
+
+class SimpleRdfsBench(object):
+    """Benchmarks for MDAnalysis.analysis.rdf.InterRDF_s"""
+
+    params = ([20, 75, 200], [[0, 5], [0, 15], [0, 20]], [10, 100], [1, 3, 9])
+
+    param_names = ["nbins", "range_val", "natoms", "npairs"]
+
+    def setup(self, nbins, range_val, natoms, npairs):
+
+        self.sel_str = "name OW"
+
+        self.u = MDAnalysis.Universe(TPR, XTC)
+
+        try:
+            self.sel = self.u.select_atoms(self.sel_str)[:natoms]
+        except AttributeError:
+            self.sel = self.u.selectAtoms(self.sel_str)[:natoms]
+
+        ags = [[self.sel, self.sel]] * npairs
+        self.rdf_s = InterRDF_s(self.u, ags, nbins=nbins, range=range_val)
+
 
     def time_interrdfs(self, nbins, range_val, natoms, npairs):
         """Benchmark a full trajectory parse
