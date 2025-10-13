6f64b1bb1abb383a72f06b176791d1ab61b1a58d
zhangc <2023024681@m.scnu.edu.cn>
2025-10-14 00:57:08 +0800

    update AUTHORS and rdf benchmark

--- benchmarks/benchmarks/analysis/rdf.py
+++ benchmarks/benchmarks/analysis/rdf.py
@@ -6,7 +6,7 @@ except:
     pass
 
 try:
-    from MDAnalysis.analysis.rdf import InterRDF
+    from MDAnalysis.analysis.rdf import InterRDF, InterRDF_s
 except:
     pass
 
@@ -14,11 +14,11 @@ except:
 class SimpleRdfBench(object):
     """Benchmarks for MDAnalysis.analysis.rdf"""
 
-    params = ([20, 75, 200], [[0, 5], [0, 15], [0, 20]], [1, 100, 1000, 10000])
+    params = ([20, 75, 200], [[0, 5], [0, 15], [0, 20]], [1, 100, 1000, 10000], [1, 3, 9])
 
-    param_names = ["nbins", "range_val", "natoms"]
+    param_names = ["nbins", "range_val", "natoms", "npairs"]
 
-    def setup(self, nbins, range_val, natoms):
+    def setup(self, nbins, range_val, natoms, npairs):
 
         self.sel_str = "name OW"
 
@@ -34,8 +34,17 @@ class SimpleRdfBench(object):
 
         self.rdf = InterRDF(g1=self.sel, g2=self.sel, nbins=nbins, range=range_val)
 
+        ags = [[self.sel, self.sel]] * npairs
+        self.rdf_s = InterRDF_s(self.u, ags, nbins=nbins, range=range_val)
+
     def time_interrdf(self, nbins, range_val, natoms):
         """Benchmark a full trajectory parse
         by MDAnalysis.analysis.rdf.InterRDF
         """
         self.rdf.run()
+
+    def time_interrdfs(self, nbins, range_val, natoms, npairs):
+        """Benchmark a full trajectory parse
+        by MDAnalysis.analysis.rdf.InterRDF_s
+        """
+        self.rdf_s.run()
