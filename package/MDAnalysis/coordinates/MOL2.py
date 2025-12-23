472d55e5fc2778e4d1b74b28cc219a8665f67328
Kushagar Garg <dreamstick909@gmail.com>
2025-12-23 22:38:58 +0530

    style: reformat MOL2.py to satisfy CI

--- package/MDAnalysis/coordinates/MOL2.py
+++ package/MDAnalysis/coordinates/MOL2.py
@@ -233,7 +233,10 @@ class MOL2Reader(base.ReaderBase):
         try:
             block = self.frames[frame]
         except IndexError:
-            errmsg = f"Invalid frame {frame} for trajectory with length " f"{len(self)}"
+            errmsg = (
+                f"Invalid frame {frame} for trajectory with length "
+                f"{len(self)}"
+            )
             raise IOError(errmsg) from None
 
         sections, coords = self.parse_block(block)
@@ -308,7 +311,9 @@ class MOL2Writer(base.WriterBase):
             units are converted to the MDAnalysis base format; [``True``]
         """
         self.filename = filename
-        self.convert_units = convert_units  # convert length and time to base units
+        self.convert_units = (
+            convert_units  # convert length and time to base units
+        )
 
         self.frames_written = 0
 
@@ -383,7 +388,9 @@ class MOL2Writer(base.WriterBase):
         atom_lines = "\n".join(atom_lines)
 
         try:
-            substructure = ["@<TRIPOS>SUBSTRUCTURE\n"] + ts.data["substructure"]
+            substructure = ["@<TRIPOS>SUBSTRUCTURE\n"] + ts.data[
+                "substructure"
+            ]
         except KeyError:
             substructure = ""
 
@@ -398,7 +405,9 @@ class MOL2Writer(base.WriterBase):
         molecule[1] = "{0}\n".format(" ".join(check_sums))
         molecule.insert(0, "@<TRIPOS>MOLECULE\n")
 
-        return_val = "".join(molecule) + atom_lines + bond_lines + "".join(substructure)
+        return_val = (
+            "".join(molecule) + atom_lines + bond_lines + "".join(substructure)
+        )
 
         molecule[0] = molecule_0_store
         molecule[1] = molecule_1_store
