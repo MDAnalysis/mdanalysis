77d58cea63c70b1e3b1ae32e618c6cb90570376a
Kushagar Garg <dreamstick909@gmail.com>
2026-01-01 18:42:14 +0530

    Fix formatting with black version 24

--- package/MDAnalysis/analysis/hydrogenbonds/hbond_autocorrel.py
+++ package/MDAnalysis/analysis/hydrogenbonds/hbond_autocorrel.py
@@ -321,16 +321,20 @@ class HydrogenBondAutoCorrel(AnalysisBase):
 
         if exclusions is not None:
             if len(exclusions[0]) != len(exclusions[1]):
-                raise ValueError("'exclusion' must be two arrays of identical length")
-            self.exclusions = np.column_stack((exclusions[0], exclusions[1])).astype(
-                np.intp
-            )
+                raise ValueError(
+                    "'exclusion' must be two arrays of identical length"
+                )
+            self.exclusions = np.column_stack(
+                (exclusions[0], exclusions[1])
+            ).astype(np.intp)
         else:
             self.exclusions = None
 
         self.bond_type = bond_type
         if self.bond_type not in ["continuous", "intermittent"]:
-            raise ValueError("bond_type must be either 'continuous' or 'intermittent'")
+            raise ValueError(
+                "bond_type must be either 'continuous' or 'intermittent'"
+            )
 
         self.a_crit = np.deg2rad(angle_crit)
         self.d_crit = dist_crit
@@ -366,7 +370,9 @@ class HydrogenBondAutoCorrel(AnalysisBase):
         if req_frames > n_frames:
             warnings.warn(
                 "Number of required frames ({}) greater than the"
-                " number of frames in trajectory ({})".format(req_frames, n_frames),
+                " number of frames in trajectory ({})".format(
+                    req_frames, n_frames
+                ),
                 RuntimeWarning,
             )
 
@@ -477,7 +483,9 @@ class HydrogenBondAutoCorrel(AnalysisBase):
         aidx = aidx[idx2]
 
         nbonds = len(hidx)  # number of hbonds at t=0
-        results = np.zeros_like(np.arange(start, stop, self._skip), dtype=np.float32)
+        results = np.zeros_like(
+            np.arange(start, stop, self._skip), dtype=np.float32
+        )
 
         if self.time_cut:
             # counter for time criteria
@@ -486,7 +494,9 @@ class HydrogenBondAutoCorrel(AnalysisBase):
         for i, ts in enumerate(self.u.trajectory[start : stop : self._skip]):
             box = self.u.dimensions if self.pbc else None
 
-            d = calc_bonds(self.h.positions[hidx], self.a.positions[aidx], box=box)
+            d = calc_bonds(
+                self.h.positions[hidx], self.a.positions[aidx], box=box
+            )
             a = calc_angles(
                 self.d.positions[hidx],
                 self.h.positions[hidx],
@@ -611,7 +621,9 @@ class HydrogenBondAutoCorrel(AnalysisBase):
             """Sum of three exponential functions"""
             A3 = 1 - (A1 + A2)
             return (
-                A1 * np.exp(-x / tau1) + A2 * np.exp(-x / tau2) + A3 * np.exp(-x / tau3)
+                A1 * np.exp(-x / tau1)
+                + A2 * np.exp(-x / tau2)
+                + A3 * np.exp(-x / tau3)
             )
 
         if self.bond_type == "continuous":
@@ -652,7 +664,9 @@ class HydrogenBondAutoCorrel(AnalysisBase):
         self.solution["ier"] = ier
 
         if ier in [1, 2, 3, 4]:  # solution found if ier is one of these values
-            self.solution["estimate"] = self._my_solve(self.solution["time"], *p)
+            self.solution["estimate"] = self._my_solve(
+                self.solution["time"], *p
+            )
         else:
             warnings.warn("Solution to results not found", RuntimeWarning)
 
