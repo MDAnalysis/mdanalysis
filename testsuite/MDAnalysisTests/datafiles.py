38446d0c6b29046d15d69f8578740ca74c3d0d1f
talagayv95@gmail.com <talagayv95@gmail.com>
2026-01-21 23:31:46 +0100

    compression of 6kya.pdb and addition of note about files in datafiles.py

--- testsuite/MDAnalysisTests/datafiles.py
+++ testsuite/MDAnalysisTests/datafiles.py
@@ -920,8 +920,9 @@ SURFACE_TRR = (_data_ref / "surface.trr").as_posix()
 # DSSP testing: from https://github.com/ShintaroMinami/PyDSSP
 DSSP = (_data_ref / "dssp").as_posix()
 
+# Files for testing the identification of `sugar` token
 GLYCAM = (_data_ref / 'GLYCAM_sugars.pdb').as_posix()
-SUGAR_PDB = (_data_ref / '6kya.pdb').as_posix()
+SUGAR_PDB = (_data_ref / '6kya.pdb.gz').as_posix()
 
 # This should be the last line: clean up namespace
 del resources
