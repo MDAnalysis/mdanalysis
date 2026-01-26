051a1b7fd5a74deca432a2300e4537ba47daa3fa
Lakshya Saraswat <lakshyasaraswat25@gmail.com>
2026-01-26 16:32:51 +0530

    commit1

--- package/MDAnalysis/units.py
+++ package/MDAnalysis/units.py
@@ -47,6 +47,7 @@ table on :ref:`table-baseunits`.
    charge       :math:`e`      :math:`1.602176565 \times 10^{-19}` As
    force        kJ/(mol·Å)     :math:`1.66053892103219 \times 10^{-11}` J/m
    speed        Å/ps           :math:`100` m/s
+   mass         u              :math:`1.66053906660 \times 10^{-27}` kg
    =========== ============== ===============================================
 
 Implementation notes
