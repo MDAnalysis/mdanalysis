3ea49c9a0cf7d2a6f537510b5a26656fe545c470
Ayush Agarwal <ayushagarwal1203@gmail.com>
2026-03-05 19:25:13 +0530

    Uses modern np random number API, simulates real random walk in custom benchmark class

--- benchmarks/benchmarks/analysis/msd.py
+++ benchmarks/benchmarks/analysis/msd.py
@@ -10,8 +10,11 @@ from MDAnalysis.tests.datafiles import RANDOM_WALK_TOPO, RANDOM_WALK
 
 class EinsteinMSDCustom:
     """
-    Benchmark for Mean-Squared Displacement (EinsteinMSD)
-    using custom value of n_atoms and range of n_frames.
+    Benchmark for Mean-Squared Displacement (EinsteinMSD).
+
+    The trajectory is created by using random forces
+    that are generated from a normal distribution
+    and then cumulatively summed to simulate a real random walk.
     """
 
     unit = "ms"
@@ -23,14 +26,19 @@ class EinsteinMSDCustom:
     timeout = 300.0
 
     param_names = ["n_frames", "fft"]
-    params = ([100, 1000, 10000], [True, False])
+    params = ([100, 1000, 5000], [True, False])
 
     def setup(self, n_frames, fft):
         """Setup method for MSD benchmark with custom number of frames."""
         n_atoms = 100
 
-        rng = np.random.default_rng()
-        res = rng.random((n_frames, n_atoms, 3)).astype(np.float32)
+        rng = np.random.default_rng(42)
+
+        # Random forces are sampled from a normal distribution
+        steps = rng.standard_normal((n_frames, n_atoms, 3), dtype=np.float32)
+
+        # Cumulative sum is taken to simulate positions over time
+        res = np.cumsum(steps, axis=0)
 
         self.u = mda.Universe.empty(n_atoms, n_frames=n_frames, trajectory=True)
         self.u.trajectory.set_array(res)
@@ -47,7 +55,11 @@ class EinsteinMSDCustom:
 class EinsteinMSDExample:
     """
     Benchmark for Mean-Squared Displacement (EinsteinMSD)
-    using the example trajectory RANDOM_WALK.
+    using the RANDOM_WALK example trajectory provided in the MDAnalysis testsuite.
+
+    The files for the trajectory can be found in the library at :
+        RANDOM_WALK      - mdanalysis/testsuite/xyz_random_walk.xtc
+        RANDOM_WALK_TOPO - mdanalysis/testsuite/RANDOM_WALK_TOPO.pdb
     """
 
     unit = "ms"
