40b912c3da96457ae27e645d101442df7740d925
Sonu Saini <sonusainihr1031@gmail.com>
2026-03-14 22:01:31 +0530

    Fix PDF documentation build configuration

---
+++ package/doc/sphinx/conf.py
@@ -0,0 +1,12 @@
+autodoc_mock_imports = [
+    "mdahole2",
+    "PathSimAnalysis",
+    "waterdynamics",
+]
+latex_engine = "pdflatex"
+
+latex_elements = {
+    "papersize": "letterpaper",
+    "pointsize": "10pt",
+    "extraclassoptions": "openany,oneside",
+}
