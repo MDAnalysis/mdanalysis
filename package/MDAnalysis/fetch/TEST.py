8fbd20369d538ff35e28283f20e07888621d24e4
Joshua Raphael Uy <jauy1@asu.edu>
2026-06-05 15:00:52 -0700

    Added Dev Things

---
+++ package/MDAnalysis/fetch/TEST.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+## File for testing changes 
+## Will get deleted when PR is DONE
+
+
+import MDAnalysis as mda
+
+from MDAnalysis.fetch.fetchers import StaticFetcher
+from MDAnalysis.fetch.pdb import from_PDB
+
--- package/MDAnalysis/fetch/fetchers.py
+++ package/MDAnalysis/fetch/fetchers.py
@@ -23,21 +23,50 @@
 
 import csv
 import sqlite3
-import pooch
-from abc import ABC
+from abc import ABC, abstractmethod
+
+try:
+    import pooch
+except ImportError:
+    HAS_POOCH = False
+else:
+    HAS_POOCH = True
+
+DEFAULT_CACHE_NAME_DOWNLOADER = "MDAnalysis_pdbs"
 
 
 class BaseFetcher(ABC):
     """Blueprint Class for all Fetchers"""
 
-    def __init__(self, base_url, progressbar):
-        self.base_url = base_url
-        self.progressbar = progressbar
+    def __init__(
+        self,
+        base_url,
+        progressbar,
+        keep_session,
+    ):
 
-    def fetch(self):
+        self.base_url = base_url
+        self.progressbar = progressbar  # Progressbar
+        self.keep_session  # Connection Spooling
+
+    @abstractmethod
+    def fetch(self, timeout, retries):
         # Starts file retrieval workflow
-        # meant to be overriden by child class
-        pass
+        #
+        # All fetchers should call _check_pooch
+
+        self.timeout = timeout  # timeout
+        self.retries = retries  # number of retries
+        self._check_pooch()
+
+    def _check_pooch(
+        self,
+    ):
+        # Note that requests is a major dependency of pooch and is guaranteed to be installed
+        if not HAS_POOCH:
+            raise ModuleNotFoundError(
+                "pooch is needed as a dependency for Fetchers"
+            )
 
 
 class StaticFetcher(BaseFetcher):
@@ -45,13 +74,18 @@ class StaticFetcher(BaseFetcher):
 
     def __init__(self, cache_path):
         super().__init__(base_url, progressbar)
-        self.base_url = base_url
-        self.progressbar = progressbar
-        self.cache_path = cache_path
+        self.cache_path = _set_cache_path(cache_path)
 
     def fetch(self):
         # Starts file retrieval workflow
         pass
+
+    def _set_cache_path(self, cache_path):
+
+        if cache_path is None:
+            return pooch.os_cache(DEFAULT_CACHE_NAME_DOWNLOADER)
+        else:
+            return cache_path
 
     def _write_cache(self):
         # Create/query hash file (either a csv or database file)
