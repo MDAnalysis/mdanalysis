dbd62be633112f5ba08b8958dfce07fb3342d3a8
Joshua Raphael Uy <jauy1@asu.edu>
2026-06-11 14:48:23 -0700

    Updated TEST.py

--- package/MDAnalysis/TEST.py
+++ package/MDAnalysis/TEST.py
@@ -9,18 +9,22 @@ from pathlib import Path
 from MDAnalysis.fetch.fetchers import StaticFetcher
 
 
+DEFAULT_CACHE_FOLDER='/nfs/homes3/jauy1/.cache/MDAnalysis_pdbs'
+downloader = StaticFetcher()
 
-shutil.rmtree('/nfs/homes3/jauy1/.cache/MDAnalysis_pdbs', ignore_errors=True)
-#test_path = Path('/nfs/homes3/jauy1/.cache/MDAnalysis_pdbs')
-#test_db = test_path / 'test.txt'
+## Inital Download Case (create db)
+print("Inital Download Case (create db)\n")
+shutil.rmtree(DEFAULT_CACHE_FOLDER, ignore_errors=True)
+path = downloader.fetch(base_url='https://files.wwpdb.org/download/', file_name='1AKE.pdb', db_name='test.txt')
 
+
+## Cache Case (has DB)
+print('Cache Case (has DB)\n')
 downloader = StaticFetcher()
 path = downloader.fetch(base_url='https://files.wwpdb.org/download/', file_name='1AKE.pdb', db_name='test.txt')
 
-print(path)
-print('/nfs/homes3/jauy1/.cache/MDAnalysis_pdbs')
-print(os.listdir('/nfs/homes3/jauy1/.cache/MDAnalysis_pdbs'))
 
-
-print('trying cache')
-path = downloader.fetch(base_url='https://files.wwpdb.org/download/', file_name='1AKE.pdb', db_name='test.txt')
+## No Database (just download)
+print('No Database (just download)\n')
+shutil.rmtree(DEFAULT_CACHE_FOLDER, ignore_errors=True)
+path = downloader.fetch(base_url='https://files.wwpdb.org/download/', file_name='1AKE.pdb', db_name=None)
