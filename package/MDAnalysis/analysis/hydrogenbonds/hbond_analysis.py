2ee249d99d49189d9d8d54a4c49b6628b54af133
Rocco Meli <r.meli@bluemail.ch>
2026-07-23 22:04:32 +0200

    fix

--- package/MDAnalysis/analysis/hydrogenbonds/hbond_analysis.py
+++ package/MDAnalysis/analysis/hydrogenbonds/hbond_analysis.py
@@ -717,20 +717,18 @@ class HydrogenBondAnalysis(AnalysisBase):
     def _filter_atoms(self, donors, acceptors):
         """Create a mask to filter donor, hydrogen and acceptor atoms.
 
-                This can be used to consider only hydrogen bonds between two or more
-                specified groups.
+        This can be used to consider only hydrogen bonds between two or more
+        specified groups.
 
-                Groups are specified with the `between` keyword when creating the
-                HydrogenBondAnalysis object.
+        Groups are specified with the `between` keyword when creating the
+        HydrogenBondAnalysis object.
 
-                   Returns
-                   -------
-                   mask: np.ndarray
+        Returns
+        -------
+            mask: np.ndarray
 
-
-                .. versionchanged:: 2.5.0
-                   Change return value to a mask instead of separate AtomGroups.
-        ``
+        .. versionchanged:: 2.5.0
+           Change return value to a mask instead of separate AtomGroups.
         """
 
         mask = np.full(donors.n_atoms, fill_value=False)
