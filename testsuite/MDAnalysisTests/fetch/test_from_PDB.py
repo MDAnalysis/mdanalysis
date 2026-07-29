f3d4bc8ea4519771cf9411246a6e17f79df33e41
Joshua Raphael Uy <jauy1@asu.edu>
2026-07-29 13:28:53 -0700

    renamed test

--- testsuite/MDAnalysisTests/fetch/test_from_PDB.py
+++ testsuite/MDAnalysisTests/fetch/test_from_PDB.py
@@ -54,7 +54,7 @@ def test_download_one_file_str(tmp_path):
     not HAS_ACCESS_TO_WWPDB,
     reason="Can not connect to https://files.wwpdb.org/",
 )
-def test_download_one_file(tmp_path):
+def test_download_one_file_list(tmp_path):
 
     path = mda.fetch.from_PDB(["1AKE"], cache_path=tmp_path)
     assert path.exists()
