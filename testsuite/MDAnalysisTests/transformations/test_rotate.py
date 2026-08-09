fd257eb46cea8b7e57f208fd4a3840c69ce041d5
ParthUppal523 <uppalparth523@gmail.com>
2026-08-09 22:41:19 +0530

    reformat using black

--- testsuite/MDAnalysisTests/transformations/test_rotate.py
+++ testsuite/MDAnalysisTests/transformations/test_rotate.py
@@ -214,9 +214,7 @@ def test_rotateby_no_velocities_forces_does_not_raise():
     u.atoms.positions = orig_pos.copy()
     ts = u.trajectory.ts
     angle = 5
-    matrix = rotation_matrix(
-        np.deg2rad(angle), [-1, 2, -3], [0, 0, 0]
-    )
+    matrix = rotation_matrix(np.deg2rad(angle), [-1, 2, -3], [0, 0, 0])
     rotation = matrix[:3, :3].T
     transformed_ts = rotateby(angle, [-1, 2, -3], point=[0, 0, 0])(ts)
     assert_array_almost_equal(
